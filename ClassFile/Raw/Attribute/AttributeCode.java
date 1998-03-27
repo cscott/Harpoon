3a8d05913cc07e659108e46143adb0d93b9c6dc6
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-03-27 05:26:32 +0000

    Restructured Trans.* (again!)  Moved huge switch list to Trans.Munch; added Trans.TMethod to hold  all the method-specific stuff that was accumulating in Trans.State.  Drastically pared down Trans.Translate.  The idea is that we will  eventually add a Trans.TClass as well. Got rid of JavaTypes.* Moved GrowableQuadList to Quad package, added new methods and  made them public. Made some changes to make naming locals and parameters nicer.

--- ClassFile/Raw/Attribute/AttributeCode.java
+++ ClassFile/Raw/Attribute/AttributeCode.java
@@ -21,9 +21,9 @@ public class AttributeCode extends AttributeInfo {
     max_locals= in.read_u2();
 
     code_length=in.read_u4();
-    code = new byte[(int)code_length]; // XXX ugly.
+    code = new byte[(int)code_length]; // XXX ugly. dies after 31-bits
     for (long l=0; l<code_length; l++)
-      code[(int)l] = in.readByte();    // XXX ugly.
+      code[(int)l] = in.readByte();    // XXX ugly. dies after 31-bits
 
     exception_table_length = in.read_u2();
     exception_table = new ExceptionTable[exception_table_length];
@@ -36,10 +36,33 @@ public class AttributeCode extends AttributeInfo {
       attributes[i] = AttributeInfo.read(in, cp);
   }
 
+  /** Returns the maximum stack depth used by this code. */
   public int max_stack() { return max_stack; }
+  /** Return the maximum number of local variables used by this code. */
   public int max_locals() { return max_locals; }
+  /** Return the bytecode array associated with this code. */
   public byte[] code() { return code; }
+  /** Return the exception table associated with this code. */
   public ExceptionTable[] exception_table() { return exception_table; }
+  /** Return the attributes associated with this code. */
   public AttributeInfo[] attributes() { return attributes; }
+
+  /** 
+   * Return the symbol name of a local variable, or null if one cannot
+   * be found.
+   * @param index the index of the local variable to look up.
+   * @return the name of the local, or null.
+   */
+  public String localName(int index) {
+    for(int i=0; i<attributes.length; i++)
+      if (attributes[i] instanceof AttributeLocalVariableTable) {
+	AttributeLocalVariableTable alvt = 
+	  (AttributeLocalVariableTable) attributes[i];
+	String name = alvt.localName(index);
+	if (name!=null) return name;
+      }
+    // Can't find a name for this in any table.
+    return null;
+  }
 }
 
--- ClassFile/Raw/Attribute/AttributeLocalVariableTable.java
+++ ClassFile/Raw/Attribute/AttributeLocalVariableTable.java
@@ -1,5 +1,13 @@
 package ClassFile;
 
+/** 
+ * This object represents a table mapping local variable indexes to
+ * symbolic names.  This attribute is optional; typically it is
+ * not included unless debugging flags are given to the compiler.
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: AttributeLocalVariableTable.java,v 1.3 1998-03-27 05:26:32 cananian Exp $
+ * @see AttributeCode
+ */
 public class AttributeLocalVariableTable extends AttributeInfo {
   int local_variable_table_length;
   LocalVariableTable local_variable_table[];
@@ -20,10 +28,11 @@ public class AttributeLocalVariableTable extends AttributeInfo {
       local_variable_table[i] = new LocalVariableTable(in, cp);
   }
   /** Get the (debugging) name of a local variable.
-   * @param index the index of the local variable to query.
-   * @return the name of the index'th local variable.
+   * @param  index the index of the local variable to query.
+   * @return the name of the index'th local variable, or null
+   *         if none can be found.
    */
-  public String getName(int index) {
+  public String localName(int index) {
     for (int i=0; i<local_variable_table.length; i++)
       if (local_variable_table[i].index == index)
 	return local_variable_table[i].name;
