ffeccf5827e39f01bbfcac448580caf43ad07360
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-03 06:42:35 +0000

    Added general array copy method.

---
+++ Util/Util.java
@@ -0,0 +1,21 @@
+package harpoon.Util;
+
+import java.lang.reflect.Array;
+/** 
+ * Miscellaneous static utility functions.
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: Util.java,v 1.1 1998-08-03 06:42:35 cananian Exp $
+ */
+public class Util {
+  /**
+   * Copy an array type to prevent modification.  Does not bother
+   * to copy array types of length 0, because they're already immutable.
+   */
+  public static Object[] copy(Object[] src) {
+    if (src.length==0) return src;
+    Object[] dst=(Object[])Array.newInstance(src.getClass().getComponentType(),
+					     src.length);
+    System.arraycopy(src,0,dst,0,src.length);
+    return dst;
+  }
+}
