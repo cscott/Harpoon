b3d115107ffa6bdd870d0d74fb5209d0842be5bb
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-03 22:14:52 +0000

    Added toString() method.

--- ClassFile/Bytecode/Operand.java
+++ ClassFile/Bytecode/Operand.java
@@ -7,7 +7,7 @@ import harpoon.ClassFile.*;
  * instruction.
  *
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Operand.java,v 1.3 1998-08-03 22:08:34 cananian Exp $
+ * @version $Id: Operand.java,v 1.4 1998-08-03 22:14:52 cananian Exp $
  * @see Instr
  * @see InGen
  */
@@ -27,4 +27,26 @@ public class Operand {
   public Object getValue() { return value; }
   /** Return the <code>HClass</code> type of this <Code>Operand</code>. */
   public HClass getType()  { return type; }
+
+  /** Return a human-readable representation of this Operand. */
+  public String toString() {
+    if (getType()==HClass.forName("java.lang.String"))
+      return "(String) \""+escape(getValue().toString())+"\"";
+    return "("+getType().getName()+") "+getValue().toString();
+  }
+  // copied from Constant.Utf8
+  static String escape(String str) {
+    StringBuffer sb = new StringBuffer();
+    for (int i=0; i<str.length(); i++) {
+      char c = str.charAt(i);
+      if (Character.isISOControl(c)) {
+	String hexval=Integer.toHexString((int)c);
+	while(hexval.length()<4) hexval="0"+hexval;
+	sb.append('\\'); sb.append('u');
+	sb.append(hexval);
+      }
+      else sb.append(c);
+    }
+    return sb.toString();
+  }
 }
