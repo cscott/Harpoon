919eb0cdc9c34b4067dd8279f882b7742a61ca74
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-07 09:52:02 +0000

    Moved string escape function here.

--- Util/Util.java
+++ Util/Util.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 /** 
  * Miscellaneous static utility functions.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Util.java,v 1.2 1998-08-03 07:19:41 cananian Exp $
+ * @version $Id: Util.java,v 1.3 1998-08-07 09:52:02 cananian Exp $
  */
 public final class Util {
   // Disable constructor.  Only static methods here.
@@ -20,4 +20,24 @@ public final class Util {
     System.arraycopy(src,0,dst,0,src.length);
     return dst;
   }
+  /** Escape the contents of a String so they are safe to print. */
+  public static final String escape(String str) {
+    StringBuffer sb = new StringBuffer();
+    for (int i=0; i<str.length(); i++) {
+      char c = str.charAt(i);
+      if (Character.isISOControl(c)) {
+	String hexval=Integer.toHexString((int)c);
+	while(hexval.length()<4) hexval="0"+hexval;
+	sb.append('\\'); sb.append('u');
+	sb.append(hexval);
+      }
+      else sb.append(c);
+    }
+    return sb.toString();
+  }
 }
+
+// set emacs indentation style.
+// Local Variables:
+// c-basic-offset:2
+// End:
