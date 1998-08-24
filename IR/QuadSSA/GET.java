f03124dfa299624202bb5a55b54264ed3a28b3a4
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-24 22:37:11 +0000

    Added isStatic() methods. Corrected SET by adding fieldref.

--- IR/QuadSSA/GET.java
+++ IR/QuadSSA/GET.java
@@ -1,13 +1,15 @@
 // GET.java, created Wed Aug  5 07:05:59 1998 by cananian
 package harpoon.IR.QuadSSA;
 
+import java.lang.reflect.Modifier;
+
 import harpoon.ClassFile.*;
 import harpoon.Temp.Temp;
 /**
  * <code>GET</code> represent field access (get) operations.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: GET.java,v 1.5 1998-08-24 19:30:01 cananian Exp $
+ * @version $Id: GET.java,v 1.6 1998-08-24 22:37:11 cananian Exp $
  */
 
 public class GET extends Quad {
@@ -30,5 +32,9 @@ public class GET extends Quad {
 	return "GET " + 
 	    field.getDeclaringClass().getName() + "." +
 	    field.getName() + " of " + src + " into " + dst;
+    }
+    /** Determines whether the GET is of a static field. */
+    public boolean isStatic() { 
+	return Modifier.isStatic(field.getModifiers());
     }
 }
--- IR/QuadSSA/SET.java
+++ IR/QuadSSA/SET.java
@@ -1,32 +1,39 @@
 // SET.java, created Wed Aug  5 07:04:09 1998 by cananian
 package harpoon.IR.QuadSSA;
 
+import java.lang.reflect.Modifier;
+
 import harpoon.ClassFile.*;
 import harpoon.Temp.Temp;
 /**
  * <code>SET</code> represents field assignment-to operations.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: SET.java,v 1.4 1998-08-24 19:30:03 cananian Exp $
+ * @version $Id: SET.java,v 1.5 1998-08-24 22:37:11 cananian Exp $
  */
 
 public class SET extends Quad {
-    public HField dst;
-    public Temp src;
+    public HField field;
+    public Temp dst, src;
 
     /** Creates a <code>SET</code>. */
     public SET(String sourcefile, int linenumber,
-	       HField dst, Temp src) {
+	       HField field, Temp dst, Temp src) {
 	super(sourcefile, linenumber);
+	this.field = field;
 	this.dst = dst;
 	this.src = src;
     }
-    SET(HCodeElement hce, HField dst, Temp src) {
-	this(hce.getSourceFile(), hce.getLineNumber(), dst, src);
+    SET(HCodeElement hce, HField field, Temp dst, Temp src) {
+	this(hce.getSourceFile(), hce.getLineNumber(), field, dst, src);
     }
     /** Returns a human-readable representation of this Quad. */
     public String toString() {
-	return "SET "+dst.getDeclaringClass().getName()+"."+dst.getName()+
-	    " to " + src.toString();
+	return "SET "+field.getDeclaringClass().getName()+"."+field.getName()+
+	    " of " + dst + " to " + src.toString();
+    }
+    /** Determines whether the SET is of a static field. */
+    public boolean isStatic() { 
+	return Modifier.isStatic(field.getModifiers());
     }
 }
