c061b18e7da3394b7326f5390e0c468654b4ddef
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-24 21:00:39 +0000

    Removed Temp/Leaf dichotomy.  Now only CONST quads can take constant operands.

---
+++ IR/QuadSSA/CONST.java
@@ -0,0 +1,40 @@
+// CONST.java, created Mon Aug 24 16:46:52 1998 by cananian
+package harpoon.IR.QuadSSA;
+
+import harpoon.ClassFile.*;
+import harpoon.Temp.Temp;
+/**
+ * <code>CONST</code> objects represent an assignment of a constant value
+ * to a compiler temporary.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: CONST.java,v 1.1 1998-08-24 21:00:39 cananian Exp $
+ */
+
+public class CONST extends Quad {
+    public Temp dst;
+    public Object value;
+    public HClass type;
+    /** Creates a <code>CONST</code> from a destination temporary, and object
+     *  value and its class type. */
+    public CONST(String sourcefile, int linenumber,
+		 Temp dst, Object value, HClass type) {
+	super(sourcefile, linenumber);
+	this.dst = dst;
+	this.value = value;
+	this.type = type;
+    }
+    CONST(HCodeElement hce, Temp dst, Object value, HClass type) {
+	this(hce.getSourceFile(), hce.getLineNumber(), dst, value, type);
+    }
+    /** Returns a human-readable representation of this Quad. */
+    public String toString() {
+	StringBuffer sb = new StringBuffer(dst.toString());
+	sb.append(" = CONST ");
+	if (type == HClass.forName("java.lang.String"))
+	    sb.append("(String)\""+Util.escape(value.toString())+"\"");
+	else
+	    sb.append("("+type.getName()+")"+value.toString());
+	return sb.toString();
+    }
+}
--- IR/QuadSSA/LET.java
+++ IR/QuadSSA/LET.java
@@ -5,31 +5,19 @@ import harpoon.ClassFile.*;
 import harpoon.Temp.Temp;
 /**
  * <code>LET</code> objects represent an assignment to a compiler temporary.
- * The source of the assignment can be either a constant or another
- * temporary.  It is roughly equivalent to the standard <code>MOVE</code>
+ * The source of the assignment must be another temporary.
+ * It is roughly equivalent to the standard <code>MOVE</code>
  * pseudo-instruction.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: LET.java,v 1.6 1998-08-24 19:42:03 cananian Exp $
+ * @version $Id: LET.java,v 1.7 1998-08-24 21:00:39 cananian Exp $
  */
 
-public class LET extends Quad {
-    public Temp dst; // LeafTemp?
-    public Leaf src;
-    /** Creates a <code>LET</code> from a Leaf and a Temporary. */
-    public LET(String sourcefile, int linenumber,
-	       Temp dst, Leaf src) {
-	super(sourcefile, linenumber);
-	this.dst = dst;
-	this.src = src;
-    }
-    /** Creates a <code>LET</code> from two Temporaries. */
+public class LET extends OPER {
+    /** Creates a <code>LET</code> from a source and destination Temporary. */
     public LET(String sourcefile, int linenumber,
 	       Temp dst, Temp src) {
-	this(sourcefile, linenumber, dst, new LeafTemp(src));
-    }
-    LET(HCodeElement hce, Temp dst, Leaf src) {
-	this(hce.getSourceFile(), hce.getLineNumber(), dst, src);
+	super(sourcefile, linenumber, "move", dst, new Temp[] { src });
     }
     LET(HCodeElement hce, Temp dst, Temp src) {
 	this(hce.getSourceFile(), hce.getLineNumber(), dst, src);
