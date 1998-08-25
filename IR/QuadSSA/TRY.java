bad412456fabb0c28213f662ec9dbca8eee9eff3
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-25 06:32:24 +0000

    Try blocks in bytecode are per-catch (no multiple catch try statements). Updated Quad to reflect this.

--- IR/QuadSSA/TRY.java
+++ IR/QuadSSA/TRY.java
@@ -6,13 +6,13 @@ import harpoon.ClassFile.*;
  * <code>TRY</code> objects represent <code>try-catch-finally</code> blocks.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: TRY.java,v 1.4 1998-08-24 19:30:03 cananian Exp $
+ * @version $Id: TRY.java,v 1.5 1998-08-25 06:32:24 cananian Exp $
  */
 
 public class TRY extends Quad {
     public Quad tryBlock;
-    public Quad[] catchBlocks;
-    public HClass[] catchExceptions;
+    public Quad catchBlock;
+    public HClass catchException;
     public Quad finallyBlock;
     /** Creates a <code>TRY</code>. The <code>tryBlock</code> is executed
      *  first.  If any exceptions are thrown, they are caught in
@@ -21,19 +21,19 @@ public class TRY extends Quad {
      *  the <Code>tryBlock</code> completed without exception or not. */
     public TRY(String sourcefile, int linenumber,
 	       Quad tryBlock, 
-	       Quad[] catchBlocks, HClass[] catchExceptions,
+	       Quad catchBlock, HClass catchException,
 	       Quad finallyBlock) {
 	super(sourcefile, linenumber);
 	this.tryBlock = tryBlock;
-	this.catchBlocks = catchBlocks;
-	this.catchExceptions = catchExceptions;
+	this.catchBlock = catchBlock;
+	this.catchException = catchException;
 	this.finallyBlock = finallyBlock;
     }
     TRY(HCodeElement hce, Quad tryBlock, 
-	Quad[] catchBlocks, HClass[] catchExceptions,
+	Quad catchBlock, HClass catchException,
 	Quad finallyBlock) {
 	this(hce.getSourceFile(), hce.getLineNumber(),
-	     tryBlock, catchBlocks, catchExceptions, finallyBlock);
+	     tryBlock, catchBlock, catchException, finallyBlock);
     }
     /** Returns human-readable representation of this Quad. */
     public String toString() { return "TRY"; }
