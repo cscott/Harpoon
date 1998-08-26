8f42e7c5008a959db845fa53de6dae457d411b2c
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-08-26 23:18:37 +0000

    Added AGET/ASET quads.

---
+++ IR/QuadSSA/AGET.java
@@ -0,0 +1,47 @@
+// AGET.java, created Wed Aug 26 19:02:57 1998 by cananian
+package harpoon.IR.QuadSSA;
+
+import harpoon.ClassFile.*;
+import harpoon.Temp.Temp;
+
+/**
+ * <code>AGET</code> represents an element fetch from an array object.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: AGET.java,v 1.1 1998-08-26 23:18:37 cananian Exp $
+ * @see ANEW
+ * @see ASET
+ * @see ALENGTH
+ */
+
+public class AGET extends Quad {
+    /** The Temp in which to store the fetched element. */
+    public Temp dst;
+    /** The array reference. */
+    public Temp objectref;
+    /** The Temp holding the index of the element to get. */
+    public Temp index;
+
+    /** Creates an <code>AGET</code> object. */
+    public AGET(String sourcefile, int linenumber,
+		Temp dst, Temp objectref, Temp index) {
+	super(sourcefile, linenumber);
+	this.dst = dst;
+	this.objectref = objectref;
+	this.index = index;
+    }
+    AGET(HCodeElement hce, Temp dst, Temp objectref, Temp index) {
+	this(hce.getSourceFile(), hce.getLineNumber(), dst, objectref, index);
+    }
+    /** Returns the Temp defined by this quad.
+     * @return the <code>dst</code> field. */
+    public Temp[] def() { return new Temp[] { dst }; }
+    /** Returns all the Temps used by this quad.
+     * @return the <code>objectref</code> and <code>index</code> fields. */
+    public Temp[] use() { return new Temp[] { objectref, index }; }
+
+    /** Returns a human-readable representation of this quad. */
+    public String toString() {
+	return "AGET " + dst + ", " + objectref + "["+index+"]";
+    }
+}
---
+++ IR/QuadSSA/ASET.java
@@ -0,0 +1,46 @@
+// ASET.java, created Wed Aug 26 19:12:32 1998 by cananian
+package harpoon.IR.QuadSSA;
+
+import harpoon.ClassFile.*;
+import harpoon.Temp.Temp;
+
+/**
+ * <code>ASET</code> represents an array element assignment.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: ASET.java,v 1.1 1998-08-26 23:18:38 cananian Exp $
+ * @see ANEW
+ * @see AGET
+ * @see ALENGTH
+ */
+
+public class ASET extends Quad {
+    /** The array reference */
+    public Temp objectref;
+    /** The Temp holding the index of the element to get. */
+    public Temp index;
+    /** The new value for the array element. */
+    public Temp src;
+
+    /** Creates an <code>ASET</code> object. */
+    public ASET(String sourcefile, int linenumber,
+		Temp objectref, Temp index, Temp src) {
+	super(sourcefile, linenumber);
+	this.objectref = objectref;
+	this.index = index;
+	this.src = src;
+    }
+    ASET(HCodeElement hce, Temp objectref, Temp index, Temp src) {
+	this(hce.getSourceFile(), hce.getLineNumber(), objectref, index, src);
+    }
+    /** Returns all the Temps used by this quad. 
+     * @return the <code>objectref</code>, <code>index</code>, and 
+     *         <code>src</code> fields.
+     */
+    public Temp[] use() { return new Temp[] { objectref, index, src }; }
+
+    /** Returns a human-readable representation of this quad. */
+    public String toString() {
+	return "ASET " + objectref + "["+index+"], " + src;
+    }
+}
