b09dee21cb32e308c5a91ca04335a5c435030e60
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-09-01 23:48:30 +0000

    Added code to print the Quads of declaredMethods.

--- Main/Main.java
+++ Main/Main.java
@@ -2,11 +2,12 @@
 package harpoon.Main;
 
 import harpoon.ClassFile.*;
+import harpoon.IR.QuadSSA.*;
 /**
  * <code>Main</code> is the command-line interface to the compiler.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Main.java,v 1.1 1998-08-07 13:38:13 cananian Exp $
+ * @version $Id: Main.java,v 1.2 1998-09-01 23:48:30 cananian Exp $
  */
 
 public final class Main  {
@@ -17,9 +18,19 @@ public final class Main  {
      *  extending <code>java.lang.Thread</code>.  These classes
      *  define the external interface of the machine. */
     public static final void main(String args[]) {
+	java.io.PrintWriter out = new java.io.PrintWriter(System.out);
+
 	HClass interfaceClasses[] = new HClass[args.length];
 	for (int i=0; i<args.length; i++)
 	    interfaceClasses[i] = HClass.forName(args[i]);
 	// Do something intelligent with these classes. XXX
+	for (int i=0; i<interfaceClasses.length; i++) {
+	    HMethod hm[] = interfaceClasses[i].getDeclaredMethods();
+	    for (int j=0; j<hm.length; j++) {
+		HCode hc = harpoon.IR.QuadSSA.Code.
+		    convertFrom(hm[j].getCode("bytecode"));
+		hc.print(out);
+	    }
+	}
     }
 }
