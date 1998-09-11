133d39448e33862da0d6277b9e352343b114a3f8
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-09-11 03:49:40 +0000

    First-pass implementation of a 'unique fifo' class.

---
+++ Util/UniqueFIFO.java
@@ -0,0 +1,79 @@
+// UniqueStack.java, created Thu Sep 10 19:08:22 1998 by cananian
+package harpoon.Util;
+
+import java.util.Hashtable;
+import java.util.EmptyStackException;
+/**
+ * The <code>UniqueFIFO</code> class represents a first-in-first-out
+ * stack of <b>unique</b> objects.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: UniqueFIFO.java,v 1.1 1998-09-11 03:49:40 cananian Exp $
+ */
+
+public class UniqueFIFO {
+
+    class List {
+	Object item;
+	List next;
+	List(Object item, List next) {
+	    this.item = item; this.next = next;
+	}
+    }
+
+    List head = null;
+    List tail = null;
+
+    Hashtable uniq = new Hashtable();
+
+    /** 
+     * Pushes an item onto the font of this fifo, if it is unique.
+     * Otherwise, does nothing.
+     * @param item the item to be pushed onto this stack.
+     * @return the <code>item</code> argument.
+     */
+    public synchronized Object push(Object item) {
+	if (!uniq.containsKey(item)) {
+	    if (tail==null) { // empty.
+		tail = head = new List(item, null);
+	    } else {
+		tail.next = new List(item, null);
+		tail = tail.next;
+	    }
+	    uniq.put(item, item);
+	}
+	return item;
+    }
+    /**
+     * Removes the object at the back of this fifo and returns that
+     * object as the value of this function.
+     * @return The object at the end of the fifo.
+     * @exception EmptyStackException if this fifo is empty.
+     */
+    public synchronized Object pull() {
+	Object obj = peek();
+
+	head = head.next;
+	if (head==null) tail=null;
+
+	uniq.remove(obj);
+	return obj;
+    }
+    /**
+     * Looks at the object at the back of this fifo without removing it.
+     * @return the object at the end of this fifo.
+     * @exception EmptyStackException if this fifo is empty.
+     */
+    public synchronized Object peek() {
+	if (head==null) throw new EmptyStackException();
+	return head.item;
+    }
+    /**
+     * Tests if this stack is empty.
+     * @return <code>true</code> if this stack is empty;
+     *         <code>false</code> otherwise.
+     */
+    public boolean empty() {
+	return (head==null);
+    }
+}
