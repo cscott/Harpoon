c83405f134c193c8f7f82dd97dbbea98761fa9e8
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-09-15 01:26:21 +0000

    new analysis class to construct a dominator tree.

---
+++ Analysis/DomTree.java
@@ -0,0 +1,195 @@
+// DomTree.java, created Mon Sep 14 17:38:43 1998 by cananian
+package harpoon.Analysis;
+
+import harpoon.ClassFile.*;
+import harpoon.IR.Properties.Edges;
+
+import java.util.Hashtable;
+import java.util.Vector;
+/**
+ * <code>DomTree</code> computes the dominator tree of a flowgraph-structured
+ * IR.  The <code>HCodeElement</code>s must implement the 
+ * <code>harpoon.IR.Properties.Edges</code> interface.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: DomTree.java,v 1.1 1998-09-15 01:26:21 cananian Exp $
+ */
+
+public class DomTree /*implements Graph*/ {
+    /** Set of analyzed HCodes for caching. */
+    Hashtable analyzed = new Hashtable();
+    /** Mapping of HCodeElements to their immediate dominators. */
+    Hashtable idom  = new Hashtable();
+    /** Reverse mapping: mapping of an HCodeElement to the elements which
+     *  have this element as their immediate dominator. */
+    SetHTable children = new SetHTable();
+
+    /** Creates a new, empty <code>DomTree</code>. */
+    public DomTree() {
+    }
+
+    /** Return the immediate dominator of an HCodeElement.
+     * @return the immediate dominator of <code>n</code>, or
+     *         <code>null</code> if <code>n</code> is the root
+     *         of the flow graph.  */
+    public HCodeElement idom(HCode hc, HCodeElement n) {
+	analyze(hc); return (HCodeElement) idom.get(n);
+    }
+    /** Return the children of an HCodeElement in the immediate dominator
+     *  tree. 
+     *  @return an array with all the children of <code>n</code> in the
+     *          immediate dominator tree, or a zero-length array if 
+     *          <code>n</code> is a tree leaf. */
+    public HCodeElement[] children(HCode hc, HCodeElement n) {
+	analyze(hc); return children.getSet(n);
+    }
+    
+    /** Analyze an HCode which implements Edges. */
+    void analyze(HCode hc) {
+	if (analyzed.get(hc) != null) return;
+	analyzed.put(hc, hc);
+
+	// Setup lists and tables.
+	final IntHTable dfnum = new IntHTable();
+	final Hashtable semi  = new Hashtable();
+	final Hashtable ancestor = new Hashtable();
+	final Hashtable parent = new Hashtable();
+	final Hashtable best  = new Hashtable();
+	
+	SetHTable bucket = new SetHTable();
+	Hashtable samedom = new Hashtable();
+
+	final Vector vertex = new Vector();
+
+	/** Utility class wraps analysis subroutines. */
+	class Utility {
+	    /** Number nodes of depth-first spanning tree */
+	    void DFS(HCodeElement p, HCodeElement n) {
+		if (dfnum.getInt(n)==0) {
+		    int N = vertex.size()+1;
+		    dfnum.putInt(n, N);
+		    if (p!=null) parent.put(n, p);
+		    vertex.addElement(n);
+		    // for each successor of n...
+		    HCodeEdge[] el = ((Edges)n).succ();
+		    for (int i=0; i<el.length; i++)
+			DFS(n, el[i].to());
+		}
+	    }
+	    /** Add edge p->n to spanning forest. */
+	    void Link(HCodeElement p, HCodeElement n) {
+		ancestor.put(n, p);
+		best.put(n, n);
+	    }
+	    /** In the forest, find nonroot ancestor of n that has
+	     *  lowest-numbered semidominator. */
+	    HCodeElement Eval(HCodeElement v) {
+		HCodeElement a = (HCodeElement) ancestor.get(v);
+		if (ancestor.get(a) != null) {
+		    HCodeElement b = Eval(a);
+		    ancestor.put(v, ancestor.get(a));
+		    if (dfnum.getInt(semi.get(b)) <
+			dfnum.getInt(semi.get(best.get(v))))
+			best.put(v, b);
+		}
+		return (HCodeElement) best.get(v);
+	    }
+	}
+	Utility u = new Utility();
+
+	// Dominators algorithm:
+	u.DFS(null, hc.getRootElement());
+	for (int i=vertex.size()-1; i>=1; i--) {
+	    // calculate the semidominator of vertex[i]
+	    HCodeElement n = (HCodeElement) vertex.elementAt(i);
+	    HCodeElement p = (HCodeElement) parent.get(n);
+	    HCodeElement s = p;
+	    HCodeElement sprime;
+
+	    //   (for each predecessor v of n)
+	    HCodeEdge el[] = ((Edges)n).pred();
+	    for (int j=0; j<el.length; j++) {
+		HCodeElement v = el[j].from();
+		// ignore unreachable nodes.
+		if (!dfnum.containsKey(v)) continue;
+		if (dfnum.getInt(v) < dfnum.getInt(n))
+		    sprime = v;
+		else
+		    sprime = (HCodeElement) semi.get(u.Eval(v));
+		if (dfnum.getInt(sprime) < dfnum.getInt(s))
+		    s = sprime;
+	    }
+	    semi.put(n, s);
+
+	    // Calculation of n's dominator is deferred until the path
+	    // from s to n has been linked into the forest.
+	    bucket.unionSet(s, n);
+	    u.Link(p, n);
+
+	    // Now that the path from p to n has been linked into the
+	    // spanning forest, calculate the dominator of v (or else defer
+	    // the calculation).
+	    //   (for each v in bucket[p])
+	    HCodeElement[] hcel = bucket.getSet(p);
+	    for (int j=0; j<hcel.length; j++) {
+		HCodeElement v = hcel[j];
+		HCodeElement y = u.Eval(v);
+		if (semi.get(y) == semi.get(v))
+		    idom.put(v, p);
+		else
+		    samedom.put(v, y);
+	    }
+	    bucket.clearSet(p);
+	}
+	// Now all the deferred dominator calculations are performed.
+	for (int i=1; i<vertex.size(); i++) {
+	    HCodeElement n = (HCodeElement) vertex.elementAt(i);
+	    if (samedom.get(n)!=null)
+		idom.put(n, idom.get(samedom.get(n)));
+	}
+	// done.  Make inverse mapping.
+	for (int i=1; i<vertex.size(); i++) {
+	    HCodeElement n  = (HCodeElement) vertex.elementAt(i);
+	    HCodeElement id = (HCodeElement) idom.get(n);
+	    children.unionSet(id, n);
+	}
+    }
+
+
+    // Useful Hashtable extensions.
+    static class IntHTable extends Hashtable {
+	void putInt(Object o, int n) {
+	    put(o, new Integer(n));
+	}
+	int getInt(Object o) {
+	    if (!containsKey(o)) return 0;
+	    return ((Integer)get(o)).intValue();
+	}
+    }
+    static class SetHTable extends Hashtable {
+	void clearSet(HCodeElement hce) {
+	    remove(hce);
+	}
+	HCodeElement[] getSet(HCodeElement hce) {
+	    if (!containsKey(hce)) return new HCodeElement[0];
+	    return (HCodeElement[]) get(hce);
+	}
+	void unionSet(HCodeElement hce, HCodeElement newEl) {
+	    if (!containsKey(hce)) {
+		put(hce, new HCodeElement[] { newEl });
+	    } else {
+		HCodeElement[] oldset = (HCodeElement[]) get(hce);
+		HCodeElement[] newset = new HCodeElement[oldset.length+1];
+		for (int i=0; i < oldset.length; i++)
+		    if (oldset[i] == newEl)
+			return; // don't add; already present.
+		    else
+			newset[i] = oldset[i];
+		newset[oldset.length] = newEl;
+		put(hce, newset);
+	    }
+	}
+    }
+}
+
+
