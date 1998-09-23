353d92dd222845f3ec4a2ec1fdab5cf13d5df90a
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-09-23 04:17:10 +0000

    Added OperVisitor class. Yay.

--- Analysis/QuadSSA/SCC/SCCAnalysis.java
+++ Analysis/QuadSSA/SCC/SCCAnalysis.java
@@ -20,7 +20,7 @@ import java.util.Enumeration;
  * with extensions to allow type and bitwidth analysis.  Fun, fun, fun.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: SCCAnalysis.java,v 1.3 1998-09-23 04:08:22 cananian Exp $
+ * @version $Id: SCCAnalysis.java,v 1.4 1998-09-23 04:17:10 cananian Exp $
  */
 
 public class SCCAnalysis implements TypeMap, ConstMap, ExecMap {
@@ -155,8 +155,12 @@ public class SCCAnalysis implements TypeMap, ConstMap, ExecMap {
 	HCode hc;
 	// local references to worklists.
 	Worklist Wv, Wq;
+	// give us an OperVisitor class to go along with this.
+	OperVisitor opVisitor;
+
 	SCCVisitor(HCode hc, Worklist Wv, Worklist Wq) {
 	    this.hc = hc;  this.Wv = Wv;  this.Wq = Wq;
+	    this.opVisitor = new SCCOpVisitor(hc, Wv, Wq);
 	}
 
 	// utility functions.
@@ -442,8 +446,10 @@ public class SCCAnalysis implements TypeMap, ConstMap, ExecMap {
 		else if (ty == HClass.Float || ty == HClass.Double)
 		    raiseV(V, Wv, q.dst, new xFloatConstant(ty, o) );
 		else throw new Error("Unknown OPER result type: "+ty);
-	    } /* else if (allWidth) ... XXX XXX XXX */
-	    else { // not all constant, not all known widths...
+	    } else if (allWidth) {
+		// do something intelligent.
+		q.visit(opVisitor);
+	    } else { // not all constant, not all known widths...
 		// special-case ACMPEQ x, null
 		if (q.opcode.equals("acmpeq") &&
 		    ((get( q.operands[0] ) instanceof xNullConstant &&
@@ -569,6 +575,94 @@ public class SCCAnalysis implements TypeMap, ConstMap, ExecMap {
 	    }
 	}
 	public void visit(THROW q) { /* do nothing. */ }
+    }
+    /*------------------------------------------------------------*/
+    // VISITOR CLASS FOR OPER (ugh.  lots of cases)
+    class SCCOpVisitor extends OperVisitor {
+	// local reference to HCode.
+	HCode hc;
+	// local references to worklists.
+	Worklist Wv, Wq;
+	// give us an OperVisitor class to go along with this.
+	OperVisitor opVisitor;
+
+	SCCOpVisitor(HCode hc, Worklist Wv, Worklist Wq) {
+	    this.hc = hc; this.Wv = Wv; this.Wq = Wq;
+	}
+
+	public void visit_default(OPER q) {
+	    HClass ty = q.evalType();
+	    if (ty.isPrimitive())
+		raiseV(V, Wv, q.dst, new xClassNonNull( ty ) );
+	    else
+		raiseV(V, Wv, q.dst, new xClass( ty ) );
+	}
+	/*    
+    public void visit_acmpeq(OPER q) { visit_default(q); }
+    public void visit_d2f(OPER q) { visit_default(q); }
+    public void visit_d2i(OPER q) { visit_default(q); }
+    public void visit_d2l(OPER q) { visit_default(q); }
+    public void visit_dadd(OPER q) { visit_default(q); }
+    public void visit_dcmpeq(OPER q) { visit_default(q); }
+    public void visit_dcmpge(OPER q) { visit_default(q); }
+    public void visit_dcmpgt(OPER q) { visit_default(q); }
+    public void visit_ddiv(OPER q) { visit_default(q); }
+    public void visit_dmul(OPER q) { visit_default(q); }
+    public void visit_dneg(OPER q) { visit_default(q); }
+    public void visit_drem(OPER q) { visit_default(q); }
+    public void visit_dsub(OPER q) { visit_default(q); }
+    public void visit_f2d(OPER q) { visit_default(q); }
+    public void visit_f2i(OPER q) { visit_default(q); }
+    public void visit_f2l(OPER q) { visit_default(q); }
+    public void visit_fadd(OPER q) { visit_default(q); }
+    public void visit_fcmpeq(OPER q) { visit_default(q); }
+    public void visit_fcmpge(OPER q) { visit_default(q); }
+    public void visit_fcmpgt(OPER q) { visit_default(q); }
+    public void visit_fdiv(OPER q) { visit_default(q); }
+    public void visit_fmul(OPER q) { visit_default(q); }
+    public void visit_fneg(OPER q) { visit_default(q); }
+    public void visit_frem(OPER q) { visit_default(q); }
+    public void visit_fsub(OPER q) { visit_default(q); }
+    public void visit_i2b(OPER q) { visit_default(q); }
+    public void visit_i2c(OPER q) { visit_default(q); }
+    public void visit_i2d(OPER q) { visit_default(q); }
+    public void visit_i2f(OPER q) { visit_default(q); }
+    public void visit_i2l(OPER q) { visit_default(q); }
+    public void visit_i2s(OPER q) { visit_default(q); }
+    public void visit_iadd(OPER q) { visit_default(q); }
+    public void visit_iand(OPER q) { visit_default(q); }
+    public void visit_icmpeq(OPER q) { visit_default(q); }
+    public void visit_icmpge(OPER q) { visit_default(q); }
+    public void visit_icmpgt(OPER q) { visit_default(q); }
+    public void visit_idiv(OPER q) { visit_default(q); }
+    public void visit_imul(OPER q) { visit_default(q); }
+    public void visit_ineg(OPER q) { visit_default(q); }
+    public void visit_ior(OPER q) { visit_default(q); }
+    public void visit_irem(OPER q) { visit_default(q); }
+    public void visit_ishl(OPER q) { visit_default(q); }
+    public void visit_ishr(OPER q) { visit_default(q); }
+    public void visit_isub(OPER q) { visit_default(q); }
+    public void visit_iushr(OPER q) { visit_default(q); }
+    public void visit_ixor(OPER q) { visit_default(q); }
+    public void visit_l2d(OPER q) { visit_default(q); }
+    public void visit_l2f(OPER q) { visit_default(q); }
+    public void visit_l2i(OPER q) { visit_default(q); }
+    public void visit_ladd(OPER q) { visit_default(q); }
+    public void visit_land(OPER q) { visit_default(q); }
+    public void visit_lcmpeq(OPER q) { visit_default(q); }
+    public void visit_lcmpge(OPER q) { visit_default(q); }
+    public void visit_lcmpgt(OPER q) { visit_default(q); }
+    public void visit_ldiv(OPER q) { visit_default(q); }
+    public void visit_lmul(OPER q) { visit_default(q); }
+    public void visit_lneg(OPER q) { visit_default(q); }
+    public void visit_lor(OPER q) { visit_default(q); }
+    public void visit_lrem(OPER q) { visit_default(q); }
+    public void visit_lshl(OPER q) { visit_default(q); }
+    public void visit_lshr(OPER q) { visit_default(q); }
+    public void visit_lsub(OPER q) { visit_default(q); }
+    public void visit_lushr(OPER q) { visit_default(q); }
+    public void visit_lxor(OPER q) { visit_default(q); }
+*/
     }
     /*-------------------------------------------------------------*/
     // Extract bitwidth information from unwilling victims.
