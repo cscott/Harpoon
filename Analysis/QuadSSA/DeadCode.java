b82bc5112c5e0be288d568225d7d992690894a3d
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-09-25 20:12:42 +0000

    Remove useless CJMPs, too.

--- Analysis/QuadSSA/DeadCode.java
+++ Analysis/QuadSSA/DeadCode.java
@@ -16,7 +16,7 @@ import java.util.Hashtable;
  * a method.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: DeadCode.java,v 1.6 1998-09-25 19:28:59 cananian Exp $
+ * @version $Id: DeadCode.java,v 1.7 1998-09-25 20:12:42 cananian Exp $
  */
 
 public class DeadCode  {
@@ -26,7 +26,7 @@ public class DeadCode  {
     public static void optimize(HCode hc) {
 	Hashtable defs = new Hashtable();
 	IntTable uses = new IntTable();
-	Worklist W = new Set();
+	Set W = new Set();
 
 	// make visitor
 	Visitor v = new Visitor(W, uses, defs);
@@ -59,10 +59,10 @@ public class DeadCode  {
     } // end OPTIMIZE METHOD
 
     static class Visitor extends QuadVisitor {
-	Worklist W;
+	Set W;
 	IntTable uses;
 	Hashtable defs;
-	Visitor(Worklist W, IntTable uses, Hashtable defs) {
+	Visitor(Set W, IntTable uses, Hashtable defs) {
 	    this.W = W;
 	    this.uses = uses;
 	    this.defs = defs;
@@ -71,6 +71,13 @@ public class DeadCode  {
 	{ /* don't throw our headers away!! */ }
 	public void visit(CALL q) 
 	{ /* call statements may have side-effects. */ }
+	public void visit(CJMP q) {
+	    // if this is a useless cjmp remove it; otherwise 
+	    // do the standard sigma optimizations on it.
+	    if (q.next(0) == q.next(1) && removeCJMP(q)) return;
+	    visit((SIGMA)q);
+	}
+
 	public void visit(PHI q) { // phis are specially compound.
 	    // arity-one phi functions are stomped on.
 	    if (q.prev().length==1) { removePhi(q); return; }
@@ -114,13 +121,16 @@ public class DeadCode  {
 		if (uses.getInt(d[i])>0)
 		    break;
 	    if (i<d.length) return; // some of the defs are used.
-	    // okay.  This statement is worthless.  Unlink it.
+	    // okay.  This statement is worthless.  
+	    // removing statement could make predecessor useless
+	    if (q.prev(0) instanceof CJMP) W.push(q.prev(0));
+	    // Unlink it.
 	    Util.assert(q.next().length==1 && q.prev().length==1);
 	    Edge before = q.prevEdge(0);
 	    Edge after  = q.nextEdge(0);
 	    Quad.addEdge((Quad)before.from(), before.which_succ(),
 			 (Quad)after.to(), after.which_pred() );
-	    // decrement uses & possible add to worklist.
+	    // decrement uses & possibly add to worklist.
 	    Temp u[] = q.use();
 	    for (i=0; i<u.length; i++)
 		markAbsent(u[i]);
@@ -137,12 +147,59 @@ public class DeadCode  {
 				   q.dst[i], q.src[i][0]);
 		Quad.addEdge(header, which_succ, qq, 0);
 		header = qq; which_succ = 0;
+		// update definition pointer; push new MOVEs on worklist.
 		defs.put(q.dst[i], qq);
+		W.push(qq);
 	    }
+	    // removing this node could have made a CJMP useless.
+	    if (q.dst.length==0 && q.prev(0) instanceof CJMP)
+		W.push(q.prev(0));
 	    // now link PHI out of the graph.
 	    Edge succE = q.nextEdge(0);
 	    Quad.addEdge(header, which_succ, 
 			 (Quad) succE.to(), succE.which_pred());
+	    W.remove(q); // can't optimize this any more.
+	}
+	boolean removeCJMP(CJMP q) { // remove useless jumps.
+	    Util.assert(q.next(0)==q.next(1));
+	    // if the targets are the same, it ought to be a phi.
+	    Util.assert(q.next(0) instanceof PHI);
+
+	    PHI phi = (PHI) q.next(0);
+	    // check that sigma and phi args match up.
+	    // a hashtable makes this easier.
+	    Hashtable h = new Hashtable();
+	    for (int i=0; i<q.dst.length; i++)
+		for (int j=0; j<q.dst[i].length; j++)
+		    h.put(q.dst[i][j], q.src[i]);
+
+	    int which_pred0 = q.nextEdge(0).which_pred();
+	    int which_pred1 = q.nextEdge(1).which_pred();
+	    for (int i=0; i<phi.src.length; i++)
+		if (h.get(phi.src[i][which_pred0]) !=
+		    h.get(phi.src[i][which_pred1]) )
+		    return false; // cjmp matters, either in sigma or in phi.
+	    
+	    // we've proven that removing this CJMP is harmless. Remove it.
+	    // Removing the CJMP could make a predecessor useless.
+	    if (q.prev(0) instanceof CJMP) W.push(q.prev(0));
+	    // Remove one of the phi entries & put phi on worklist
+	    phi.remove(which_pred1);
+	    W.push(phi); // might be arity 1 now.
+	    int which_pred = q.nextEdge(0).which_pred(); // diff from wp0
+
+	    // update phi args.
+	    for (int i=0; i<phi.src.length; i++) {
+		Temp t = (Temp) h.get(phi.src[i][which_pred]);
+		if (t!=null) { phi.src[i][which_pred] = t; }
+	    }
+
+	    // link out CJMP
+	    Quad.addEdge(q.prev(0), q.prevEdge(0).which_succ(), 
+			 phi, which_pred);
+	    markAbsent(q.test);
+	    W.remove(q); // can't optimize this any more.
+	    return true;
 	}
 
 	void markAbsent(Temp t) {
