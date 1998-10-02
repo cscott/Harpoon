574217efd0551a1614bf45fa8c78353e638bdd74
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-10-02 04:44:25 +0000

    Added better documentation.

--- IR/QuadSSA/CONST.java
+++ IR/QuadSSA/CONST.java
@@ -7,10 +7,27 @@ import harpoon.Temp.TempMap;
 import harpoon.Util.Util;
 /**
  * <code>CONST</code> objects represent an assignment of a constant value
- * to a compiler temporary.
+ * to a compiler temporary.<p>
+ * The <code>type</code> field of a <code>CONST</code> must be one of:
+ * <code>String</code>, <code>int</code>, <code>long</code>,
+ * <code>float</code>, <code>double</code> or <code>void</code.
+ * A <code>void</code> type corresponds to a <code>null</code> literal
+ * constant, and in this case the <code>value</code> field will be
+ * <code>null</code>.  In all other cases, <code>value</code> will contain
+ * an object of the specified type; values of primitive types will use
+ * the standard wrapper class.  An example may clarify:
+ * <UL>
+ *  <LI>The integer constant 0 is represented by <BR>
+ *     <CODE>new CONST(source, dst, new Integer(0), HClass.Int);</code>
+ *  <LI>The string constant "hello, world" is represented by <BR>
+ *     <CODE>new CONST(source, dst, "hello, world",
+ *           HClass.forName("java.lang.String") );</code>
+ *  <LI>The null literal is represented by <BR>
+ *     <CODE>new CONST(source, dst, null, HClass.Void);</code>
+ * </UL>
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: CONST.java,v 1.10 1998-09-16 06:32:47 cananian Exp $
+ * @version $Id: CONST.java,v 1.11 1998-10-02 04:44:25 cananian Exp $
  */
 
 public class CONST extends Quad {
@@ -25,6 +42,10 @@ public class CONST extends Quad {
 	this.dst = dst;
 	this.value = value;
 	this.type = type;
+	Util.assert(type==HClass.Int   || type==HClass.Long   ||
+		    type==HClass.Float || type==HClass.Double ||
+		    type==HClass.Void  || 
+		    type==HClass.forName("java.lang.String"));
     }
 
     /** Returns the Temp defined by this Quad.
