27ebd59d883cc3ecf62e550ea171431ecc1e3aab
C. Scott Ananian <cananian@alumni.princeton.edu>
1998-12-11 20:40:36 +0000

    Added the ARRAYINIT quad to compactly represent array initialization. Nothing generates this quad, yet, but the rewritten bytecode-to-quad translator will.  You should start thinking about supporting this quad in your analysis routines.

---
+++ IR/Quads/ARRAYINIT.java
@@ -0,0 +1,84 @@
+// ARRAYINIT.java, created Fri Dec 11 07:06:49 1998 by cananian
+// Copyright (C) 1998 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.IR.Quads;
+
+import harpoon.ClassFile.*;
+import harpoon.Temp.Temp;
+import harpoon.Temp.TempMap;
+import harpoon.Util.Util;
+
+/**
+ * <code>ARRAYINIT</code> represents an array initialization operation.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: ARRAYINIT.java,v 1.1.2.1 1998-12-11 20:40:36 cananian Exp $
+ */
+public class ARRAYINIT extends Quad {
+    /** The array reference to initialize. */
+    protected Temp objectref;
+    /** The component type. */
+    protected HClass type;
+    /** The array initializers. */
+    protected Object[] value;
+
+    /** Creates a <code>ARRAYINIT</code> representing an array initializer.
+     *  The values in the <code>value</code> array are stored in sequential
+     *  elements of the array referenced by <code>objectref</code> starting
+     *  at element 0.
+     * @param objectref
+     *        the array to initialize.
+     * @param type
+     *        the component type of the array.
+     * @param value
+     *        the values to store in the array.
+     */
+    public ARRAYINIT(HCodeElement source,
+		     Temp objectref, HClass type, Object[] value) {
+        super(source);
+	this.objectref = objectref;
+	this.type = type;
+	this.value = value;
+	// VERIFY legality of ARRAYINIT.
+	Util.assert(objectref!=null && type!=null && value!=null);
+	Util.assert(type.isPrimitive() && type!=HClass.Void);
+    }
+    /** Returns the <code>Temp</code> referencing the array to be
+     *  initialized. */
+    public Temp objectref() { return objectref; }
+    /** Returns the component type of the array to be initialized. */
+    public HClass type() { return type; }
+    /** Returns the array initializers. */
+    public Object[] value() { return (Object[]) value.clone(); }
+
+    /** Returns the <code>Temp</code> used by this <code>Quad</code>.
+     * @return the <code>objectref</code> field.
+     */
+    public Temp[] use() { return new Temp[] { objectref }; }
+
+    /** Rename all defined variables in this Quad according to a mapping. */
+    public void renameUses(TempMap tm) {
+	objectref = tm.tempMap(objectref);
+    }
+
+    public void visit(QuadVisitor v) { v.visit(this); }
+
+    /** Returns human-readable representation of this quad. */
+    public String toString() {
+	StringBuffer sb = new StringBuffer();
+	sb.append(objectref.toString());
+	sb.append("[] = ARRAYINIT (");
+	sb.append(type.getName());
+	sb.append(") { ");
+	for (int i=0; i<value.length; i++) {
+	    if (type.equals(HClass.forClass(String.class)))
+		sb.append("\""+Util.escape(value.toString())+"\"");
+	    else
+		sb.append(value.toString());
+	    if (i < value.length-1)
+		sb.append(", ");
+	}
+	sb.append(" }");
+	return sb.toString();
+    }
+}
