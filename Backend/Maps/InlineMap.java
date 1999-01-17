1bcc3f9c753f362b07825480689b4b28b991f815
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-01-17 02:19:09 +0000

    Minor changes to Duncan's map definitions.  Mostly more explicit javadoc about the intended function of these maps.  Also changed the maps from interfaces to abstract superclasses for efficiency.  Maybe we'll have to change them back, but we'll leave them as classes for as long as possible.

--- Backend/Maps/InlineMap.java
+++ Backend/Maps/InlineMap.java
@@ -4,17 +4,37 @@ import harpoon.ClassFile.HField;
 
 /**
  * An <code>InlineMap</code> maps an <code>HField</code> to a boolean value 
- * indicated whether the <code>HField</code> should be inlined.
+ * indicated whether the <code>HField</code> can be inlined.
+ * We leave the policy decision of whether it is wise to do so to other
+ * code, which will also be an InlineMap.  So, the idea is that a core
+ * InlineMap implements the conservative analysis of inline-safety, then
+ * a second inline map takes the first inline map and twiddles the inlining
+ * results according to whether or not it thinks inlining is really a good
+ * idea, using some heuristic.  Or you can leave out the second inline map
+ * and its heuristics, which just means you'd like to inline wherever
+ * possible.  Have I confused everyone yet?
  * 
  * @author  Duncan Bryce  <duncan@lcs.mit.edu>
- * @version 1.1.2.1
+ * @version $Id: InlineMap.java,v 1.1.2.3 1999-01-17 02:19:09 cananian Exp $
  */
-public interface InlineMap
+public abstract class InlineMap // may need to be an interface later, but
+                                // we'll try to keep it a class if we can.
 {
   /**
-   * @return true if the <code>HField</code> should be inlined
+   * @return <code>true</code> if the <code>HField</code> can be inlined
+   * using type 1 inlining (the class descriptor for the inlined object is
+   * preserved). This can be done even if the field escapes, as long as it
+   * is a final field.
    */
-  public boolean shouldInline(HField hf);
+  public abstract boolean canInline1(HField hf);
+  /**
+   * @return <code>true</code> if the <code>HField</code> should be inlined
+   * using type 2 inlining (the class descriptor for the inlined object is
+   * omitted). This can only be done if the field doesn't escape.
+   * Returning <code>true</code> implies shouldInline1() returns
+   * <code>true</code>, too.
+   */
+  public abstract boolean canInline2(HField hf);
 }
 
 
--- Backend/Maps/OffsetMap.java
+++ Backend/Maps/OffsetMap.java
@@ -6,19 +6,27 @@ import harpoon.ClassFile.HClass;
 
 /**
  * An <code>OffsetMap</code> maps an <code>HField</code> or an 
- * <code>HMethod</code> to an offset in bytes.  
+ * <code>HMethod</code> to an offset in bytes.  It also reports the
+ * total size of an <code>HClass</code> object.
  * 
  * @author  Duncan Bryce  <duncan@lcs.mit.edu>
- * @version 1.1.2.1
+ * @version $Id: OffsetMap.java,v 1.1.2.4 1999-01-17 02:19:09 cananian Exp $
  */
-public interface OffsetMap
+public abstract class OffsetMap // use an abstract class, if we can.
 {
-  /** Maps an <code>HField</code> to an offset (in bytes) */
-  public int offset(HField hf, InlineMap iMap);
+  /** Maps an <code>HField</code> to an offset (in bytes).
+   *  If the field is inlined using type 1 inlining (which preserves
+   *  the class pointer) then the specified offset points just after the
+   *  class descriptor, in the same place a normal object pointer points.
+   *  If the field in inlined using type 2 inlining (which omits the
+   *  class pointer) then the specified offset points to the first field
+   *  of the object. */
+  public abstract int offset(HField hf);
 
-  /** Maps an <code>HMethod</code> to an offset (in bytes) */
-  public int offset(HMethod hm);
+  /** Maps an <code>HMethod</code> to an offset (in bytes).
+   *  This method must work for interface methods as well as class methods. */
+  public abstract int offset(HMethod hm);
 
   /** Maps an <code>HClass</code> to a size (in bytes) */
-  public int size(HClass hc);
+  public abstract int size(HClass hc);
 }
