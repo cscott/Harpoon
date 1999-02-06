b1ad3009b7880f1ebb4cce685dc5cf4ce5c403ed
Duncan Bryce <duncan@lcs.mit.edu>
1999-02-06 21:51:03 +0000

    Fixed various bugs in the ToNoSSA translation

--- IR/Quads/ToNoSSA.java
+++ IR/Quads/ToNoSSA.java
@@ -12,91 +12,95 @@ import harpoon.Util.Util;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
-
+ 
 /**
  * The <code>ToNoSSA</code> class implements the translation between SSA 
  * and No-SSA form.  
  *
  * @author  Duncan Bryce <duncan@lcs.mit.edu>
- * @version $Id: ToNoSSA.java,v 1.1.2.2 1999-02-04 23:10:06 cananian Exp $
+ * @version $Id: ToNoSSA.java,v 1.1.2.3 1999-02-06 21:51:03 duncan Exp $
  */
 public class ToNoSSA 
 {
   /**
    * Translates the code in the supplied codeview from SSA to No-SSA form, 
-   * returning the new root <code>Quad</code>.
-   */ // this method makes it clearer to the user that derivation information
-      // is optional.
-  public static Quad translate(final QuadFactory qf, final Code code)
-    {
-      return translate(qf, null, code);
-    }
-  /**
-   * Translates the code in the supplied codeview from SSA to No-SSA form, 
    * returning the new root <code>Quad</code>.  The <code>Hashtable</code>
    * parameter is used to maintain derivation information.
+   * 
+   * @parameter qf    the <code>QuadFactory</code> which will manufacture the
+   *                  translated <code>Quad</code>s.
+   * @parameter hD    a <code>Hashtable</code> containing the derivation 
+   *                  information of the specified codeview.
+   * @parameter code  the codeview containing the <code>Quad</code>s to 
+   *                  translate.
    */
-  public static Quad translate(final QuadFactory qf,
-			       final Hashtable hD, 
-			       final Code code)
+  public static Quad translate(QuadFactory qf, Hashtable hD, Code code)
     {
-      CloningTempMap     ctm;
-      QuadMap            qm;
-      QuadNoSSAVisitor   v;
-      NameMap            nm;
-      Quad               old_header, qTmp;
-      
       Util.assert(code.getName().equals("quad-ssa") ||
 		  code.getName().equals("low-quad-ssa") ||
 		  code.getName().equals("quad-with-try"));
-      
-      
-      old_header = (Quad)code.getRootElement();
-      ctm        = new CloningTempMap(old_header.getFactory().tempFactory(), 
-				      qf.tempFactory());
-      qm         = new QuadMap();
-      nm         = new NameMap(); 
-      v          = new QuadNoSSAVisitor(ctm, qf, qm, hD, nm);
 
+      
+      CloningTempMap     ctm;
+      SIGMAVisitor       sigmaVisitor;
+      NameMap            nm;
+      PHIVisitor         phiVisitor;
+      Quad               old_header, qTmp;
+      QuadMap            qm;
+
+      
+      old_header   = (Quad)code.getRootElement();
+      ctm          = new CloningTempMap(old_header.getFactory().tempFactory(), 
+					qf.tempFactory());
+      nm           = new NameMap(); 
+      qm           = new QuadMap();
+      sigmaVisitor = new SIGMAVisitor(ctm, nm, qf, qm);
+      
       for (Enumeration e = code.getElementsE(); e.hasMoreElements();)
-	{
-	  ((Quad)e.nextElement()).visit(v);
-	}
+	((Quad)e.nextElement()).visit(sigmaVisitor);
       
       qm.rename(qf, nm, nm);
+      
       for (Enumeration e = code.getElementsE(); e.hasMoreElements();)
-	{
-	  qTmp       = (Quad)e.nextElement();
-	  Edge[] el  = qTmp.nextEdge();	  
-	  for (int i=0; i<el.length; i++)
-	    Quad.addEdge(qm.getFoot((Quad)el[i].from()),
-			 el[i].which_succ(),
-			 qm.getHead((Quad)el[i].to()),
-			 el[i].which_pred());
-	}
+        {
+          qTmp       = (Quad)e.nextElement();
+          Edge[] el  = qTmp.nextEdge();   
+          for (int i=0; i<el.length; i++)
+            Quad.addEdge(qm.getFoot((Quad)el[i].from()),
+                         el[i].which_succ(),
+                         qm.getHead((Quad)el[i].to()),
+                         el[i].which_pred());
+        }
+
+      phiVisitor = new PHIVisitor(qf, hD, nm);
+      for (Enumeration e = code.getElementsE(); e.hasMoreElements();)
+	qm.getFoot((Quad)e.nextElement()).visit(phiVisitor);
+
       return qm.getHead(old_header);
     }
 }
 
-/**
- * Implements the SSA to No-SSA translation 
+/*
+ * Performs the first phase of the transformation to NoSSA form:
+ * removing the SIGMAs.  This visitor also serves the purpose of cloning
+ * the quads in the codeview, which is necessary because the PHIVisitor
+ * will actually modify these quads, and we do not want these effects
+ * to propagate outside of the ToNoSSA class.
  */
-class QuadNoSSAVisitor extends LowQuadVisitor
+class SIGMAVisitor extends LowQuadVisitor
 {
-  private CloningTempMap  m_ctm;
-  private Hashtable       m_dT;
-  private QuadFactory     m_qf;
-  private QuadMap         m_qm;
-  private NameMap         m_nm;
-
-  public QuadNoSSAVisitor(CloningTempMap ctm, QuadFactory qf, 
-			  QuadMap qm, Hashtable dT, NameMap nm)
-    {      
+  private CloningTempMap m_ctm;
+  private NameMap        m_nm;
+  private QuadFactory    m_qf;
+  private QuadMap        m_qm;
+  
+  public SIGMAVisitor(CloningTempMap ctm, NameMap nm, 
+		      QuadFactory qf, QuadMap qm)
+    {
       m_ctm = ctm;
-      m_dT  = dT;
+      m_nm  = nm;
       m_qf  = qf;
       m_qm  = qm;
-      m_nm  = nm;
     }
   
   public void visit(Quad q)
@@ -104,66 +108,141 @@ class QuadNoSSAVisitor extends LowQuadVisitor
       Quad qm = (Quad)(q.clone(m_qf, m_ctm));
       m_qm.put(q, qm, qm);
     }
-
-  public void visit(PHI q)
-    {
-      PHI   q0;
-      int   arity, numPhis;
-
-      q0       = (PHI)(q.clone(m_qf, m_ctm));
-      numPhis  = q.numPhis();
-      arity    = q.arity();
-      
-      for (int i = 0; i < numPhis; i++)
-	{
-	  for (int j = 0; j < arity; j++)
-	    {
-	      pushBack(q, i, j);
-	    }
-	  q0.removePhi(0);
-	}
-
-      m_qm.put(q, q0, q0);
-    }
   
-  public void visit(SIGMA q)
+  public void visit(AGET q)    { visit((Quad)q); }
+  public void visit(ASET q)    { visit((Quad)q); }
+  public void visit(CALL q)    { visit((Quad)q); }
+  public void visit(GET q)     { visit((Quad)q); }
+  public void visit(HANDLER q) { visit((Quad)q); }
+  public void visit(OPER q)    { visit((Quad)q); }
+  public void visit(SET q)     { visit((Quad)q); }
+
+
+  public void visit(CJMP q)
     {
       SIGMA  q0;
       int    arity, numSigmas;
       
-      q0         = (SIGMA)(q.clone(m_qf, m_ctm));
+      q0         = new CJMP(m_qf, q, map(q.test()), new Temp[] {});
       numSigmas  = q.numSigmas();
       arity      = q.arity();
 
       for (int i=0; i<numSigmas; i++)
-	{
-	  for (int j=0; j<arity; j++)
-	    {
-	      renameSigma(q, j, i);
-	    }
-	  q0.removeSigma(0);
-	}
+	for (int j=0; j<arity; j++)
+	  renameSigma(q, j, i);
 
       m_qm.put(q, q0, q0);
     }
 
-  private void renameSigma(SIGMA q, int dstIndex, int srcIndex)
+  public void visit(SWITCH q)
     {
-      m_nm.map(q.dst(srcIndex, dstIndex), q.src(srcIndex));
-    }
+      SIGMA  q0;
+      int    arity, numSigmas;
+      int[]  keys;
+      
+      keys       = new int[q.keysLength()];
+      System.arraycopy(q.keys(), 0, keys, 0, q.keysLength());
+      q0         = new SWITCH(m_qf, q, map(q.index()), keys, new Temp[] {});
+      numSigmas  = q.numSigmas();
+      arity      = q.arity();
 
-  private void pushBack(PHI q, int dstIndex, int srcIndex)
-    {
-      Quad q0 = new MOVE(m_qf, q, 
-			 map(q.dst(dstIndex)), 
-			 map(q.src(dstIndex, srcIndex)));
-      m_qm.put((Quad)q.prevEdge(srcIndex).from(), q0, q0, false);
-    }
+      for (int i=0; i<numSigmas; i++)
+	for (int j=0; j<arity; j++)
+	  renameSigma(q, j, i);
 
+      m_qm.put(q, q0, q0);
+    }
+  
   private Temp map(Temp t) 
     {
       return (t==null)?null:m_ctm.tempMap(t);
     }  
+
+  private void renameSigma(SIGMA q, int dstIndex, int srcIndex)
+    {
+      m_nm.map(map(q.dst(srcIndex, dstIndex)), map(q.src(srcIndex)));
+    }
+}
+
+/**
+ * Performs the second phase of the transformation to NoSSA form:
+ * the removal of the PHI nodes.  This is done by actually modifying
+ * the CFG directly, so it is advisable to use this visitor only
+ * on a clone of the actual CFG you wish to translate.  
+ */
+class PHIVisitor extends LowQuadVisitor
+{
+  private Hashtable       m_dT;
+  private QuadFactory     m_qf;
+  private NameMap         m_nm;
+
+  public PHIVisitor(QuadFactory qf, Hashtable dT, NameMap nm)
+    {      
+      m_dT  = dT;
+      m_qf  = qf;
+      m_nm  = nm;
+    }
+  
+  public void visit(Quad q) { }
+
+  public void visit(AGET q)    { visit((Quad)q); }
+  public void visit(ASET q)    { visit((Quad)q); }
+  public void visit(CALL q)    { visit((Quad)q); }
+  public void visit(GET q)     { visit((Quad)q); }
+  public void visit(HANDLER q) { visit((Quad)q); }
+  public void visit(OPER q)    { visit((Quad)q); }
+  public void visit(SET q)     { visit((Quad)q); }
+  
+  public void visit(LABEL q)
+    {
+      int numPhis = q.numPhis(), arity = q.arity();
+      
+      for (int i=0; i<numPhis; i++)
+	for (int j=0; j<arity; j++)
+	  pushBack(q, i, j);
+      
+      removePHIs(q, new LABEL(m_qf, q, q.label(), new Temp[] {}, q.arity()));
+    }
+      
+  public void visit(PHI q)
+    {
+      int numPhis = q.numPhis(), arity = q.arity();
+      
+      for (int i=0; i<numPhis; i++)
+	for (int j=0; j<arity; j++)
+	  pushBack(q, i, j);
+
+      removePHIs(q, new PHI(m_qf, q, new Temp[] {}, q.arity()));
+    }
+
+  private void removePHIs(PHI q, PHI q0)
+    {
+      Edge[] el;
+      
+      el = q.prevEdge();
+      for (int i=0; i<el.length; i++)
+	Quad.addEdge(q.prev(i), q.prevEdge(i).which_succ(),
+		     q0, q.prevEdge(i).which_pred());
+      
+      el = q.nextEdge();
+      for (int i=0; i<el.length; i++)
+	Quad.addEdge(q0, q.nextEdge(i).which_pred(),
+		     q.next(i), q.nextEdge(i).which_succ());
+      
+    }
+
+  private void pushBack(PHI q, int dstIndex, int srcIndex)
+    {
+      // Must insert a MOVE between the PHI and its previous Quad
+
+      Edge from = q.prevEdge(srcIndex);
+      MOVE m = new MOVE(m_qf, q, 
+			q.dst(dstIndex), 
+			q.src(dstIndex, srcIndex));
+      Quad.addEdge(q.prev(srcIndex), from.which_succ(), m, 0);
+      Quad.addEdge(m, 0, q, from.which_pred());
+    }
+
 }
 
 
