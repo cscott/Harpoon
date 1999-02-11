4990b362454f29f030e4b9fb1849f122232579d3
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-02-11 20:56:45 +0000

    Normalized harpoon.Main.Graph so it accepts a -code option like the rest of harpoon.Main.*.  Removed harpoon.Main.LQMain since it is not needed anymore (harpoon.Main.Main -code low-quad-ssa does the same thing).

--- Main/Graph.java
+++ Main/Graph.java
@@ -14,34 +14,37 @@ import harpoon.Util.Util;
  * <code>Graph</code> is a command-line graph generation tool.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Graph.java,v 1.12.2.4 1999-02-04 07:18:25 duncan Exp $
+ * @version $Id: Graph.java,v 1.12.2.5 1999-02-11 20:56:45 cananian Exp $
  */
 
 public abstract class Graph extends harpoon.IR.Registration {
 
     public static final void main(String args[]) {
 	java.io.PrintWriter out = new java.io.PrintWriter(System.out, true);
+	HCodeFactory hcf = // default code factory.
+	    harpoon.Analysis.QuadSSA.SCC.SCCOptimize.codeFactory
+	    (harpoon.IR.Quads.QuadSSA.codeFactory()
+	     );
+	int n=0;  // count # of args/flags processed.
+	for (; n < args.length ; n++) {
+	    if (args[n].startsWith("-code")) {
+		if (++n >= args.length)
+		    throw new Error("-code option needs codename");
+		hcf = HMethod.getCodeFactory(args[n]);
+		if (hcf==null)
+		    throw new Error("Invalid codename: "+args[n]);
+	    } else break; // no more command-line options.
+	}
 
-	HClass cls = HClass.forName(args[0]);
+	HClass cls = HClass.forName(args[n++]);
 	HMethod hm[] = cls.getDeclaredMethods();
 	HMethod m = null;
 	for (int i=0; i<hm.length; i++)
-	    if (hm[i].getName().equals(args[1])) {
-		m = hm[i];
+	    if (hm[i].getName().equals(args[n])) {
+		m = hm[i]; n++;
 		break;
 	    }
-
-	HCodeFactory hcf = null;
-	if (args.length <=3 || args[3].equals("quad-ssa"))
-	    hcf = harpoon.IR.Quads.QuadSSA.codeFactory();
-	if (args.length > 3 && args[3].equals("quad-no-ssa"))
-	    hcf = harpoon.IR.Quads.QuadNoSSA.codeFactory();
-	if (args.length > 3 && args[3].equals("quad-with-try"))
-	    hcf = harpoon.IR.Quads.QuadWithTry.codeFactory();
-	if (args.length > 3 && args[3].equals("low-quad-ssa"))
-	    hcf = harpoon.IR.LowQuad.LowQuadSSA.codeFactory();
-	if (args.length > 3 && args[3].equals("low-quad-no-ssa"))
-	    hcf = harpoon.IR.LowQuad.LowQuadNoSSA.codeFactory();
+	if (m==null) throw new Error("Couldn't find method "+args[n]);
 
 	HCode hc = hcf.convert(m);
 
