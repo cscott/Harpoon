d8d9ec329fd63b8a06e9b675831c04a2812ba744
Duncan Bryce <duncan@lcs.mit.edu>
1999-02-15 08:36:39 +0000

    Added a couple more methods to Frame.java.  Additionally added a DefaultFrame class.  This class is probably not very useful, I simply added it to allow me to test my Tree translator.

---
+++ Backend/Generic/DefaultFrame.java
@@ -0,0 +1,103 @@
+package harpoon.Backend.Generic;
+
+import harpoon.Backend.Allocation.AllocationStrategy;
+import harpoon.Backend.Allocation.DefaultAllocationStrategy;
+import harpoon.Backend.Allocation.DefaultAllocationInfo;
+import harpoon.Backend.Maps.OffsetMap;
+import harpoon.Backend.Maps.OffsetMap32;
+import harpoon.ClassFile.HCodeElement;
+import harpoon.IR.Tree.Exp;
+import harpoon.IR.Tree.CALL;
+import harpoon.IR.Tree.CONST;
+import harpoon.IR.Tree.MEM;
+import harpoon.IR.Tree.NAME;
+import harpoon.IR.Tree.Stm;
+import harpoon.IR.Tree.TEMP;
+import harpoon.IR.Tree.TreeFactory;
+import harpoon.IR.Tree.Type;
+import harpoon.Temp.Label;
+import harpoon.Temp.Temp;
+import harpoon.Temp.TempFactory;
+
+public class DefaultFrame extends Frame implements DefaultAllocationInfo {
+
+    private AllocationStrategy  m_allocator;
+    private int                 m_nextPtr;
+    private OffsetMap           m_offsetMap;
+    private Temp[]              m_registers;
+    private TempFactory         m_tempFactory;
+
+    public DefaultFrame() {
+        m_allocator   = new DefaultAllocationStrategy(this);
+	m_nextPtr     = 0x0fff0000;  // completely arbitrary
+        m_offsetMap   = new OffsetMap32(null);
+	m_tempFactory = Temp.tempFactory("global");
+	m_registers = new Temp[16];
+	for (int i=0; i<16; i++) 
+	    m_registers[i] = new Temp(m_tempFactory);
+    }
+
+    public Exp malloc(Exp size) {
+        return m_allocator.malloc(size);
+    }
+    
+    public OffsetMap offsetMap() {
+        return m_offsetMap;
+    }
+
+    public boolean pointersAreLong() {
+        return false;
+    }
+
+    public Temp RV() {
+        return registers()[0];
+    }
+
+    public Temp FP() {
+        return registers()[1];
+    }
+
+    public Temp[] registers() {
+        return m_registers;
+    }
+
+    public TempFactory tempFactory() { 
+        return m_tempFactory;
+    }
+
+    /* Implementation of the DefaultAllocationInfo interface.
+     * NOTE that this is not really a realistic implementation,
+     * rather, it is a placeholder that allows me to test other
+     * parts of the code.  
+     */
+    public Stm GC(TreeFactory tf, HCodeElement src) { 
+        return new CALL(tf, src, 
+			new TEMP(tf, src, 
+				 Type.POINTER, new Temp(tf.tempFactory())), 
+			new TEMP(tf, src, 
+				 Type.POINTER, new Temp(tf.tempFactory())), 
+		        new NAME(tf, src, new Label("RUNTIME_GC")),
+			null); 
+    }
+    
+    public Exp mem_limit(TreeFactory tf, HCodeElement src) { 
+        return new CONST(tf, src, 4000000);  // again, completely arbitrary
+    }
+  
+    public MEM next_ptr(TreeFactory tf, HCodeElement src) { 
+        return new MEM(tf, src, Type.INT,
+		       new CONST(tf, src, m_nextPtr));
+    }
+    
+    public Stm out_of_memory(TreeFactory tf, HCodeElement src) {
+        return new CALL(tf, src, 
+			new TEMP(tf, src, 
+				 Type.POINTER, new Temp(tf.tempFactory())), 
+			new TEMP(tf, src, 
+				 Type.POINTER, new Temp(tf.tempFactory())), 
+		        new NAME(tf, src, new Label("RUNTIME_OOM")),
+			null); 
+    }
+
+}
+
--- Backend/Generic/Frame.java
+++ Backend/Generic/Frame.java
@@ -3,16 +3,29 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Backend.Generic;
 
+import harpoon.Backend.Maps.OffsetMap;
+import harpoon.IR.Tree.Exp;
 import harpoon.Temp.Temp;
+import harpoon.Temp.TempFactory;
 
 /**
  * A <code>Frame</code> encapsulates the machine-dependent information
  * needed for compilation.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Frame.java,v 1.1.2.2 1999-02-09 05:45:32 andyb Exp $
+ * @version $Id: Frame.java,v 1.1.2.3 1999-02-15 08:36:39 duncan Exp $
  */
 public abstract class Frame  {
+
+    /** Returns a <code>Tree.Exp</code> object which represents a pointer
+     *  to a newly allocated block of memory, of the specified size.  
+     *  Generates code to handle garbage collection, and OutOfMemory errors.
+     */
+    public abstract Exp malloc(Exp size);
+    
+    /** Returns the appropriate offset map for this frame */
+    public abstract OffsetMap offsetMap();
+
     /** Returns <code>false</code> if pointers can be represented in
      *  32 bits, or <code>true</code> otherwise. */
     public abstract boolean pointersAreLong();
@@ -28,4 +41,8 @@ public abstract class Frame  {
     /** Returns an array of <code>Temp</code>s which represent all
      *  the available registers on the machine. */
     public abstract Temp[] registers();
+  
+    /** Returns the TempFactory used by this Frame */
+    public abstract TempFactory tempFactory();
+      
 }
