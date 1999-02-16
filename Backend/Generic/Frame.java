de0c71f83a66ea7d1488d0ddc0d0401669fe3a7f
Andrew Berkheimer <andyb@mit.edu>
1999-02-16 21:11:38 +0000

    Merged in the work I was doing on Frame with Duncan's modifications. No major changes.

--- Backend/Generic/Frame.java
+++ Backend/Generic/Frame.java
@@ -3,19 +3,23 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Backend.Generic;
 
-import harpoon.Backend.Maps.OffsetMap;
-import harpoon.IR.Tree.Exp;
 import harpoon.Temp.Temp;
+import harpoon.Backend.Allocation.DefaultAllocationInfo;
+import harpoon.IR.Tree.Exp;
+import harpoon.IR.Tree.Stm;
+import harpoon.IR.Tree.MEM;
+import harpoon.Backend.Maps.OffsetMap;
 import harpoon.Temp.TempFactory;
+import harpoon.ClassFile.HCodeElement;
 
 /**
  * A <code>Frame</code> encapsulates the machine-dependent information
  * needed for compilation.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Frame.java,v 1.1.2.3 1999-02-15 08:36:39 duncan Exp $
+ * @version $Id: Frame.java,v 1.1.2.4 1999-02-16 21:11:38 andyb Exp $
  */
-public abstract class Frame  {
+public abstract class Frame {
 
     /** Returns a <code>Tree.Exp</code> object which represents a pointer
      *  to a newly allocated block of memory, of the specified size.  
@@ -34,14 +38,14 @@ public abstract class Frame  {
      *  for procedures will be stored. */
     public abstract Temp RV();
 
-    /** Returns a <code>Temp</code> to represent where the frame
-     *  pointer will be stored. */
+    /** Returns a specially-named Temp to use for the frame pointer.
+     */
     public abstract Temp FP();
 
     /** Returns an array of <code>Temp</code>s which represent all
      *  the available registers on the machine. */
     public abstract Temp[] registers();
-  
+
     /** Returns the TempFactory used by this Frame */
     public abstract TempFactory tempFactory();
       
