904d704da17afcb57f073776d417176bd9233f55
Andrew Berkheimer <andyb@mit.edu>
1999-02-17 03:29:44 +0000

    Renamed Code and Frame to SACode and SAFrame respectively to help avoid confusion and name duplication with their superclasses.
    Also added code factory related methods to SACode and
    the beginnings of a TreeVisitor subclass to CodeGen.
    @RENAME: /Code /SACode@

---
+++ Backend/StrongARM/SACode.java
@@ -0,0 +1,71 @@
+// SACode.java, created by andyb
+// Copyright (C) 1999 Andrew Berkheimer <andyb@mit.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Backend.StrongARM;
+
+import harpoon.Backend.Generic.Code;
+import harpoon.ClassFile.HCode;
+import harpoon.ClassFile.HCodeFactory;
+import harpoon.ClassFile.HMethod;
+import harpoon.IR.Assem.Instr;
+import harpoon.IR.Tree.TreeCode;
+
+/**
+ * <code>SACode</code> is a code-view for StrongARM
+ * assembly-like syntax (currently without register allocation).
+ *
+ * @author  Andrew Berkheimer <andyb@mit.edu>
+ * @version $Id: SACode.java,v 1.1.2.1 1999-02-17 03:29:44 andyb Exp $
+ */
+public class SACode extends Code {
+    /** The name of this code view. */
+    public static final String codename = "strongarm";
+
+    SACode(TreeCode tree) {
+        super(tree.getMethod(), null);
+
+        instrs = CodeGen.codegen(tree, this);
+    }
+
+    private SACode(HMethod parent, Instr[] instrs) {
+        super(parent, instrs);
+    }
+
+    /** Returns the name of this code view.
+     *
+     *  @return         The String naming this codeview, "strongarm".
+     */
+    public String getName() { return codename; }
+
+    public HCode clone(HMethod newMethod) throws CloneNotSupportedException {
+        throw new CloneNotSupportedException(this.toString());
+    }
+
+    public static HCodeFactory codeFactory(final HCodeFactory hcf) {
+        if (hcf.getCodeName().equals(TreeCode.codename)) {
+            return new HCodeFactory() {
+                public HCode convert(HMethod m) {
+                    HCode c = hcf.convert(m);
+                    return (c==null) ? null :
+                        new SACode((TreeCode)c);
+                }
+                public void clear(HMethod m) { hcf.clear(m); }
+                public String getCodeName() { return codename; }
+            };
+        } else {
+            throw new Error("Cannot make " + codename + " from " +
+                            hcf.getCodeName());
+        }
+    }
+
+    public static HCodeFactory codeFactory() {
+        return codeFactory(TreeCode.codeFactory(new SAFrame()));
+    }
+            
+    /** Registers this codeview so that it will be recognized later. 
+     */
+    public static void register() { 
+        HMethod.register(codeFactory());
+    }
+
+}
