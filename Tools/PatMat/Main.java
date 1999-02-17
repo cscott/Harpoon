d99a2a37af4a4fcc358109f6682f97b6f62e84fc
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-02-17 21:10:13 +0000

    More work on pattern-matching instruction-selection tool parser/lexer. Now reads in StrongARM spec without problems.  Just hafta flesh out the parser action rules to build a representation of the input file.

---
+++ Tools/PatMat/Main.java
@@ -0,0 +1,21 @@
+package harpoon.Tools.PatMat;
+
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/* Test skeleton for java parser/lexer.
+ * Copyright (C) 1998 C. Scott Ananian <cananian@alumni.princeton.edu>
+ * This is released under the terms of the GPL with NO WARRANTY.
+ * See the file COPYING for more details.
+ */
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    Reader r = new BufferedReader(new FileReader(args[0]));
+    ErrorMsg e = new ErrorMsg(args[0]);
+    Lexer l = new Lexer(r, e);
+    Parser p = new Parser(l, e);
+    p./*debug_*/parse();
+  }
+}
