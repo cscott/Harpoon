285ea70486bddd1fb33fa5b5a638bb441744f74c
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-02-18 11:46:38 +0000

    Cleaned up comments a bit.

--- Tools/PatMat/Main.java
+++ Tools/PatMat/Main.java
@@ -1,15 +1,17 @@
+// Main.java, created Wed Feb 17 22:05:18 1999 by cananian
+// Copyright (C) 1999 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Tools.PatMat;
 
 import java.io.Reader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-/* Test skeleton for java parser/lexer.
- * Copyright (C) 1998 C. Scott Ananian <cananian@alumni.princeton.edu>
- * This is released under the terms of the GPL with NO WARRANTY.
- * See the file COPYING for more details.
+/* Main entry point for the instruction selection tool.
+ *
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: Main.java,v 1.1.2.3 1999-02-18 11:46:38 cananian Exp $
  */
-
 public class Main {
   public static void main(String args[]) throws Exception {
     Reader r = new BufferedReader(new FileReader(args[0]));
