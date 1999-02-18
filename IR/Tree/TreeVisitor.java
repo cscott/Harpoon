a03d4b73eae2b8a8a90d27f5ee66994a1419d19f
Duncan Bryce <duncan@lcs.mit.edu>
1999-02-18 23:03:17 +0000

    Added new Tree classes to the TreeVisitor class

--- IR/Tree/TreeVisitor.java
+++ IR/Tree/TreeVisitor.java
@@ -13,16 +13,18 @@ public abstract class TreeVisitor
   public abstract void visit(Exp e);
   public abstract void visit(Stm e);
   public void visit(BINOP e)          { visit((OPER)e); }
-  public void visit(CALL e)           { visit((Stm)e); }
+  public void visit(CALL e)           { visit((INVOCATION)e); }
   public void visit(CJUMP e)          { visit((Stm)e); }
   public void visit(CONST e)          { visit((Exp)e); }
   public void visit(ESEQ e)           { visit((Exp)e); }
   public void visit(EXP e)            { visit((Stm)e); }
+  public void visit(INVOCATION e)     { visit((Stm)e); }
   public void visit(JUMP e)           { visit((Stm)e); }
   public void visit(LABEL e)          { visit((Stm)e); }
   public void visit(MEM e)            { visit((Exp)e); }
   public void visit(MOVE e)           { visit((Stm)e); }
   public void visit(NAME e)           { visit((Exp)e); }
+  public void visit(NATIVECALL e)     { visit((INVOCATION)e); }
   public void visit(OPER e)           { visit((Exp)e); }
   public void visit(SEQ e)            { visit((Stm)e); }
   public void visit(TEMP e)           { visit((Exp)e); }
