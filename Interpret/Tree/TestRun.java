5e80d5fd6f52da4066a72f1f916fda161aa9ed7e
Duncan Bryce <duncan@lcs.mit.edu>
1999-03-27 22:45:30 +0000

    Cleaned up the code to TestRun a bit

--- Interpret/Tree/TestRun.java
+++ Interpret/Tree/TestRun.java
@@ -23,71 +23,26 @@ import java.util.zip.GZIPOutputStream;
  * <code>Run</code> invokes the interpreter.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: TestRun.java,v 1.1.2.1 1999-03-27 22:05:10 duncan Exp $
+ * @version $Id: TestRun.java,v 1.1.2.2 1999-03-27 22:45:30 duncan Exp $
  */
 public abstract class TestRun {
     public static void main(String args[]) {
-      
-      // Test HClassInfo
-
-      /*
-	HClassInfo hci = new HClassInfo();
-	HClass hvec = HClass.forName("java.util.Hashtable");
-	HField hf[] = hvec.getDeclaredFields();
-	HMethod hm[] = hvec.getDeclaredMethods();
-	
-	for (int i=0; i<hf.length; i++) {
-	System.err.print(hf[i].isStatic()?"STATIC_":"NON_STATIC_");
-	System.err.println("FIELD: " + hf[i] + ", " + hci.getFieldOffset(hf[i]));
-	}
-	for (int i=0; i<hm.length; i++) {
-	System.err.print(hm[i].isStatic()?"STATIC_":"NON_STATIC_");
-	System.err.println("METHOD: " + hm[i] + ", " + hci.getMethodOffset(hm[i]));
-	}
-      */
         java.io.PrintWriter out = new java.io.PrintWriter(System.err, true);
 
 	HCodeFactory hcf = // default code factory.
 	    harpoon.IR.Quads.QuadSSA.codeFactory();
 
-	int i=0; // count # of args/flags processed.
-	// check for "-prof" and "-code" flags in arg[i]
-
 	HClass cls = HClass.forName(args[0]);
-	
-	/*	Frame frame =new DefaultFrame
-	  (new InterpreterOffsetMap
-	   (new ClassHierarchy(cls.getMethod
-			       ("main", new HClass[] { 
-				 HClass.forName("[Ljava/lang/String;") }),
-			       hcf)),
-	   new InterpreterAllocationStrategy());
-	*/
 	Frame frame = new DefaultFrame(new InterpreterOffsetMap(null),
 				       new InterpreterAllocationStrategy());
 
-	//	hcf.convert(HClass.forName("java.util.Properties").getMethod("getProperty", new HClass[] { HClass.forName("java.lang.String") })).print(out);
-
 	hcf = harpoon.IR.LowQuad.LowQuadSSA.codeFactory(hcf);
-
-	//	hcf.convert(HClass.forName("java.util.Properties").getMethod("getProperty", new HClass[] { HClass.forName("java.lang.String") })).print(out);
-
 	hcf = harpoon.IR.LowQuad.LowQuadNoSSA.codeFactory(hcf);
-	//	hcf.convert(HClass.forName("java.util.Properties").getMethod("getProperty", new HClass[] { HClass.forName("java.lang.String") })).print(out);
-
        	hcf = harpoon.IR.Tree.TreeCode.codeFactory(hcf, frame);
-
-	//	hcf.convert(HClass.forName("java.util.Properties").getMethod("getProperty", new HClass[] { HClass.forName("java.lang.String") })).print(out);
-
 	hcf = new CachingCodeFactory(hcf);
 
 	PrintWriter prof = null;
 
-
-
-	// construct caching code factory.
-	//hcf = new CachingCodeFactory(hcf);
-	//////////// now call interpreter with truncated args list.
 	String[] params = new String[args.length-1];
 	System.arraycopy(args, 1, params, 0, params.length);
 	harpoon.Interpret.Tree.Method.run
