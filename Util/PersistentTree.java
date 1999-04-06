0c479e24084e4e56f83ad6e4d8d40a2795bf9894
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-04-06 14:57:40 +0000

    Work on the harpoon.Util.PersistentTree class.  Complete, but untested, now.
    Also added some javadoc to harpoon.Util.AbstractMapEntry.

--- Util/PersistentTree.java
+++ Util/PersistentTree.java
@@ -8,20 +8,22 @@ import java.util.AbstractSet;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Stack;
 /**
- * <code>PersistentTree</code>
+ * <code>PersistentTree</code> implements a persistent binary search tree.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: PersistentTree.java,v 1.1.2.2 1999-04-03 18:02:10 cananian Exp $
+ * @version $Id: PersistentTree.java,v 1.1.2.3 1999-04-06 14:57:40 cananian Exp $
  */
 public class PersistentTree  {
     /*final*/ Node root;
     /*final*/ Comparator c;
 
-    /** Creates an empty <code>PersistentTree</code>. 
-     *  Key objects must implement <code>java.lang.Comparable</code>. 
+    /** Creates an empty <code>PersistentTree</code> whose
+     *  key objects will all implement <code>java.lang.Comparable</code>. 
      */
     public PersistentTree() {
 	this.root = null; this.c = default_comparator;
@@ -38,13 +40,25 @@ public class PersistentTree  {
 	this.root = root; this.c = c;
     }
 
-    /** Determines if this <code>PersistentTree</code> is empty. */
+    /** Determines if this <code>PersistentTree</code> has any mappings. */
     public boolean isEmpty() { return (root==null); }
 
-    /* Creates and returns a new PersistantTree containing the mapping
-     * <code>key->value</code>. */
+    /** Count the number of key->value mappings in this
+     *  <code>PersistentTree</code>. */
+    public int size() {
+	return _size(root);
+    }
+    private int _size(Node n) {
+	return (n==null) ? 0 : (_size(n.left) + _size(n.right));
+    }
+
+    /** Creates and returns a new <code>PersistantTree</code> identical to
+     *  this one, except it contains a mapping from <code>key</code> to
+     *  <code>value. */
     public PersistentTree put(Object key, Object value) {
-	return new PersistentTree(_put(this.root, key, value), c);
+	Node new_root = _put(this.root, key, value);
+	return (this.root == new_root) ? this :
+	    new PersistentTree(new_root, c);
     }
     private Node _put(Node n, Object key, Object value) {
 	if (n==null)
@@ -52,17 +66,23 @@ public class PersistentTree  {
 
 	int r = c.compare(key, n.key);
 	if (r==0)
-	    return new Node(key, value, n.left, n.right);
+	    return newNode(n, key, value, n.left, n.right);
 	if (r < 0)
-	    return new Node(n.key, n.value,
-			    _put(n.left, key, value), n.right);
+	    return newNode(n, n.key, n.value,
+			   _put(n.left, key, value), n.right);
 	if (r > 0)
-	    return new Node(n.key, n.value,
-			    n.left, _put(n.right, key, value));
+	    return newNode(n, n.key, n.value,
+			   n.left, _put(n.right, key, value));
 	throw new Error("Impossible!");
     }
+    /** Make a new node, but use the old node if we can. */
+    private Node newNode(Node oldNode,
+			 Object key, Object value, Node left, Node right) {
+	Node n = new Node(key, value, left, right);
+	return (oldNode.equals(n)) ? oldNode : n;
+    }
 
-    /** Gets the mapping for <code>key</code>. */
+    /** Gets the value which <code>key</code> maps to. */
     public Object get(Object key) {
 	Node np = lookup(key);
 	return (np==null)?null:np.value;
@@ -80,27 +100,41 @@ public class PersistentTree  {
 	}
 	return null; /* no node with this key. */
     }
-
-    /* FIXME:
-    public Map asMap() {
-	return new AbstractMap() {
-	    public Set entrySet() {
-		return new AbstractSet() {
-		    public int size() {
-			int n=0;
-			for (Iterator it=iterator(); it.hasNext(); it.next())
-			    n++;
-			return n;
-		    }
-		    public Iterator iterator() {
-			// do iteration. 
-			return null;
-		    }
-		};
-	    }
-	};
+    /** Make a new <code>PersistentTree</code> identical to this one,
+     *  except that it does not contain a mapping for <code>key</code>. */
+    public PersistentTree remove(Object key) {
+	Node new_root = _remove(this.root, key);
+	return (this.root == new_root) ? this :
+	    new PersistentTree(new_root, c);
     }
-    */
+    private Node _remove(Node n, Object key) {
+	if (n==null) return null; // key not found.
+
+	int r = c.compare(key, n.key);
+	if (r==0) // remove this node.
+	    return _merge(n.left, n.right, true);
+	if (r < 0)
+	    return newNode(n, n.key, n.value,
+			   _remove(n.left, key), n.right);
+	if (r > 0)
+	    return newNode(n, n.key, n.value,
+			   n.left, _remove(n.right, key));
+	throw new Error("Impossible!");
+    }
+    /** Merge two nodes into one. */
+    private Node _merge(Node left, Node right, boolean toggle) {
+	if (left==null) return right;
+	if (right==null) return left;
+	if (toggle) // try not to pile everything on one side.
+	    return new Node(right.key, right.value,
+			    _merge(left, right.left, !toggle), right.right);
+	else
+	    return new Node(left.key, left.value,
+			    left.left, _merge(left.right, right, !toggle));
+    }
+	
+    /** Human-readable representation of the map. */
+    public String toString() { return asMap().toString(); }
 
     /*---------------------------------------------------------------*/
     private static class Node {
@@ -110,9 +144,6 @@ public class PersistentTree  {
 	    this.key = key;  this.value = value;
 	    this.left= left; this.right = right;
 	}
-	public String toString() {
-	    return "<"+key+", "+value+">";
-	}
 	public boolean equals(Object o) {
 	    if (!(o instanceof Node)) return false;
 	    Node n = (Node) o;
@@ -120,9 +151,16 @@ public class PersistentTree  {
 		isSame(key,  n.key)  && isSame(value, n.value) &&
 		isSame(left, n.left) && isSame(right, n.right);
 	}
-	private /*static*/ boolean isSame(Object o1, Object o2) {
+	/*static*/ boolean isSame(Object o1, Object o2) {
 	    return ((o1==null) && (o2==null)) || (o1!=null && o1.equals(o2));
 	}
+	Map.Entry asMapEntry() {
+	    return new AbstractMapEntry() {
+		public Object getKey() { return key; }
+		public Object getValue() { return value; }
+	    };
+	}
+	public String toString() { return asMapEntry().toString(); }
     }
     /* A <code>Comparator</code> for objects that implement 
      *   <code>Comparable</code>. */
@@ -131,4 +169,51 @@ public class PersistentTree  {
 	    return ((Comparable)o1).compareTo(o2);
 	}
     };
+
+    /*---------------------------------------------------------------*/
+    /** <code>java.util.Collection</code>s view of the mapping. */
+    public Map asMap() {
+	return new AbstractMap() {
+	    public boolean containsKey(Object key) {
+		return PersistentTree.this.containsKey(key);
+	    }
+	    public Object get(Object key) {
+		return PersistentTree.this.get(key);
+	    }
+	    public boolean isEmpty() {
+		return PersistentTree.this.isEmpty();
+	    }
+	    public int size() {
+		return PersistentTree.this.size();
+	    }
+	    public Set entrySet() {
+		return new AbstractSet() {
+		    public int size() {
+			return PersistentTree.this.size();
+		    }
+		    public Iterator iterator() {
+			final Stack s = new Stack();
+			if (root!=null) s.push(root);
+
+			return new Iterator() {
+			    public boolean hasNext() {
+				return !s.isEmpty();
+			    }
+			    public Object next() {
+				if (s.isEmpty())
+				    throw new NoSuchElementException();
+				final Node n = (Node) s.pop();
+				if (n.right!=null) s.push(n.right);
+				if (n.left!=null)  s.push(n.left);
+				return n.asMapEntry();
+			    }
+			    public void remove() {
+				throw new UnsupportedOperationException();
+			    }
+			};
+		    }
+		};
+	    }
+	};
+    }
 }
