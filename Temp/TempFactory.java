4486c5abc70498e25dfd2669af4ad0e1ab781f0d
Felix S. Klock II <pnkfelix@mit.edu>
1999-05-24 19:09:41 +0000

    Added carriage returns in between the method descriptions

--- Temp/TempFactory.java
+++ Temp/TempFactory.java
@@ -2,25 +2,28 @@
 package harpoon.Temp;
 
 /**
- * A <code>TempFactory</code> assigned unique identifiers to 
+ * A <code>TempFactory</code> assigns unique identifiers to 
  * <code>Temp</code>s within a given scope.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: TempFactory.java,v 1.1.2.3 1999-02-17 21:14:10 andyb Exp $
+ * @version $Id: TempFactory.java,v 1.1.2.4 1999-05-24 19:09:41 pnkfelix Exp $
  */
 public abstract class TempFactory  {
     /** Returns the static scope of this <code>TempFactory</code>.
      *  Should be unique among <code>TempFactory</code>s and invariant
      *  for a given <code>TempFactory</code>. */
     public abstract String getScope();
+
     /** Returns a unique identifier within this scope.  Not
      *  required to be unique among all <code>TempFactory</code>s.
      *  Should be repeatable; that is, the n'th call to getUniqueID()
      *  with a given suggestion String for a given <code>TempFactory</code>
      *  should always return the same String. */
     protected abstract String getUniqueID(String suggestion);
+
     /** Human-readable representation of <code>TempFactory</code> */
     public String toString() { return "TempFactory["+getScope()+"]"; }
+
     /** Return a hashcode for this <code>TempFactory</code>. */
     public int hashCode() { return getScope().hashCode(); }
 }
