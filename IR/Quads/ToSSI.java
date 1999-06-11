a5518473070086f570cd97a4f3dd7afead9cd877
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-06-11 11:31:31 +0000

    Check in this code, which has been languishing in my directory for some time now.  This was a rewrite of the SSI form, dated Apr 8, that never ended up being used.  However, it is much closer to the Truely Marvelous algorithm that is being written up in my thesis than the crufty one we currently use, so when it comes time to generate numbers, they'll probably come from here.   [CSA]

--- IR/Quads/ToSSI.java
+++ IR/Quads/ToSSI.java
@@ -5,12 +5,18 @@ package harpoon.IR.Quads;
 
 import harpoon.Analysis.SESE;
 import harpoon.ClassFile.HCode;
+import harpoon.ClassFile.HCodeElement;
+import harpoon.Temp.Temp;
+import harpoon.Util.PersistentSet;
+
+import java.util.Iterator;
+import java.util.Map;
 /**
  * The <code>ToSSI</code> class places phi and sigma functions and
  * renames variables to convert to SSI form.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ToSSI.java,v 1.1.2.1 1999-04-03 18:04:43 cananian Exp $
+ * @version $Id: ToSSI.java,v 1.1.2.2 1999-06-11 11:31:31 cananian Exp $
  */
 public class ToSSI  {
     
@@ -18,8 +24,40 @@ public class ToSSI  {
     public ToSSI() {
     }
 
-    private void place(HCode hc) {
+    private void place(HCode hc, Map info) {
+	// REWRITE TO PROCESS REGIONS TOP-DOWN
+	// WHICH MEANS WE NEED A FUNCTION TO ITERATE OVER ALL THE
+	// HCODEELEMENTS IN A GIVEN REGION.
+
 	SESE sese = new SESE(hc, true);
+	// make sets for all regions
+	for (Iterator it=sese.topDown(); it.hasNext(); )
+	    info.put(it.next(), new PersistentSet());
+
+	for (Iterator it=hc.getElementsI(); it.hasNext(); ) {
+	    HCodeElement hce = (HCodeElement) it.next();
+	    SESE.Region r = (SESE.Region) sese.smallestSESE.get(hce);
+	    PersistentSet ps = (PersistentSet) info.get(r);
+	    Temp[] use = ((harpoon.IR.Properties.UseDef) hce).use();
+	    Temp[] def = ((harpoon.IR.Properties.UseDef) hce).def();
+	    
+	    for (int i=0; i<use.length; i++) ps = ps.add(use[i]);
+	    for (int i=0; i<def.length; i++) ps = ps.add(def[i]);
+	    
+	    info.put(r, ps);
+	}
+	// smear down.
+	for (Iterator it=sese.topDown(); it.hasNext(); ) {
+	    SESE.Region r = (SESE.Region) it.next();
+	    PersistentSet ps = (PersistentSet) info.get(r);
+	    for (Iterator itC=r.children().iterator(); itC.hasNext(); ) {
+		SESE.Region rC = (SESE.Region) itC.next();
+		PersistentSet psC = (PersistentSet) info.get(rC);
+		for (Iterator itT=psC.asSet().iterator(); itT.hasNext(); )
+		    ps = ps.add( (Temp) itT.next());
+	    }
+	    info.put(r, ps);
+	}
     }
     private void rename() {
     }
