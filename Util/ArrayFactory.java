4d16093529e779ca6c32977fe2c8279bac085a8d
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-06-16 17:02:59 +0000

    Found missing javadoc, and added it.

--- Util/ArrayFactory.java
+++ Util/ArrayFactory.java
@@ -7,10 +7,17 @@ package harpoon.Util;
  * the object's type.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ArrayFactory.java,v 1.1.2.2 1999-01-22 23:34:00 cananian Exp $
+ * @version $Id: ArrayFactory.java,v 1.1.2.3 1999-06-16 17:02:59 cananian Exp $
  */
 
 public interface ArrayFactory  {
-    /* Create and return a new array of the specified length. */
+    /** Create and return a new array of the specified length.
+     *  The type is determined by the specific <code>ArrayFactory</code>
+     *  that you are using.
+     * @see harpoon.Temp.Temp#arrayFactory
+     * @see harpoon.Temp.Temp#doubleArrayFactory
+     * @see harpoon.IR.Quads.Quad#arrayFactory
+     * @see harpoon.ClassFile.HCode#elementArrayFactory
+     */
     public Object[] newArray(int len);
 }
