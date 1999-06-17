997ad883f3cc4da30e6ae810ae48a98623a7ed08
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-06-17 20:55:13 +0000

    Fix a javadoc blooper (forgot to double the star, so the doc comment wasn't showing up).
    Added nullEnumerator and nullIterator fields to replace the deprecated
    harpoon.Util.NullEnumerator class.
    
    Added singletonIterator() method to replace the deprecated
    harpoon.Util.SingletonEnumerator class.

--- Util/Default.java
+++ Util/Default.java
@@ -3,21 +3,40 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Util;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * <code>Default</code> contains one-off or 'standard, no-frills'
  * implementations.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Default.java,v 1.1.2.1 1999-04-08 06:56:39 cananian Exp $
+ * @version $Id: Default.java,v 1.1.2.2 1999-06-17 20:55:13 cananian Exp $
  */
 public abstract class Default  {
-    /* A <code>Comparator</code> for objects that implement 
+    /** A <code>Comparator</code> for objects that implement 
      *   <code>Comparable</code>. */
     public static final Comparator comparator = new Comparator() {
 	public int compare(Object o1, Object o2) {
 	    return ((Comparable)o1).compareTo(o2);
 	}
     };
+    /** An <code>Enumerator</code> over the empty set.
+     * @deprecated Use nullIterator. */
+    public static final Enumeration nullEnumerator = new Enumeration() {
+	public boolean hasMoreElements() { return false; }
+	public Object nextElement() { throw new NoSuchElementException(); }
+    };
+    /** An <code>Iterator</code> over the empty set. */
+    public static final Iterator nullIterator = new UnmodifiableIterator() {
+	public boolean hasNext() { return false; }
+	public Object next() { throw new NoSuchElementException(); }
+    };
+    /** An <code>Iterator</code> over a singleton set. */
+    public static final Iterator singletonIterator(Object o) {
+	return Collections.singleton(o).iterator();
+    } 
 }
