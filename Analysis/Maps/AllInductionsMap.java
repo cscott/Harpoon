612891f5c13a048c34ba2ead4115caae7a874f55
Brian Demsky <bdemsky@mit.edu>
1999-07-01 19:23:00 +0000

    More bugs.......

--- Analysis/Maps/AllInductionsMap.java
+++ Analysis/Maps/AllInductionsMap.java
@@ -6,18 +6,18 @@ package harpoon.Analysis.Maps;
 import harpoon.Analysis.Loops.Loops;
 import harpoon.ClassFile.HCode;
 
-import java.util.Set;
+import java.util.HashMap;
 
 /**
  * <code>AllInductionsMap</code> is a mapping from <code>Loops</code> to a
  * <code>Set</code> of basic induction <code>Temp</code>s.
  *
  * @author  Brian Demsky <bdemsky@mit.edu>
- * @version $Id: AllInductionsMap.java,v 1.1.2.1 1999-06-29 18:55:55 bdemsky Exp $
+ * @version $Id: AllInductionsMap.java,v 1.1.2.2 1999-07-01 19:23:00 bdemsky Exp $
  */
 public interface AllInductionsMap {
     /** Returns a <code>Set</code> of basic induction <code>Temp</code>s. */
-    public Set allInductionsMap(HCode hc, Loops lp);
+    public HashMap allInductionsMap(HCode hc, Loops lp);
 }
 
 
--- Analysis/Maps/BasicInductionsMap.java
+++ Analysis/Maps/BasicInductionsMap.java
@@ -6,7 +6,7 @@ package harpoon.Analysis.Maps;
 import harpoon.Analysis.Loops.Loops;
 import harpoon.ClassFile.HCode;
 
-import java.util.Set;
+import java.util.HashMap;
 
 
 /**
@@ -14,9 +14,9 @@ import java.util.Set;
  * <code>Set</code> of basic induction <code>Temp</code>s.
  *
  * @author  Brian Demsky <bdemsky@mit.edu>
- * @version $Id: BasicInductionsMap.java,v 1.1.2.1 1999-06-29 18:55:56 bdemsky Exp $
+ * @version $Id: BasicInductionsMap.java,v 1.1.2.2 1999-07-01 19:23:00 bdemsky Exp $
  */
 public interface BasicInductionsMap {
     /** Returns a <code>Set</code> of basic induction <code>Temp</code>s. */
-    public Set basicInductionsMap(HCode hc, Loops lp);
+    public HashMap basicInductionsMap(HCode hc, Loops lp);
 }
