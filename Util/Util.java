91632f6f74e1b59c9cfd408ce3470dd6aa086b6a
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-07-17 09:48:36 +0000

    Uncomment some code which Darko commented out a while ago when various parts of the bytecode->Quad translation engine couldn't handle it.
    Added population-count and zero-count functions to Util.

--- Util/Util.java
+++ Util/Util.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Array;
 /** 
  * Miscellaneous static utility functions.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Util.java,v 1.12.2.7 1999-02-25 19:03:28 cananian Exp $
+ * @version $Id: Util.java,v 1.12.2.8 1999-07-17 09:48:36 cananian Exp $
  */
 public abstract class Util {
   // Util contains only static fields and methods.
@@ -160,46 +160,60 @@ public abstract class Util {
     8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 /* 256 */
   };
   static { assert(bytemsb.length==0x100); }
-  ///** Lowest bit set in a byte. */
-  //static final byte bytelsb[] = {
-  //  0, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
-  //  4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
-  //  5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 7, 1, 2, 1, 3, 1, 2, 1,
-  //  4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
-  //  6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
-  //  4, 1, 2, 1, 3, 1, 2, 1, 8, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
-  //  5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1,
-  //  4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
-  //  7, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
-  //  4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
-  //  5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1  };
-  //static { assert(bytelsb.length==0x100); }
-  //
-  //
-  ///** Find first set (least significant bit).
-  // *  @return the first bit set in the argument.  
-  // *          <code>ffs(0)==0</code> and <code>ffs(1)==1</code>. */
-  //public static final int ffs(int v) {
-  //  if ( (v & 0x0000FFFF) != 0)
-  //    if ( (v & 0x000000FF) != 0)
-  //      return bytelsb[v&0xFF];
-  //    else
-  //      return 8 + bytelsb[(v>>8)&0xFF];
-  //  else
-  //    if ( (v & 0x00FFFFFF) != 0)
-  //      return 16 + bytelsb[(v>>16)&0xFF];
-  //    else
-  //      return 24 + bytelsb[(v>>24)&0xFF];
-  //}
-  ///** Find first set (least significant bit).
-  // *  @return the first bit set in the argument.  
-  // *          <code>ffs(0)==0</code> and <code>ffs(1)==1</code>. */
-  //public static final int ffs(long v) {
-  //  if ( (v & 0xFFFFFFFFL) != 0) 
-  //    return ffs( (int) (v & 0xFFFFFFFFL) );
-  //  else
-  //    return 32 + ffs( (int) ( v >> 32 ) );
-  //}
+  /** Lowest bit set in a byte. */
+  static final byte bytelsb[] = {
+    0, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
+    4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
+    5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 7, 1, 2, 1, 3, 1, 2, 1,
+    4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
+    6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
+    4, 1, 2, 1, 3, 1, 2, 1, 8, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
+    5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1,
+    4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
+    7, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1, 5, 1, 2, 1, 3, 1, 2, 1,
+    4, 1, 2, 1, 3, 1, 2, 1, 6, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1,
+    5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1  };
+  static { assert(bytelsb.length==0x100); }
+  /** Number of zeros in a byte. */
+  static final byte bytezeros[] = {
+    8, 7, 7, 6, 7, 6, 6, 5, 7, 6, 6, 5, 6, 5, 5, 4, 7, 6, 6, 5, 6, 5, 5, 4,
+    6, 5, 5, 4, 5, 4, 4, 3, 7, 6, 6, 5, 6, 5, 5, 4, 6, 5, 5, 4, 5, 4, 4, 3,
+    6, 5, 5, 4, 5, 4, 4, 3, 5, 4, 4, 3, 4, 3, 3, 2, 7, 6, 6, 5, 6, 5, 5, 4,
+    6, 5, 5, 4, 5, 4, 4, 3, 6, 5, 5, 4, 5, 4, 4, 3, 5, 4, 4, 3, 4, 3, 3, 2,
+    6, 5, 5, 4, 5, 4, 4, 3, 5, 4, 4, 3, 4, 3, 3, 2, 5, 4, 4, 3, 4, 3, 3, 2,
+    4, 3, 3, 2, 3, 2, 2, 1, 7, 6, 6, 5, 6, 5, 5, 4, 6, 5, 5, 4, 5, 4, 4, 3,
+    6, 5, 5, 4, 5, 4, 4, 3, 5, 4, 4, 3, 4, 3, 3, 2, 6, 5, 5, 4, 5, 4, 4, 3,
+    5, 4, 4, 3, 4, 3, 3, 2, 5, 4, 4, 3, 4, 3, 3, 2, 4, 3, 3, 2, 3, 2, 2, 1,
+    6, 5, 5, 4, 5, 4, 4, 3, 5, 4, 4, 3, 4, 3, 3, 2, 5, 4, 4, 3, 4, 3, 3, 2,
+    4, 3, 3, 2, 3, 2, 2, 1, 5, 4, 4, 3, 4, 3, 3, 2, 4, 3, 3, 2, 3, 2, 2, 1,
+    4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0
+  };
+  static { assert(bytezeros.length==0x100); }
+  
+  /** Find first set (least significant bit).
+   *  @return the first bit set in the argument.  
+   *          <code>ffs(0)==0</code> and <code>ffs(1)==1</code>. */
+  public static final int ffs(int v) {
+    if ( (v & 0x0000FFFF) != 0)
+      if ( (v & 0x000000FF) != 0)
+        return bytelsb[v&0xFF];
+      else
+        return 8 + bytelsb[(v>>8)&0xFF];
+    else
+      if ( (v & 0x00FFFFFF) != 0)
+        return 16 + bytelsb[(v>>16)&0xFF];
+      else
+        return 24 + bytelsb[(v>>24)&0xFF];
+  }
+  /** Find first set (least significant bit).
+   *  @return the first bit set in the argument.  
+   *          <code>ffs(0)==0</code> and <code>ffs(1)==1</code>. */
+  public static final int ffs(long v) {
+    if ( (v & 0xFFFFFFFFL) != 0) 
+      return ffs( (int) (v & 0xFFFFFFFFL) );
+    else
+      return 32 + ffs( (int) ( v >> 32 ) );
+  }
   /** Find last set (most significant bit).
    *  @return the last bit set in the argument.
    *          <code>fls(0)==0</code> and <code>fls(1)==1</code>. */
@@ -226,6 +240,27 @@ public abstract class Util {
   /** Returns ceil(log2(n)) */
   public static final int log2c(int v) {
     return (v==0)?-1:fls(v-1);
+  }
+  /** Return the number of zeros in the binary representation of the
+   *  value of the argument. */
+  public static final int zerocount(int v) {
+    return bytezeros[v&0xFF] + bytezeros[(v>>8)&0xFF] +
+      bytezeros[(v>>16)&0xFF] + bytezeros[(v>>24)&0xFF];
+  }
+  /** Return the number of zeros in the binary representation of the
+   *  value of the argument. */
+  public static final int zerocount(long v) {
+    return zerocount((int)v)+zerocount((int)(v>>32));
+  }
+  /** Return the number of ones in the binary representation of the
+   *  value of the argument. */
+  public static final int popcount(int v) {
+    return 32-zerocount(v);
+  }
+  /** Return the number of ones in the binary representation of the
+   *  value of the argument. */
+  public static final int popcount(long v) {
+    return 64-zerocount(v);
   }
 }
 
