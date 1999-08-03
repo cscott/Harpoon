3b78da08a747ccb904896f9fe33a5f89b282677c
Felix S. Klock II <pnkfelix@mit.edu>
1999-08-03 00:46:57 +0000

    FSK: a quick Main class to compile to StrongARM code; hate FSK: to pollute the CVS repository with it, but desperate FSK: times call for desperate measures and this will make FSK: the collaboration between duncan and me easier.

---
+++ Main/SAMain.java
@@ -0,0 +1,87 @@
+// SAMain.java, created Mon Aug  2 19:41:06 1999 by pnkfelix
+// Copyright (C) 1999 Felix S. Klock II <pnkfelix@mit.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Main;
+
+import harpoon.ClassFile.CachingCodeFactory;
+import harpoon.ClassFile.HClass;
+import harpoon.ClassFile.HCode;
+import harpoon.ClassFile.HCodeFactory;
+import harpoon.ClassFile.HMethod;
+import harpoon.ClassFile.HCodeElement;
+import harpoon.IR.Properties.HasEdges;
+import harpoon.Analysis.DataFlow.LiveVars;
+import harpoon.Analysis.DataFlow.InstrSolver;
+import harpoon.Analysis.DataFlow.BasicBlock;
+import harpoon.Analysis.Instr.RegAlloc;
+import harpoon.Backend.StrongARM.SAFrame;
+import harpoon.Backend.StrongARM.SACode;
+
+import java.util.Iterator;
+
+/**
+ * <code>SAMain</code> is a program to compile java classes to some
+ * approximation of StrongARM assembly.  It is for development testing
+ * purposes, not production use.
+ * 
+ * @author  Felix S. Klock II <pnkfelix@mit.edu>
+ * @version $Id: SAMain.java,v 1.1.2.1 1999-08-03 00:46:57 pnkfelix Exp $
+ */
+public class SAMain extends harpoon.IR.Registration {
+ 
+    private static boolean PRINT_ORIG = true;
+    private static boolean REG_ALLOC = true;
+    private static boolean LIVENESS_TEST = true;
+        
+    public static void main(String[] args) {
+	java.io.PrintWriter out = new java.io.PrintWriter(System.out, true);
+	HCodeFactory hcf = // default code factory.
+	    harpoon.Analysis.QuadSSA.SCC.SCCOptimize.codeFactory
+	    (harpoon.IR.Quads.QuadSSA.codeFactory()
+	     );
+	hcf = SACode.codeFactory();
+
+	int n=0;  // count # of args/flags processed.
+	// rest of command-line options are class names.
+	HClass interfaceClasses[] = new HClass[args.length-n];
+	for (int i=0; i<args.length-n; i++)
+	    interfaceClasses[i] = HClass.forName(args[n+i]);
+	// Do something intelligent with these classes. XXX
+	for (int i=0; i<interfaceClasses.length; i++) {
+	    HMethod hm[] = interfaceClasses[i].getDeclaredMethods();
+	    for (int j=0; j<hm.length; j++) {
+		HCode hc = hcf.convert(hm[j]);
+		
+		if (PRINT_ORIG) {
+		    if (hc!=null) hc.print(out);
+		    out.println();
+		}
+		
+		if (LIVENESS_TEST) {
+		    HCodeElement root = hc.getRootElement();
+		    BasicBlock block = 
+			BasicBlock.computeBasicBlocks((HasEdges)root);
+		    Iterator iter= BasicBlock.basicBlockIterator(block);
+		    LiveVars livevars = new LiveVars(iter); 
+		    InstrSolver.worklistSolver
+			(BasicBlock.basicBlockIterator(block), livevars);
+		    out.println(livevars.dump());
+		}
+
+		if (REG_ALLOC &&
+		    hcf.getCodeName().equals("strongarm")) {
+		    HCodeFactory regAllocCF = RegAlloc.codeFactory(hcf, new SAFrame());
+		    HCode rhc = regAllocCF.convert(hm[j]);
+		    if (rhc != null) rhc.print(out);
+		    out.println();
+		}
+
+		
+		out.flush();
+ 
+	    }
+	}
+	
+    }
+
+}
