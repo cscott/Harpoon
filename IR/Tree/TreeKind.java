d3321749562023a1aa862930083a92f4949e8b16
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-08-05 15:17:14 +0000

    Duncan forgot this.

--- IR/Tree/TreeKind.java
+++ IR/Tree/TreeKind.java
@@ -9,7 +9,7 @@ package harpoon.IR.Tree;
  * class. 
  * 
  * @author  Duncan Bryce <duncan@lcs.mit.edu>
- * $Id: TreeKind.java,v 1.1.2.6 1999-08-04 06:31:00 cananian Exp $
+ * $Id: TreeKind.java,v 1.1.2.7 1999-08-05 15:17:14 cananian Exp $
  */
 public abstract class TreeKind  {
     public final static int BINOP      = 0;
@@ -22,18 +22,19 @@ public abstract class TreeKind  {
     public final static int JUMP       = 7;
     public final static int LABEL      = 8;
     public final static int MEM        = 9;
-    public final static int MOVE       = 10;
-    public final static int NAME       = 11;
-    public final static int NATIVECALL = 12;
-    public final static int RETURN     = 13;
-    public final static int SEGMENT    = 14;
-    public final static int SEQ        = 15;
-    public final static int TEMP       = 16;
-    public final static int THROW      = 17;
-    public final static int UNOP       = 18;
+    public final static int METHOD     = 10;
+    public final static int MOVE       = 11;
+    public final static int NAME       = 12;
+    public final static int NATIVECALL = 13;
+    public final static int RETURN     = 14;
+    public final static int SEGMENT    = 15;
+    public final static int SEQ        = 16;
+    public final static int TEMP       = 17;
+    public final static int THROW      = 18;
+    public final static int UNOP       = 19;
 
     public static int min() { return 0; }
-    public static int max() { return 19; }
+    public static int max() { return 20; }
 
     public static boolean isValid(int k) {
 	return (min()<=k) && (k<max());
