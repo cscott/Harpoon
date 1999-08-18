174764e7786d83eae0160a2a7338c7016a9b4a8d
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-08-18 21:14:07 +0000

    Make ESEQ implement PreciselyTyped.  Still uneasy with this interface--- what are the feelings about removing PreciseType.SMALL and adding an additional isSmall() or isPrecise() method to PreciselyTyped? Felix doesn't like the fact that SMALL isn't in Type, and I wish enumerated Types were stricter to enforce the fact that SMALL isn't a valid Type, it's only a valid PreciseType...

--- IR/Tree/ESEQ.java
+++ IR/Tree/ESEQ.java
@@ -14,13 +14,17 @@ import java.util.Set;
  * <code>ESEQ</code> objects are expressions which chain a statement and
  * an expressions together.  The statement is evaluated for side effects, the
  * the expression is evaluated.  The value of the expression is the value of
- * the <code>ESEQ</code>.
+ * the <code>ESEQ</code>.<p>
+ * <code>ESEQ</code>s are <code>PreciselyTyped</code> because the enclosed
+ * <code>Exp</code> may be <code>PreciselyTyped</code>.  In contexts such
+ * as <code>MOVE(ESEQ(stm, MEM<small>(e1)), e2)</code>, the <code>ESEQ</code>
+ * clearly has the same small type as the <code>MEM</code>.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>, based on
  *          <i>Modern Compiler Implementation in Java</i> by Andrew Appel.
- * @version $Id: ESEQ.java,v 1.1.2.13 1999-08-12 03:37:27 cananian Exp $
+ * @version $Id: ESEQ.java,v 1.1.2.14 1999-08-18 21:14:07 cananian Exp $
  */
-public class ESEQ extends Exp {
+public class ESEQ extends Exp implements PreciselyTyped {
     /** The statement to evaluate for side-effects. */
     public Stm stm;
     /** The expression whose value is the the value of the <code>ESEQ</code>.*/
@@ -62,12 +66,9 @@ public class ESEQ extends Exp {
 			(Exp)exp.rename(tf, ctm));
     }
 
-    public int type() {
-	// this is an unfortunate hack because Exps can possibily implement
-	// the 'PreciseTyped' interface.
-	int ty = exp.type();
-	return (ty==PreciseType.SMALL)?Type.INT:ty;
-    }
+    public int type() { return exp.type(); }
+    public int bitwidth() { return ((PreciselyTyped)exp).bitwidth(); }
+    public boolean signed() { return ((PreciselyTyped)exp).signed(); }
 
     public String toString() {
         return "ESEQ(#" + stm.getID() + ", #" + exp.getID() + ")";
