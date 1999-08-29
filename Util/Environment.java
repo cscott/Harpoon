3c81c33d63411e1cfb41540c8577a1c0ad79faf4
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-08-29 04:23:07 +0000

    Implement environments (maps with scoping) because the correct SSI renaming algorithm needs it.  The SSI algorithm currently checked in is incorrect, (provably so!) but the current placement algorithm is conservative enough that you'll never actually see incorrect naming.  The provably-correct algorithm currently exists only in my thesis (with its proof) but will eventually make it to the CVS code base.  All the sooner now that environments exist in harpoon.Util...

---
+++ Util/Environment.java
@@ -0,0 +1,24 @@
+// Environment.java, created Sat Aug 28 22:12:08 1999 by cananian
+// Copyright (C) 1999 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Util;
+
+/**
+ * An <code>Environment</code> is a <code>Map</code> with scoping:
+ * you can save marks into the environment and undo all changes
+ * since a mark.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: Environment.java,v 1.1.2.1 1999-08-29 04:23:07 cananian Exp $
+ */
+public interface Environment extends java.util.Map {
+    /** A abstract property for marks into an environment. */
+    public interface Mark { };
+
+    /** Get a mark that will allow you to restore the current state of
+     *  this environment. */
+    Mark getMark();
+    /** Undo all changes since the supplied mark, restoring the map to
+     *  its state at the time the mark was taken. */
+    void undoToMark(Mark m);
+}
