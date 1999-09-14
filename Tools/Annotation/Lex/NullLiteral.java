ed2c7f52c6be541459e58d51f2868c4ea20f7d56
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-09-14 05:38:36 +0000

    Highlight all literals (numbers, null) in dark cyan.

--- Tools/Annotation/Lex/NullLiteral.java
+++ Tools/Annotation/Lex/NullLiteral.java
@@ -8,11 +8,5 @@ class NullLiteral extends Literal {
 
   Symbol token() { return new Symbol(Sym.NULL_LITERAL); }
 
-  void annotate(LinePos left, LinePos right) {
-    System.out.println(left.line+" "+left.pos+"\t"+right.line+" "+right.pos);
-    System.out.println("<font color=green>");
-    System.out.println("</font>");
-  }
-
   public String toString() { return "NullLiteral <null>"; }
 }
