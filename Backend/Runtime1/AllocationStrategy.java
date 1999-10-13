60d29c14d571fc213a7b3a836998c986f06c315d
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-13 16:16:53 +0000

    Moving memory allocation interface around, as it is now runtime-specific.
    @RENAME: Backend/Allocation/ Backend/Runtime1/@

---
+++ Backend/Runtime1/AllocationStrategy.java
@@ -0,0 +1,14 @@
+// AllocationStrategy.java, created Mon Feb 15  3:38:00 1999 by duncan
+// Copyright (C) 1998 Duncan Bryce <duncan@lcs.mit.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Backend.Runtime1;
+
+import harpoon.IR.Tree.Exp;
+
+/* <b>FILL ME IN</b>
+ * @author Duncan Bryce <duncan@lcs.mit.edu>
+ * @version $Id: AllocationStrategy.java,v 1.1.2.1 1999-10-13 16:16:53 cananian Exp $
+ */
+public interface AllocationStrategy {
+    public Exp memAlloc(Exp size);
+}
