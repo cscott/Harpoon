29c48127105a111f1f25cade41389a99472bad19
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-14 20:22:08 +0000

    Make instantiatedClasses a full-fledged member of the abstract ClassHierarchy object, as it is needed to correctly determine possible dispatch sites in a call graph.

--- Analysis/ClassHierarchy.java
+++ Analysis/ClassHierarchy.java
@@ -11,7 +11,7 @@ import java.util.Set;
  * and methods.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ClassHierarchy.java,v 1.1.2.1 1999-09-08 19:30:06 cananian Exp $
+ * @version $Id: ClassHierarchy.java,v 1.1.2.2 1999-10-14 20:22:08 cananian Exp $
  */
 public abstract class ClassHierarchy {
     // tree of callable classes
@@ -28,7 +28,15 @@ public abstract class ClassHierarchy {
      */
     public abstract Set callableMethods();
     /** Returns the set of all reachable/usable classes.
+     *  If any method in a class is callable (including static methods),
+     *  then the class will be a member of the returned set.
      *  @return <code>Set</code> of <code>HClass</code>es.
      */
     public abstract Set classes();
+    /** Returns the set of all *instantiated* classes.
+     *  This is a subset of the set returned by the <code>classes()</code>
+     *  method.  A class is included in the return set only if an
+     *  object of that type is at some point created.
+     */
+    public abstract Set instantiatedClasses();
 }
