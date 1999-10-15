8f3729e1113dacadd5078e7f3e42b52eb42e4716
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-15 19:58:29 +0000

    A header file containing macro definitions for the various code and data segments used in the runtime code.

---
+++ arch/arm32/segments.h
@@ -0,0 +1,15 @@
+/* machine-specific segment info */
+#ifdef arm32
+/* these defines cribbed out of the CodeGen.spec file */
+# define CLASS_SEGMENT		.data 1 @ .section class
+# define CODE_SEGMENT		.text 0 @ .section code
+# define GC_SEGMENT		.data 2 @ .section gc
+# define INIT_DATA_SEGMENT	.data 3 @ .section init_data
+# define STATIC_OBJECTS_SEGMENT	.data 4 @ .section static_objects
+# define STATIC_PRIMITIVES_SEGMENT .data 5 @ .section static_primitives
+# define STRING_CONSTANTS_SEGMENT .data 6 @ .section string_constants
+# define STRING_DATA_SEGMENT	.data 7 @ .section string_data
+# define TEXT_SEGMENT		.text   @ .section text
+# define ZERO_DATA_SEGMENT	.bss    @ .section zero
+# define FIXUP_SEGMENT		.text 10 @ .section fixup
+#endif
