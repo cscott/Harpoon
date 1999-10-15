421ba8eeede8f6cea91eca7c179130e040abc7b8
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-15 20:01:44 +0000

    A header file for use inside C code to refer to the various constants embedded in the FLEX compiler output.  This includes segment start/end points, a pointer to the java main() method to start execution at, a pointer to the list of static initializers, & etc.

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+/* handy constants (mostly locations) defined by the runtime interface */
+
+/* first method in the java program. */
+extern void (*javamain)(void);
+/* null-terminated ordered list of static initializers */
+extern void *static_inits;
+
+/* starts and ends of various segments */
+extern void *gc_start, *gc_end;
+extern void *static_objects_start, *static_objects_end;
+extern void *string_constants_start, *string_constants_end;
+extern void *fixup_start, *fixup_end;
+extern void *code_start, *code_end;
