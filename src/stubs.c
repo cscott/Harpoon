2c4be1d1c3334c93797819e93c399f13fc36951c
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-20 06:44:35 +0000

    Changed method symbol prefix from _Java_ to _Flex_ so that our compiled java methods don't conflict with the JNI-standard C-native method naming.
    Also updated stubs to deal with latest compiler tweaks: we convinced
    ClassHierarchy that java.lang.FloatingDecimal doesn't matter by commenting
    out two characters in java/util/Hashtable.java (check-in to come).

--- src/stubs.c
+++ src/stubs.c
@@ -2,48 +2,39 @@
 
 /* native methods */
 /* in java.io.FileDescriptor */
-void *Java_java_io_FileDescriptor_initSystemFD__Ljava_io_FileDescriptor_2I;
+void *Flex_java_io_FileDescriptor_initSystemFD__Ljava_io_FileDescriptor_2I;
 /* in java.io.FileOutputStream */
-void *Java_java_io_FileOutputStream_writeBytes___3BII;
+void *Flex_java_io_FileOutputStream_writeBytes___3BII;
 /* in java.lang.Class */
-void *Java_java_lang_Class_getPrimitiveClass__Ljava_lang_String_2;
-void *Java_java_lang_Class_getName__;
+void *Flex_java_lang_Class_getPrimitiveClass__Ljava_lang_String_2;
+void *Flex_java_lang_Class_getName__;
 /* in java.lang.Math */
-void *Java_java_lang_Math_floor__D;
+void *Flex_java_lang_Math_floor__D;
 /* in java.lang.String */
-void *Java_java_lang_String_intern__;
+void *Flex_java_lang_String_intern__;
 /* in java.lang.System */
-void *Java_java_lang_System_arraycopy__Ljava_lang_Object_2ILjava_lang_Object_2II;
-void *Java_java_lang_System_initProperties__Ljava_util_Properties_2;
-void *Java_java_lang_System_setIn0__Ljava_io_InputStream_2;
-void *Java_java_lang_System_setOut0__Ljava_io_PrintStream_2;
-void *Java_java_lang_System_setErr0__Ljava_io_PrintStream_2;
-void *Java_java_lang_System_currentTimeMillis__;
+void *Flex_java_lang_System_arraycopy__Ljava_lang_Object_2ILjava_lang_Object_2II;
+void *Flex_java_lang_System_initProperties__Ljava_util_Properties_2;
+void *Flex_java_lang_System_setIn0__Ljava_io_InputStream_2;
+void *Flex_java_lang_System_setOut0__Ljava_io_PrintStream_2;
+void *Flex_java_lang_System_setErr0__Ljava_io_PrintStream_2;
+void *Flex_java_lang_System_currentTimeMillis__;
 /* in java.lang.Thread */
-void *Java_java_lang_Thread_currentThread__;
+void *Flex_java_lang_Thread_currentThread__;
 /* in java.lang.Throwable */
-void *Java_java_lang_Throwable_fillInStackTrace__;
+void *Flex_java_lang_Throwable_fillInStackTrace__;
 
 /* errors in class hierarchy */
 // all gone.
 
 /* errors in backend */
-asm(".global L1944\nL1944:");
-asm(".global L1947\nL1947:");
-asm(".global L2174\nL2174:");
-asm(".global L2177\nL2177:");
-asm(".global L4371\nL4371:");
-asm(".global L5415\nL5415:");
-asm(".global L5427\nL5427:");
-asm(".global L7454\nL7454:");
+asm(".global L2290\nL2290:");
+asm(".global L2302\nL2302:");
+asm(".global L4289\nL4289:");
 
 /* errors in register allocator */
 void *Class_java_lang_Integer;
 void *Class_java_lang_Integer_9classinfo;
-void *Class_java_lang_FloatingDecimal;
-void *Class_java_lang_FloatingDecimal_9classinfo;
-void *Java_java_lang_FloatingDecimal__0003cclinit_0003e__;
-void *Java_java_lang_FloatingDecimal__0003cinit_0003e__F;
-void *Java_java_lang_Integer__0003cclinit_0003e__;
-void *Java_java_lang_Integer_toHexString__I;
-void *Java_java_lang_Integer_toString__II;
+void *Flex_java_lang_Integer__0003cclinit_0003e__;
+void *Flex_java_lang_Integer_toHexString__I;
+void *Flex_java_lang_Integer_toString__II;
