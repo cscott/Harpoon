c540d3cc74707e46461385cf7dab79aed56ad5fd
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-10-27 02:30:04 +0000

    Add assembly-language calls to static initializers.

--- src/test-hello.c
+++ src/test-hello.c
@@ -2,6 +2,7 @@
 #include "constants.h"
 
 JNIEnv * FNI_ThreadInit(void); /* from jni-private.h */
+void FNI_do_static_init(JNIEnv *env);
 extern JNIEnv *FNI_JNIEnv; /* temporary hack. */
 
 int main(int argc, char *argv[]) {
@@ -11,6 +12,7 @@ int main(int argc, char *argv[]) {
   
   env = FNI_ThreadInit();
   FNI_JNIEnv = env;
+  FNI_do_static_init(env);
   cls = (*env)->FindClass(env, "Hello3");
   mid = (*env)->GetStaticMethodID(env, cls, "main",
 				  "([Ljava/lang/String;)V");
