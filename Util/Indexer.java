7ff656dc70062185e59bda29914d1f1958b16bf7
Felix S. Klock II <pnkfelix@mit.edu>
1999-11-09 06:49:19 +0000

    FSK: Took out overly stringent requirement on use of getID in Indexer FSK: specification, so that one can now check that an Object can FSK: be indexed by a given Indexer (otherwise, it would be very FSK: difficult to implement BitSetFactory in terms of Indexer) FSK: (note: implementing BitSetFactory in terms of Indexer without FSK: carrying around an internal Set or Map may still be impossible, FSK: due to the need to have an inverse mapping of indexes to Objects FSK: for methods like iterator()...so I may be able to be convinced FSK: that getID() should return to its old specification for FSK: efficiency's sake...whaddya think Scott?

--- Util/Indexer.java
+++ Util/Indexer.java
@@ -14,17 +14,18 @@ package harpoon.Util;
  * Factory or Object type.
  * 
  * @author  Felix S. Klock II <pnkfelix@mit.edu>
- * @version $Id: Indexer.java,v 1.1.2.1 1999-11-09 06:28:27 pnkfelix Exp $
+ * @version $Id: Indexer.java,v 1.1.2.2 1999-11-09 06:49:19 pnkfelix Exp $
  */
 interface Indexer {
     
     /** Returns the "small" integer uniquely associated with
 	<code>o</code> in <code>this</code>.
-	<BR> <B>requires:</B> <code>o</code> is a member of the set of
-   	     objects indexed by <code>this</code>.
-	<BR> <B>effects:</B> returns the integer uniquely associated
+	<BR> <B>effects:</B> 
+	     If <code>o</code> is a member of the objects indexed by
+	     this, returns the integer uniquely associated
 	     with <code>o</code> from a densely-packed, non-negative
 	     set of integers whose smallest element is close to zero. 
+	     Else, returns -1.
     */
     int getID(Object o); 
     
