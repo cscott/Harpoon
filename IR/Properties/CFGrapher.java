246f374e7c69e42f86d12d14ef4df8adc71f4d60
C. Scott Ananian <cananian@alumni.princeton.edu>
1999-11-30 05:00:48 +0000

    CFGrapher and CFGraphable classes that pnkfelix, duncan, and I discussed last week. CFGraphable is the new name for IR.Properties.HasEdges. I'll be doing a global search-and-replace after this commit.
    @RENAME: /HasEdges.java /CFGraphable.java@

---
+++ IR/Properties/CFGrapher.java
@@ -0,0 +1,80 @@
+// CFGrapher.java, created Mon Nov 29 23:32:45 1999 by cananian
+// Copyright (C) 1999 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.IR.Properties;
+
+import harpoon.ClassFile.HCodeEdge;
+import harpoon.ClassFile.HCodeElement;
+
+import java.util.Collection;
+/**
+ * <code>CFGrapher</code> provides a means to externally associate
+ * control-flow graph information with elements of an intermediate
+ * representation.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: CFGrapher.java,v 1.1.2.1 1999-11-30 05:00:48 cananian Exp $
+ */
+public abstract class CFGrapher {
+    /** Returns an array of all the edges to and from the specified
+     *  <code>HCodeElement</code>. */
+    public abstract HCodeEdge[] edges(HCodeElement hc);
+    /** Returns an array of all the edges entering the specified
+     *  <code>HCodeElement</code>. */
+    public abstract HCodeEdge[] pred(HCodeElement hc);
+    /** Returns an array of all the edges leaving the specified
+     *  <code>HCodeElement</code>. */
+    public abstract HCodeEdge[] succ(HCodeElement hc);
+
+    // JDK 1.2 collections API: [CSA, 15-Jun-1999]
+    /** Returns a <code>Collection</code> of all the edges to and from
+     *  this <code>HCodeElement</code>. */
+    public abstract Collection edgeC(HCodeElement hc);
+    /** Returns a <code>Collection</code> of all the edges to
+	this <code>HCodeElement</code>. 
+        Each <code>HCodeEdge</code> returned is guaranteed to return 
+	<code>hc</code> in response to a call to <code>to()</code>;
+	the actual predecessor will be returned from
+	<code>from()</code>.  
+     */
+    public abstract Collection predC(HCodeElement hc);
+    /** Returns a <code>Collection</code> of all the edges from
+	this <code>HCodeElement</code>. 
+        Each <code>HCodeEdge</code> returned is guaranteed to return
+	<code>hc</code> in response to a call to
+	<code>from()</code>; the actual successor to <code>this</code>
+	will be returned from <code>to()</code>.
+     */
+    public abstract Collection succC(HCodeElement hc);
+
+    /** Default <code>CFGrapher</code> for <code>HCodeElement</code>s
+     *  which implement <code>CFGraphable</code>.  Does nothing
+     *  but cast the supplied <code>HCodeElement</code> to a
+     *  <code>CFGraphable</code> and invoke the appropriate
+     *  corresponding method in the <code>CFGraphable</code>
+     *  interface.
+     * @see java.util.Comparator
+     * @see java.lang.Comparable
+     * @see harpoon.Util.Default.comparator
+     */
+    public static final CFGrapher DEFAULT = new CFGrapher() {
+	public HCodeEdge[] edges(HCodeElement hc) {
+	    return ((CFGraphable)hc).edges();
+	}
+	public HCodeEdge[] pred(HCodeElement hc) {
+	    return ((CFGraphable)hc).pred();
+	}
+	public HCodeEdge[] succ(HCodeElement hc) {
+	    return ((CFGraphable)hc).succ();
+	}
+	public Collection edgeC(HCodeElement hc) {
+	    return ((CFGraphable)hc).edgeC();
+	}
+	public Collection predC(HCodeElement hc) {
+	    return ((CFGraphable)hc).predC();
+	}
+	public Collection succC(HCodeElement hc) {
+	    return ((CFGraphable)hc).succC();
+	}
+    };
+}
