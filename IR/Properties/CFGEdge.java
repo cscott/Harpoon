9158f49065749f26f696d7a25f9bc4f8f9575a53
Duncan Bryce <duncan@lcs.mit.edu>
2000-01-05 03:52:37 +0000

    An edge connecting two CFGraphable objects.

---
+++ IR/Properties/CFGEdge.java
@@ -0,0 +1,27 @@
+package harpoon.IR.Properties;
+
+import harpoon.ClassFile.HCodeElement;
+
+/**
+ * An <code>CFGEdge</code> connects two <code>CFGraphable</code>s
+ * in a graph structure.
+ * 
+ * @author  Duncan Bryce <duncan@lcs.mit.edu>
+ * @version $Id: CFGEdge.java,v 1.1.2.1 2000-01-05 03:52:37 duncan Exp $
+ * @see CFGraphable
+ */
+public abstract class CFGEdge implements harpoon.ClassFile.HCodeEdge {
+    /** Returns the source of this <code>CFGEdge</code>. 
+     *  The return value is guaranteed to implement to 
+     *  <code>CFGraphable</code> interface. */
+    public HCodeElement from() { return this.fromCFG(); }
+    /** Returns the source of this <code>CFGEdge</code>. */
+    public abstract CFGraphable fromCFG(); 
+
+    /** Returns the destination of this <code>HCodeEdge</code>.
+     *  The return value is guaranteed to implement to  
+     *  <code>CFGraphable</code> interface. */ 
+    public HCodeElement to() { return this.toCFG(); } 
+    /** Returns the destination of this <code>HCodeEdge</code>. */
+    public abstract CFGraphable toCFG(); 
+}
