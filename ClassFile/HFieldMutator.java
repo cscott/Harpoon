8539d062ccd50d920474c3cc56d23d9c1fc60598
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-01-11 01:50:54 +0000

    Added mutator interfaces for fields.

---
+++ ClassFile/HFieldMutator.java
@@ -0,0 +1,21 @@
+// HFieldMutator.java, created Mon Jan 10 20:05:22 2000 by cananian
+// Copyright (C) 1999 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.ClassFile;
+
+/**
+ * <code>HFieldMutator</code> allows you to change properties of
+ * an <code>HField</code>.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: HFieldMutator.java,v 1.1.2.1 2000-01-11 01:50:54 cananian Exp $
+ */
+public interface HFieldMutator {
+    public void addModifiers(int m);
+    public void setModifiers(int m);
+    public void removeModifiers(int m);
+
+    public void setType(HClass type);
+    public void setConstant(Object co);
+    public void setSynthetic(boolean isSynthetic);
+}
