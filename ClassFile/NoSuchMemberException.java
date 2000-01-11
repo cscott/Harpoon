e895bdab1ede7c9c890b4e2b1fcac5b57e489ca9
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-01-11 04:50:36 +0000

    Added two special RuntimeException subclasses to indicate errors due to duplicate or missing fields/methods in HClassMutator actions. These error conditions could quite legitimately be simple assertions, but I feel that adding them as genuine objects declared by 'throws' clauses makes the behavior of HClassMutator more obvious to the user.

---
+++ ClassFile/NoSuchMemberException.java
@@ -0,0 +1,17 @@
+// NoSuchMemberException.java, created Mon Jan 10 22:11:57 2000 by cananian
+// Copyright (C) 1999 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.ClassFile;
+
+/**
+ * <code>NoSuchMemberException</code> is thrown to indicate an
+ * attempt to remove a member from a class which does not contain it.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: NoSuchMemberException.java,v 1.1.2.1 2000-01-11 04:50:37 cananian Exp $
+ */
+public class NoSuchMemberException extends RuntimeException {
+    /** Creates a <code>NoSuchMemberException</code> with the
+     *  supplied detail message. */
+    public NoSuchMemberException(String message) { super(message); }
+}
