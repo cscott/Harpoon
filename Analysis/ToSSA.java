98038d19ed1e4a22711e5c13aaca20f64514ca89
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-01-13 09:31:01 +0000

    Added 'paramTypes' field to LowQuad PCALL.  This will allow us to properly translate Tree.CALLs to Assem.Instrs --- previously we didn't know how many registers the Assem.Instr corresponding to the CALL "used" because we didn't know the number and types of the parameters being passed to the called method.

--- Analysis/ToSSA.java
+++ Analysis/ToSSA.java
@@ -15,7 +15,7 @@ import harpoon.IR.LowQuad.*;
  * Converts SSI to SSA.  Should work on LowQuads and Quads. 
  *
  * @author  Brian Demsky <bdemsky@mit.edu>
- * @version $Id: ToSSA.java,v 1.1.2.6 1999-10-23 05:59:19 cananian Exp $
+ * @version $Id: ToSSA.java,v 1.1.2.7 2000-01-13 09:31:01 cananian Exp $
  */
 
 public final class ToSSA {
@@ -138,7 +138,8 @@ public final class ToSSA {
 	    PCALL newcall=new PCALL((LowQuadFactory)q.getFactory(), q, q.ptr(),
 				  nparams, ssitossamap.tempMap(q.retval()),
 				  ssitossamap.tempMap(q.retex()),
-				  new Temp[0], q.isVirtual(), q.isTailCall());
+				  new Temp[0], q.paramTypes(),
+				  q.isVirtual(), q.isTailCall());
 	    Quad []prev=q.prev();
 	    Quad []next=q.next();
 	    for(int i=0;i<prev.length;i++) {
--- IR/LowQuad/Translate.java
+++ IR/LowQuad/Translate.java
@@ -26,7 +26,7 @@ import java.util.Map;
  * <code>LowQuadSSA</code>/<code>LowQuadNoSSA</code> translation.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Translate.java,v 1.1.2.14 1999-10-23 05:59:32 cananian Exp $
+ * @version $Id: Translate.java,v 1.1.2.15 2000-01-13 09:31:15 cananian Exp $
  */
 final class Translate { // not public
     public static final Quad translate(final LowQuadFactory qf,
@@ -199,10 +199,14 @@ final class Translate { // not public
 	    for (int i=0; i<nsrc.length; i++) { // copy copy copy
 		nsrc[i] = q.src(i); ndst[i] = q.dst(i);
 	    }
+	    // make paramtypes array.
+	    HClass[] paramTypes = new HClass[q.paramsLength()];
+	    for (int i=0; i<paramTypes.length; i++)
+		paramTypes[i] = q.paramType(i);
 	    // CALL->PCALL.
 	    Quad q3 = new PCALL(qf, q, qN.def()[0], map(q.params()),
 				map(q.retval()), map(q.retex()),
-				map(ndst), map(nsrc),
+				map(ndst), map(nsrc), paramTypes,
 				isVirtual, q.isTailCall());
 	    Quad.addEdge(qN, 0, q3, 0);
 	    lqm.put(q, q0, q3);
