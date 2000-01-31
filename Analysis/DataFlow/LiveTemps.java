e97a7df00f5f203ef134a1e476a747826b420102
Felix S. Klock II <pnkfelix@mit.edu>
2000-01-31 16:20:06 +0000

    FSK: 1. Was in act of revising LiveTemps; those changes will FSK:    probably be merged into a new Liveness analysis by FSK:    the end of the day. FSK: 2. In preperation for revising BasicBlock.java, got FSK:    rid of crufty old Whaley code that was the only FSK:    thing forcing me to make severe interface mistakes FSK:    in BasicBlock

--- Analysis/DataFlow/LiveTemps.java
+++ Analysis/DataFlow/LiveTemps.java
@@ -4,7 +4,7 @@
 package harpoon.Analysis.DataFlow;
 
 import harpoon.Analysis.BasicBlock;
-import harpoon.IR.Properties.UseDef;
+import harpoon.IR.Properties.UseDefer;
 import harpoon.ClassFile.HCodeElement;
 import harpoon.Temp.Temp;
 import harpoon.Util.CloneableIterator; 
@@ -25,21 +25,26 @@ import java.util.Iterator;
  * performing liveness analysis on <code>Temp</code>s.
  * 
  * @author  Felix S. Klock II <pnkfelix@mit.edu>
- * @version $Id: LiveTemps.java,v 1.1.2.8 2000-01-27 14:55:49 pnkfelix Exp $
+ * @version $Id: LiveTemps.java,v 1.1.2.9 2000-01-31 16:20:06 pnkfelix Exp $
  */
 public class LiveTemps extends LiveVars {
     private Map hceToBB; 
 
     // may be null; code using this should check
     private Set liveOnProcExit;
+
+    private SetFactory mySetFactory;
+
+    // calculates use/def information for the IR passed in.
+    private UseDefer ud;
     
     /** Constructs a new <code>LiveTemps</code> for <code>basicblocks</code>.
 	<BR> <B>requires:</B> <OL>
 	     <LI> <code>basicblocks</code> is a
 	          <code>Iterator</code> of <code>BasicBlock</code>s,	       
-	     <LI> All of the instructions in
-	          <code>basicblocks</code> implement
-		  <code>UseDef</code>
+	     <LI> <code>ud</code> contains use/def information for all
+	          of all of the instructions in
+	          <code>basicblocks</code>.
 	     <LI> No element of <code>basicblocks</code> links to a
 	          <code>BasicBlock</code> not contained within
 		  <code>basicblocks</code>
@@ -56,28 +61,27 @@ public class LiveTemps extends LiveVars {
 	 @param basicblocks <code>Iterator</code> of <code>BasicBlock</code>s to be analyzed.
 	 @param liveOnProcExit <code>Set</code> of <code>Temp</code>s that are live on exit from the method (for example, r0 for assembly code). 
     */	     
-    public LiveTemps(Iterator basicBlocks, Set liveOnProcExit) {
-	// calling "special" ctor so that I can set up
-	// liveOnProcExit before calling anything else.
-        super(); 
-
+    public LiveTemps(Iterator basicBlocks, Set liveOnProcExit,
+		     UseDefer ud) {
 	// duplicating code from LiveVars.java
 	CloneableIterator blocks = new CloneableIterator(basicBlocks);
 	Set universe = findUniverse((Iterator) blocks.clone());
 	universe.addAll(liveOnProcExit);
-	SetFactory setFact = new BitSetFactory(universe);
+	
+	mySetFactory = new BitSetFactory(universe);
+	this.ud = ud;
 	
 	initializeHceToBB( (Iterator)blocks.clone() ); 
 
 	// KEY difference: set liveOnProcExit before calling initBBtoLVI
 	this.liveOnProcExit = liveOnProcExit;
 
-	initializeBBtoLVI( blocks, setFact );
-	
-	
+	initializeBBtoLVI( blocks, mySetFactory );	
     }
     
-    
+    public LiveTemps(Iterator basicBlocks, Set liveOnProcExit) {
+	this(basicBlocks, liveOnProcExit, UseDefer.DEFAULT);
+    }
 
     /** Constructor for LiveVars that allows the user to pass in their
 	own <code>SetFactory</code> for constructing sets of the
@@ -88,14 +92,25 @@ public class LiveTemps extends LiveVars {
 	     
 	<BR> Doc TODO: Add all of the above documentation from the
 	     standard ctor.
-	     
-        <BR> FSK: Note to Self: Actual SUPPORT for liveOnProcExit would be 
-	nice... 
     */
     public LiveTemps(Iterator basicBlocks, 
 		     Set liveOnProcExit, 
-		     SetFactory tempSetFact) {
-	super( basicBlocks, tempSetFact );
+		     SetFactory tempSetFact,
+		     UseDefer ud) {
+	// calling "special" ctor so that I can set up
+	// liveOnProcExit before calling anything else.
+	super();
+
+	this.ud = ud;
+
+	mySetFactory = tempSetFact;
+	CloneableIterator blocks = new CloneableIterator(basicBlocks);
+	initializeHceToBB( (Iterator)blocks.clone() ); 
+
+	// KEY difference: set liveOnProcExit before calling initBBtoLVI
+	this.liveOnProcExit = liveOnProcExit;
+
+	initializeBBtoLVI( blocks, tempSetFact );
     }
 
     /** Returns the <code>Set</code> of <code>Temp</code>s that are
@@ -110,16 +125,20 @@ public class LiveTemps extends LiveVars {
 	     <code>hce</code>. 
     */
     public Set getLiveBefore(HCodeElement hce) {
-	// hce must implement UseDef.  
-	UseDef udHce = (UseDef)hce; 
 	// live_before(hce) <-- UNION(USE(hce), (live_after(hce) - DEF(hce)))
 	Set liveBefore = this.getLiveAfter(hce); 
-	liveBefore.removeAll(udHce.defC()); 
-	liveBefore.addAll(udHce.useC());
+	liveBefore.removeAll(ud.defC(hce)); 
+	liveBefore.addAll(ud.useC(hce));
 	
 	return liveBefore; 
     }
     
+
+    // a cache of results for the getLiveAfter(HCodeElement) method
+    // (cleared every time a different basic block is accessed)
+    private HashMap hce2liveAfter = new HashMap();
+    private BasicBlock lastBB;
+
     /** Returns the <code>Set</code> of <code>Temp</code>s that are
 	live on exit from <code>hce</code>.
 	<BR> <B>requires:</B> A DataFlow Equation Solver has been run
@@ -134,23 +153,41 @@ public class LiveTemps extends LiveVars {
     public Set getLiveAfter(HCodeElement hce) {
 	Util.assert(this.hceToBB.containsKey(hce)); 
 
+	System.out.println("FSK: getLiveAfter called");
+
 	BasicBlock bb        = (BasicBlock)this.hceToBB.get(hce); 
-	Set        liveAfter = this.getLiveOnExit(bb); 
-	HCodeElement current   = bb.getLast(); 
-
-	// Starting from the last element in hce's basic block, traverse
-	// the block in reverse order, until hce is reached.  Each step 
-	// updates the liveness information.
-
-	// for (; current != hce; current = current.pred()[0].from()) {
-	Iterator iter = new ReverseIterator(bb.iterator());
-	while(iter.hasNext()) {
-	    UseDef udCurrent = (UseDef) iter.next();
-	    liveAfter.addAll(udCurrent.useC()); 
-	    liveAfter.removeAll(udCurrent.defC()); 
+	
+	if (lastBB != bb) {
+	    hce2liveAfter.clear();
+	    lastBB = bb;
 	}
+	
+	if (hce2liveAfter.containsKey(hce)) {
+	    return (Set) hce2liveAfter.get(hce);
+	} else {
+	    Set        liveAfter = this.getLiveOnExit(bb); 
+	    HCodeElement current   = bb.getLast(); 
 
-	return liveAfter; 
+	    // Starting from the last element in hce's basic block,
+	    // traverse the block in reverse order, until hce is
+	    // reached.  Each step updates the liveness information.
+
+	    // for (; current != hce; current = current.pred()[0].from()) {
+	    Iterator iter = new ReverseIterator(bb.iterator());
+	    while(current != hce) {
+		current = (HCodeElement) iter.next();
+		// put every element into the cache, since we're
+		// bothering to calculate the live sets anyway
+		hce2liveAfter.put(current, 
+				  mySetFactory.makeSet(liveAfter));
+
+		// update set for before 'current'
+		liveAfter.addAll(ud.useC(current)); 
+		liveAfter.removeAll(ud.defC(current)); 
+	    }
+	    
+	    return liveAfter; 
+	}
     }
 
     /** Constructs a <code>Set</code> of all of the <code>Temp</code>s
@@ -158,8 +195,6 @@ public class LiveTemps extends LiveVars {
 	<BR> <B>requires:</B> <OL>
 	     <LI> <code>blocks</code> is an <code>Iterator</code> of
 	          <code>BasicBlock</code>s. 
-	     <LI> All of the instructions in each element of
-	          <code>blocks</code> implement <code>UseDef</code>. 
 	     </OL>
 	<BR> <B>modifies:</B> <code>blocks</code>
 	<BR> <B>effects:</B> Iterates through all of the instructions
@@ -174,9 +209,9 @@ public class LiveTemps extends LiveVars {
 	    BasicBlock bb = (BasicBlock) blocks.next();
 	    Iterator useDefs = bb.iterator();
 	    while(useDefs.hasNext()) {
-		UseDef useDef = (UseDef) useDefs.next();
-		temps.addAll(useDef.useC());
-		temps.addAll(useDef.defC());
+		HCodeElement h = (HCodeElement) useDefs.next();
+		temps.addAll(ud.useC(h));
+		temps.addAll(ud.defC(h));
 	    }
 	}
 	return temps;	
@@ -215,21 +250,21 @@ public class LiveTemps extends LiveVars {
 	Iterator instrs = bb.listIterator();	
 	
 	while (instrs.hasNext()) {
-	    UseDef ud = (UseDef) instrs.next();
+	    HCodeElement h = (HCodeElement) instrs.next();
 	    
 	    // check for usage before definition, to handle the case
 	    // of a <- a+1 (which should lead to a USE(a), *not* a DEF
 	    
 	    // USE: set of vars used in block before being defined
-	    for(int i=0; i<ud.use().length; i++) {
-		Temp t = ud.use()[i];
+	    for(int i=0; i<ud.use(h).length; i++) {
+		Temp t = ud.use(h)[i];
 		if ( !info.def.contains(t) ) {
 		    info.use.add(t);
 		}
 	    }	    
 	    // DEF: set of vars defined in block before being used
-	    for(int i=0; i<ud.def().length; i++) {
-		Temp t = ud.def()[i];
+	    for(int i=0; i<ud.def(h).length; i++) {
+		Temp t = ud.def(h)[i];
 		if ( !info.use.contains(t) ) {
 		    info.def.add(t);
 		}
