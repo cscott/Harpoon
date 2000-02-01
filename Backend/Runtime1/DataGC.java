a70039ab5a56054c2de7b721db0e59c00e3df229
Karen K. Zee <kkz@alum.mit.edu>
2000-02-01 20:42:18 +0000

    Now dumps stack data. Quite ugly, really. The question is whether using delta tables is really saving us any space. The other question is how hard the decoding hit will be. Using delta tables is good if the same stack offsets tend to be live over multiple gc points. Otherwise, we end up using more space since we need to add pointers back to the base table. Similarly, each gc point is associated with a one-byte long descriptor that, among other things, encode whether the live registers/stack locations/derivations are the same as those from the previous gc point. Which costs us an additional pointer back to the previous gc point. I could probably reduce this to an offset, but I'm not sure how big we can anticipate these gc tables being...

--- Backend/Runtime1/DataGC.java
+++ Backend/Runtime1/DataGC.java
@@ -9,6 +9,7 @@ import harpoon.Backend.Generic.GCInfo;
 import harpoon.Backend.Generic.GCInfo.GCPoint;
 import harpoon.Backend.Generic.RegFileInfo;
 import harpoon.Backend.Generic.RegFileInfo.MachineRegLoc;
+import harpoon.Backend.Generic.RegFileInfo.StackOffsetLoc;
 import harpoon.Backend.Maps.NameMap;
 import harpoon.ClassFile.HClass;
 import harpoon.ClassFile.HDataElement;
@@ -18,12 +19,15 @@ import harpoon.IR.Tree.CONST;
 import harpoon.IR.Tree.LABEL;
 import harpoon.IR.Tree.SEGMENT;
 import harpoon.IR.Tree.Stm;
+import harpoon.Temp.Label;
 import harpoon.Util.Util;
 
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -33,12 +37,13 @@ import java.util.TreeSet;
  * <code>DataGC</code> outputs the tables needed by the garbage collector.
  * 
  * @author  Karen K. Zee <kkz@tesuji.lcs.mit.edu>
- * @version $Id: DataGC.java,v 1.1.2.1 2000-01-31 23:43:14 kkz Exp $
+ * @version $Id: DataGC.java,v 1.1.2.2 2000-02-01 20:42:18 kkz Exp $
  */
 public class DataGC extends Data {
     final protected GCInfo m_gc;
     final protected NameMap m_nm;
     final protected int numInts;
+    final protected static boolean DEBUG = true;
     
     /** Creates a <code>DataGC</code>. */
     static final int BITS_PER_INT = 32;
@@ -83,15 +88,39 @@ public class DataGC extends Data {
     private static final int STACK_SAME_AS_PREV  =   16;
     private static final int DERIVS_ARE_ZERO     =    8;
     private static final int DERIVS_SAME_AS_PREV =    4;
+    private static final int HAS_LABEL_TO_PREV   =    2;
     private Stm outputGCData(HMethod hm) {
 	List stmlist = new ArrayList();
+	// first output base table for stack locations
+	Set baseTableSet = new HashSet();
+	for(Iterator it=m_gc.gcPoints(hm).iterator(); it.hasNext(); ) {
+	    Set stack = ((GCPoint)it.next()).liveStackOffsetLocs();
+	    baseTableSet.addAll(stack);
+	}
+	if (!baseTableSet.isEmpty()) {
+	    stmlist.add(_DATUM(m_nm.label(hm, "gcbt")));
+	    stmlist.add(_DATUM(new CONST(tf, null, baseTableSet.size())));
+	    if (DEBUG) System.out.println("Size of base table = " +
+					  baseTableSet.size());
+	}
+	// map each live StackOffsetLoc to a numbered bit
+	Map baseTableMap = new HashMap();
+	int counter = 0;
+	for(Iterator it = baseTableSet.iterator(); it.hasNext(); ) {
+	    StackOffsetLoc sol = (StackOffsetLoc)it.next();
+	    stmlist.add(_DATUM(new CONST(tf, null, sol.stackOffset())));
+	    baseTableMap.put(sol, new Integer(counter++));
+	}
+	int index = 0;
 	GCPoint prev = null;
+	Label prevL = null;
 	for(Iterator it=m_gc.gcPoints(hm).iterator(); it.hasNext(); ) {
 	    GCPoint gcp = (GCPoint)it.next();
 	    byte desc = 0; // descriptor
 	    boolean needRegs = true;
 	    boolean needStack = true;
 	    boolean needDerivs = true;
+	    boolean needPrev = false;
 	    Set regs = gcp.liveMachineRegLocs();
 	    Set stack = gcp.liveStackOffsetLocs();
 	    Map derivs = gcp.liveDerivations();
@@ -102,6 +131,7 @@ public class DataGC extends Data {
 		       regs.equals(prev.liveMachineRegLocs())) {
 		desc |= REGS_SAME_AS_PREV;
 		needRegs = false;
+		needPrev = true;
 	    }
 	    if (stack.isEmpty()) {
 		desc |= STACKLOCS_ARE_ZERO;
@@ -110,6 +140,7 @@ public class DataGC extends Data {
 		       stack.equals(prev.liveStackOffsetLocs())) {
 		desc |= STACK_SAME_AS_PREV;
 		needStack = false;
+		needPrev = true;
 	    }   
 	    if (derivs.isEmpty()) {
 		desc |= DERIVS_ARE_ZERO;
@@ -118,14 +149,25 @@ public class DataGC extends Data {
 		       derivs.equals(prev.liveDerivations())) {
 		desc |= DERIVS_SAME_AS_PREV;
 		needDerivs = false;
+		needPrev = true;
 	    }
+	    if (needPrev) desc |= HAS_LABEL_TO_PREV;
 	    // dump data
+	    Label l = new Label(m_nm.mangle(hm, "gcdata_"+index++));
+	    stmlist.add(new LABEL(tf, null, l, true));
 	    stmlist.add(_DATUM(gcp.label()));
 	    stmlist.add(_DATUM(new CONST(tf, null, desc))); // descriptor
+	    if (DEBUG) System.out.println("Descriptor: " + 
+					  Integer.toBinaryString((int)desc));
+	    if (needPrev) stmlist.add(_DATUM(prevL));
 	    if (needRegs) stmlist.add(outputRegs(regs));
-	    //if (needStack) stmlist.add(outputStack(stack));
+	    int numSE = // number of stack entries
+		(int)Math.ceil(((double)counter)/((double)BITS_PER_INT));
+	    if (needStack) 
+		stmlist.add(outputStack(stack, baseTableMap, numSE));
 	    //if (needDerivs) stmlist.add(outputDerivs(derivs));
 	    prev = gcp;
+	    prevL = l;
 	} // for loop
 	return Stm.toStm(stmlist);
     } // outputGCData
@@ -141,10 +183,26 @@ public class DataGC extends Data {
 	}
 	for(int k=0; k < numInts; k++) {
 	    stmlist.add(_DATUM(new CONST(tf, null, data[k])));
+	    if (DEBUG) System.out.println("Registers: " +
+					  Integer.toBinaryString(data[k]));
 	}
 	return Stm.toStm(stmlist);
     }
-    private void outputStack(Set stack) {
+    private Stm outputStack(Set stack, Map m, int size) {
+	List stmlist = new ArrayList();
+	int[] data = new int[size];
+	for(Iterator it=stack.iterator(); it.hasNext(); ) {
+	    int index = ((Integer)m.get(it.next())).intValue();
+	    int i = index/BITS_PER_INT;
+	    int j = index%BITS_PER_INT;
+	    data[i] |= 1 << (BITS_PER_INT - j - 1);
+	}
+	for(int k=0; k < size; k++) {
+	    stmlist.add(_DATUM(new CONST(tf, null, data[k])));
+	    if (DEBUG) System.out.println("Stack Offsets: " +
+					  Integer.toBinaryString(data[k]));
+	}
+	return Stm.toStm(stmlist);
     }
     private void outputDerivs(Map derivs) {
     }
