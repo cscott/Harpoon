81fc5969eb31395a555e2948aa6c1261250e2c7a
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-02-02 08:30:28 +0000

    Converted ReachingDefs from an abstract class to an interface, as I noticed that there were subclasses of Analysis.DataFlow.DataFlowBasicBlockVisitor which might want to implement ReachingDefs.

--- Analysis/ReachingDefs.java
+++ Analysis/ReachingDefs.java
@@ -3,24 +3,20 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Analysis;
 
+import harpoon.ClassFile.HCodeElement;
+import harpoon.Temp.Temp;
+
+import java.util.Set;
 /**
- * <code>ReachingDefs</code> defines an abstract class for
+ * <code>ReachingDefs</code> defines an interface for
  * analyzing reaching definitions.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ReachingDefs.java,v 1.1.2.1 2000-02-02 08:19:15 cananian Exp $
+ * @version $Id: ReachingDefs.java,v 1.1.2.2 2000-02-02 08:30:28 cananian Exp $
  */
-public abstract class ReachingDefs {
-    /** The <code>HCode</code> for which this object contains analysis
-     *  results. */
-    protected final HCode hc;
-    
-    /** Creates a <code>ReachingDefs</code> object for the provided
-     *  <code>HCode</code>. */
-    public ReachingDefs(HCode hc) { this.hc = hc; }
-
+public interface ReachingDefs {
     /** Returns the Set of <code>HCodeElement</code>s providing definitions
      *  of <code>Temp</code> <code>t</code> which reach 
      *  <code>HCodeElement</code> <code>hce</code>. */
-    public abstract Set reachingDef(HCodeElement hce, Temp t);
+    public Set reachingDefs(HCodeElement hce, Temp t);
 }
