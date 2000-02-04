be4fa53de7a79e1e810e7ac75bd1ca5c38e22d35
Karen K. Zee <kkz@alum.mit.edu>
2000-02-04 03:42:30 +0000

    Theoretically encodes all relevant gc data including derivations. I guess the next step is actually testing to make sure it does!

--- Backend/Runtime1/DataGC.java
+++ Backend/Runtime1/DataGC.java
@@ -4,10 +4,12 @@
 package harpoon.Backend.Runtime1;
 
 import harpoon.Analysis.ClassHierarchy;
+import harpoon.Analysis.Maps.Derivation;
 import harpoon.Backend.Generic.Frame;
 import harpoon.Backend.Generic.GCInfo;
 import harpoon.Backend.Generic.GCInfo.GCPoint;
 import harpoon.Backend.Generic.RegFileInfo;
+import harpoon.Backend.Generic.RegFileInfo.CommonLoc;
 import harpoon.Backend.Generic.RegFileInfo.MachineRegLoc;
 import harpoon.Backend.Generic.RegFileInfo.StackOffsetLoc;
 import harpoon.Backend.Maps.NameMap;
@@ -20,6 +22,7 @@ import harpoon.IR.Tree.LABEL;
 import harpoon.IR.Tree.SEGMENT;
 import harpoon.IR.Tree.Stm;
 import harpoon.Temp.Label;
+import harpoon.Temp.Temp;
 import harpoon.Util.Util;
 
 import java.lang.reflect.Modifier;
@@ -37,24 +40,26 @@ import java.util.TreeSet;
  * <code>DataGC</code> outputs the tables needed by the garbage collector.
  * 
  * @author  Karen K. Zee <kkz@tesuji.lcs.mit.edu>
- * @version $Id: DataGC.java,v 1.1.2.2 2000-02-01 20:42:18 kkz Exp $
+ * @version $Id: DataGC.java,v 1.1.2.3 2000-02-04 03:42:30 kkz Exp $
  */
 public class DataGC extends Data {
-    final protected GCInfo m_gc;
-    final protected NameMap m_nm;
-    final protected int numInts;
-    final protected static boolean DEBUG = true;
+    final GCInfo m_gc;
+    final NameMap m_nm;
+    final TreeBuilder m_tb;
+    final private int numBR; // number of bytes needed for register data
+    final private static boolean DEBUG = true;
     
     /** Creates a <code>DataGC</code>. */
-    static final int BITS_PER_INT = 32;
+    static final int BYTESIZE = 8;
     public DataGC(Frame f, HClass hc, ClassHierarchy ch) {
         super("gc-data", hc, f);
 	Util.assert(f.getGCInfo() != null);
-	m_gc = f.getGCInfo();
-	m_nm = f.getRuntime().nameMap;
-	root = build(ch);
-	double maxRI = (double)f.getRegFileInfo().maxRegIndex();
-	numInts = (int)Math.ceil(maxRI/((double)BITS_PER_INT));
+	this.m_gc = f.getGCInfo();
+	this.m_nm = f.getRuntime().nameMap;
+	this.m_tb = (TreeBuilder) f.getRuntime().treeBuilder;
+	this.root = build(ch);
+	double numR = (double)f.getRegFileInfo().maxRegIndex();
+	this.numBR = (int)Math.ceil(numR/((double)BYTESIZE));
     }
     private HDataElement build(ClassHierarchy ch) {
 	List stmlist = new ArrayList();
@@ -66,20 +71,9 @@ public class DataGC extends Data {
 	hmset.retainAll(callableMs);
 	for (Iterator it=hmset.iterator(); it.hasNext(); ) {
 	    HMethod hm = (HMethod)it.next();
-	    if (!Modifier.isAbstract(hm.getModifiers())) {
-		Util.assert(m_gc.gcPoints(hm) != null);
+	    if (!Modifier.isAbstract(hm.getModifiers()))
 		stmlist.add(outputGCData(hm));
-	    }
 	}
-
-	stmlist.add(new LABEL(tf, null, m_nm.label(hc, "gc"), true));
-	// name of class
-	stmlist.add(_DATUM(m_nm.label(hc, "namestr")));
-	// number of methods
-	int size = hc.getMethods().length;
-	stmlist.add(_DATUM(new CONST(tf, null, size)));
-	// address of the class gc label
-	stmlist.add(_DATUM(m_nm.label(hc, "gc")));
 	return (HDataElement) Stm.toStm(stmlist);
     }
     private static final int REGS_ARE_ZERO       = -128;
@@ -88,39 +82,43 @@ public class DataGC extends Data {
     private static final int STACK_SAME_AS_PREV  =   16;
     private static final int DERIVS_ARE_ZERO     =    8;
     private static final int DERIVS_SAME_AS_PREV =    4;
-    private static final int HAS_LABEL_TO_PREV   =    2;
     private Stm outputGCData(HMethod hm) {
+	List gcps = m_gc.gcPoints(hm);
+	Util.assert(gcps != null);
 	List stmlist = new ArrayList();
 	// first output base table for stack locations
 	Set baseTableSet = new HashSet();
-	for(Iterator it=m_gc.gcPoints(hm).iterator(); it.hasNext(); ) {
+	for(Iterator it = gcps.iterator(); it.hasNext(); ) {
 	    Set stack = ((GCPoint)it.next()).liveStackOffsetLocs();
 	    baseTableSet.addAll(stack);
 	}
 	if (!baseTableSet.isEmpty()) {
-	    stmlist.add(_DATUM(m_nm.label(hm, "gcbt")));
-	    stmlist.add(_DATUM(new CONST(tf, null, baseTableSet.size())));
-	    if (DEBUG) System.out.println("Size of base table = " +
-					  baseTableSet.size());
+	    short size = (short)baseTableSet.size();
+	    report("Number of entries in base table = " + size);
+	    size *= 4; // each entry is a 32-bit integer
+	    size += 2; // account for size field
+	    size |= 1 << (2 * BYTESIZE - 1); // flag as base table
+	    stmlist.add(_DATUM(new CONST(tf, null, size)));
 	}
-	// map each live StackOffsetLoc to a numbered bit
+	// map each live StackOffsetLoc to an index
 	Map baseTableMap = new HashMap();
 	int counter = 0;
 	for(Iterator it = baseTableSet.iterator(); it.hasNext(); ) {
 	    StackOffsetLoc sol = (StackOffsetLoc)it.next();
+	    report("Base table entry: " + sol.stackOffset());
 	    stmlist.add(_DATUM(new CONST(tf, null, sol.stackOffset())));
 	    baseTableMap.put(sol, new Integer(counter++));
 	}
+	Util.assert(baseTableSet.size() == counter &&
+		    baseTableMap.size() == counter);
 	int index = 0;
 	GCPoint prev = null;
-	Label prevL = null;
-	for(Iterator it=m_gc.gcPoints(hm).iterator(); it.hasNext(); ) {
+	for(Iterator it = gcps.iterator(); it.hasNext(); ) {
 	    GCPoint gcp = (GCPoint)it.next();
 	    byte desc = 0; // descriptor
 	    boolean needRegs = true;
 	    boolean needStack = true;
 	    boolean needDerivs = true;
-	    boolean needPrev = false;
 	    Set regs = gcp.liveMachineRegLocs();
 	    Set stack = gcp.liveStackOffsetLocs();
 	    Map derivs = gcp.liveDerivations();
@@ -131,7 +129,6 @@ public class DataGC extends Data {
 		       regs.equals(prev.liveMachineRegLocs())) {
 		desc |= REGS_SAME_AS_PREV;
 		needRegs = false;
-		needPrev = true;
 	    }
 	    if (stack.isEmpty()) {
 		desc |= STACKLOCS_ARE_ZERO;
@@ -140,7 +137,6 @@ public class DataGC extends Data {
 		       stack.equals(prev.liveStackOffsetLocs())) {
 		desc |= STACK_SAME_AS_PREV;
 		needStack = false;
-		needPrev = true;
 	    }   
 	    if (derivs.isEmpty()) {
 		desc |= DERIVS_ARE_ZERO;
@@ -149,62 +145,135 @@ public class DataGC extends Data {
 		       derivs.equals(prev.liveDerivations())) {
 		desc |= DERIVS_SAME_AS_PREV;
 		needDerivs = false;
-		needPrev = true;
 	    }
-	    if (needPrev) desc |= HAS_LABEL_TO_PREV;
-	    // dump data
-	    Label l = new Label(m_nm.mangle(hm, "gcdata_"+index++));
-	    stmlist.add(new LABEL(tf, null, l, true));
-	    stmlist.add(_DATUM(gcp.label()));
-	    stmlist.add(_DATUM(new CONST(tf, null, desc))); // descriptor
-	    if (DEBUG) System.out.println("Descriptor: " + 
-					  Integer.toBinaryString((int)desc));
-	    if (needPrev) stmlist.add(_DATUM(prevL));
-	    if (needRegs) stmlist.add(outputRegs(regs));
-	    int numSE = // number of stack entries
-		(int)Math.ceil(((double)counter)/((double)BITS_PER_INT));
-	    if (needStack) 
-		stmlist.add(outputStack(stack, baseTableMap, numSE));
-	    //if (needDerivs) stmlist.add(outputDerivs(derivs));
+	    List tmplist = new ArrayList();
+	    // generate data
+	    tmplist.add(_DATUM(gcp.label()));
+	    tmplist.add(_DATUM(new CONST(tf, null, desc))); // descriptor
+	    report("Descriptor: " + Integer.toBinaryString((int)desc));
+ 	    short size = (short)(m_tb.POINTER_SIZE + 3 * m_tb.WORD_SIZE);
+	    if (needRegs) {
+		tmplist.add(outputRegs(regs));
+		size += numBR; 
+	    }
+	    if (needStack) {
+		int numBSE = // number of bytes needed for stack entries
+		    (int)Math.ceil(((double)counter)/((double)BYTESIZE));
+		tmplist.add(outputStack(stack, baseTableMap, numBSE));
+		size += numBSE;
+	    }
+	    if (needDerivs) {
+		tmplist.add(outputDerivs(derivs));
+		size += sizeD;
+	    }
+	    stmlist.add(_DATUM(new CONST(tf, null, size)));
+	    stmlist.add(Stm.toStm(tmplist));
 	    prev = gcp;
-	    prevL = l;
 	} // for loop
 	return Stm.toStm(stmlist);
     } // outputGCData
-    // dump register information in 32-bit chunks
+    private int sizeD;
+    // dump register data in 8-bit chunks
     private Stm outputRegs(Set regs) {
 	List stmlist = new ArrayList();
-	int[] data = new int[numInts];
+	byte[] data = new byte[numBR];
 	for(Iterator it=regs.iterator(); it.hasNext(); ) {
 	    int regIndex = ((MachineRegLoc)it.next()).regIndex();
-	    int i = regIndex/BITS_PER_INT;
-	    int j = regIndex%BITS_PER_INT;
-	    data[i] |= 1 << (BITS_PER_INT - j - 1);
+	    int i = regIndex / BYTESIZE;
+	    int j = regIndex % BYTESIZE;
+	    data[i] |= 1 << (BYTESIZE - j - 1);
 	}
-	for(int k=0; k < numInts; k++) {
+	for(int k=0; k < numBR; k++) {
 	    stmlist.add(_DATUM(new CONST(tf, null, data[k])));
-	    if (DEBUG) System.out.println("Registers: " +
-					  Integer.toBinaryString(data[k]));
+	    report("Registers: " + Integer.toBinaryString((int)data[k]));
 	}
 	return Stm.toStm(stmlist);
     }
+    // dump stack data in 8-bit chunks
     private Stm outputStack(Set stack, Map m, int size) {
 	List stmlist = new ArrayList();
-	int[] data = new int[size];
+	byte[] data = new byte[size];
 	for(Iterator it=stack.iterator(); it.hasNext(); ) {
 	    int index = ((Integer)m.get(it.next())).intValue();
-	    int i = index/BITS_PER_INT;
-	    int j = index%BITS_PER_INT;
-	    data[i] |= 1 << (BITS_PER_INT - j - 1);
+	    int i = index / BYTESIZE;
+	    int j = index % BYTESIZE;
+	    data[i] |= 1 << (BYTESIZE - j - 1);
 	}
 	for(int k=0; k < size; k++) {
 	    stmlist.add(_DATUM(new CONST(tf, null, data[k])));
-	    if (DEBUG) System.out.println("Stack Offsets: " +
-					  Integer.toBinaryString(data[k]));
+	    report("Stack Offsets: " + Integer.toBinaryString((int)data[k]));
 	}
 	return Stm.toStm(stmlist);
     }
-    private void outputDerivs(Map derivs) {
+    private Stm outputDerivs(Map derivs) {
+	Util.assert(!derivs.isEmpty());
+	List stmlist = new ArrayList();
+	stmlist.add(_DATUM(new CONST(tf, null, (byte)derivs.size())));
+	sizeD = 1;
+	for(Iterator it=derivs.keySet().iterator(); it.hasNext(); ) {
+	    CommonLoc cl = (CommonLoc)it.next();
+	    int deriv;
+	    if (cl.kind() == StackOffsetLoc.KIND)
+		deriv = ((StackOffsetLoc)cl).stackOffset();
+	    else
+		deriv = ((MachineRegLoc)cl).regIndex();
+	    stmlist.add(_DATUM(new CONST(tf, null, deriv)));
+	    sizeD += 4; // 32-bits for the above Temp
+	    List rEntries = new ArrayList();
+	    List sEntries = new ArrayList();
+	    Derivation.DList dl = (Derivation.DList)derivs.get(cl);
+	    while(dl != null) {
+		if (((CommonLoc)dl.base).kind() == StackOffsetLoc.KIND) {
+		    sEntries.add(dl);
+		} else
+		    rEntries.add(dl);
+		dl = dl.next;
+	    }
+	    stmlist.add(_DATUM(new CONST(tf, null, (byte)rEntries.size())));
+	    stmlist.add(_DATUM(new CONST(tf, null, (byte)sEntries.size())));
+	    sizeD += 2;
+	    if (rEntries.size() != 0) {
+		byte[] rData = new byte[numBR];
+		byte[] rSign = new byte[numBR];
+		for(Iterator rIt = rEntries.iterator(); rIt.hasNext(); ) {
+		    Derivation.DList rDL = (Derivation.DList)rIt.next();
+		    int regIndex = ((MachineRegLoc)rDL.base).regIndex();
+		    int i = regIndex / BYTESIZE;
+		    int j = regIndex % BYTESIZE;
+		    rData[i] |= 1 << (BYTESIZE - j - 1);
+		    rSign[i] |= (rDL.sign ? 0 : 1) << (BYTESIZE - j - 1);
+		}
+		for(int k=0; k < numBR; k++)
+		    stmlist.add(_DATUM(new CONST(tf, null, rSign[k])));
+		for(int k=0; k < numBR; k++)
+		    stmlist.add(_DATUM(new CONST(tf, null, rData[k])));
+		sizeD += 2 * numBR;
+	    }
+	    if (sEntries.size() != 0) {
+		int numBSD = 
+		    (int)Math.ceil((double)sEntries.size()/(double)BYTESIZE);
+		byte[] sSign = new byte[numBSD];
+		int i = 0, j = 0;
+		List tmplist = new ArrayList();
+		for(Iterator sIt = sEntries.iterator(); sIt.hasNext(); ) {
+		    Derivation.DList sDL = (Derivation.DList)sIt.next();
+		    int stackOffset = ((StackOffsetLoc)sDL.base).stackOffset();
+		    tmplist.add(_DATUM(new CONST(tf, null, stackOffset)));
+		    sSign[i] |= (sDL.sign ? 0 : 1) << (BYTESIZE - j - 1);
+		    if (j == BYTESIZE - 1) {
+			i++; j = 0;
+		    } else j++;
+		}
+		for(int k=0; k < numBSD; k++)
+		    stmlist.add(_DATUM(new CONST(tf, null, sSign[k])));
+		stmlist.add(Stm.toStm(tmplist));
+		sizeD += numBSD + (8 * i + j) * 4;
+	    }
+	}
+	return Stm.toStm(stmlist);
+    }
+    private void report(String str) {
+	if (DEBUG) System.out.println(str);
     }
 }
 
