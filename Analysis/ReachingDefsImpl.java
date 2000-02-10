187c22c3c4ec3ac5babf75de4b9ed8ec71cc6ee2
Karen K. Zee <kkz@alum.mit.edu>
2000-02-10 17:08:13 +0000

    Cleaned up code and fixed some bugs.

--- Analysis/ReachingDefsImpl.java
+++ Analysis/ReachingDefsImpl.java
@@ -25,11 +25,13 @@ import java.util.Set;
  * created if the code has been modified.
  * 
  * @author  Karen K. Zee <kkz@tesuji.lcs.mit.edu>
- * @version $Id: ReachingDefsImpl.java,v 1.1.2.1 2000-02-10 01:54:02 kkz Exp $
+ * @version $Id: ReachingDefsImpl.java,v 1.1.2.2 2000-02-10 17:08:13 kkz Exp $
  */
 public class ReachingDefsImpl extends ReachingDefs {
     final private CFGrapher cfger;
     final private BasicBlock.Factory bbf;
+    final private Map Temp_to_BitSetFactories = new HashMap();
+    final Map cache = new HashMap(); // maps BasicBlocks to in Sets 
     /** Creates a <code>ReachingDefsImpl</code> object for the
 	provided <code>HCode</code> using the provided 
 	<code>CFGrapher</code>. This may take a while since the
@@ -39,7 +41,9 @@ public class ReachingDefsImpl extends ReachingDefs {
 	super(hc);
 	this.cfger = cfger;
 	this.bbf = new BasicBlock.Factory(hc.getRootElement(), cfger);
+	report("Entering analyze()");
 	analyze();
+	report("Leaving analyze()");
     }
     /** Creates a <code>ReachingDefsImpl</code> object for the
 	provided <code>HCode</code>. Uses <code>CFGrapher.DEFAULT</code>.
@@ -57,47 +61,89 @@ public class ReachingDefsImpl extends ReachingDefs {
 	Map m = (Map)cache.get(b);
 	// get the BitSetFactory
 	BitSetFactory bsf = (BitSetFactory)Temp_to_BitSetFactories.get(t);
-	// make a copy of the in Set for the BasicBlock
+	// make a copy of the in Set for the Temp
 	Set results = bsf.makeSet((Set)m.get(t));
-	// go through the HCodeElements in order
+	// propagate in Set through the HCodeElements 
+	// of the BasicBlock in correct order
 	for(Iterator it=b.statements().iterator(); it.hasNext(); ) {
 	    HCodeElement curr = (HCodeElement)it.next();
-	    if (curr == hce) break;
+	    if (curr == hce) return results;
 	    if (((UseDef)curr).defC().contains(t)) 
 		results = bsf.makeSet(Collections.singleton(curr));
 	}
-	return results;
+	return null; // should never happen
     }
-    final private Map Temp_to_BitSetFactories = new HashMap();
     // do analysis
     private void analyze() {
+	report("Entering getDefPts()");
 	final Map Temp_to_DefPts = getDefPts();
+	report("Leaving getDefPts()");
+	report("Entering getDefPts()");
 	getBitSets(Temp_to_DefPts);
+	report("Leaving getDefPts()");
 	
 	// build Gen and Kill sets
+	report("Entering buildGenKillSets()");
 	buildGenKillSets(Temp_to_DefPts);
+	report("Leaving buildGenKillSets()");
+
 	// solve for fixed point
+	report("Entering solve()");
 	solve();
+	report("Leaving solve()");
 	// store only essential information
 	for(Iterator it=cache.keySet().iterator(); it.hasNext(); ) {
 	    BasicBlock b = (BasicBlock)it.next();
 	    Map m = (Map)cache.get(b);
 	    for(Iterator temps=m.keySet().iterator(); temps.hasNext(); ) {
 		Temp t = (Temp)temps.next();
-		Set[] results = (Set[])m.get(b);
-		m.put(b, results[IN]);
+		Set[] results = (Set[])m.get(t);
+		m.put(t, results[IN]);
 	    }
 	}
     }
-    final Map cache = new HashMap(); // maps BasicBlocks to in Sets 
+    // create a mapping of Temps to a Set of possible definition points
+    private Map getDefPts() {
+	Map m = new HashMap();
+	for(Iterator it=hc.getElementsI(); it.hasNext(); ) {
+	    HCodeElement hce = (HCodeElement)it.next();
+	    Temp[] tArray = ((UseDef)hce).def();
+	    for(int i=0; i < tArray.length; i++) {
+		Temp t = tArray[i];
+		Set defPts = (Set)m.get(t);
+		if (defPts == null) {
+		    // have not yet encountered this Temp
+		    defPts = new HashSet();
+		    defPts.add(hce);
+		    m.put(t, defPts);
+		} else {
+		    // simply add this definition pt to set
+		    defPts.add(hce);
+		}
+	    }
+	}
+	return m;
+    }
+    // create a mapping of Temps to BitSetFactories
+    // using a mapping of Temps to Sets of definitions points
+    private void getBitSets(Map input) {
+	for(Iterator it=input.keySet().iterator(); it.hasNext(); ) {
+	    Temp t = (Temp)it.next();
+	    BitSetFactory bsf = new BitSetFactory((Set)input.get(t));
+	    Temp_to_BitSetFactories.put(t, bsf);
+	}
+    }
+    private final int IN = 0;
+    private final int OUT = 1;
+    private final int GEN = 2;
+    private final int KILL = 3;
     // return a mapping of BasicBlocks to a mapping of Temps to
     // an array of bitsets where the indices are organized as follows:
     // 0 - gen Set
     // 1 - kill Set
     // 2 - in Set
     // 3 - out Set
-    private Map buildGenKillSets(Map DefPts) {
-	Map m = new HashMap();
+    private void buildGenKillSets(Map DefPts) {
 	// calculate Gen and Kill sets
 	for(Iterator blocks=bbf.blockSet().iterator(); blocks.hasNext(); ) {
 	    BasicBlock b = (BasicBlock)blocks.next();
@@ -132,76 +178,44 @@ public class ReachingDefsImpl extends ReachingDefs {
 		    bitSets[OUT] = bsf.makeSet(Collections.EMPTY_SET); //out
 		}
 	    }
-	    m.put(b, Temp_to_BitSets);
+	    cache.put(b, Temp_to_BitSets);
 	}
-	return m;
     }
-    private final int IN = 0;
-    private final int OUT = 1;
-    private final int GEN = 2;
-    private final int KILL = 3;
     // uses Worklist algorithm to solve for reaching definitions
     // given a map of BasicBlocks to Maps of Temps to arrays of bit Sets
     private void solve() {
-	Map Temp_to_InOut = new HashMap();
 	Worklist worklist = new WorkSet(bbf.blockSet());
 	while(!worklist.isEmpty()) {
 	    BasicBlock b = (BasicBlock)worklist.pull();
+	    // get all the bitSets for this BasicBlock
 	    Map bitSets = (Map)cache.get(b);
-	    for(Iterator it=cache.keySet().iterator(); it.hasNext(); ) {
+	    for(Iterator it=bitSets.keySet().iterator(); it.hasNext(); ) {
 		Temp t = (Temp)it.next();
 		Set[] bitSet = (Set[])bitSets.get(t);
 		BitSetFactory bsf = 
 		    (BitSetFactory)Temp_to_BitSetFactories.get(t);
 		Set[] old = new Set[2];
-		old[IN] = bsf.makeSet(bitSet[IN]);
-		old[OUT] = bsf.makeSet(bitSet[OUT]);
-		for(Iterator preds=b.prevSet().iterator(); it.hasNext(); ) {
-		    BasicBlock pred = (BasicBlock)it.next();
+		old[IN] = bsf.makeSet(bitSet[IN]); // clone old in Set
+		old[OUT] = bsf.makeSet(bitSet[OUT]); // clone old out Set
+		for(Iterator preds=b.prevSet().iterator(); preds.hasNext(); ) {
+		    BasicBlock pred = (BasicBlock)preds.next();
 		    Set[] pBitSet = (Set[])((Map)cache.get(pred)).get(t);
 		    bitSet[IN].addAll(pBitSet[OUT]); // union 
 		}
 		bitSet[OUT] = bsf.makeSet(bitSet[IN]);
 		bitSet[OUT].removeAll(bitSet[KILL]);
 		bitSet[OUT].addAll(bitSet[GEN]);
-		if (old[IN].equals(bitSet[IN]) && 
-		    old[OUT].equals(bitSet[OUT]))
+		if (old[IN].equals(bitSet[IN]) && old[OUT].equals(bitSet[OUT]))
 		    continue;
 		for(Iterator succs=b.nextSet().iterator(); succs.hasNext(); )
-		    worklist.push(succs.next());
+		    worklist.push((BasicBlock)succs.next());
 	    }
 	}
     }
-    // create a mapping of Temps to a Set of possible definition points
-    private Map getDefPts() {
-	Map m = new HashMap();
-	for(Iterator it=hc.getElementsI(); it.hasNext(); ) {
-	    HCodeElement hce = (HCodeElement)it.next();
-	    Temp[] tArray = ((UseDef)hce).def();
-	    for(int i=0; i < tArray.length; i++) {
-		Temp t = tArray[i];
-		Set defPts = (Set)m.get(t);
-		if (defPts == null) {
-		    // have not yet encountered this Temp
-		    defPts = new HashSet();
-		    defPts.add(hce);
-		    m.put(t, defPts);
-		} else {
-		    // simply add this definition pt to set
-		    defPts.add(hce);
-		}
-	    }
-	}
-	return m;
-    }
-    // create a mapping of Temps to BitSetFactories
-    // using a mapping of Temps to Sets of definitions points
-    private void getBitSets(Map input) {
-	for(Iterator it=input.keySet().iterator(); it.hasNext(); ) {
-	    Temp t = (Temp)it.next();
-	    BitSetFactory bsf = new BitSetFactory((Set)input.get(t));
-	    Temp_to_BitSetFactories.put(t, bsf);
-	}
+    // debugging utility
+    private final boolean DEBUG = true;
+    private void report(String str) {
+	if (DEBUG) System.out.println(str);
     }
 }
 
