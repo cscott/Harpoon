9d25c0a70782f5ada1fead62f3509d685124fd4f
Alexandru SALCIANU <salcianu@alum.mit.edu>
2000-02-12 23:05:20 +0000

    Javadoc added.

--- Analysis/PointerAnalysis/PAEdgeVisitor.java
+++ Analysis/PointerAnalysis/PAEdgeVisitor.java
@@ -6,12 +6,17 @@ package harpoon.Analysis.PointerAnalysis;
 import harpoon.Temp.Temp;
 
 /**
- * <code>PAEdgeVisitor</code>
+ * <code>PAEdgeVisitor</code> is a wrapper for the functions that are
+ called on an edge of the form <code>&lt;var,node&gt;</code> or
+ <code>&lt;node1,f,node2&gt;</code>.
+ There is no other way to pass a function in Java (no pointers to methods ...)
  * 
  * @author  Alexandru SALCIANU <salcianu@MIT.EDU>
- * @version $Id: PAEdgeVisitor.java,v 1.1.2.1 2000-02-07 02:01:17 salcianu Exp $
+ * @version $Id: PAEdgeVisitor.java,v 1.1.2.2 2000-02-12 23:05:20 salcianu Exp $
  */
 interface  PAEdgeVisitor {
-    public void visit(Temp var,PANode node);    
+    /** Visits a <code>&lt;var,node&gt;</code> edge. */
+    public void visit(Temp var,PANode node);
+    /** Visits a <code>&lt;node1,f,node2&gt;</code> edge. */
     public void visit(PANode node1, String f, PANode node2);
 }
--- Analysis/PointerAnalysis/PANodeVisitor.java
+++ Analysis/PointerAnalysis/PANodeVisitor.java
@@ -1,14 +1,17 @@
 // PANodeVisitor.java, created Thu Jan 13 16:21:14 2000 by salcianu
-// Copyright (C) 2000 Alexandru SALCIANU <salcianu@retezat.lcs.mit.edu>
+// Copyright (C) 2000 Alexandru SALCIANU <salcianu@MIT.EDU>
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Analysis.PointerAnalysis;
 
 /**
- * <code>PANodeVisitor</code>
+ * <code>PANodeVisitor</code> is a wrapper for a function that is
+ called on a <code>PANode</code>.
+ There is no other way to pass a function in Java (no pointers to methods ...)
  * 
- * @author  Alexandru SALCIANU <salcianu@retezat.lcs.mit.edu>
- * @version $Id: PANodeVisitor.java,v 1.1.2.2 2000-01-17 23:49:03 cananian Exp $
+ * @author  Alexandru SALCIANU <salcianu@MIT.EDU>
+ * @version $Id: PANodeVisitor.java,v 1.1.2.3 2000-02-12 23:05:21 salcianu Exp $
  */
 public interface PANodeVisitor {
+    /** Visits a <code>PANode</code>. */
     public void visit(PANode node);
 }
