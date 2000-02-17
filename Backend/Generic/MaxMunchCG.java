3bdc60d6452583e7d2aaf828504a7353e43b0a1b
Felix S. Klock II <pnkfelix@mit.edu>
2000-02-17 02:01:25 +0000

    FSK: Further changes to MaxMunchCG to prepare for supporting FSK: derivation support.

--- Backend/Generic/MaxMunchCG.java
+++ Backend/Generic/MaxMunchCG.java
@@ -8,6 +8,9 @@ import harpoon.IR.Assem.Instr;
 import harpoon.Temp.Temp;
 import harpoon.Analysis.Maps.Derivation;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * <code>MaxMunchCG</code> is a <code>MaximalMunchCGG</code> specific 
  * extension of <code>CodeGen</code>.  Its purpose is to incorporate
@@ -17,7 +20,7 @@ import harpoon.Analysis.Maps.Derivation;
  * their own extensions of <code>CodeGen</code>.
  * 
  * @author  Felix S. Klock <pnkfelix@mit.edu>
- * @version $Id: MaxMunchCG.java,v 1.1.2.2 2000-02-15 23:35:20 pnkfelix Exp $ */
+ * @version $Id: MaxMunchCG.java,v 1.1.2.3 2000-02-17 02:01:25 pnkfelix Exp $ */
 public abstract class MaxMunchCG extends CodeGen {
     
     /** Creates a <code>MaxMunchCG</code>. */
@@ -45,16 +48,35 @@ public abstract class MaxMunchCG extends CodeGen {
 	return i;
     }
 
+    // tXi -> TypeAndDerivation
+    private Map ti2td = new HashMap();
+
     // stores type information for Temps
     protected TypeState TYPE_STATE = new TypeState();
     
     static public class TypeState {
+	Map tempToType = new HashMap();
+	
 	public void declare(Temp t, HClass clz) {
-
+	    // System.out.println(t + " " + clz);
+	    tempToType.put(t, new TypeAndDerivation(clz));
 	}
 
 	public void declare(Temp t, Derivation.DList dl) {
+	    // System.out.println(t + " " + dl);
+	    tempToType.put(t, new TypeAndDerivation(dl));
+	}
+    }
 
+    // union type for Derivation.DList and HClass
+    static class TypeAndDerivation {
+	Derivation.DList dlist;
+	HClass type;
+	TypeAndDerivation(HClass hc) {
+	    type = hc;
+	}
+	TypeAndDerivation(Derivation.DList dl) {
+	    dlist = dl;
 	}
     }
 
