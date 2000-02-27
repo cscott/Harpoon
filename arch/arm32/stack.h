8c42765dffa904b8f37e24c6e6f556fdb91b4d6f
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-02-27 21:31:37 +0000

    Updated stack.h to fix documentation error in frame pointer offsets (they should be negative).  Also added get_frameptr() and get_stackptr() functions.

--- arch/arm32/stack.h
+++ arch/arm32/stack.h
@@ -2,16 +2,16 @@
 
 
 /* gcc stack layout:
- *    [fp, #0]  = start_of_function + 16
- *    [fp, #4]  = return address.
- *    [fp, #8]  = parent's stack ptr (points to last value on parent's stack)
- *    [fp, #12] = parent's frame ptr (points to first value on parent's stack)
+ *    [fp, #-0]  = start_of_function + 16
+ *    [fp, #-4]  = return address.
+ *    [fp, #-8]  = parent's stack ptr (points to last value on parent's stack)
+ *    [fp, #-12] = parent's frame ptr (points to first value on parent's stack)
  *    ...         parent's 'n' registers r10-r4 that need to be saved,
  *                r10 highest, r4 lowest.
- *    [fp, #16+(4*n)] = space for this method's r3 (caller save)
- *    [fp, #20+(4*n)] = space for this method's r2 (caller save)
- *    [fp, #24+(4*n)] = space for this method's r1 (caller save)
-[sp]= [fp, #28+(4*n)] = space for this method's r0 (caller save)
+ *    [fp, #-16-(4*n)] = space for this method's r3 (caller save)
+ *    [fp, #-20-(4*n)] = space for this method's r2 (caller save)
+ *    [fp, #-24-(4*n)] = space for this method's r1 (caller save)
+[sp]= [fp, #-28-(4*n)] = space for this method's r0 (caller save)
  */
 
 /* return address is in [fp, #-4] */
@@ -21,6 +21,17 @@
    __pc; })
 #define set_retaddr(__pc) \
    asm("str %0, [fp, #-4]" : : "r" (__pc) : "memory")
+
+/* parent's frame pointer is in [fp, #-12] */
+#define get_frameptr() \
+({ void *__fp; \
+   asm("ldr %0, [fp, #-12]" : "=r" (__fp) :: "memory"); \
+   __fp; })
+/* parent's stack pointer is in [fp, #-8] */
+#define get_stackptr() \
+({ void *__sp; \
+   asm("ldr %0, [fp, #-8]" : "=r" (__sp) :: "memory"); \
+   __sp; })
 
 /* Do lookup & update of this function's return address, in order to
  * throw an exception. */
