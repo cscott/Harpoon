ec66770ced2b970eb7fa6de88f7479c03efc8b6b
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-02-28 15:43:00 +0000

    Add rudimentary clone() support.

--- src/java.lang/java_lang_Object.c
+++ src/java.lang/java_lang_Object.c
@@ -2,6 +2,8 @@
 #include <jni-private.h>
 #include "java_lang_Object.h"
 
+#include <assert.h>
+
 /*
  * Class:     java_lang_Object
  * Method:    getClass
@@ -22,15 +24,47 @@ JNIEXPORT jint JNICALL Java_java_lang_Object_hashCode
     return Java_java_lang_System_identityHashCode(env, NULL, obj);
 }
 
-#if 0
+/* helper for clone functions. */
+static jobject cloneHelper(JNIEnv *env, jobject obj, jsize len) {
+  jclass clazz = FNI_GetObjectClass(env, obj);
+  struct FNI_classinfo *info = FNI_GetClassInfo(clazz);
+  jobject clone = FNI_Alloc(env, info, len);
+  memcpy(FNI_UNWRAP(clone)->field_start,
+	 FNI_UNWRAP(obj  )->field_start,
+	 len - sizeof(struct oobj));
+  return clone;
+}
+
 /*
  * Class:     java_lang_Object
  * Method:    clone
  * Signature: ()Ljava/lang/Object;
  */
 JNIEXPORT jobject JNICALL Java_java_lang_Object_clone
-  (JNIEnv *, jobject);
+  (JNIEnv *env, jobject obj) {
+    jclass clazz = (*env)->GetObjectClass(env, obj);
+    struct FNI_classinfo *info = FNI_GetClassInfo(clazz);
+    u_int32_t size = info->claz->size;
+    assert(Java_java_lang_Class_isArray(env, clazz)==JNI_FALSE);
+    return cloneHelper(env, obj, size);
+}
 
+#define PRIMITIVEARRAYCLONE(name, type, sig)\
+JNIEXPORT jobject JNICALL Java__3##sig##_clone\
+  (JNIEnv *env, jobject obj) {\
+    jsize len = (*env)->GetArrayLength(env, (jarray) obj);\
+    return cloneHelper(env, obj, sizeof(struct aarray) + sizeof(type)*len);\
+}
+PRIMITIVEARRAYCLONE(Boolean, jboolean, Z);
+PRIMITIVEARRAYCLONE(Byte, jbyte, B);
+PRIMITIVEARRAYCLONE(Char, jchar, C);
+PRIMITIVEARRAYCLONE(Short, jshort, S);
+PRIMITIVEARRAYCLONE(Int, jint, I);
+PRIMITIVEARRAYCLONE(Long, jlong, J);
+PRIMITIVEARRAYCLONE(Float, jfloat, F);
+PRIMITIVEARRAYCLONE(Double, jdouble, D);
+
+#if 0
 /*
  * Class:     java_lang_Object
  * Method:    notify
