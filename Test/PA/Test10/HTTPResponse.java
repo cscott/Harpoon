2d959c8b78c7948dd5dc5308357016903448ac5e
Alexandru SALCIANU <salcianu@alum.mit.edu>
2000-03-21 19:02:29 +0000

    I added a new test for the Pointer Analysis: a web server. I promse to remove this as soon as the testing phase is over.

---
+++ Test/PA/Test10/HTTPResponse.java
@@ -0,0 +1,7 @@
+
+package harpoon.Test.PA.Test10;
+
+public class HTTPResponse{
+    public int returnCode;
+    public long sentBytes;
+}
---
+++ Test/PA/Test10/JhttpServer.java
@@ -0,0 +1,67 @@
+package harpoon.Test.PA.Test10;
+
+//****************************************************************************
+// Programmer: Duane M. Gran, ragnar@cs.bsu.edu
+// Program:    JhttpServer
+// Date:       April 24, 1998
+//****************************************************************************
+
+import java.net.*;
+import java.io.*;
+
+public class JhttpServer extends Thread{
+
+    private ServerSocket server;
+
+//****************************************************************************
+// Constructor: JhttpServer(int)
+//****************************************************************************
+    public JhttpServer(int port)
+    {
+	System.out.println("starting...");
+	try{
+	    System.out.println("creating the port");
+	    server = new ServerSocket(port);
+	}
+	catch (IOException e){
+	    System.err.println(e);
+	    System.exit(1);
+	}
+    }
+
+
+    public void run(){
+	// infinite loop 
+	while (true){
+	    try{
+		Socket client = server.accept();
+		(new JhttpWorker(client)).start();
+		System.out.println("accepted connection.");
+	    }
+	    catch (Exception e){
+		System.err.println(e);
+	    }
+	}
+    }
+  
+//*****************************************************************************
+// Method:  main()
+// Purpose: Creates a new JhttpServer based on the parameters.   
+//*****************************************************************************
+
+  public static void main(String args[]) throws Exception
+  {
+    if (args.length < 1)
+    {
+      System.out.println("Usage:");
+      System.out.println("   java JhttpServer <port>");
+      System.out.println();
+      System.out.println("Ex: java JhttpServer 10000");
+      System.exit(1);
+    }
+
+    (new JhttpServer(Integer.parseInt(args[0]))).start();
+  }
+}
+
+
