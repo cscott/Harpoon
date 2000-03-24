7be3127b87777361e07f22c1b95129f28decefa3
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-03-24 06:53:31 +0000

    a.out-style segment directives don't work with our elf build process. ./configure now determines whether we're on an a.out or elf platform and adjusts the generated segment directives accordingly.

--- arch/arm32/segments.h
+++ arch/arm32/segments.h
@@ -1,20 +1,31 @@
 /* machine-specific segment info */
-#ifdef arm32
-/* these defines cribbed out of the CodeGen.spec file */
-# define CLASS_SEGMENT		.data 1 @ .section class
-# define CODE_SEGMENT		.text 0 @ .section code
-# define GC_SEGMENT		.data 2 @ .section gc
-# define INIT_DATA_SEGMENT	.data 3 @ .section init_data
-# define STATIC_OBJECTS_SEGMENT	.data 4 @ .section static_objects
-# define STATIC_PRIMITIVES_SEGMENT .data 5 @ .section static_primitives
-# define STRING_CONSTANTS_SEGMENT .data 6 @ .section string_constants
-# define STRING_DATA_SEGMENT	.data 7 @ .section string_data
-# define REFLECTION_OBJECTS_SEGMENT .data 8 @ .section reflection_data
-# define REFLECTION_DATA_SEGMENT .data 9 @ .section reflection_data
-# define TEXT_SEGMENT		.text   @ .section text
-# define ZERO_DATA_SEGMENT	.bss    @ .section zero
-# define FIXUP_SEGMENT		.text 10 @ .section fixup
-# define GC_INDEX_SEGMENT       .data 10 @ .section gc_index
-#else
+#ifndef arm32
 # error Wrong segment information included.
 #endif
+
+/* the aoutelf selects *either* an aout-format section declaration
+ * (ugh, backwards compatibility) or an elf-format section declaration (yay!)
+ */
+#if defined(AOUT_SEGMENTS)
+# define aoutelf(aout,elf) aout
+#elif defined(ELF_SEGMENTS)
+# define aoutelf(aout,elf) elf
+#else
+# error what kind of output format is this, anyhow?
+#endif
+
+/* these defines cribbed out of the CodeGen.spec file */
+# define CLASS_SEGMENT		aoutelf(.data 1, .section class)
+# define CODE_SEGMENT		aoutelf(.text 0, .section code)
+# define GC_SEGMENT		aoutelf(.data 2, .section gc)
+# define INIT_DATA_SEGMENT	aoutelf(.data 3, .section init_data)
+# define STATIC_OBJECTS_SEGMENT	aoutelf(.data 4, .section static_objects)
+# define STATIC_PRIMITIVES_SEGMENT aoutelf(.data 5, .section static_primitives)
+# define STRING_CONSTANTS_SEGMENT aoutelf(.data 6, .section string_constants)
+# define STRING_DATA_SEGMENT	aoutelf(.data 7, .section string_data)
+# define REFLECTION_OBJECTS_SEGMENT aoutelf(.data 8, .section reflection_data)
+# define REFLECTION_DATA_SEGMENT aoutelf(.data 9, .section reflection_data)
+# define TEXT_SEGMENT		aoutelf(.text, .section text)
+# define ZERO_DATA_SEGMENT	aoutelf(.bss, .section zero)
+# define FIXUP_SEGMENT		aoutelf(.text 10, .section fixup)
+# define GC_INDEX_SEGMENT       aoutelf(.data 10, .section gc_index)
