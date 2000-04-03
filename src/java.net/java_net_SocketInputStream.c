4ff2d62d60659d83b20cffe2ab68df1d332c8265
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-04-03 00:37:58 +0000

    Radically simplify the socket input/output methods by using inheritance! What a great idea!  We implement socketRead and socketWrite using calls to FileInputStream.readBytes and FileOutputStream.writeBytes -- because in unix Everything Is A File.  This reduces code duplication, and means that the fixes I just checked in for readBytes/writeBytes will magically apply to socket io as well.

--- src/java.net/java_net_SocketInputStream.c
+++ src/java.net/java_net_SocketInputStream.c
@@ -1,77 +1,17 @@
 /* Implementation for class java_net_SocketInputStream */
 #include <jni.h>
 #include "java_net_SocketInputStream.h"
-
-#include <assert.h> /* assert */
-#include <errno.h>  /* errno */
-#include <string.h> /* strerror */
-#include <unistd.h> /* write */
-
-#include "flexthread.h" /* for mutex ops */
-#include "../java.io/javaio.h" /* for getfd/setfd */
-
-static jfieldID fdObjID = 0; /* The field ID of SocketInputStream.fd */
-static jclass IOExcCls  = 0; /* The java/io/IOException class object. */
-static int inited = 0; /* whether the above variables have been initialized */
-#ifdef WITH_THREADS
-static flex_mutex_t init_mutex = FLEX_MUTEX_INITIALIZER;
-#endif
-
-static int initializeSIS(JNIEnv *env) {
-    jclass SISCls;
-
-#ifdef WITH_THREADS
-    flex_mutex_lock(&init_mutex);
-    // other thread may win race to lock and init before we do.
-    if (inited) goto done;
-#endif
-
-    SISCls  = (*env)->FindClass(env, "java/net/SocketOutputStream");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    fdObjID = (*env)->GetFieldID(env, SISCls, "fd","Ljava/io/FileDescriptor;");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    IOExcCls = (*env)->FindClass(env, "java/io/IOException");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    /* make IOExcCls into a global reference for future use */
-    IOExcCls = (*env)->NewGlobalRef(env, IOExcCls);
-    /* done. */
-    inited = 1;
- done:
-#ifdef WITH_THREADS
-    flex_mutex_unlock(&init_mutex);
-#endif
-    return inited;
-}
+#include "../java.io/java_io_FileInputStream.h"
 
 /*
  * Class:     java_net_SocketInputStream
  * Method:    socketRead
  * Signature: ([BII)I
  */
-// stolen from src/java.io/java_io_FileInputStream.c
 JNIEXPORT jint JNICALL Java_java_net_SocketInputStream_socketRead
   (JNIEnv *env, jobject _this, jbyteArray ba, jint start, jint len) {
-    int              fd, result;
-    jobject          fdObj;
-    jbyte            buf[len];
-
-    /* If static data has not been loaded, load it now */
-    if (!inited && !initializeSIS(env)) return 0;/* exception occurred; bail */
-
-    if (len==0) return 0; /* don't even try to read anything. */
-
-    fdObj  = (*env)->GetObjectField(env, _this, fdObjID);
-    fd     = Java_java_io_FileDescriptor_getfd(env, fdObj);
-    
-    result = read(fd, (void*)buf, len);
-
-    if (result==-1) {
-	(*env)->ThrowNew(env, IOExcCls, strerror(errno));
-	return -1; /* could return anything; value is ignored. */
-    }
-
-    (*env)->SetByteArrayRegion(env, ba, start, result, buf); 
-
-    /* Java language spec requires -1 at EOF, not 0 */ 
-    return (jint)(result ? result : -1);
+  // steal implementation from java_io_FileInputStream, verbatim.
+  // (note, on windows platforms sockets are not the same as file
+  // descriptors, so this would have to be different)
+  return Java_java_io_FileInputStream_readBytes(env, _this, ba, start, len);
 }
--- src/java.net/java_net_SocketOutputStream.c
+++ src/java.net/java_net_SocketOutputStream.c
@@ -1,47 +1,7 @@
 /* Implementation for class java_net_SocketOutputStream */
 #include <jni.h>
 #include "java_net_SocketOutputStream.h"
-
-#include <assert.h> /* assert */
-#include <errno.h>  /* errno */
-#include <string.h> /* strerror */
-#include <unistd.h> /* write */
-
-#include "flexthread.h" /* for mutex ops */
-#include "../java.io/javaio.h" /* for getfd/setfd */
-
-static jfieldID fdObjID = 0; /* The field ID of SocketOutputStream.fd */
-static jclass IOExcCls  = 0; /* The java/io/IOException class object. */
-static int inited = 0; /* whether the above variables have been initialized */
-#ifdef WITH_THREADS
-static flex_mutex_t init_mutex = FLEX_MUTEX_INITIALIZER;
-#endif
-
-static int initializeSOS(JNIEnv *env) {
-    jclass SOSCls;
-
-#ifdef WITH_THREADS
-    flex_mutex_lock(&init_mutex);
-    // other thread may win race to lock and init before we do.
-    if (inited) goto done;
-#endif
-
-    SOSCls  = (*env)->FindClass(env, "java/net/SocketOutputStream");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    fdObjID = (*env)->GetFieldID(env, SOSCls, "fd","Ljava/io/FileDescriptor;");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    IOExcCls = (*env)->FindClass(env, "java/io/IOException");
-    if ((*env)->ExceptionOccurred(env)) goto done;
-    /* make IOExcCls into a global reference for future use */
-    IOExcCls = (*env)->NewGlobalRef(env, IOExcCls);
-    /* done. */
-    inited = 1;
- done:
-#ifdef WITH_THREADS
-    flex_mutex_unlock(&init_mutex);
-#endif
-    return inited;
-}
+#include "../java.io/java_io_FileOutputStream.h"
 
 /*
  * Class:     java_net_SocketOutputStream
@@ -51,31 +11,8 @@ static int initializeSOS(JNIEnv *env) {
 // stolen from src/java.io/java_io_FileOutputStream.c
 JNIEXPORT void JNICALL Java_java_net_SocketOutputStream_socketWrite
   (JNIEnv *env, jobject _this, jbyteArray ba, jint start, jint len) {
-    int fd, result;
-    jobject fdObj;
-    jbyte buf[len];
-    int written = 0;
-
-    /* If static data has not been loaded, load it now */
-    if (!inited && !initializeSOS(env)) return; /* exception occurred; bail */
-
-    fdObj = (*env)->GetObjectField(env, _this, fdObjID);
-    fd = Java_java_io_FileDescriptor_getfd(env, fdObj);
-    (*env)->GetByteArrayRegion(env, ba, start, len, buf);
-    if ((*env)->ExceptionOccurred(env)) return; /* bail */
-
-    if (len==0) return; /* don't even try to write anything. */
-
-    while (written < len) {
-	result = write(fd, (void*)(buf+written), len-written);
-	if (result==0) {
-	    (*env)->ThrowNew(env, IOExcCls, "No bytes written");
-	    return;
-	}
-	if (result==-1) {
-	    (*env)->ThrowNew(env, IOExcCls, strerror(errno));
-	    return;
-	}
-	written+=result;
-    }
+  // steal implementation from java_io_FileOutputStream, verbatim.
+  // (note, on windows platforms sockets are not the same as file
+  // descriptors, so this would have to be different)
+  return Java_java_io_FileOutputStream_writeBytes(env, _this, ba, start, len);
 }
