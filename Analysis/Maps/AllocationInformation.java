2fa114b31c3b8cf9a160a55e14ce75c15d4875b4
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-04-04 01:37:40 +0000

    New allocation property!  oh, boy!
    This flag indicates that the object should be created on *it's own* heap.
    Thread creation will use this property.

--- Analysis/Maps/AllocationInformation.java
+++ Analysis/Maps/AllocationInformation.java
@@ -10,7 +10,7 @@ import harpoon.Temp.Temp;
  * to information about the allocation done at that site.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: AllocationInformation.java,v 1.1.2.1 2000-03-30 10:53:13 cananian Exp $
+ * @version $Id: AllocationInformation.java,v 1.1.2.2 2000-04-04 01:37:40 cananian Exp $
  */
 public interface AllocationInformation  {
     
@@ -25,26 +25,37 @@ public interface AllocationInformation  {
 	 *  site has no interior pointers; that is, it is a primitive
 	 *  array, or all fields in the allocated object are primitive.
 	 */
-	boolean hasInteriorPointers();
+	public boolean hasInteriorPointers();
 	/** @return <code>true</code> if the object can be allocated on
 	 *  the stack; that is, the lifetime of the object does not
 	 *  exceed the execution of the method containing the allocation.
 	 */
-	boolean canBeStackAllocated();
+	public boolean canBeStackAllocated();
 	/** @return <code>true</code> if the object can be allocated on
 	 *  a thread-local heap; that is, the lifetime of the object does
 	 *  not exceed the lifetime of the thread object specified
 	 *  by the <code>allocationHeap</code> method. */
-	boolean canBeThreadAllocated();
+	public boolean canBeThreadAllocated();
+	/** @return <code>true</code> if the object should be allocated on
+	 *  *it's own* thread-local heap --- typically this means that
+	 *  the allocation site is a thread creation.  If this is
+	 *  <code>true</code>, the <code>canBeThreadAllocated()</code>
+	 *  should also be <code>true</code>.
+	 */
+	public boolean useOwnHeap();
 	/** @return a <code>Temp</code> which at the allocation site
 	 * contains a reference to either the thread object of a
 	 * thread-local allocation, or to another object whose lifetime
 	 * is correlated with that of the (not-yet-created) thread object.
 	 * Returns <code>null</code> if the allocation should use the
-	 * heap associated with the "current" thread. */
-	Temp    allocationHeap();
+	 * heap associated with the "current" thread. 
+	 * If this returns non-<code>null</code>, then
+	 * <code>useOwnHeap()</code> should return <code>false</code> and
+	 * <code>canBeThreadAllocated()</code> should return <code>true</code>.
+	 */
+	public Temp    allocationHeap();
     }
 
     /** Query the properties of the allocation at the specified site. */
-    AllocationProperties query(HCodeElement allocationSite);
+    public AllocationProperties query(HCodeElement allocationSite);
 }
