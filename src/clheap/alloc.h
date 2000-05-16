e31662a903b0268f497c1cec72b725d0383e9b9b
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-05-16 00:20:28 +0000

    fixed typo in prototype.

--- src/clheap/alloc.h
+++ src/clheap/alloc.h
@@ -8,7 +8,7 @@
 #include <stdlib.h> /* for size_t */
 
 void *NGBL_malloc(size_t size);
-void *NGBL_malloc_atmoic(size_t size);
+void *NGBL_malloc_atomic(size_t size);
 void *NSTK_malloc(size_t size);
 void *NTHR_malloc(size_t size);
 void *NTHR_malloc_first(size_t size);
