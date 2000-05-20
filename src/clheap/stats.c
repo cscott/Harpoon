c3aa0d284b065fafda4125112fae03a71340392d
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-05-20 18:40:09 +0000

    Improve statistics print-out:  a) start with newline, to make output cleaner when triggered by signal.  b) print out built values of HEAPSIZE and POOLSIZE  c) also print out whether or not we're actually doing thread and stack     allocations (via new #defines REALLY_DO_THR_ALLOC / REALLY_DO_STK_ALLOC)  d) No more divide-by-zero error when #undef REALLY_DO_THR_ALLOC due to     threads_created being zero.

--- src/clheap/stats.c
+++ src/clheap/stats.c
@@ -15,17 +15,30 @@ static void stat_signal_handler(int sig) { print_statistics(); }
 
 void print_statistics(void) {
 #ifdef MAKE_STATS
-  printf("Stack allocation:        %8ld bytes %8ld objects\n"
+  printf("\n"
+	 "HEAPSIZE: %ld  POOLSIZE: %ld  THRALLOC: %d  STKALLOC: %d\n"
+	 "Stack allocation:        %8ld bytes %8ld objects\n"
 	 "Thread-local allocation: %8ld bytes %8ld objects\n"
 	 "Global allocation:       %8ld bytes %8ld objects\n"
 	 "Threads created:         %8d\n"
 	 "Average heap size/thread:%8ld bytes\n"
 	 "Thread heap overflow:    %8ld bytes\n",
+	 (long) HEAPSIZE, (long) POOLSIZE,
+#ifdef REALLY_DO_THR_ALLOC
+	 1,
+#else
+	 0,
+#endif
+#ifdef REALLY_DO_STK_ALLOC
+	 1,
+#else
+	 0,
+#endif
 	 (long) stk_bytes_alloc, (long) stk_objs_alloc,
 	 (long) thr_bytes_alloc, (long) thr_objs_alloc,
 	 (long) gbl_bytes_alloc, (long) gbl_objs_alloc,
 	 threads_created,
-	 (long) thr_bytes_alloc/threads_created,
+	 threads_created ? (long) thr_bytes_alloc/threads_created: 0L,
 	 (long) thr_bytes_overflow);
   /* print out statistics on demand */
   signal(SIGALRM, stat_signal_handler);
