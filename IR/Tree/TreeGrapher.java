477a4b1c03d024042aedb9d6858c22d9a05fb5fd
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-07-16 23:40:39 +0000

    Rewrote TreeGrapher, because the old version had a bug I didn't feel like tracking down.  New version is a lot cleaner and simpler. Probably more efficient, too, as it uses just two depth-first passes through the tree.

--- IR/Tree/TreeGrapher.java
+++ IR/Tree/TreeGrapher.java
@@ -1,5 +1,5 @@
-// TreeGrapher.java, created Mon Dec 20  3:54:16 1999 by duncan
-// Copyright (C) 1998 Duncan Bryce <duncan@lcs.mit.edu>
+// TreeGrapher2.java, created Sun Jul 16 18:23:48 2000 by cananian
+// Copyright (C) 2000 C. Scott Ananian <cananian@alumni.princeton.edu>
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.IR.Tree;
 
@@ -7,213 +7,114 @@ import harpoon.ClassFile.HCode;
 import harpoon.ClassFile.HCodeEdge; 
 import harpoon.ClassFile.HCodeElement; 
 import harpoon.IR.Properties.CFGrapher; 
-import harpoon.IR.Tree.TreeVisitor; 
+import harpoon.Temp.Label;
 import harpoon.Temp.LabelList; 
+import harpoon.Util.Collections.Factories;
+import harpoon.Util.Collections.GenericMultiMap;
+import harpoon.Util.Collections.MultiMap;
 import harpoon.Util.Util; 
 
 import java.util.Collection; 
 import java.util.HashMap; 
-import java.util.HashSet; 
 import java.util.Iterator; 
 import java.util.Map; 
-import java.util.Set; 
-import java.util.Stack; 
-
 
 /**
  * <code>TreeGrapher</code> provides a means to externally associate
- * control-flow graph information with elements of an canonical tree. 
+ * control-flow graph information with elements of a canonical tree.
  * 
- * @author  Duncan Bryce <duncan@lcs.mit.edu>
- * @version $Id: TreeGrapher.java,v 1.1.2.8 2000-02-16 22:33:23 cananian Exp $
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: TreeGrapher.java,v 1.1.2.9 2000-07-16 23:40:39 cananian Exp $
  */
-class TreeGrapher extends CFGrapher { 
-    private Map predecessors = new HashMap(); 
-    private Map successors   = new HashMap(); 
-
-    /** Class constructor.  Don't call this directly -- use
-     *  the getGrapher() method in <code>harpoon.IR.Tree.Code</code> instead.
-     */ 
-    TreeGrapher(Code code) { 
+class TreeGrapher extends CFGrapher {
+    Tree firstElement = null;
+    final MultiMap predMap = new GenericMultiMap(Factories.arrayListFactory,
+						 Factories.hashMapFactory);
+    final MultiMap succMap = new GenericMultiMap(Factories.arrayListFactory,
+						 Factories.hashMapFactory);
+   /** Class constructor.  Don't call this directly -- use
+    *  the getGrapher() method in <code>harpoon.IR.Tree.Code</code> instead.
+    */ 
+    TreeGrapher(Code code) {
 	// Tree grapher only works on canonical trees. 
-	Util.assert(code.getName().equals("canonical-tree")); 
-
-	EdgeVisitor grapher = new EdgeVisitor
-	    (code, this.predecessors, this.successors); 
+	Util.assert(code.getName().equals("canonical-tree"));
+	Edger e = new Edger(code, new Labeler(code));
+	// done.
     }
+    public HCodeElement getFirstElement(HCode hcode) { return firstElement; }
+    public Collection predC(HCodeElement hc) { return predMap.getValues(hc); }
+    public Collection succC(HCodeElement hc) { return succMap.getValues(hc); }
 
-    /** Returns the first element in the control flow graph. */
-    public HCodeElement getFirstElement(HCode hcode) {
-	return RS((Stm)hcode.getRootElement());
-    }
-    /** Returns an array of all the edges to and from the specified
-     *  <code>HCodeElement</code>. */
-    public HCodeEdge[] edges(HCodeElement hc) { 
-	Collection edges = this.edgeC(hc);
-	return (HCodeEdge[])edges.toArray(new HCodeEdge[edges.size()]);
-    }
-
-    /** Returns an array of all the edges entering the specified
-     *  <code>HCodeElement</code>. */
-    public HCodeEdge[] pred(HCodeElement hc) { 
-	Collection pred = this.predC(hc);
-	return (HCodeEdge[])pred.toArray(new HCodeEdge[pred.size()]); 
-    }
-
-    /** Returns an array of all the edges leaving the specified
-     *  <code>HCodeElement</code>. */
-    public HCodeEdge[] succ(HCodeElement hc) { 
-	Collection succ = this.succC(hc);
-	return (HCodeEdge[])succ.toArray(new HCodeEdge[succ.size()]); 
-    }
-
-    /** Returns a <code>Collection</code> of all the edges to and from
-     *  this <code>HCodeElement</code>. */
-    public Collection edgeC(HCodeElement hc) { 
-	Set edges = new HashSet(); 
-	edges.addAll(this.predC(hc)); 
-	edges.addAll(this.succC(hc)); 
-
-	return edges; 
-    }
-    /** Returns a <code>Collection</code> of all the edges to
-	this <code>HCodeElement</code>. 
-        Each <code>HCodeEdge</code> returned is guaranteed to return 
-	<code>hc</code> in response to a call to <code>to()</code>;
-	the actual predecessor will be returned from
-	<code>from()</code>.  
-     */
-    public Collection predC(HCodeElement hc) { 
-	return (Collection)this.predecessors.get(hc); 
-    }
-
-    /** Returns a <code>Collection</code> of all the edges from
-	this <code>HCodeElement</code>. 
-        Each <code>HCodeEdge</code> returned is guaranteed to return
-	<code>hc</code> in response to a call to
-	<code>from()</code>; the actual successor to <code>this</code>
-	will be returned from <code>to()</code>.
-     */
-    public Collection succC(HCodeElement hc) { 
-	return (Collection)this.successors.get(hc); 
-    }
-	
-
-    /* Only for canonical views, a class to initialize
-     *  the edges representing the CFG of this Tree form
-     */
-    private static class EdgeVisitor extends TreeVisitor { 
-	private Code   code; 
-	private Map    labels = new HashMap();
-	private Map    predecessors;
-	private Map    successors;
-	private Stack  nodes = new Stack();
-	private Stm    nextNode;
-
-	EdgeVisitor(Code code, Map predecessors, Map successors) { 
-	    this.code = code; 
-	    this.predecessors = predecessors; 
-	    this.successors = successors; 
-	    this.initSets(); 
-	    this.computeEdges(); 
-	}
-	
-	private void computeEdges() { 
-	    this.nextNode = (Stm)this.code.getRootElement();
-	    while (this.nextNode!=null) { 
-		this.nextNode.accept(this); 
+    private static class Labeler {
+	private final Map labelmap = new HashMap();
+	Labeler(Code code) {
+	    class LVisitor extends TreeVisitor {
+		public void visit(Tree e) { /* no op */ }
+		public void visit(LABEL l) { labelmap.put(l.label, l); }
 	    }
+	    TreeVisitor tv = new LVisitor();
+	    for (Iterator it=code.getElementsI(); it.hasNext(); )
+		((Tree)it.next()).accept(tv);
 	}
-
-	public void visit(Tree t) { throw new Error("No defaults here."); }
-	public void visit(Exp e)  { /* Do nothing for Exps */ } 
-
-	public void visit(final CALL s) { 
-	    this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
-	    Util.assert(labels.containsKey(s.getHandler().label),
-			"labels should contain Label:" + 
-			s.getHandler().label);
-	    Util.assert(this.nextNode!=null, 
-			"nextNode shouldn't be null");
-	    Util.assert(RS(this.nextNode)!=(Stm)labels.get(s.getHandler().label),
-			new Object() {
-			    public String toString() {
-				return "both normal and exceptional"+
-				" return should not target same location"+
-				" for "+Print.print(s);}});
-	    
-	    addEdge(s, RS(this.nextNode)); 
-	    addEdge(s, (Stm)labels.get(s.getHandler().label));
+	LABEL lookup(Label l) {
+	    Util.assert(labelmap.containsKey(l));
+	    return (LABEL)labelmap.get(l);
 	}
-
-	public void visit(CJUMP s) { 
-	    Util.assert(labels.containsKey(s.iftrue));
-	    Util.assert(labels.containsKey(s.iffalse));
-	    addEdge(s, (Stm)labels.get(s.iftrue));
-	    addEdge(s, (Stm)labels.get(s.iffalse));
-	    this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
+    }
+    private class Edger {
+	void addEdge(final Tree from, final Tree to) {
+	    HCodeEdge hce = new HCodeEdge() {
+		public HCodeElement from() { return from; }
+		public HCodeElement to() { return to; }
+		public String toString() { return "Edge from "+from+" to "+to;}
+	    };
+	    predMap.add(to, hce);
+	    succMap.add(from, hce);
 	}
-	
-	public void visit(JUMP s) { 
-	    for (LabelList l = s.targets; l!=null; l=l.tail) { 
-		Util.assert(labels.containsKey(l.head));
-		addEdge(s, (Stm)labels.get(l.head));
-		this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
-	    }
-	}
-
-	public void visit(SEQ s) { 
-	    Util.assert(s.getLeft()!=null && s.getRight()!=null);
-	    nodes.push(s.getRight());
-	    this.nextNode = s.getLeft();
-	}
-
-	public void visit(RETURN s) { 
-	    this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
-	}
-
-	public void visit(THROW s) { 
-	    this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
-	}
-
-	public void visit(Stm s) { 
-	    this.nextNode = nodes.isEmpty()?null:(Stm)nodes.pop();
-	    if (this.nextNode!=null) addEdge(s, RS(this.nextNode)); 
-	}
-
-	private void initSets() {
-	    for (Iterator i = this.code.getElementsI(); i.hasNext();) {
-		Tree next = (Tree)i.next();
-		// Add predecessor and successor sets for "next"
-		this.successors.put(next, new HashSet()); 
-		this.predecessors.put(next, new HashSet()); 
-
-		// Map labels to their LABEL in the tree form. 
-		if (next.kind() == TreeKind.LABEL) { 
-		    labels.put(((LABEL)next).label, next); 
+	Edger(Code code, final Labeler labeler) {
+	    class EVisitor extends TreeVisitor {
+		Tree last = null;
+		void linkup(Stm s, boolean canFallThrough) {
+		    if (firstElement==null) firstElement = s;
+		    if (last!=null) addEdge(last, s);
+		    last = canFallThrough ? s : null;
+		}
+		public void visit(Tree t) { /* ignore */ }
+		public void visit(Stm s) { linkup(s, true); }
+		public void visit(CALL c) {
+		    // edge to handler; also fall-through.
+		    addEdge(c, labeler.lookup(c.getHandler().label));
+		    linkup(c, true);
+		}
+		public void visit(CJUMP c) {
+		    // edges to iftrue and iffalse; no fall-through.
+		    addEdge(c, labeler.lookup(c.iffalse));
+		    addEdge(c, labeler.lookup(c.iftrue));
+		    linkup(c, false);
+		}
+		public void visit(ESEQ e) {
+		    Util.assert(false, "Not in canonical form!");
+		}
+		public void visit(JUMP j) {
+		    // edges to targets list. no fall-through.
+		    for (LabelList ll=j.targets; ll!=null; ll=ll.tail)
+			addEdge(j, labeler.lookup(ll.head));
+		    linkup(j, false);
+		}
+		public void visit(RETURN r) {
+		    // no fall-through.
+		    linkup(r, false);
+		}
+		public void visit(SEQ s) { /* ignore this guy! */ }
+		public void visit(THROW t) {
+		    // no fall-through.
+		    linkup(t, false);
 		}
 	    }
+	    TreeVisitor tv = new EVisitor();
+	    // iterate in depth-first pre-order:
+	    for (Iterator it=code.getElementsI(); it.hasNext(); )
+		((Tree)it.next()).accept(tv);
 	}
-	
-	private void addEdge(Stm from, Stm to) { 
-	    TreeEdge te = new TreeEdge(from, to);
-	    Set pred, succ;
-	    
-	    pred = (Set)this.predecessors.get(to);
-	    succ = (Set)this.successors.get(from);
-	    pred.add(te);
-	    succ.add(te);
-	}	    
-    }
-    private static Stm RS(Stm seq) { 
-	while (seq.kind()==TreeKind.SEQ) seq = ((SEQ)seq).getLeft();  
-	return seq;
-    }
-    static class TreeEdge implements HCodeEdge {
-	private final Tree from;
-	private final Tree to;
-	TreeEdge(Tree from, Tree to) { this.from = from; this.to = to; }
-	public HCodeElement from() { return from; }
-	public HCodeElement to() { return to; }
     }
 }
