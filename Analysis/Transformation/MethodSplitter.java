f929c4f4e5243edeeea41dcca714d3d12f6ff521
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-10-06 23:54:33 +0000

    Made MethodSplitter demand driven.  This has some nice features, and some bad features.  The bad features made it unworkable at the moment: if you wait too long to do the HCode mutation, the original HCode could be clear()ed from the cache, which leaves us without anything to perform our mutations upon.  Also, this forces the entire MethodSplitter to be Serializable, because we're always deferring all our work.
    For these reasons, these changes are going into a stub branch
    and not being incorporated into the mainline revision.
    But they're here checked in if anyone wants to revive them.

--- Analysis/Transformation/MethodSplitter.java
+++ Analysis/Transformation/MethodSplitter.java
@@ -19,24 +19,25 @@ import java.util.*;
  * to effect the specialization.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: MethodSplitter.java,v 1.1.2.4 2000-10-06 23:49:40 cananian Exp $
+ * @version $Id: MethodSplitter.java,v 1.1.2.4.2.1 2000-10-06 23:54:33 cananian Exp $
  */
-public class MethodSplitter {
+public class MethodSplitter implements java.io.Serializable {
     /** The <code>ORIGINAL</code> token represents the original pre-split
      *  version of a method. */
     public static final Token ORIGINAL = new Token(null);
     /** This is the code factory which contains the representations of the
-     *  new split methods. */
-    private final CachingCodeFactory hcf;
-
+     *  original (unsplit) methods. */
+    private final HCodeFactory parent;
     /** Creates a <code>MethodSplitter</code>, based on the method
      *  representations in the <code>parent</code> <code>HCodeFactory</code>.
      */
     public MethodSplitter(HCodeFactory parent) {
-        this.hcf = new CachingCodeFactory(parent, true/* save cache */);
+	// make parent a caching code factory, because we're going to
+	// call convert(m) on a method m once *for each possible token*.
+        this.parent = new CachingCodeFactory(parent);
     }
     
-    /** Maps split methods to the original method they were derived from. */
+    /** Maps split methods to <original method, token> pairs. */
     private final Map split2orig = new HashMap();
     /** Maps <original method, token> pairs to created split methods. */
     private final Map versions = new HashMap();
@@ -45,10 +46,11 @@ public class MethodSplitter {
      *  method named by the token <code>which</code>. */
     public final synchronized HMethod select(HMethod source, Token which) {
 	Util.assert(isValidToken(which), "token is not valid");
-	HMethod orig = split2orig.containsKey(source) ?
-	    (HMethod) split2orig.get(source) : source;
+	HMethod orig = !split2orig.containsKey(source) ? source :
+	    (HMethod) ((List) split2orig.get(source)).get(0);
 	if (which == ORIGINAL) return orig;
-	HMethod splitM = (HMethod) versions.get(Default.pair(source, which));
+	List swpair = Default.pair(source, which);
+	HMethod splitM = (HMethod) versions.get(swpair);
 	if (splitM == null) {
 	    HClassMutator hcm = orig.getDeclaringClass().getMutator();
 	    Util.assert(hcm!=null, "You're using a linker, not a relinker.");
@@ -64,23 +66,40 @@ public class MethodSplitter {
 			    "exists");
 	    }
 	    /* now add this to known versions */
-	    versions.put(Default.pair(source, which), splitM);
-	    split2orig.put(splitM, orig);
-	    /* and create an HCode for this */
-	    HCode hc = hcf.convert(orig);
-	    if (hc!=null) {
-		try {
-		    hcf.put(splitM, mutateHCode(hc.clone(splitM), which));
-		} catch (CloneNotSupportedException ex) {
-		    Util.assert(false, "cloning HCode failed: "+ex);
-		}
-	    }
+	    versions.put(swpair, splitM);
+	    split2orig.put(splitM, swpair);
+	    /* HCodes will now be created demand-driven */
 	}
 	return splitM;
     }
     /** Returns a <code>HCodeFactory</code> containing representations for
      *  the methods split by the <code>MethodSplitter</code>. */
     public final HCodeFactory codeFactory() { return hcf; }
+    /** This is the code factory which contains the representations of the
+     *  new split methods. */
+    private final HCodeFactory hcf = new SerializableCodeFactory() {
+	private final Map cache = new HashMap();
+	public String getCodeName() { return parent.getCodeName(); }
+	public HCode convert(HMethod m) {
+	    if (cache.containsKey(m)) return (HCode) cache.get(m);
+	    List swpair = (List) split2orig.get(m);
+	    HCode hc = parent.convert(swpair==null?m:(HMethod)swpair.get(0));
+	    if (hc!=null)
+		try {
+		    hc = mutateHCode(hc.clone(m), (Token) swpair.get(1));
+		} catch (CloneNotSupportedException ex) {
+		    Util.assert(false, "cloning HCode failed: "+ex);
+		}
+	    cache.put(m, hc);
+	    return hc;
+	}
+	public void clear(HMethod m) {
+	    cache.remove(m);
+	    /*parent.clear(m);*/ // XXX can't clear original until all splits
+	                         // have been made & we can't tell when that
+	                         // might be, because creation is demand-driven
+	}
+    };
 
     /** Override this method if you want to create mutated methods
      *  with descriptors differing from that of the original method.
@@ -107,7 +126,7 @@ public class MethodSplitter {
      *  suffix for the newly-split method's name.  Don't forget
      *  to extend <code>MethodSplitter.isValidToken()</code> to
      *  include your new <code>MethodSplitter.Token</code> subclasses. */
-    protected static class Token {
+    protected static class Token implements java.io.Serializable {
 	final String suffix;
 	protected Token(String suggestedSuffix) {
 	    this.suffix = suggestedSuffix;
