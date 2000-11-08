e285542eb7843121c1dde06064dc306d37d980b5
Brian Demsky <bdemsky@mit.edu>
2000-11-08 14:47:57 +0000

    Changes added to stop counter before we print results out.

--- Runtime/Instrumentate.java
+++ Runtime/Instrumentate.java
@@ -7,30 +7,35 @@ package harpoon.Runtime;
  * <code>Instrumentate</code>
  * 
  * @author  bdemsky <bdemsky@lm.lcs.mit.edu>
- * @version $Id: Instrumentate.java,v 1.1.2.1 2000-11-08 02:23:44 bdemsky Exp $
+ * @version $Id: Instrumentate.java,v 1.1.2.2 2000-11-08 14:47:57 bdemsky Exp $
  */
 public class Instrumentate {
     static int size;
     static long[] array;
     static Object lock=new Object();
+    static boolean counton;
     static {
 	size=10;
 	array=new long[size];
+	counton=true;
     }
     
     static void count(int value) {
 	synchronized(lock) {
-	    if (value>size) {
-		long[] newarray=new long[size*2];
-		System.arraycopy(array,0,newarray,0,size);
-		array=newarray;
-		size=size*2;
+	    if (counton) {
+		if (value>size) {
+		    long[] newarray=new long[size*2];
+		    System.arraycopy(array,0,newarray,0,size);
+		    array=newarray;
+		    size=size*2;
+		}
+		array[value]++;
 	    }
-	    array[value]++;
 	}
     }
     
     static void exit() {
+	counton=false;
 	for(int i=0;i<size;i++)
 	    System.out.println(i+"  "+array[i]);
     }
