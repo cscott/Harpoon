9b79e9eafb2d06e7045b293a5662309a708415f9
Brian Demsky <bdemsky@mit.edu>
2000-11-11 00:17:36 +0000

    Fix

--- Backend/Runtime2/TreeBuilder.java
+++ Backend/Runtime2/TreeBuilder.java
@@ -63,7 +63,7 @@ import java.util.Set;
  * <p>Pretty straightforward.  No weird hacks.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: TreeBuilder.java,v 1.1.2.1 2000-11-10 21:49:48 bdemsky Exp $
+ * @version $Id: TreeBuilder.java,v 1.1.2.2 2000-11-11 00:17:36 bdemsky Exp $
  */
 public class TreeBuilder extends harpoon.Backend.Runtime1.TreeBuilder {
     TreeBuilder(Runtime runtime, Linker linker, ClassHierarchy ch,
@@ -100,5 +100,121 @@ public class TreeBuilder extends harpoon.Backend.Runtime1.TreeBuilder {
 		 DECLARE(dg,objectType, Tobj,
 		 new TEMP(tf,source,Type.POINTER,Tobj)));
 	else return old;
+    }
+    // XXX in single-threaded mode, this can be a NOP.
+    public Translation.Exp monitorEnter(TreeFactory tf, HCodeElement source,
+					DerivationGenerator dg,
+					Translation.Exp objectref) {
+	// call FNI_MonitorEnter()
+	return new Translation.Nx(_call_FNI_Monitor(tf, source, dg, objectref,
+						    true));
+    }
+    // XXX in single-threaded mode, this can be a NOP.
+    public Translation.Exp monitorExit(TreeFactory tf, HCodeElement source,
+				       DerivationGenerator dg,
+				       Translation.Exp objectref) {
+	// call FNI_MonitorExit()
+	return new Translation.Nx(_call_FNI_Monitor(tf, source, dg, objectref,
+						    false));
+    }
+    private Stm _call_FNI_Monitor(TreeFactory tf, HCodeElement source,
+				  DerivationGenerator dg,
+				  Translation.Exp objectref,
+				  boolean isEnter/*else exit*/) {
+	// keep this synchronized with StubCode.java
+	// and Runtime/include/jni-private.h
+	final int REF_OFFSET = 3 * POINTER_SIZE;
+
+	// first get JNIEnv *
+	Temp envT = new Temp(tf.tempFactory(), "env");
+	Stm result0 = new NATIVECALL
+	    (tf, source, (TEMP)
+	     DECLARE(dg, HClass.Void/* JNIEnv * */, envT,
+	     new TEMP(tf, source, Type.POINTER, envT)) /*retval*/,
+	     DECLARE(dg, HClass.Void/* c function ptr */,
+	     new NAME(tf, source, new Label
+		      (runtime.nameMap.c_function_name("FNI_GetJNIEnv")))),
+	     null/* no args*/);
+	Temp object= new Temp(tf.tempFactory(), "BRIANSOBJECT");
+	// wrap objectref.
+	Temp objT = new Temp(tf.tempFactory(), "obj");
+	result0 = new SEQ
+	    (tf, source, result0,
+	     new NATIVECALL
+	     (tf, source, (TEMP)
+	      DECLARE(dg, HClass.Void/* jobject */, objT,
+	      new TEMP(tf, source, Type.POINTER, objT)) /*retval*/,
+	      DECLARE(dg, HClass.Void/* c function ptr */,
+	      new NAME(tf, source, new Label
+		       (runtime.nameMap.c_function_name("FNI_NewLocalRef")))),
+	      new ExpList
+	      (DECLARE(dg, HClass.Void/* JNIEnv * */, envT,
+	       new TEMP(tf, source, Type.POINTER, envT)),
+	       new ExpList
+	       (DECLARE(dg, HClass.Void, object,
+	        new TEMP(tf,source,Type.POINTER,object)), null))));
+
+	// call FNI_MonitorEnter or FNI_MonitorExit
+	// proto is 'jint FNI_Monitor<foo>(JNIEnv *env, jobject obj);
+	// i'm going to be anal and make a temp for the return value,
+	// because some architectures might conceivably do weird things if
+	// i just pretend the function is void.  but we don't need the retval.
+	Temp disT = new Temp(tf.tempFactory(), "discard");
+	Stm result1 = new NATIVECALL
+	    (tf, source,
+	     new TEMP(tf, source, Type.INT, disT) /*retval*/,
+	     DECLARE(dg, HClass.Void/* c function ptr */,
+	     new NAME(tf, source, new Label
+		      (runtime.nameMap.c_function_name
+		       (isEnter?"FNI_MonitorEnter":"FNI_MonitorExit")))),
+	     new ExpList
+	     (DECLARE(dg, HClass.Void/* JNIEnv * */, envT,
+	      new TEMP(tf, source, Type.POINTER, envT)),
+	      new ExpList
+	      (DECLARE(dg, HClass.Void/* jobject */, objT,
+	       new TEMP(tf, source, Type.POINTER, objT)),
+	       null)));
+
+	// okay, now free the localref and we're set.
+	result1 = new SEQ
+	    (tf, source, result1,
+	     new NATIVECALL
+	     (tf, source, null/*void retval*/,
+	      DECLARE(dg, HClass.Void/* c function ptr */,
+	      new NAME(tf, source, new Label(runtime.nameMap.c_function_name
+					     ("FNI_DeleteLocalRefsUpTo")))),
+	      new ExpList
+	      (DECLARE(dg, HClass.Void/* JNIEnv * */, envT,
+	       new TEMP(tf, source, Type.POINTER, envT)),
+	       new ExpList
+	       (DECLARE(dg, HClass.Void/* jobject */, objT,
+		new TEMP(tf, source, Type.POINTER, objT)),
+		null))));
+	Stm old=new SEQ(tf,source,result0,result1);
+
+	Label DLabel=new Label();
+	Label SLabel=new Label();
+	Exp lockvalue=new MEM(tf,source,Type.INT,
+		       new BINOP(tf, source,Type.POINTER,Bop.ADD,
+                         (DECLARE(dg, HClass.Void, object,
+			 new TEMP(tf,source,Type.POINTER,object))),
+		           new CONST(tf, source, OBJ_HASH_OFF)));
+	Exp testcond=new BINOP(tf, source, Type.INT, Bop.AND, lockvalue,
+		       new CONST(tf, source, 2));
+	Stm dotest=new CJUMP(tf,source,testcond,SLabel,DLabel);
+	
+	Stm monitor=new SEQ(tf,source,new LABEL(tf,source,DLabel,false),old);
+	Stm labels=new SEQ(tf,source,monitor,new LABEL(tf, source, SLabel,false));
+	
+	Stm testit=new SEQ(tf,source,dotest,labels);
+
+	Stm myresult=new SEQ(tf,source,
+                        new MOVE(tf,source,
+                         (DECLARE(dg, HClass.Void, object,
+			  new TEMP(tf,source,Type.POINTER,object))),
+                          objectref.unEx(tf)),
+			testit);
+
+	return myresult;
     }
 }
