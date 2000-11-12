014864e542787759315fc45d94c88492518b2f4f
Wes Beebee <wbeebee@mit.edu>
2000-11-12 19:42:50 +0000

    Adding class that will eventually contain the top-level Realtime java analysis...

---
+++ Analysis/Realtime/Realtime.java
@@ -0,0 +1,10 @@
+package harpoon.Analysis.Realtime;
+
+// This class is for Realtime JAVA extensions.
+
+public class Realtime {
+  public static boolean REALTIME_JAVA = false; // Is Realtime JAVA support turned on?
+
+
+
+}
