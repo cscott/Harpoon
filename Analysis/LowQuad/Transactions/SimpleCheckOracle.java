42b2a537fdfcf6a7414cd787b6927ab11bf8a0d0
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-11-15 17:56:14 +0000

    Static GET/SET shouldn't get checks added.  Not yet, at least; maybe we'll revert this patch once we figure out exactly how we're going to handle transactions on static fields.

--- Analysis/LowQuad/Transactions/SimpleCheckOracle.java
+++ Analysis/LowQuad/Transactions/SimpleCheckOracle.java
@@ -12,28 +12,28 @@ import java.util.*;
  * <code>SimpleCheckOracle</code>
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: SimpleCheckOracle.java,v 1.1.2.1 2000-11-14 19:37:33 cananian Exp $
+ * @version $Id: SimpleCheckOracle.java,v 1.1.2.2 2000-11-15 17:56:14 cananian Exp $
  */
 class SimpleCheckOracle extends CheckOracle {
     public Set createReadVersions(HCodeElement hce) {
 	if (hce instanceof AGET)
 	    return Collections.singleton(((AGET)hce).objectref());
-	if (hce instanceof GET)
+	if (hce instanceof GET && !((GET)hce).isStatic())
 	    return Collections.singleton(((GET)hce).objectref());
 	return Collections.EMPTY_SET;
     }
     public Set createWriteVersions(HCodeElement hce) {
 	if (hce instanceof ASET)
 	    return Collections.singleton(((ASET)hce).objectref());
-	if (hce instanceof SET)
+	if (hce instanceof SET && !((SET)hce).isStatic())
 	    return Collections.singleton(((SET)hce).objectref());
 	return Collections.EMPTY_SET;
     }
     public Set checkField(HCodeElement hce) {
-	if (hce instanceof GET)
+	if (hce instanceof GET && !((GET)hce).isStatic())
 	    return Collections.singleton
 		(new RefAndField(((GET)hce).objectref(),((GET)hce).field()));
-	if (hce instanceof SET)
+	if (hce instanceof SET && !((SET)hce).isStatic())
 	    return Collections.singleton
 		(new RefAndField(((SET)hce).objectref(),((SET)hce).field()));
 	return Collections.EMPTY_SET;
