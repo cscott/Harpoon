2bdd4517608bfe18c918160ad3cdca90441c056b
Brian Demsky <bdemsky@mit.edu>
2000-11-16 17:26:44 +0000

    *** empty log message ***

--- src/java.io/java_io_File.c
+++ src/java.io/java_io_File.c
@@ -4,11 +4,14 @@
 #include <assert.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <unistd.h>
 #include "flexthread.h" /* for mutex ops */
 
 static jfieldID pathID = 0; /* The field ID of File.path */
 static int inited = 0; /* whether the above variables have been initialized */
+static jclass MEMExcCls  = 0; /* The java/io/IOException class object */
 FLEX_MUTEX_DECLARE_STATIC(init_mutex);
 
 static int initializeFI(JNIEnv *env) {
@@ -22,22 +25,44 @@ static int initializeFI(JNIEnv *env) {
   if ((*env)->ExceptionOccurred(env)) goto done;
   pathID = (*env)->GetFieldID(env,FIcls,"path","Ljava/lang/String;");
   if ((*env)->ExceptionOccurred(env)) goto done;
-  
+
+  MEMExcCls = (*env)->FindClass(env, "java/lang/OutOfMemoryError");
+  if ((*env)->ExceptionOccurred(env)) goto done;
+  /* make IOExcCls into a global reference for future use */
+  MEMExcCls = (*env)->NewGlobalRef(env, MEMExcCls);
+
   inited = 1;
  done:
   FLEX_MUTEX_UNLOCK(&init_mutex);
   return inited;
 }
 
-#if 0 /* unimplemented */
+
 /*
  * Class:     java_io_File
  * Method:    exists0
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_java_io_File_exists0
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_java_io_File_exists0(JNIEnv *env, jobject this) {
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  r = stat(cstr, &buf);	
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+	
+  if (r != 0) 
+    return ((jboolean)0);
+  else
+    return ((jboolean)1);
+}
 
+#if 0
 /*
  * Class:     java_io_File
  * Method:    canWrite0
@@ -54,30 +79,78 @@ JNIEXPORT jboolean JNICALL Java_java_io_File_canWrite0
 JNIEXPORT jboolean JNICALL Java_java_io_File_canRead0
   (JNIEnv *, jobject);
 
+#endif
 /*
  * Class:     java_io_File
  * Method:    isFile0
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_java_io_File_isFile0
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_java_io_File_isFile0(JNIEnv * env, jobject this){
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  r = stat(cstr, &buf);	
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+	
+  if ((r == 0) && S_ISDIR(buf.st_mode))
+    return ((jboolean)1);
+  else
+    return ((jboolean)0);
+}
+
 
 /*
  * Class:     java_io_File
  * Method:    isDirectory0
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_java_io_File_isDirectory0
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_java_io_File_isDirectory0(JNIEnv *env, jobject this) {
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  r = stat(cstr, &buf);	
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+	
+  if ((r == 0) && S_ISREG(buf.st_mode))
+    return ((jboolean)1);
+  else
+    return ((jboolean)0);
+
+}
 
 /*
  * Class:     java_io_File
  * Method:    lastModified0
  * Signature: ()J
  */
-JNIEXPORT jlong JNICALL Java_java_io_File_lastModified0
-  (JNIEnv *, jobject);
-#endif /* unimplemented */
+JNIEXPORT jlong JNICALL Java_java_io_File_lastModified0(JNIEnv *env, jobject this) {
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  r = stat(cstr, &buf);	
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+	
+  return ((jlong)buf.st_mtime*(jlong)1000);
+}
+
 
 /*
  * Class:     java_io_File
@@ -105,15 +178,32 @@ JNIEXPORT jlong JNICALL Java_java_io_File_length0(JNIEnv * env, jobject this)
   return ((jlong)buf.st_size);
 }
 
-#if 0 /* unimplemented */
+
 /*
  * Class:     java_io_File
  * Method:    mkdir0
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_java_io_File_mkdir0
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_java_io_File_mkdir0(JNIEnv *env, jobject this) {
 
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  r = mkdir(cstr, 0777);	
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+  if (r != 0)
+    return ((jboolean)0);
+  else
+    return ((jboolean)1);
+}
+
+
+#if 0
 /*
  * Class:     java_io_File
  * Method:    renameTo0
@@ -137,29 +227,131 @@ JNIEXPORT jboolean JNICALL Java_java_io_File_delete0
  */
 JNIEXPORT jboolean JNICALL Java_java_io_File_rmdir0
   (JNIEnv *, jobject);
+#endif
 
 /*
  * Class:     java_io_File
  * Method:    list0
  * Signature: ()[Ljava/lang/String;
  */
-JNIEXPORT jobjectArray JNICALL Java_java_io_File_list0
-  (JNIEnv *, jobject);
+JNIEXPORT jobjectArray JNICALL Java_java_io_File_list0(JNIEnv *env, jobject this) {
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * path;    
+  DIR* dir;
+  struct dirent* entry;
+  struct dentry {
+    struct dentry* next;
+    char name[1];
+  };
+  struct dentry* dirlist;
+  struct dentry* mentry;
+  jobjectArray array;
+  jclass clscls;
+  int count;
+  int i;
+  int oom = 0;
+  
+
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  path=(*env)->GetStringUTFChars(env,jstr,0);
+
+  
+  /* XXX make part of jsyscall interface !? */
+  dir = opendir(path);
+  if (dir == 0) {
+    (*env)->ReleaseStringUTFChars(env,jstr,path);
+    return (0);
+  }
+  
+  dirlist = 0;
+  count = 0;
+  /* XXX make part of jsyscall interface !? */
+  while ((entry = readdir(dir)) != 0) {
+    /* We skip '.' and '..' */
+    if (strcmp(".", entry->d_name) == 0 ||
+	strcmp("..", entry->d_name) == 0) {
+      continue;
+    }
+    mentry = malloc(sizeof(struct dentry) + _D_EXACT_NAMLEN(entry));
+
+    if (!mentry) {
+      while (dirlist) {
+	mentry = dirlist;
+	dirlist = dirlist->next;
+	free(mentry);
+      }
+
+      (*env)->ReleaseStringUTFChars(env,jstr,path);
+      (*env)->Throw(env, MEMExcCls);
+      return NULL;
+    }
+    strcpy(mentry->name, entry->d_name);
+    mentry->next = dirlist;
+    dirlist = mentry;
+    count++;
+  }
+  /* XXX make part of jsyscall interface !? */
+  closedir(dir);
+  clscls=(*env)->FindClass(env, "java/lang/String");
+
+  if (!clscls) return NULL;
+  r = (*env)->NewObjectArray(env, array, clscls, NULL);
+  if (!r) return NULL;
+
+  /* XXX: This assert is a noop.  If AllocObjectArray throws an
+     exception, we leak. */
+
+  for (i = 0; i < count; i++) {
+    jstring jstr;
+    mentry = dirlist;
+    dirlist = mentry->next;
+    jstr=(*env)->NewString(env, mentry->name,_D_EXACT_NAMLEN(entry));
+    (*env)->SetObjectArrayElement(env, array, i, jstr);
+    free(mentry);
+  }
+  if (oom) {
+    (*env)->ReleaseStringUTFChars(env,jstr,path);
+    (*env)->Throw(env, MEMExcCls);
+    return NULL;
+  }
+  (*env)->ReleaseStringUTFChars(env,jstr,path);
+  return (array);
+}
 
 /*
  * Class:     java_io_File
  * Method:    canonPath
  * Signature: (Ljava/lang/String;)Ljava/lang/String;
  */
-JNIEXPORT jstring JNICALL Java_java_io_File_canonPath
-  (JNIEnv *, jobject, jstring);
+JNIEXPORT jstring JNICALL Java_java_io_File_canonPath(JNIEnv *env, jobject this, jstring jstr) {
+  return jstr;
+}
 
 /*
  * Class:     java_io_File
  * Method:    isAbsolute
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_java_io_File_isAbsolute
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_java_io_File_isAbsolute(JNIEnv *env, jobject this) {
+  struct stat buf;
+  int r;
+  jobject jstr;
+  const char * cstr;    
+  jboolean jbool;
+  
+  if (!inited && !initializeFI(env)) return 0; /* exception occurred; bail */
+  
+  jstr=(*env)->GetObjectField(env, this, pathID);
+  cstr=(*env)->GetStringUTFChars(env,jstr,0);
+  jbool=(jboolean) (cstr[0]=='/');
 
-#endif /* unimplemented */
+  (*env)->ReleaseStringUTFChars(env,jstr,cstr);
+  
+  return jbool;
+}
+
+
