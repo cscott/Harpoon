ebc3ccbdf0eec16a4538de64921322839a1206c0
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-11-16 15:25:09 +0000

    Add src/java.lang.reflect package to the Runtime with implementations of native methods in the java.lang.reflect package.  At the moment, this is only java.lang.reflect.Field.getModifiers().  I may decide that I want to provide alternate implementations of the classes in this package, and if so that stuff would go here too.

---
+++ src/java.lang.reflect/java_lang_reflect_Field.c
@@ -0,0 +1,159 @@
+#include <assert.h>
+#include <jni.h>
+#include "java_lang_reflect_Field.h"
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getModifiers
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_java_lang_reflect_Field_getModifiers
+	(JNIEnv *env, jobject thisfield) {
+  assert(0);
+}
+
+#if 0
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    get
+ * Signature: (Ljava/lang/Object;)Ljava/lang/Object;
+ */
+JNIEXPORT jobject JNICALL Java_java_lang_reflect_Field_get
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getBoolean
+ * Signature: (Ljava/lang/Object;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_java_lang_reflect_Field_getBoolean
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getByte
+ * Signature: (Ljava/lang/Object;)B
+ */
+JNIEXPORT jbyte JNICALL Java_java_lang_reflect_Field_getByte
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getChar
+ * Signature: (Ljava/lang/Object;)C
+ */
+JNIEXPORT jchar JNICALL Java_java_lang_reflect_Field_getChar
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getShort
+ * Signature: (Ljava/lang/Object;)S
+ */
+JNIEXPORT jshort JNICALL Java_java_lang_reflect_Field_getShort
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getInt
+ * Signature: (Ljava/lang/Object;)I
+ */
+JNIEXPORT jint JNICALL Java_java_lang_reflect_Field_getInt
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getLong
+ * Signature: (Ljava/lang/Object;)J
+ */
+JNIEXPORT jlong JNICALL Java_java_lang_reflect_Field_getLong
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getFloat
+ * Signature: (Ljava/lang/Object;)F
+ */
+JNIEXPORT jfloat JNICALL Java_java_lang_reflect_Field_getFloat
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    getDouble
+ * Signature: (Ljava/lang/Object;)D
+ */
+JNIEXPORT jdouble JNICALL Java_java_lang_reflect_Field_getDouble
+  (JNIEnv *, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    set
+ * Signature: (Ljava/lang/Object;Ljava/lang/Object;)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_set
+  (JNIEnv *, jobject, jobject, jobject);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setBoolean
+ * Signature: (Ljava/lang/Object;Z)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setBoolean
+  (JNIEnv *, jobject, jobject, jboolean);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setByte
+ * Signature: (Ljava/lang/Object;B)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setByte
+  (JNIEnv *, jobject, jobject, jbyte);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setChar
+ * Signature: (Ljava/lang/Object;C)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setChar
+  (JNIEnv *, jobject, jobject, jchar);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setShort
+ * Signature: (Ljava/lang/Object;S)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setShort
+  (JNIEnv *, jobject, jobject, jshort);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setInt
+ * Signature: (Ljava/lang/Object;I)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setInt
+  (JNIEnv *, jobject, jobject, jint);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setLong
+ * Signature: (Ljava/lang/Object;J)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setLong
+  (JNIEnv *, jobject, jobject, jlong);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setFloat
+ * Signature: (Ljava/lang/Object;F)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setFloat
+  (JNIEnv *, jobject, jobject, jfloat);
+
+/*
+ * Class:     java_lang_reflect_Field
+ * Method:    setDouble
+ * Signature: (Ljava/lang/Object;D)V
+ */
+JNIEXPORT void JNICALL Java_java_lang_reflect_Field_setDouble
+  (JNIEnv *, jobject, jobject, jdouble);
+#endif /* 0 */
