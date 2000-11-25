b09dd40974bdf7b29342f81e15e93704447b3b65
C. Scott Ananian <cananian@alumni.princeton.edu>
2000-11-25 16:29:02 +0000

    Specification of abstract graph interface.  This is meant to be completely indepdendent of the underlying graph representation, whether it is adjacency graphs or whatever.

---
+++ Util/Grapher.java
@@ -0,0 +1,19 @@
+// Grapher.java, created Thu Nov 23 13:09:52 2000 by cananian
+// Copyright (C) 2000 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Util;
+
+import java.util.Set;
+/**
+ * <code>Grapher</code> is an abstract interface for specifying the
+ * graph properties of data structures so that generic graph algorithms
+ * may be applied.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: Grapher.java,v 1.1.2.1 2000-11-25 16:29:02 cananian Exp $
+ */
+public interface Grapher {
+    boolean isEdge(Object from, Object to);
+    Set succSet(Object node);
+    Set predSet(Object node);
+}
