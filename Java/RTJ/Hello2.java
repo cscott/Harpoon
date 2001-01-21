bf334da8d1f0616769111837af2c9dc3794ef1a0
Wes Beebee <wbeebee@mit.edu>
2001-01-21 04:21:26 +0000

    Some test cases for RTJ - more to come soon.

---
+++ Java/RTJ/Hello2.java
@@ -0,0 +1,38 @@
+import realtime.RealtimeThread;
+import realtime.HeapMemory;
+import realtime.MemoryArea;
+
+public class Hello2 {
+  public static void main(String argv[]) {
+    int baz = 3;
+    int bar = 2;
+    int[] foo = new int[] { 1, baz, 2, bar};
+    System.out.println(foo.length);
+    if (MemoryArea.getMemoryArea(foo) instanceof HeapMemory) {
+      System.out.println("Hooray!");
+      if ((foo[0]==1)&&(foo[1]==3)&&(foo[2]==2)&&(foo[3]==2)) {
+	System.out.println("cool!");
+      } else {
+	System.out.println("Not so cool");
+      }
+    } else {
+      System.out.println("whoops!");
+    }
+
+    System.out.println(MemoryArea.getMemoryArea(foo).toString());
+    Thread t = new Thread();
+    Hello2 hello2 = new Hello2();
+  }
+    
+  public Hello2() {
+    System.out.println("Hello world");
+    System.out.println(RealtimeThread.currentRealtimeThread().toString());
+    if (Thread.currentThread() instanceof RealtimeThread) {
+      System.out.println("RealtimeThread");
+    } else {
+      System.out.println("Thread");
+    }
+  }
+}
+
+
---
+++ Java/RTJ/Tester.java
@@ -0,0 +1,38 @@
+import realtime.RealtimeThread;
+import realtime.MemoryArea;
+import realtime.HeapMemory;
+
+// This is a testing driver for the Realtime Java support.
+
+public class Tester {
+  
+  
+  private static void testing(String description, boolean pass) {
+    System.out.print("  Testing "+description+" ... ");
+    if (pass) {
+      System.out.println("passed.");
+    } else {
+      System.out.println("FAILED!!!");
+      System.exit(1);
+    }
+  }
+  
+  private static void section(String name) {
+    System.out.println("---------- Testing "+name+" -------------");
+  }
+
+  public static void main(String argv[]) {
+    section("RealtimeThread");
+    testing("Thread.currentThread() instanceof RealtimeThread",
+	    Thread.currentThread() instanceof RealtimeThread);
+    testing("RealtimeThread.currentRealtimeThread().getMemoryArea()!=null",
+	    RealtimeThread.currentRealtimeThread().getMemoryArea() != null);
+    testing("RealtimeThread.currentRealtimeThread().getMemoryArea() instanceof HeapMemory",
+	    RealtimeThread.currentRealtimeThread().getMemoryArea() 
+	    instanceof HeapMemory);
+
+
+    section("ScopedMemory");
+  }
+}
+
