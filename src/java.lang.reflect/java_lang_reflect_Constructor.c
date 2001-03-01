a9c1b02f8a9e58fcc21bc7e58eaa7068403fc997
C. Scott Ananian <cananian@alumni.princeton.edu>
2001-03-01 22:22:26 +0000

    Implemeted Method.invoke() and Constructor.newInstance().
    Constructor.newInstance() is missing a check that the object
    it is trying to create is not abstract, because FLEX doesn't
    currently output the access modifiers of a class as part of
    the runtime information.  This will be fixed shortly.

--- src/java.lang.reflect/java_lang_reflect_Constructor.c
+++ src/java.lang.reflect/java_lang_reflect_Constructor.c
@@ -3,6 +3,7 @@
 #include "jni-private.h"
 #include "java_lang_reflect_Constructor.h"
 #include "java_lang_reflect_Method.h"
+#include "java_lang_reflect_Modifier.h"
 
 /*
  * Class:     java_lang_reflect_Constructor
@@ -14,15 +15,41 @@ JNIEXPORT jint JNICALL Java_java_lang_reflect_Constructor_getModifiers
     return Java_java_lang_reflect_Method_getModifiers(env, _this);
 }
 
-#if 0
 /*
  * Class:     java_lang_reflect_Constructor
  * Method:    newInstance
  * Signature: ([Ljava/lang/Object;)Ljava/lang/Object;
  */
 JNIEXPORT jobject JNICALL Java_java_lang_reflect_Constructor_newInstance
-  (JNIEnv *, jobject, jobjectArray);
-#endif
+  (JNIEnv *env, jobject constructorobj, jobjectArray args) {
+  struct FNI_method2info *method; /* method information */
+  jclass methodclazz; /* declaring class of method */
+  jobject result;
+  
+  assert(constructorobj!=NULL);
+  method = FNI_GetMethodInfo(constructorobj);
+  assert(method!=NULL);
+  methodclazz = FNI_WRAP(method->declaring_class_object);
+  assert(!(*env)->ExceptionOccurred(env));
+
+  /* XXX: check that declaring class is not abstract.  Can't do that right now
+   * because modifiers for class are not currently available to the flex
+   * runtime. */
+  if (0 & java_lang_reflect_Modifier_ABSTRACT) {
+      jclass excls=(*env)->FindClass(env,"java/lang/IllegalAccessException");
+      (*env)->ThrowNew(env, excls,
+		       "attempted instantiation of an abstract class");
+      return NULL;
+  }
+  /* create zero-filled object instance. */
+  result = (*env)->AllocObject(env, methodclazz);
+  if ((*env)->ExceptionOccurred(env)) return NULL; /* bail */
+  /* okay, now invoke constructor */
+  Java_java_lang_reflect_Method_invoke(env, constructorobj, result, args);
+  if ((*env)->ExceptionOccurred(env)) return NULL; /* bail */
+  /* done, ta-da! */
+  return result;
+}
 
 #if !defined(WITHOUT_HACKED_REFLECTION) /* this is our hacked implementation */
 /*
--- src/java.lang.reflect/java_lang_reflect_Method.c
+++ src/java.lang.reflect/java_lang_reflect_Method.c
@@ -3,6 +3,7 @@
 #include <jni.h>
 #include "jni-private.h"
 #include "java_lang_reflect_Method.h"
+#include "java_lang_reflect_Modifier.h"
 #include "reflect-util.h"
 
 /*
@@ -15,15 +16,180 @@ JNIEXPORT jint JNICALL Java_java_lang_reflect_Method_getModifiers
     return FNI_GetMethodInfo(_this)->modifiers;
 }
 
-#if 0
 /*
  * Class:     java_lang_reflect_Method
  * Method:    invoke
  * Signature: (Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;
  */
 JNIEXPORT jobject JNICALL Java_java_lang_reflect_Method_invoke
-  (JNIEnv *, jobject, jobject, jobjectArray);
-#endif
+  (JNIEnv *env, jobject methodobj, jobject receiverobj, jobjectArray args) {
+  struct FNI_method2info *method; /* method information */
+  jclass methodclazz; /* declaring class of method */
+  char *desc; /* method descriptor */
+  char *sigptr; /* temporary pointer within the method descriptor */
+  int nparams; /* number of parameters to the method */
+  char retdesc; /* first character of return type descriptor */
+  jvalue retval; /* return value of the method */
+  jboolean isConstructor=JNI_FALSE; /* is this methodobj a constructor? */
+  
+  assert(methodobj!=NULL);
+  method = FNI_GetMethodInfo(methodobj);
+  assert(method!=NULL);
+  methodclazz = FNI_WRAP(method->declaring_class_object);
+  assert(!(*env)->ExceptionOccurred(env));
+
+  /* according to spec, we need to do receiver checks first */
+  if (!(method->modifiers & java_lang_reflect_Modifier_STATIC)) {
+    if (receiverobj==NULL) {
+      jclass excls = (*env)->FindClass(env, "java/lang/NullPointerException");
+      (*env)->ThrowNew(env, excls, "null receiver for non-static field");
+      return NULL;
+    }
+    if (!(*env)->IsInstanceOf(env, receiverobj, methodclazz)) {
+      jclass excls=(*env)->FindClass(env,"java/lang/IllegalArgumentException");
+      (*env)->ThrowNew(env, excls,
+		       "receiver not instance of field declaring class");
+      return NULL;
+    }
+  }
+  if (method->modifiers & java_lang_reflect_Modifier_ABSTRACT) {
+      jclass excls=(*env)->FindClass(env,"java/lang/IllegalAccessException");
+      (*env)->ThrowNew(env, excls,
+		       "attempted invocation of an abstract method");
+      return NULL;
+  }
+  /* check number of args */
+  desc = method->methodID->desc; assert(desc && *desc=='(');
+  for (nparams=0, sigptr=desc+1; sigptr!=NULL && *sigptr!=')';
+       sigptr=REFLECT_advanceDescriptor(sigptr), nparams++)
+    /* do nothing */;
+  if (nparams != (*env)->GetArrayLength(env, args)) {
+    jclass excls=(*env)->FindClass(env, "java/lang/IllegalArgumentException");
+    (*env)->ThrowNew(env, excls, "incorrect number of arguments");
+    return NULL;
+  }
+  /* okay, create argument array */
+  {
+    jvalue unwrapped[nparams];
+    for (nparams=0, sigptr=desc+1; sigptr!=NULL && *sigptr!=')';
+	 sigptr=REFLECT_advanceDescriptor(sigptr), nparams++) {
+      jobject onearg = (*env)->GetObjectArrayElement(env, args, nparams);
+      if (*sigptr=='L' || *sigptr=='[') {
+	jclass argtype = REFLECT_parseDescriptor(env, sigptr);
+	if ((*env)->ExceptionOccurred(env)) return NULL; /* bail on error */
+	if (!(*env)->IsInstanceOf(env, onearg, argtype)) {
+	  jclass excls=(*env)->FindClass(env,
+					 "java/lang/IllegalArgumentException");
+	  (*env)->ThrowNew(env, excls,
+			   "couldn't widen object to parameter type");
+	  return NULL;
+	}
+	unwrapped[nparams].l = onearg;
+      } else {
+	unwrapped[nparams]=REFLECT_unwrapPrimitive(env, onearg, *sigptr);
+	if ((*env)->ExceptionOccurred(env)) return NULL; /* bail on error */
+      }
+    }
+    /* determine if this method is a constructor */
+    {
+      jclass constrclazz =
+	(*env)->FindClass(env, "java/lang/reflect/Constructor");
+      if ((*env)->ExceptionOccurred(env)) (*env)->ExceptionClear(env);
+      else isConstructor = (*env)->IsInstanceOf(env, methodobj, constrclazz);
+    }
+    /* find return type */
+    retdesc = *(strchr(desc, ')')+1);
+    /* invoke! */
+    assert(!(*env)->ExceptionOccurred(env));
+    if (method->modifiers & java_lang_reflect_Modifier_STATIC) {
+      /* ----------------- static methods --------------------- */
+      switch (retdesc) {
+      case 'V':
+	(*env)->CallStaticVoidMethodA
+	  (env, methodclazz, method->methodID, unwrapped);
+	break;
+#define STATICCASE(typename,shortname,longname,descchar,fieldname)\
+      case descchar:\
+	retval.fieldname = (*env)->CallStatic##shortname##MethodA\
+	  (env, methodclazz, method->methodID, unwrapped);\
+	break;
+      FORPRIMITIVETYPESX(STATICCASE);
+#undef STATICCASE
+      default:
+	/* object type */
+	retval.l = (*env)->CallStaticObjectMethodA
+	  (env, methodclazz, method->methodID, unwrapped);
+	break;
+      }
+    } else if (isConstructor ||
+	       method->modifiers & java_lang_reflect_Modifier_PRIVATE) {
+      /* ----------------- non-virtual methods --------------------- */
+      switch (retdesc) {
+      case 'V':
+	(*env)->CallNonvirtualVoidMethodA
+	  (env, receiverobj, methodclazz, method->methodID, unwrapped);
+	break;
+#define NONVIRTUALCASE(typename,shortname,longname,descchar,fieldname)\
+      case descchar:\
+	retval.fieldname = (*env)->CallNonvirtual##shortname##MethodA\
+	  (env, receiverobj, methodclazz, method->methodID, unwrapped);\
+	break;
+      FORPRIMITIVETYPESX(NONVIRTUALCASE);
+#undef NONVIRTUALCASE
+      default:
+	/* object type */
+	retval.l = (*env)->CallNonvirtualObjectMethodA
+	  (env, receiverobj, methodclazz, method->methodID, unwrapped);
+	break;
+      }
+    } else {
+      /* ----------------- virtual methods --------------------- */
+      switch (retdesc) {
+      case 'V':
+	(*env)->CallVoidMethodA
+	  (env, receiverobj, method->methodID, unwrapped);
+	break;
+#define VIRTUALCASE(typename,shortname,longname,descchar,fieldname)\
+      case descchar:\
+	retval.fieldname = (*env)->Call##shortname##MethodA\
+	  (env, receiverobj, method->methodID, unwrapped);\
+	break;
+      FORPRIMITIVETYPESX(VIRTUALCASE);
+#undef VIRTUALCASE
+      default:
+	/* object type */
+	retval.l = (*env)->CallObjectMethodA
+	  (env, receiverobj, method->methodID, unwrapped);
+	break;
+      }
+    }
+  } /* call is done! (don't need the unwrapped args anymore) */
+  if ((*env)->ExceptionOccurred(env)) { /* oops, exception occurred */
+    /* wrap the exception in an InvocationTargetException and throw it */
+    jthrowable exception, wrapped_exception;
+    jclass wrapcls; jmethodID mid;
+    exception = (*env)->ExceptionOccurred(env);
+    (*env)->ExceptionClear(env);
+    wrapcls = (*env)->FindClass
+      (env, "java/lang/reflect/InvocationTargetException");
+    if ((*env)->ExceptionOccurred(env)) return NULL; /* bail */
+    mid = (*env)->GetMethodID
+      (env, wrapcls, "<init>", "(Ljava/lang/Throwable;)V");
+    if ((*env)->ExceptionOccurred(env)) return NULL; /* bail */
+    wrapped_exception = (*env)->NewObject(env, wrapcls, mid, exception);
+    if ((*env)->ExceptionOccurred(env)) return NULL; /* bail */
+    (*env)->Throw(env, wrapped_exception);
+    return NULL;
+  }
+  /* no exceptions!  wrap possibly-primitive retval and return */
+  switch(retdesc) {
+  case 'V': return NULL;
+  case 'L': case '[': return retval.l;
+  default:
+    return REFLECT_wrapPrimitive(env, retval, retdesc); /* wrap! */
+  }
+  /* done */
+}
 
 #if !defined(WITHOUT_HACKED_REFLECTION) /* this is our hacked implementation */
 /*
