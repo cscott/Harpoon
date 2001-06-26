fedd3d9b63441812d31adc4b56f196e459be7d0f
Wes Beebee <wbeebee@mit.edu>
2001-06-26 16:47:03 +0000

    This class represents the fact that no garbage collector is currently running.

---
+++ NoGarbageCollector.java
@@ -0,0 +1,18 @@
+// NoGarbageCollector.java, created by wbeebee
+// Copyright (C) 2001 Wes Beebee <wbeebee@mit.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package javax.realtime;
+
+/** <code>NoGarbageCollector</code> provides a means of informing
+ *  the program that no garbage collector is present.
+ */
+
+public class NoGarbageCollector extends GarbageCollector {
+    NoGarbageCollector() {
+    }
+
+//      public RelativeTime getPreemptionLatency() {
+//  	return new RelativeTime();
+//      }
+
+}
