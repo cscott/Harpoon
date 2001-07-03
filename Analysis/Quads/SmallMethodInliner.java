f9d43f7fdcd24d6f02a7fc8d544871825203cfdd
C. Scott Ananian <cananian@alumni.princeton.edu>
2001-07-03 07:06:56 +0000

    Update SmallMethodInliner to much more accurately bloat the executable with inlined methods.  The system property harpoon.inliner.percent allows you to specify more-or-less exactly how much code-size bloat you want, due to inlined methods.

--- Analysis/Quads/SmallMethodInliner.java
+++ Analysis/Quads/SmallMethodInliner.java
@@ -10,27 +10,28 @@ import harpoon.ClassFile.HCodeFactory;
 import harpoon.ClassFile.HMethod;
 import harpoon.IR.Quads.CALL;
 import harpoon.IR.Quads.Quad;
+import harpoon.Util.Collections.BinaryHeap;
 import harpoon.Util.Collections.GenericMultiMap;
+import harpoon.Util.Collections.Heap;
 import harpoon.Util.Collections.MultiMap;
 
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 /**
  * <code>SmallMethodInliner</code> will inline small methods until
  * the code is bloated by the specified amount.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: SmallMethodInliner.java,v 1.1.2.1 2001-06-18 19:42:40 cananian Exp $
+ * @version $Id: SmallMethodInliner.java,v 1.1.2.2 2001-07-03 07:06:56 cananian Exp $
  */
 public class SmallMethodInliner extends MethodInliningCodeFactory {
-    static final int NPERCENT=10;
+    static final int NPERCENT= // default to 10 percent bloat.
+	Integer.parseInt(System.getProperty("harpoon.inliner.percent", "10"));
 
     /** Creates a <code>SmallMethodInliner</code>. */
     public SmallMethodInliner(HCodeFactory hcf, ClassHierarchy ch) {
@@ -40,6 +41,11 @@ public class SmallMethodInliner extends MethodInliningCodeFactory {
     private SmallMethodInliner(CachingCodeFactory ccf, ClassHierarchy ch) {
 	super(ccf);
 	long totalsize = 0;
+
+	IntMap methodSize = new IntMap();
+	MultiMap callSites = new GenericMultiMap();
+	Map call2caller = new HashMap();
+
 	for (Iterator it=ch.callableMethods().iterator(); it.hasNext(); ) {
 	    HMethod hm = (HMethod) it.next();
 	    HCode hc = ccf.convert(hm);
@@ -54,38 +60,54 @@ public class SmallMethodInliner extends MethodInliningCodeFactory {
 		CALL call = (CALL) q;
 		if (call.isVirtual()) continue;
 		callSites.add(call.method(), call);
+		call2caller.put(call, hm);
 	    }
 	    // keep track of total program size
 	    totalsize+=methodSize.getInt(hm);
 	}
-	List mlist = new ArrayList(ch.callableMethods());
-	Collections.sort(mlist, new Comparator() {
-	    public int compare(Object o1, Object o2) {
-		HMethod hm1 = (HMethod) o1, hm2 = (HMethod) o2;
-		return score(hm1) - score(hm2);
-	    }
-	});
+	Heap h = new BinaryHeap();
+	Map method2entry = new HashMap();
+	for (Iterator it=ch.callableMethods().iterator(); it.hasNext(); ) {
+	    HMethod hm = (HMethod) it.next();
+	    // compute score for hm.
+	    int score = score(hm, methodSize, callSites);
+	    // add to heap, and to method-to-entry map.
+	    if (callSites.getValues(hm).size()>0) // unless no sites to inline
+		method2entry.put(hm, h.insert(new Integer(score), hm));
+	}
 	// inline to bloat the program by the specified percentage of
 	// total program size.
 	long bloat = 0;
-	Iterator it=mlist.iterator();
-	while (it.hasNext() && bloat < (NPERCENT*totalsize/100)) {
-	    HMethod hm = (HMethod) it.next(); // inline this!
+	while (bloat < (NPERCENT*totalsize/100)) {
+	    if (h.isEmpty()) break;
+	    HMethod hm = (HMethod) h.extractMinimum().getValue();//inline this!
+	    method2entry.remove(hm);
 	    if (Modifier.isNative(hm.getModifiers())) continue;//unless native
-	    Iterator it2=callSites.getValues(hm).iterator();
 	    int size = methodSize.getInt(hm);
+	    Iterator it2=callSites.getValues(hm).iterator();
+	    if (it2.hasNext())
+		System.err.println("INLINING all nonvirtual calls to "+hm);
 	    while (it2.hasNext()) { 
-		inline((CALL)it2.next());
-		// XXX: add 'size' to caller's method size, re-sort.
+		CALL call = (CALL) it2.next();
+		inline(call);
 		bloat+=size;
+		// add 'size' to caller's method size...
+		HMethod caller = (HMethod) call2caller.get(call);
+		methodSize.putInt(caller, methodSize.getInt(caller)+size);
+		// ...and adjust caller's key in pqueue.
+		Map.Entry me = (Map.Entry) method2entry.get(caller);
+		if (me!=null) {
+		    int score = score(hm, methodSize, callSites);
+		    h.updateKey(me, new Integer(score));
+		}
 	    }
-	    System.out.println("INLINING all nonvirtual calls to "+hm);
 	}
 	// done!
+	System.out.println("ACTUAL INLINING BLOAT: " +
+			   ((1+2*100*bloat)/(2*totalsize)) + "%");
     }
-    final IntMap methodSize = new IntMap();
-    final MultiMap callSites = new GenericMultiMap();
-    private int score(HMethod hm) {
+    private static int score(HMethod hm, IntMap methodSize, MultiMap callSites)
+    {
 	return methodSize.getInt(hm) * callSites.getValues(hm).size();
     }
 
