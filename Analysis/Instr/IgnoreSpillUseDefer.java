d63a530d50df5a3de831766db7288af84b3e913b
Felix S. Klock II <pnkfelix@mit.edu>
2001-08-03 02:40:33 +0000

    FSK: Added nice documentation (a year late!) to IgnoreSpillUseDefer, FSK: since it really *is* needed for precise-gc analyses.

--- Analysis/Instr/IgnoreSpillUseDefer.java
+++ Analysis/Instr/IgnoreSpillUseDefer.java
@@ -1,4 +1,4 @@
-// IgnoreSpillUDr.java, created Fri Jun 30 19:14:06 2000 by pnkfelix
+// IgnoreSpillUseDefer.java, created Fri Jun 30 19:14:06 2000 by pnkfelix
 // Copyright (C) 2000 Felix S. Klock II <pnkfelix@mit.edu>
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Analysis.Instr;
@@ -10,10 +10,23 @@ import java.util.Collection;
 import java.util.Collections;
 
 /**
- * <code>IgnoreSpillUseDefer</code>
- * 
+ * <code>IgnoreSpillUseDefer</code> defines a view of the code that
+ * will skip over newly inserted spill instructions when deciding what
+ * <code>Temp</code>s instructions read and write.  This is useful for
+ * the garbage collection analyses which need to know what Temps are
+ * live in terms of the program itself, ignoring memory
+ * communication due to spill code. 
+ * <p>
+ * For example, a SpillLoad from the stack location for <tt>t0</tt> to
+ * <tt>r3</tt> is no longer considered a definition of <tt>t0</tt> by
+ * this <code>UseDefer</code>.
+ *
+ * <p> This class may need to be updated to take a
+ * <code>UseDefer</code> parameter, rather than relying on the
+ * underlying IR implementing the <code>UseDefable</code> interface. 
+ *
  * @author  Felix S. Klock II <pnkfelix@mit.edu>
- * @version $Id: IgnoreSpillUseDefer.java,v 1.1.2.8 2001-06-17 22:29:52 cananian Exp $
+ * @version $Id: IgnoreSpillUseDefer.java,v 1.1.2.9 2001-08-03 02:40:33 pnkfelix Exp $
  */
 public class IgnoreSpillUseDefer extends UseDefer {
 
