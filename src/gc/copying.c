c64ce7ad72e3f3b3acfc45488739a6c40fdb5762
Karen K. Zee <kkz@alum.mit.edu>
2001-08-06 20:02:51 +0000

    Refactoring of code. Cleanup. Changes in heuristics for when to collect versus when to expand the heap.

--- src/gc/copying.c
+++ src/gc/copying.c
@@ -11,19 +11,28 @@
 #ifdef WITH_THREADED_GC
 #include "flexthread.h"
 #endif
+#include "deflate_objs.h"
 
-/* data structures for list of inflated objects */
-struct obj_list {
-  jobject_unwrapped obj;
-  struct obj_list *next;
+#ifndef WITH_PRECISE_C_BACKEND
+# error unimplemented
+#endif
+
+#define BOTTOM 0
+#define TOP    1
+
+
+/* data structure for semi-spaces */
+struct semi_space {
+  void *begin;
+  void *free;
+  void *end;
 };
 
 /* variables locked by copying_gc_mutex */
-static void *free_ptr;
-static void *to_space;
-static void *from_space;
-static void *top_of_space;
-static void *top_of_to_space;
+static struct semi_space heap[2];
+/* indices into copying_heap */
+static int from;
+static int to;
 /* file descriptor for mmap */
 static int fd;
 /* current size of heap */
@@ -34,7 +43,7 @@ static size_t heap_size;
 static struct obj_list *inflated_objs = NULL;
 
 /* convenience macros */
-#define IN_HEAP(void_p) ((void_p) >= from_space && (void_p) < top_of_space)
+#define IN_HEAP(void_p) ((void_p) >= heap[from].begin && (void_p) < heap[from].free)
 
 #ifdef DEBUG_GC
 static int num_gcs = 0;
@@ -100,7 +109,7 @@ jlong copying_free_memory()
 {
   jlong result;
   ENTER_COPYING_GC();
-  result = (jlong)(top_of_space - free_ptr);
+  result = (jlong)(heap[from].end - heap[from].free);
   EXIT_COPYING_GC();
   return result;
 }
@@ -124,7 +133,7 @@ jlong copying_get_heap_size()
 /* effects: overwrites to-space with recognizable garbage for easy debugging */
 void debug_overwrite_to_space()
 {
-  memset(to_space, 7, heap_size/2);
+  memset(heap[to].begin, 7, heap_size/2);
 }
 
 
@@ -152,8 +161,8 @@ void copying_handle_reference(jobject_unwrapped *ref)
   if (IN_HEAP((void *)obj))
     {
       // handle objects that have already been moved
-      if (((void *)(obj->claz) >= to_space) && 
-	  ((void *)(obj->claz) < top_of_to_space))
+      if (((void *)(obj->claz) >= heap[to].begin) && 
+	  ((void *)(obj->claz) <  heap[to].free))
 	{
 	  // forward pointer appropriately
 	  (*ref) = (jobject_unwrapped)(obj->claz);
@@ -178,15 +187,15 @@ void relocate(jobject_unwrapped *ref) {
   /* figure out how big the object is */
   size_t obj_size = align(FNI_ObjectSize(obj));
   /* relocated objects should not exceed size of heap */
-  assert(free_ptr + obj_size <= top_of_to_space);
-  /* copy over to to_space */
-  forwarding_address = TAG_HEAP_PTR(memcpy(free_ptr, obj, obj_size));
+  assert(heap[to].free + obj_size <= heap[to].end);
+  /* copy over to to-space */
+  forwarding_address = TAG_HEAP_PTR(memcpy(heap[to].free, obj, obj_size));
   /* write forwarding address to previous location;
      the order of the following two operations are critical */
   obj->claz = /* not really */(struct claz *)forwarding_address;
   (*ref) = forwarding_address;
   /* increment free */
-  free_ptr += obj_size;
+  heap[to].free += obj_size;
 }
 
 
@@ -207,8 +216,8 @@ void deflate_freed_objs ()
       // printf("Inspecting %p: ", infl_obj);
       
       // update objects that have been moved
-      if (((void *)(infl_obj->claz) >= to_space) && 
-	  ((void *)(infl_obj->claz) < top_of_to_space))
+      if (((void *)(infl_obj->claz) >= heap[to].begin) && 
+	  ((void *)(infl_obj->claz) <  heap[to].free))
 	{
 	  // forward pointer appropriately
 	  unit->obj = (jobject_unwrapped)PTRMASK(infl_obj->claz);
@@ -240,62 +249,120 @@ void deflate_freed_objs ()
   FLEX_MUTEX_UNLOCK(&inflated_objs_mutex);
 }
 
+/* initial number of pages to reserve */
+#define INITIAL_PAGES_TO_MAP 65536
 
-/* magic number (quite arbitrary) used to determine start size of heap */
-#define INITIAL_HEAP_SIZE     131072
+/* beginning and end of reserved space */
+static void *mapped_begin;
+static void *mapped_end;
+
+/* initial number of pages in the heap */
+#define INITIAL_PAGES_IN_HEAP 32
+
 /* more magic numbers specifying the target heap occupancy */
-#define TARGET_OCCUPANCY      0.25
-#define HEAP_DIVISOR          2
+#define HEAP_DIVISOR          4
 
-/* statistics for determining when to grow heap. */
-// start with a conservative guess for the occupancy
-static float avg_occupancy = 0.4; // 0 <= avg_occupancy <= 0.5
-static int num_collections = 0;
-static size_t max_heap_size = 0;
+static size_t SYSTEM_PAGE_SIZE = 0;
+static size_t PAGE_MASK = 0;
+#define ROUND_TO_NEXT_PAGE(x) (((x) + PAGE_MASK) & (~PAGE_MASK))
 
-/* initializatiion: set up heap */
+/* effects: initializes heap */
 void copying_gc_init()
 {
-  heap_size = INITIAL_HEAP_SIZE;
+  size_t bytes_to_map;
+
+  // find out the system page size and pre-calculate some constants
+  SYSTEM_PAGE_SIZE = getpagesize();
+  PAGE_MASK = SYSTEM_PAGE_SIZE - 1;
+
+  printf("SYSTEM_PAGE_SIZE %d\n", SYSTEM_PAGE_SIZE);
+
+  bytes_to_map = INITIAL_PAGES_TO_MAP*SYSTEM_PAGE_SIZE;
+
+  // reserve a large amount of memory in which to grow the heap
   fd = open("/dev/zero", O_RDONLY);
-  from_space = mmap(0, heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  mapped_begin = mmap(0, bytes_to_map, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
 
   // could not allocate memory. we really should throw an exception,  
   // but there are various problems with this. even if we pre-allocated
   // the exception object, throwing the exception would involve
   // allocating more memory.
-  assert(from_space != MAP_FAILED);
+  assert(mapped_begin != MAP_FAILED);
 
-  error_gc("Initializing copying heap of size x%x\n", heap_size);
-  free_ptr = from_space;
-  to_space = from_space + heap_size/2;
-  top_of_space = to_space;
-  top_of_to_space = to_space + heap_size/2;
-  error_gc("Free space from %p ", from_space);
-  error_gc("to %p\n\n", top_of_space);
+  mapped_end = mapped_begin + bytes_to_map;
+
+  // allocate heap
+  heap_size = INITIAL_PAGES_IN_HEAP*SYSTEM_PAGE_SIZE;
+
+  // initialized to- and from- spaces
+  from = BOTTOM;
+  to = TOP;
+  
+  // from-space is initially allocated at the beginning of the mapped space
+  heap[from].begin = mapped_begin;
+  heap[from].free  = mapped_begin;
+  heap[from].end   = mapped_begin + heap_size/2;
+
+  // to-space is initially allocated at the midpoint of the mapped space
+  heap[to].begin = mapped_begin + bytes_to_map/2;
+  heap[to].free  = heap[to].begin;
+  heap[to].end   = heap[to].begin + heap_size/2;
+}
+
+
+/* requires: needed_space > 0
+   effects: grows heap by enough to accomodate needed_space, which is given in bytes
+   returns: 0 on success, -1 on failure
+*/
+int grow_heap(size_t needed_space)
+{
+  size_t bytes;
+
+  // we are going to expand the heap by enough to fit at 
+  // least needed_space; at the same time, we might as
+  // well grow the heap a reasonable amt (1/HEAP_DIVISOR 
+  // of the current space) for good measure.
+  bytes = ROUND_TO_NEXT_PAGE(needed_space + heap_size/HEAP_DIVISOR);
+
+  // if we don't have enough space reserved, just fail for now
+  if (heap[TOP].end + bytes > mapped_end)
+    return -1;
+  
+  // update the size of the heap
+  heap_size += 2*bytes;
+  
+  // update the bounds of the semispaces
+  heap[to].end   += bytes;
+  heap[from].end += bytes;
+
+  assert(((heap[from].end - heap[from].begin) +
+	 (heap[to].end - heap[to].begin)) == heap_size);
+  printf("heap_size %d\n", heap_size);
+
+  return 0;
 }
 
 
 void copying_print_stats ()
 {
-    printf("Maximum heap size = %d bytes.\n", max_heap_size);
+  printf("heap_size %d\n", heap_size);
 }
 
 
-#define GC_PAGE_SIZE 65536
-#define GC_PAGE_MASK (GC_PAGE_SIZE - 1)
-#define ROUND_TO_NEXT_PAGE(x) (((x) + GC_PAGE_MASK) & (~GC_PAGE_MASK))
-#define GC_BLK_SIZE  4096
-#define GC_BLK_MASK  (GC_BLK_SIZE - 1)
-#define ROUND_TO_NEXT_BLK(x) (((x) + GC_BLK_MASK) & (~GC_BLK_MASK))
-
-
-#ifdef WITH_PRECISE_C_BACKEND
-void *copying_malloc (size_t size_in_bytes)
-#else
-void *copying_malloc (size_t size_in_bytes, void *saved_registers[])
-#endif
+/* returns: 1 if we can afford to garbage collect, 0 otherwise */
+int collection_makes_sense(size_t bytes_since_last_GC, float occupancy_estimate)
 {
+  size_t cost = occupancy_estimate * heap_size;
+  return (bytes_since_last_GC > cost);
+}
+
+
+/* returns: on success, a pointer to an amt (size_in_bytes) of memory; on failure, NULL */
+void *copying_malloc (size_t size_in_bytes)
+{
+  static size_t bytes_allocated = 0; // bytes allocated since last collection
+  static float avg_occupancy = 0;
+
   size_t aligned_size_in_bytes;
   void *result;
 
@@ -304,78 +371,43 @@ void *copying_malloc (size_t size_in_bytes, void *saved_registers[])
   /* calculate the actual number of bytes we need */
   aligned_size_in_bytes = align(size_in_bytes);
 
-#ifndef GC_EVERY_TIME
-  if (free_ptr + aligned_size_in_bytes > top_of_space)
-#endif
-    {
-      float heap_occupancy;
-      size_t expected_free_space;
-      size_t expand_heap = 0; // minimum amt by which we want to expand the heap
-      // if not enough memory to allocate, run a collection
-      error_gc("\nx%x bytes needed ", aligned_size_in_bytes);
-      error_gc("but only x%x bytes available\n", top_of_space - free_ptr);
+  // if not enough memory, need to expand heap or run GC
+  if (heap[from].free + aligned_size_in_bytes > heap[from].end)
+    // if we can afford the cost of a collection, go ahead; else expand heap
+    if (!collection_makes_sense(bytes_allocated, avg_occupancy))
+      {
+	int retval = grow_heap(aligned_size_in_bytes);
 
-      // printf("avg_occupancy %f\n", avg_occupancy);
+	// if we couldn't grow the heap, try collecting
+	if (retval != 0)
+	  {
+	    avg_occupancy = copying_collect();
+	    // reset bytes_allocated
+	    bytes_allocated = 0;
+	  }
+      }
+    else
+      {
+	avg_occupancy = copying_collect();
 
-      if (aligned_size_in_bytes > GC_PAGE_SIZE || 
-	  avg_occupancy > TARGET_OCCUPANCY)
-	expand_heap = aligned_size_in_bytes;
-      else
-	{
-	  // guess if we want to grow heap based on statistics
-	  expected_free_space = (0.5 - avg_occupancy)*heap_size;
-	  error_gc("Expected free space after collection is %d bytes.\n", expected_free_space);
-
-	  // guess if we have enough space. also try to maintain a low occupancy
-	  if ((expected_free_space < ROUND_TO_NEXT_BLK(aligned_size_in_bytes)))
-	    // if we are going to expand the heap, make sure we do it by enough
-	    expand_heap = aligned_size_in_bytes;
-	}
-
-      // setup_for_threaded_GC is a nop if not using threads
-      setup_for_threaded_GC();
-#ifdef WITH_PRECISE_C_BACKEND
-      copying_collect(expand_heap);
-#else
-      copying_collect(saved_registers, expand_heap);
-#endif
-      // done with collection, calculate statistics
-      heap_occupancy = ((float)(free_ptr - from_space)) / ((float)heap_size);
-      // avg_occupancy = (heap_occupancy + num_collections*avg_occupancy)/(num_collections+1);
-      avg_occupancy = (heap_occupancy + avg_occupancy)/2;
-      assert(avg_occupancy >= 0 && avg_occupancy <= 0.5);
-      num_collections++;
-      error_gc("Heap occupancy %.4f", avg_occupancy);
-      error_gc(" after %d collections\n", num_collections);
-
-      error_gc("Actual free space after collection is %d bytes\n", (top_of_space - free_ptr));
-
-      // if we are still out of space then we have to expand the heap
-      if (free_ptr + aligned_size_in_bytes > top_of_space)
-	{
-	  error_gc("ALERT -- second collection needed for allocating %d bytes!\n", aligned_size_in_bytes);
-	  // we should never have to expand the heap twice in the same allocation
-	  assert(expand_heap == 0);
-#ifdef WITH_PRECISE_C_BACKEND
-	  copying_collect(aligned_size_in_bytes);
-#else
-	  copying_collect(saved_registers, aligned_size_in_bytes);
-#endif
-	}
-      // cleanup_after_threaded_GC is a nop if not using threads
-      cleanup_after_threaded_GC();
-    }
+	// reset bytes_allocated
+	bytes_allocated = 0;
+	
+	// if still not enough memory, grow heap
+	if (heap[from].free + aligned_size_in_bytes > heap[from].end)
+	  {
+	    int retval = grow_heap(aligned_size_in_bytes);
+	   
+	    // if we couldn't collect and couldn't expand heap, just die
+	    assert(retval == 0);
+	  }
+      }
   
-  result = free_ptr;
-  free_ptr += aligned_size_in_bytes;
+  result = heap[from].free;
+  heap[from].free += aligned_size_in_bytes;
+  bytes_allocated += aligned_size_in_bytes;
 
-  //calculate statistics for max_heap_size
-  max_heap_size = (max_heap_size > (free_ptr - from_space)) ? max_heap_size : (free_ptr - from_space);
-
-  error_gc("%d\t", ++num_mallocs);
-  error_gc("Allocated x%x bytes ", aligned_size_in_bytes);
-  error_gc("at %p (for a total of ", result);
-  error_gc("x%x bytes)\n", (total_memory_requested += aligned_size_in_bytes));
+  assert(heap[from].free <= heap[from].end);
 
   EXIT_COPYING_GC();
 
@@ -383,58 +415,22 @@ void *copying_malloc (size_t size_in_bytes, void *saved_registers[])
 }
 
 
-/* collect performs the actual tracing and copying collection */
-#ifdef WITH_PRECISE_C_BACKEND
-void copying_collect(int expand_amt)
-#else
-void copying_collect(void *saved_registers[], int expand_amt)
-#endif
+/* effects: performs compacting garbage collection on heap and flips semi-spaces
+   returns: average heap occupancy, a number between 0 and 0.5, inclusive */
+float copying_collect()
 {
+  static int num_collections = 0;
+  static float avg_occupancy = 0;
   void *scan;
-  size_t old_heap_size;
-  void *old_heap = NULL; // ptr to old heap (for when we grow it)
-  void *next_to_space = from_space; // unless we grow the heap
+  float occupancy;
 
-  error_gc("GC number %d\n", ++num_gcs);
+  setup_for_threaded_GC();
 
-  // see if we need to grow the heap
-  if (expand_amt != 0)
-    {
-      // before doing anything we need a handle on the memory that we are going to munmap
-      old_heap = (to_space < from_space) ? to_space : from_space;
-      old_heap_size = heap_size;
+  scan = heap[to].free;
 
-      // printf("heapsize %d -> ", heap_size);
-      // we are going to expand the heap by enough to fit at 
-      // least expand_amt; the doubling takes into account
-      // the fact that half of the heap is empty at any point
-      // since we're expanding, grow the heap a reasonable amt
-      // (1/HEAP_DIVISOR of the current space) for good measure
-      heap_size += ROUND_TO_NEXT_PAGE(2*expand_amt + heap_size/HEAP_DIVISOR);
-      // printf("%d\n", heap_size);
-      to_space = mmap(0, heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-
-      // NOTE TO SELF: turn this assert into an exception!!!
-      assert(to_space != MAP_FAILED);
-      error_gc("Expanding heap from %d", old_heap_size);
-      error_gc(" to %d bytes\n", heap_size);
-
-      top_of_to_space = to_space + heap_size/2;
-      next_to_space = top_of_to_space;
-    }
-
-  free_ptr = to_space;
-  scan = free_ptr;
-
-  error_gc("FLIP -- New free space from %p to ", free_ptr);
-  error_gc("%p\n\n", top_of_to_space);
-
-#ifdef WITH_PRECISE_C_BACKEND
   find_root_set();
-#else
-  find_root_set(saved_registers);
-#endif
-  while(scan < free_ptr)
+
+  while(scan < heap[to].free)
     {
       // trace object at scan for references,
       // then increment scan by size of object
@@ -442,27 +438,30 @@ void copying_collect(void *saved_registers[], int expand_amt)
       trace((jobject_unwrapped)scan);
       scan += align(FNI_ObjectSize(scan));
     }
-  assert(scan == free_ptr);
+  assert(scan == heap[to].free);
 
   // free up any resources from inflated objs that have been GC'd
   deflate_freed_objs();
 
-  // the new from_space is what used to be to_space
-  from_space = to_space;
-  top_of_space = top_of_to_space;
+  // flip semi-spaces and re-set new to-space
+  from = to;
+  to = (to == BOTTOM) ? TOP : BOTTOM;
+  heap[to].free = heap[to].begin;
 
-  // set up new to_space
-  to_space = next_to_space;
-  top_of_to_space = to_space + heap_size/2;
-
-  // if necessary, munmap the previous heap
-  if (old_heap != NULL)
-    {
-      // on success, munmap returns 0, on failure -1
-      int munmap_result = munmap(old_heap, old_heap_size);
-      assert(munmap_result == 0);
-    }
-  
   // this function is a nop if not debug
   debug_overwrite_to_space();
+
+  // calculate heap occupancy after compacting garbage-collection
+  occupancy = ((float) (heap[from].free - 
+			heap[from].begin))/((float) heap_size);
+  avg_occupancy = (avg_occupancy*num_collections +
+		   occupancy)/(num_collections + 1);
+  num_collections++;
+
+  error_gc("GC number %d\n", num_collections);
+  printf("GC number %d\n", num_collections);
+
+  cleanup_after_threaded_GC();
+
+  return avg_occupancy;
 }
