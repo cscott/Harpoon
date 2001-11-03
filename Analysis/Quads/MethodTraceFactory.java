b86146da9f110a17935a6b3aa2ef198ed334b377
C. Scott Ananian <cananian@alumni.princeton.edu>
2001-11-03 19:18:58 +0000

    The MethodTraceFactory adds DEBUG quads to allow you to see what methods are being called in your code.  This is helpful when debugging against a known-good executable: you can look for the first non-identical line to find out where your 'bad guy' starts going wrong.

---
+++ Analysis/Quads/MethodTraceFactory.java
@@ -0,0 +1,62 @@
+// MethodTraceFactory.java, created Fri Nov  2 14:30:00 2001 by cananian
+// Copyright (C) 2000 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Analysis.Quads;
+
+import harpoon.Analysis.Transformation.*;
+import harpoon.ClassFile.*;
+import harpoon.IR.Quads.*;
+import harpoon.Util.*;
+
+import java.util.*;
+/**
+ * <code>MethodTraceFactory</code> adds <code>DEBUG</code> quads
+ * to the entry and exit points of every method.  Comparing
+ * the debug output of a working and 'broken' program should
+ * make it easier to isolate which part of the 'broken' binary
+ * isn't behaving as expected.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: MethodTraceFactory.java,v 1.1.2.1 2001-11-03 19:18:58 cananian Exp $
+ */
+public class MethodTraceFactory extends MethodMutator {
+    
+    /** Creates a <code>MethodTraceFactory</code>. */
+    public MethodTraceFactory(HCodeFactory parent) {
+	super(parent);
+    }
+    public HCode mutateHCode(HCodeAndMaps input) {
+	final HCode hc = input.hcode();
+	QuadVisitor qv = new QuadVisitor() {
+		public void visit(Quad q) { /* do nothing */ }
+		public void visit(METHOD q) {
+		    addAt(q.nextEdge(0),
+			  new DEBUG(q.getFactory(), q,
+				    ("ENTERING "+hc.getMethod()).intern()));
+		}
+		public void visit(RETURN q) {
+		    addAt(q.prevEdge(0),
+			  new DEBUG(q.getFactory(), q,
+				    ("RETURN from "+hc.getMethod()).intern()));
+		}
+		public void visit(THROW q) {
+		    addAt(q.prevEdge(0),
+			  new DEBUG(q.getFactory(), q,
+				    ("THROW from "+hc.getMethod()).intern()));
+		}
+	    };
+	Quad[] qa = (Quad[]) hc.getElements();
+	for (int i=0; i<qa.length; i++)
+	    qa[i].accept(qv);
+	return hc;
+    }
+    /** helper routine to add a quad on an edge. */
+    private static Edge addAt(Edge e, Quad q) { return addAt(e, 0, q, 0); }
+    private static Edge addAt(Edge e, int which_pred, Quad q, int which_succ) {
+	Quad frm = (Quad) e.from(); int frm_succ = e.which_succ();
+	Quad to  = (Quad) e.to();   int to_pred = e.which_pred();
+	Quad.addEdge(frm, frm_succ, q, which_pred);
+	Quad.addEdge(q, which_succ, to, to_pred);
+	return to.prevEdge(to_pred);
+    }
+}
