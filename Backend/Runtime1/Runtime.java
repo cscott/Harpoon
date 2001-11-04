5e9ef6cf7df38e01f3ee8322fb430e771fe661b4
C. Scott Ananian <cananian@alumni.princeton.edu>
2001-11-04 22:36:51 +0000

    The method and class roots lists are now read in from properties files (wherein we can specify the proper classes/methods much more elegantly, with copious comments, etc) instead of being hard-coded in the runtimeCallableMethods() source.  Look for the files Backend/Runtime1/method-root.properties and Backend/Runtime1/class-root.properties if you need to change something.
    Note that this makes it possible to elegantly subclass the Runtime:
    your runtime can override resourcePath() to have it fetch all resources
    from (say) harpoon/Backend/Runtime2.  In your new properties files, you
    can 'include harpoon/Backend/Runtime1/method-root.properties' to use
    all of the old method roots, and then add your own afterward.

--- Backend/Runtime1/Runtime.java
+++ Backend/Runtime1/Runtime.java
@@ -14,10 +14,12 @@ import harpoon.ClassFile.HCode;
 import harpoon.ClassFile.HCodeFactory;
 import harpoon.ClassFile.HMethod;
 import harpoon.ClassFile.Linker;
+import harpoon.Util.ParseUtil;
 import harpoon.Util.Util;
 
 import java.lang.reflect.Modifier;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
@@ -29,7 +31,7 @@ import java.util.Set;
  * abstract class.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Runtime.java,v 1.1.2.42 2001-10-29 16:42:37 cananian Exp $
+ * @version $Id: Runtime.java,v 1.1.2.43 2001-11-04 22:36:51 cananian Exp $
  */
 public class Runtime extends harpoon.Backend.Generic.Runtime {
     // The package and subclasses should be able to access these fields. WSB
@@ -109,122 +111,53 @@ public class Runtime extends harpoon.Backend.Generic.Runtime {
     }
 
     public Collection runtimeCallableMethods() {
-	return runtimeCallableMethods(frame.getLinker());
+	return runtimeCallableMethods(frame.getLinker(),
+				      resourcePath("method-root.properties"),
+				      resourcePath("class-root.properties"));
     }
     /** @deprecated Use frame.getRuntime().runtimeCallableMethods() instead */
-    public static Collection runtimeCallableMethods(Linker linker) {
-	HClass HCobject = linker.forName("java.lang.Object");
-	HClass HCsystem = linker.forName("java.lang.System");
-	HClass HCstring = linker.forName("java.lang.String");
-	HClass HCcharA  = linker.forDescriptor("[C");
-	HClass HCthread = linker.forName("java.lang.Thread");
-	HClass HCthreadGroup = linker.forName("java.lang.ThreadGroup");
-	return Arrays.asList(new Object[] {
-	    // implicitly called during startup/shutdown.
-	    HCsystem.getMethod("initializeSystemClass", "()V"),
-	    HCthread.getConstructor(new HClass[] {
-		    HCthreadGroup,
-		    linker.forName("java.lang.Runnable"),
-		    linker.forName("java.lang.String"),
-			}),
-	    HCthread.getMethod("exit", new HClass[0]),
-	    HCthread.getMethod("getThreadGroup", new HClass[0]),
-	    HCthreadGroup.getConstructor(new HClass[0]),
-	    HCthreadGroup.getMethod("uncaughtException", new HClass[] {
-		    HCthread, linker.forName("java.lang.Throwable")
-			}),
-	    // this is the actual implementation used for any array
-	    // clone method, so hack it into the hierarchy.
-	    linker.forDescriptor("[Ljava/lang/Object;")
-		.getMethod("clone", new HClass[0]),
-	    // jni implementation uses these:
-	    linker.forName("java.lang.NoClassDefFoundError")
-		.getConstructor(new HClass[] { HCstring }),
-	    linker.forName("java.lang.NoSuchMethodError")
-		.getConstructor(new HClass[] { HCstring }),
-	    linker.forName("java.lang.NoSuchFieldError")
-		.getConstructor(new HClass[] { HCstring }),
-	    HCstring.getConstructor(new HClass[] { HCcharA }),
-	    HCstring.getMethod("length", "()I"),
-	    HCstring.getMethod("toCharArray","()[C"),
-	    linker.forName("java.lang.OutOfMemoryError")
-		.getConstructor(new HClass[] { HCstring }),
-	    // runtime's reflection implementation mentions these
-	    // (and they are (staticly) instantiated, so reference the
-	    //  constructors)
-	    linker.forName("java.lang.Class")
-		.getConstructor(new HClass[0]),
-	    linker.forName("java.lang.reflect.Constructor")
-		.getConstructor(new HClass[0]),
-	    linker.forName("java.lang.reflect.Field")
-		.getConstructor(new HClass[0]),
-	    linker.forName("java.lang.reflect.Method")
-		.getConstructor(new HClass[0]),
-	    // reflection creates these (wrappers for primitive types)
-	    // *THESE SHOULD REALLY ONLY BE MARKED CALLABLE*
-	    //  when a call to Field.get() is found.  Eventually
-	    //  I'll update QuadClassHierarchy to handle these
-	    //  sorts of tricky dependencies.
-	    linker.forName("java.lang.Boolean")
-		.getConstructor(new HClass[] { HClass.Boolean }),
-	    linker.forName("java.lang.Byte")
-		.getConstructor(new HClass[] { HClass.Byte }),
-	    linker.forName("java.lang.Character")
-		.getConstructor(new HClass[] { HClass.Char }),
-	    linker.forName("java.lang.Short")
-		.getConstructor(new HClass[] { HClass.Short }),
-	    linker.forName("java.lang.Integer")
-		.getConstructor(new HClass[] { HClass.Int }),
-	    linker.forName("java.lang.Long")
-		.getConstructor(new HClass[] { HClass.Long }),
-	    linker.forName("java.lang.Float")
-		.getConstructor(new HClass[] { HClass.Float }),
-	    linker.forName("java.lang.Double")
-		.getConstructor(new HClass[] { HClass.Double }),
-	    // FNI_ExceptionDescribe uses this
-	    linker.forName("java.lang.Throwable")
-		.getMethod("toString", new HClass[0]),
-
-	    // in java.io implementations
-	    linker.forName("java.io.IOException") 
-		.getConstructor(new HClass[0]),
-	    linker.forName("java.io.IOException")
-		.getConstructor(new HClass[] { HCstring }),
-
-	    // in java.lang implementations
-	    linker.forName("java.lang.ArrayIndexOutOfBoundsException")
-		.getConstructor(new HClass[] { HCstring }),
-	    linker.forName("java.lang.ArrayStoreException")
-		.getConstructor(new HClass[] { HCstring }),
-	    linker.forName("java.lang.ClassNotFoundException")
-		.getConstructor(new HClass[] { HCstring }),
-	    linker.forName("java.lang.InstantiationException") // by Class
-		.getConstructor(new HClass[] { HCstring }), // .newInstance()
-		/* This is a JDK1.2-and-up method:
-	    linker.forName("java.util.Properties")
-		.getMethod("setProperty", new HClass[] { HCstring, HCstring }),
-		** We use the JDK1.1-and-up version instead: */
-	    linker.forName("java.util.Properties")
-		.getMethod("put", new HClass[] { HCobject, HCobject }),
-		// java.lang.Throwable.printStackTrace0 uses println([C)
-		// of whatever object it is passed.  Let's assume that's
-		// java.io.PrintStream (for System.out/System.err)
-	    linker.forName("java.io.PrintStream")
-		.getMethod("println", new HClass[] { HCcharA }),
-
-	    // in java.net implementations
-		// in static initializer of java.net.InetAddress:
-	    linker.forName("java.net.InetAddressImpl")
-		.getConstructor(new HClass[0]),
-
-	    // referenced by name in static initializers for primitive type
-	    // wrappers (java.lang.Integer, java.lang.Character, etc)
-	    HClass.Boolean, HClass.Byte, HClass.Short, HClass.Int,
-	    HClass.Long, HClass.Float, HClass.Double, HClass.Char,
-	    HClass.Void, /* referenced by java.lang.Void! */
-	    // passed to main()
-	    linker.forDescriptor("[Ljava/lang/String;"),
-	});
+    public static Collection runtimeCallableMethods(final Linker linker) {
+	// yuck!
+	String root = "harpoon/Backend/Runtime1/";
+	return runtimeCallableMethods(linker,
+				      root+"method-root.properties",
+				      root+"class-root.properties");
+    }
+    // yuck yuck yuck yuck.  have to declare it this way because of how much
+    // evil old code uses the original (and now deprecated)
+    // runtimeCallableMethods interface directly above.
+    private static Collection runtimeCallableMethods
+	(final Linker linker,
+	 String methodResourceName, String classResourceName) {
+	// read in root methods and classes from properties files.
+	try {
+	    final List result = new ArrayList();
+	    // read in method roots
+	    ParseUtil.readResource
+		(methodResourceName,
+		 new ParseUtil.StringParser() {
+			 public void parseString(String s)
+			     throws ParseUtil.BadLineException {
+			     result.add(ParseUtil.parseMethod(linker, s));
+			 }
+		     });
+	    // read in class roots.
+	    ParseUtil.readResource
+		(classResourceName,
+		 new ParseUtil.StringParser() {
+			 public void parseString(String s)
+			     throws ParseUtil.BadLineException {
+			     result.add(linker.forDescriptor(s));
+			 }
+		     });
+	    // done!
+	    return result;
+	} catch (java.io.IOException ioex) {
+	    // this includes BadLineExceptions.  We could just skip the
+	    // rest of the file, but the importance of these sets
+	    // justifies halting compilation with a RuntimeException.
+	    throw new RuntimeException("Can't read roots: "+ioex);
+	}
     }
 
     private synchronized void freeze() {
