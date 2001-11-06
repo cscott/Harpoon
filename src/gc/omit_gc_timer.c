3c9cde80a8d0f940cc7cf3ac580d2a0bb51469dc
Karen K. Zee <kkz@alum.mit.edu>
2001-11-06 20:06:59 +0000

    Timers for write barrier elimination.

---
+++ src/gc/omit_gc_timer.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "omit_gc_timer.h"
+
+static double start_time = 0;
+static double total_time = 0;
+
+double current_seconds()
+{
+  struct timeval tv;
+  struct timezone tz;
+  
+  gettimeofday(&tv,&tz);
+  // gets time
+  return ((double) tv.tv_sec + ((double) tv.tv_usec)/1000000.);
+}
+
+double get_total_time()
+{
+  if (start_time == 0) return total_time;
+  return total_time + (current_seconds() - start_time);
+}
+
+void init_timer()
+{
+  atexit(print_timer_output);
+  start_timer();
+}
+
+void pause_timer()
+{
+  total_time += current_seconds() - start_time;
+  assert(total_time > 0);
+  start_time = 0;
+}
+
+void print_timer_output()
+{
+  printf("Time without gc is %lf seconds.\n", get_total_time());
+}
+
+void start_timer()
+{
+  start_time = current_seconds();
+}
---
+++ src/gc/omit_gc_timer.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDED_TIMER_H
+#define INCLUDED_TIMER_H
+
+double get_total_time();
+
+void init_timer();
+
+void pause_timer();
+
+void print_timer_output();
+
+void start_timer();
+
+#endif
