bfcfdca96497f6f62adbf8a82f1c1ab078d24128
C. Scott Ananian <cananian@alumni.princeton.edu>
2001-11-07 21:48:47 +0000

    Factor out 'must be this' analysis into separate module 'MustParamOracle'. The MustParamOracle actually works for *any* parameter (not just parameter 0, which is 'this') and is thus useful for analyses other than the DefiniteInitOracle.

--- Analysis/Quads/DefiniteInitOracle.java
+++ Analysis/Quads/DefiniteInitOracle.java
@@ -23,10 +23,10 @@ import harpoon.IR.Quads.QuadVisitor;
 import harpoon.IR.Quads.RETURN;
 import harpoon.IR.Quads.SET;
 import harpoon.IR.Quads.SIGMA;
-import harpoon.Util.Util;
 import harpoon.Util.Collections.AggregateSetFactory;
 import harpoon.Util.Collections.GenericMultiMap;
 import harpoon.Util.Collections.MultiMap;
+import harpoon.Util.Util;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -45,7 +45,7 @@ import java.util.Set;
  * before its definite initialization point.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: DefiniteInitOracle.java,v 1.1.2.5 2001-11-07 19:08:09 cananian Exp $
+ * @version $Id: DefiniteInitOracle.java,v 1.1.2.6 2001-11-07 21:48:47 cananian Exp $
  */
 public class DefiniteInitOracle {
     private static final boolean DEBUG=true;
@@ -109,14 +109,14 @@ public class DefiniteInitOracle {
 	    Set notDefinitelyInitialized = new HashSet();
 	    HCode hc = hcf.convert(hm);
 	    Util.assert(hc!=null, hm);
-	    // first, a quick pass to mark all variables which *must*
-	    // contain 'this'.
-	    Set thisvars = findThisVars(hc);
 	    // compute dominator tree.
 	    DomTree dt = new DomTree(hc, false);
 	    // compute MayReadOracle
 	    MayReadOracle mro = new MayReadOracle(hc, fso, cg,
 						  hm.getDeclaringClass());
+	    // compute MustParamOracle (to find variables which must contain
+	    // 'this')
+	    MustParamOracle mpo = new MustParamOracle(hc);
 	    // make analysis results cache.
 	    MultiMap cache = new GenericMultiMap(new AggregateSetFactory());
 	    // for each exit point:
@@ -124,7 +124,7 @@ public class DefiniteInitOracle {
 		// recursively determine set of 'definitely initialized'
 		// fields.
 		Set definit = findDefInit((HCodeElement)it2.next(),
-					  dt, thisvars, mro, cache);
+					  dt, mpo, mro, cache);
 		// add any fields which are *not* definitely initialized to
 		// our set.
 		Iterator it3=Arrays.asList
@@ -172,79 +172,6 @@ public class DefiniteInitOracle {
 	     .getSuperclass());
     }
 
-    // XXX: factor out into 'find vars which *must* equal a method param'
-    // export sets, or query one, your choice.  this is used both to
-    // find 'this' vars here as well as elsewhere to determine fields
-    // which *must* be set to the same value as a method parameter.
-    Set findThisVars(HCode hc) {
-	final Set thisvars = new HashSet();
-	final Set notthisvars = new HashSet();
-	// create visitor.
-	// lattice: don't know, this, not-this.  -->move-->
-	// presence in 'not-this' overrides presence in 'this'.
-	// sets can only grow.
-	final Set relevant = new HashSet();
-	QuadVisitor v = new QuadVisitor() {
-		public void visit(Quad q) {
-		    /* look for overwrites, which are always not 'this' */
-		    notthisvars.addAll(q.defC());
-		}
-		public void visit(METHOD q) {
-		    // param 0 is 'this'; all others are 'not-this'
-		    thisvars.add(q.params(0));
-		    for (int i=1; i<q.paramsLength(); i++)
-			notthisvars.add(q.params(i));
-		}
-		public void visit(MOVE q) {
-		    relevant.add(q);
-		    if (thisvars.contains(q.src()) &&
-			!notthisvars.contains(q.src()))
-			thisvars.add(q.dst());
-		    else
-			notthisvars.add(q.dst());
-		}
-		public void visit(SIGMA q) {
-		    relevant.add(q);
-		    // get rid of overwrites.
-		    Set s = new HashSet(q.defC());
-		    for (int i=0; i<q.numSigmas(); i++)
-			s.removeAll(Arrays.asList(q.dst(i)));
-		    notthisvars.addAll(s);
-		    // now look for src==this.
-		    for (int i=0; i<q.numSigmas(); i++)
-			if (notthisvars.contains(q.src(i)))
-			    notthisvars.addAll(Arrays.asList(q.dst(i)));
-			else if (thisvars.contains(q.src(i)))
-			    // found! add all dst to thisvars.
-			    thisvars.addAll(Arrays.asList(q.dst(i)));
-		}
-		public void visit(PHI q) {
-		    relevant.add(q);
-		    // phi(x,y) is 'this' iff x *and* y are 'this' and
-		    // neither x nor y is *not* 'this'
-		    for (int i=0; i<q.numPhis(); i++)
-			for (int j=0; j<q.arity(); j++)
-			    if (notthisvars.contains(q.src(i, j)))
-				notthisvars.add(q.dst(i));
-			    else if (thisvars.contains(q.src(i, j)))
-				thisvars.add(q.dst(i));
-		}
-	    };
-	// once through all elements.
-	for (Iterator it=hc.getElementsI(); it.hasNext(); )
-	    ((Quad)it.next()).accept(v);
-	// iterate through relevant elements until the sets stop changing size.
-	int oldsize = 0, size = thisvars.size() + notthisvars.size();
-	while (size > oldsize) {
-	    for (Iterator it=relevant.iterator(); it.hasNext(); )
-		((Quad)it.next()).accept(v);
-	    oldsize = size;
-	    size = thisvars.size() + notthisvars.size();
-	}
-	// done!
-	thisvars.removeAll(notthisvars);
-	return Collections.unmodifiableSet(thisvars);
-    }
     Set findExitPoints(HCode hc) {
 	// exit points are returns.
 	Set exits = new HashSet();
@@ -273,20 +200,20 @@ public class DefiniteInitOracle {
 	    (hm, ((Quad)hce).getFactory().getMethod().getDeclaringClass());
     }
     Set findDefInit(HCodeElement exit,
-		    DomTree dt, Set thisvars, MayReadOracle mro,
+		    DomTree dt, MustParamOracle mpo, MayReadOracle mro,
 		    MultiMap cache) {
 	return Collections.unmodifiableSet
-	    (getDefBefore(exit, dt, thisvars, mro, cache));
+	    (getDefBefore(exit, dt, mpo, mro, cache));
     }
     private Set getDefBefore(HCodeElement hce,
-			     DomTree dt, Set thisvars, MayReadOracle mro,
-			     MultiMap cache) {
+			     DomTree dt, MustParamOracle mpo,
+			     MayReadOracle mro, MultiMap cache) {
 	if (!cache.containsKey(hce)) {
 	    // get immediate dominator.
 	    HCodeElement idom = dt.idom(hce);
 	    if (idom!=null) { // if this *has* an immediate dominator...
 		// get *its* definite initializations.
-		Set definit = getDefBefore(idom, dt, thisvars, mro, cache);
+		Set definit = getDefBefore(idom, dt, mpo, mro, cache);
 		// all of these are also definitely initialized at this point.
 		cache.addAll(hce, definit);
 		// collect set of reads before this quad.
@@ -298,7 +225,8 @@ public class DefiniteInitOracle {
 		if (idom instanceof SET) {
 		    SET q = (SET) idom;
 		    if (q.objectref()!=null &&
-			thisvars.contains(q.objectref())) {
+			mpo.isMustParam(q.objectref()) &&
+			mpo.whichMustParam(q.objectref())==0/*'this'*/) {
 			// not a definite init if this field can be read before
 			if (!readBefore.contains(q.field()))
 			    // baby! add this to the definitely-init'd set!
---
+++ Analysis/Quads/MustParamOracle.java
@@ -0,0 +1,141 @@
+// MustParamOracle.java, created Wed Nov  7 15:39:11 2001 by cananian
+// Copyright (C) 2000 C. Scott Ananian <cananian@alumni.princeton.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Analysis.Quads;
+
+import harpoon.ClassFile.*;
+import harpoon.IR.Quads.*;
+import harpoon.Temp.*;
+import harpoon.Util.Collections.*;
+import harpoon.Util.*;
+
+import java.util.*;
+
+/**
+ * A <code>MustParamOracle</code> tells you what method variables
+ * *must* contain the values passed in as parameters to the method.
+ * This can tell you which values must be the 'this' object, for
+ * non-static methods, or which values in (say) a field-set
+ * operation directly correspond to constructor parameters.
+ * 
+ * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
+ * @version $Id: MustParamOracle.java,v 1.1.2.1 2001-11-07 21:48:47 cananian Exp $
+ */
+public class MustParamOracle {
+    // could make this an invertibleMap if you want sets.
+    final Map results = new HashMap();
+
+    /** Returns <code>true</code> iff the given <code>Temp</code> *must*
+     *  contain the value of some parameter. */
+    boolean isMustParam(Temp t) { return results.containsKey(t); }
+    /** Returns the parameter which the given <code>Temp</code> *must*
+     *  contain the value of. */
+    int whichMustParam(Temp t) { return ((Integer)results.get(t)).intValue(); }
+    
+    /** Creates a <code>MustParamOracle</code> which gives you information
+     *  on variables in <code>HCode</code> <code>hc</code> (which should
+     *  be in SSI or SSA form, for best results). */
+    public MustParamOracle(HCode hc) {
+	// compute # of method parameters
+	METHOD method = (METHOD) ((HEADER)hc.getRootElement()).next(1);
+	int nparam = method.paramsLength();
+        // for each parameter...
+	for (int i=0; i<nparam; i++) {
+	    // do analysis.
+	    OracleVisitor ov = new OracleVisitor(hc, i);
+	    // transfer results into more compact form.
+	    Integer paramNum = new Integer(i);
+	    for (Iterator it=ov.paramvars.iterator(); it.hasNext(); ) {
+		Object old = results.put((Temp)it.next(), paramNum);
+		Util.assert(old==null, "temp can't be multiple params!");
+	    }
+	    // and do next.
+	}
+	// done!
+    }
+
+    static class OracleVisitor extends QuadVisitor {
+	final int which_param; // which parameter are we interested in?
+	/** an array of sets: one for each method parameter.  the sets
+	 *  contain all variables which 'may' contain the method parameter. */
+	final Set paramvars = new HashSet();
+	/** an array of sets, as above.  the sets in this case contain
+	 *  all variables which *may not* contain the method parameter. */
+	final Set notparamvars = new HashSet();
+	// the paramvars set minus the notparamvars set is the
+	// set of variables which *must* contain the method parameter.
+
+	/** The 'relevant' set consists of those quads which change the
+	 *  paramvars or notparamvars sets.  We can save time in our
+	 *  dataflow analysis by only iterating over these. */
+	final Set relevant = new HashSet();
+
+	OracleVisitor(HCode hc, int which) {
+	    this.which_param = which;
+	    // once through all elements.
+	    for (Iterator it=hc.getElementsI(); it.hasNext(); )
+		((Quad)it.next()).accept(this);
+	    // iterate through relevant elements until the sets stop
+	    // changing size.
+	    int oldsize = 0, size = paramvars.size() + notparamvars.size();
+	    while (size > oldsize) {
+		for (Iterator it=relevant.iterator(); it.hasNext(); )
+		    ((Quad)it.next()).accept(this);
+		oldsize = size;
+		size = paramvars.size() + notparamvars.size();
+	    }
+	    // clean up paramvars set by removing not-param variables.
+	    paramvars.removeAll(notparamvars);
+	    // done!
+	}
+	// lattice: don't know, this, not-this.  -->move-->
+	// presence in 'not-this' overrides presence in 'this'.
+	// sets can only grow.
+	public void visit(Quad q) {
+	    /* look for overwrites, which are always not 'this' */
+	    notparamvars.addAll(q.defC());
+	}
+	public void visit(METHOD q) {
+	    // param 'which' is 'param'; all others are 'not-param'
+	    for (int i=0; i<q.paramsLength(); i++)
+		if (i==which_param)
+		    paramvars.add(q.params(i));
+		else
+		    notparamvars.add(q.params(i));
+	}
+	public void visit(MOVE q) {
+	    relevant.add(q);
+	    if (paramvars.contains(q.src()) &&
+		!notparamvars.contains(q.src()))
+		paramvars.add(q.dst());
+	    else
+		notparamvars.add(q.dst());
+	}
+	public void visit(SIGMA q) {
+	    relevant.add(q);
+	    // get rid of overwrites.
+	    Set s = new HashSet(q.defC());
+	    for (int i=0; i<q.numSigmas(); i++)
+		s.removeAll(Arrays.asList(q.dst(i)));
+	    notparamvars.addAll(s);
+	    // now look for src==this.
+	    for (int i=0; i<q.numSigmas(); i++)
+		if (notparamvars.contains(q.src(i)))
+		    notparamvars.addAll(Arrays.asList(q.dst(i)));
+		else if (paramvars.contains(q.src(i)))
+		    // found! add all dst to paramvars.
+		    paramvars.addAll(Arrays.asList(q.dst(i)));
+	}
+	public void visit(PHI q) {
+	    relevant.add(q);
+	    // phi(x,y) is 'param' iff x *and* y are 'param' and
+	    // neither x nor y is *not* 'param'
+	    for (int i=0; i<q.numPhis(); i++)
+		for (int j=0; j<q.arity(); j++)
+		    if (notparamvars.contains(q.src(i, j)))
+			notparamvars.add(q.dst(i));
+		    else if (paramvars.contains(q.src(i, j)))
+			paramvars.add(q.dst(i));
+	}
+    }
+}
