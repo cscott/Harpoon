4a5d81ba277170b9de7ae4e3ab3774ef397573bf
Alexandru SALCIANU <salcianu@alum.mit.edu>
2002-04-11 00:03:42 +0000

    SSICallGraph implements harpoon.Analysis.Quads.CallGraph for SSI code. It is basically a wrapper around CallGraphImpl2 to hide the different calling format for CallGraphImpl2.calls(cs, rd, etm). To avoid creating ReachingDefs and ExactTypeMap multiple times, when we call it for the first time with a call site from a method hm, we analyze all the call sites from that method and cache the results.  Has caching for both calls(hm) and calls(hm, cs);
    This way one can use the power of CallGraphImpl2 in a regular
    harpoon.Analysis.Quads.CallGraph (CallGraphImpl2 itself implements
    just harpoon.Analysis.CallGraph).
    
    ----------------------------------------------------------------------
    Analysis/Quads/SSICallGraph.java CVS:
    ----------------------------------------------------------------------

---
+++ Analysis/Quads/SSICallGraph.java
@@ -0,0 +1,101 @@
+// SSICallGraph.java, created Mon Apr  8 22:51:08 2002 by salcianu
+// Copyright (C) 2000 Alexandru SALCIANU <salcianu@MIT.EDU>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Analysis.Quads;
+
+import java.util.Set;
+import java.util.Vector;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+
+import harpoon.ClassFile.HMethod;
+import harpoon.ClassFile.HCodeFactory;
+import harpoon.ClassFile.HCode;
+
+import harpoon.Analysis.ClassHierarchy;
+import harpoon.Analysis.ReachingDefs;
+import harpoon.Analysis.SSxReachingDefsImpl;
+import harpoon.Analysis.Quads.SCC.SCCAnalysis;
+import harpoon.Analysis.Maps.ExactTypeMap;
+import harpoon.Analysis.Quads.TypeInfo;
+import harpoon.IR.Quads.Code;
+
+import harpoon.IR.Quads.CALL;
+import harpoon.IR.Quads.Quad;
+import harpoon.IR.Quads.QuadKind;
+import harpoon.IR.Quads.QuadSSI;
+
+import harpoon.Util.Util;
+
+import harpoon.Analysis.PointerAnalysis.Debug;
+
+/**
+ * <code>SSICallGraph</code>
+ * 
+ * @author  Alexandru SALCIANU <salcianu@MIT.EDU>
+ * @version $Id: SSICallGraph.java,v 1.1 2002-04-11 00:03:42 salcianu Exp $
+ */
+public class SSICallGraph implements CallGraph {
+
+    private CallGraphImpl2 cg;
+    private ClassHierarchy ch;
+    private HCodeFactory  hcf;
+    
+    /** Creates a <code>SSICallGraph</code>. */
+    public SSICallGraph(ClassHierarchy ch, HCodeFactory hcf) {
+	assert hcf.getCodeName().equals(QuadSSI.codename) :
+	    "works only with an SSI code factory";
+	this.ch  = ch;
+	this.hcf = hcf;
+	cg = new CallGraphImpl2(ch, hcf);
+    }
+    
+    public HMethod[] calls(final HMethod hm) {
+	return cg.calls(hm);
+    }
+
+
+    public HMethod[] calls(final HMethod hm, final CALL cs) {
+	return calls(cs);
+    }
+
+
+    public HMethod[] calls(final CALL cs) {
+	HMethod[] retval = (HMethod[]) cs2callees.get(cs); 
+	if(retval != null)
+	    return retval;
+
+	// For efficiency reasons, we compute the callees for all call
+	// sites; this way we construct the auxiliary objects
+	// "ReachingDefs rd" and "ExactTypeMap etm" once for each
+	// method.
+	HCode hcode = Util.quad2code(cs);
+	ReachingDefs rd  = new SSxReachingDefsImpl(hcode);
+	ExactTypeMap etm = new TypeInfo((Code) hcode); 
+
+	CALL[] calls = Util.selectCALLs(hcode);
+	for(int i = 0; i < calls.length; i++)
+	    cs2callees.put(calls[i], cg.calls(calls[i], rd, etm));
+
+	return (HMethod[]) cs2callees.get(cs);
+    }
+    private Map cs2callees = new HashMap();
+
+
+    public CALL[] getCallSites(final HMethod hm) {
+	CALL[] retval = (CALL[]) cache_cs.get(hm);
+	if(retval == null) {
+	    retval = Util.selectCALLs(hcf.convert(hm));
+	    cache_cs.put(hm, retval);
+	}
+	return retval;
+    }
+    final private Map cache_cs = new HashMap();
+
+
+    public Set callableMethods() {
+	return ch.callableMethods();
+    }
+
+}
