7043ddb06aadfb47e321a641599f389e5c9626a9
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-07-12 03:52:18 +0000

    Use standard JDK1.4 'assert' statement instead of home-spun ASSERT() method in FIFO class imported with java 1.5 lexer.

--- Tools/Annotation/Lex/FIFO.java
+++ Tools/Annotation/Lex/FIFO.java
@@ -28,20 +28,20 @@ class FIFO {
       end = backing.length-1;
       backing = nbacking;
     }
-    ASSERT(!isFull());
+    assert !isFull();
     backing[end++] = o;
     if (end == backing.length)
       end = 0;
-    ASSERT(!isEmpty());
+    assert !isEmpty();
   }
   public java_cup.runtime.Symbol get() throws java.io.IOException {
     if (isEmpty())
       put(getter.next());
-    ASSERT(!isEmpty());
+    assert !isEmpty();
     java_cup.runtime.Symbol o = backing[start++];
     if (start == backing.length)
       start = 0;
-    ASSERT(!isFull());
+    assert !isFull();
     return o;
   }
   public java_cup.runtime.Symbol peek(int i) throws java.io.IOException {
@@ -49,15 +49,12 @@ class FIFO {
       put(getter.next());
     int index = start+i;
     if (index >= backing.length) index -= backing.length;
-    ASSERT(0<= index && index < backing.length);
+    assert 0<= index && index < backing.length;
     return backing[index];
   }
   abstract static class Getter {
     abstract java_cup.runtime.Symbol next()
       throws java.io.IOException;
-  }
-  private static void ASSERT(boolean b) {
-    if (!b) throw new RuntimeException();
   }
 }
 	
