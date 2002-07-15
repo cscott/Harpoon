0e029053a8b9d35f8e40a966fa7ae286dbec4dec
Wes Beebee <wbeebee@mit.edu>
2002-07-15 19:38:14 +0000

    Whoops - well I think that throwing the exception should be enough warning...

--- ScopedCycleException.java
+++ ScopedCycleException.java
@@ -6,13 +6,11 @@ public class ScopedCycleException extends Exception
 
     /** A constructor for <code>ScopedCycleException</code>. */
     public ScopedCycleException() {
-	assert false: "ScopedCycleException should never be thrown.";
 	super();
     }
 
     /** A descriptive constructor for <code>ScopedCycleException</code>. */
     public ScopedCycleException(String s) {
-	assert false: "ScopedCycleException should never be thrown: " + s;
 	super(s);
     }
 }
