e5159295963ad59b1f0100d273699a52b8ea3fc0
Dumitru Daniliuc <dum_dani@mit.edu>
2002-07-17 22:37:29 +0000

    Implemented (with scott's help, of course...) a native method for SizeEstimator.java from Harpoon/Realtime

---
+++ src/realtime/SizeEstimator.c
@@ -0,0 +1,8 @@
+#include "SizeEstimator.h"
+
+JNIEXPORT jlong JNICALL Java_javax_realtime_SizeEstimator_objSize
+(JNIEnv *env, jclass claz, jclass objClaz) {
+  jlong result = (jlong)FNI_ClassSize(objClaz);
+  result += ((result % 16) == 0) ? 0 : (16 - (result % 16));
+  return result;
+}
