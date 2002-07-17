3407494d1eb4083131d2b55097b8e929ba8c7d25
Dumitru Daniliuc <dum_dani@mit.edu>
2002-07-17 18:38:49 +0000

    Some small changes to SizeEstimator.java

--- SizeEstimator.java
+++ SizeEstimator.java
@@ -25,20 +25,24 @@ public final class SizeEstimator {
      *  when estimating the size of the <code>MemoryArea</code>.
      */
     public void reserve(Class c, int n) {
-	// TODO
+	// objSize(Class) is not implemented yet.
+	//	estimate += n * objSize(c);
     }
 
     /** Take into account an additional instance of <code>SizeEstimator s</code>
      *  when estimating the size of the <code>MemoryArea</code>.
      */
     public void reserve(SizeEstimator s) {
-	// TODO
+	estimate += s.estimate;
     }
 
     /** Take into account additional <code>n</code> instances of <code>SizeEstimator s</code>
      *  when estimating the size of the <code>MemoryArea</code>.
      */
     public void reserve(SizeEstimator s, int n) {
-	// TODO
+	estimate += n * s.estimate;
     }
+
+    // Not implemented yet
+    //    public native static long objSize(Class c);
 }
