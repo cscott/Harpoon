b3d43a79042b8fb02cc66809514bd1ae033e12c1
Wes Beebee <wbeebee@mit.edu>
2002-08-16 16:55:16 +0000

    We don't necessarily want the world to know the IP of the car.

--- JavaCar/TestServer.java
+++ JavaCar/TestServer.java
@@ -1,6 +1,6 @@
 public class TestServer {
     public static void main(String args[]) {
-	IPaqServoController ip = new IPaqServoController("18.24.6.219");
+	IPaqServoController ip = new IPaqServoController(args[0]);
 	while (true) {
 	    for (int i=1; i<255; i++) ip.move(1, i);
 	    for (int i=254; i>=1; i--) ip.move(1, i);
