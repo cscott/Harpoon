ab22d0407393d90f6ac85f8d1af28f2eb6eea232
Wes Beebee <wbeebee@mit.edu>
2002-08-26 15:31:45 +0000

    New version from Paul.

--- JavaCar/JavaCar.java
+++ JavaCar/JavaCar.java
@@ -7,15 +7,15 @@ public class JavaCar extends JFrame{
     public static int WIDTH=500;
     public static int HEIGHT=300;
     public static boolean p;
-    public JavaCar (String a, String ip) {
+    public JavaCar (String a) {
 	super(a);
         getContentPane().setLayout(new GridLayout(5 ,3));
 	JButton con = new JButton("Connect");
 	final JTextField u = new JTextField();
-        final IPaqServoController foo = new IPaqServoController(ip); 
+        final IPaqServoController foo = new IPaqServoController("18.24.6.250"); 
         	con.addActionListener(new ActionListener(){
 		public void actionPerformed(ActionEvent e) {
-		    foo.move(1,128);
+		    foo.move(1,126);
 		    foo.move(2,128);
 		}
 	    });
@@ -33,8 +33,8 @@ public class JavaCar extends JFrame{
        public void keyReleased(KeyEvent e) {
 	    if (e.getKeyCode()==38) {foo.move(2,128);}else{
 		if(e.getKeyCode()==40) {foo.move(2,128);}else{
-		    if(e.getKeyCode()==39){foo.move(1,128);}else{
-		      	if(e.getKeyCode()==37){foo.move(1,128);}
+		    if(e.getKeyCode()==39){foo.move(1,126);}else{
+		      	if(e.getKeyCode()==37){foo.move(1,126);}
 		    }
 		}
 	    }
@@ -50,7 +50,7 @@ public class JavaCar extends JFrame{
     }
     
     public static void main(String s[]){
-	JavaCar frame = new JavaCar("Connection info", s[0]);
+	JavaCar frame = new JavaCar("Connection info");
 	frame.addWindowListener(new WindowAdapter(){
 		public void windowClosing(WindowEvent e) {
 		    System.exit(0);}
