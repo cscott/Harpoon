e584b4f1a7a7ba94119daac0b52819722357bfc2
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-08-30 23:27:09 +0000

    As per Patrick's advice, create a 'SnapshotIterator' class which can be used when we want to mutate a collection underneath an iterator without the iterator noticing.  So the old:    Quad[] ql = (Quad[]) hc.getElements();    for (int i=0; i<ql.length; i++)        ql[i].accept(visitor); pattern can safely become:    for(Iterator<X> it=new SnapshotIterator(hc.getElementsI()); it.hasNext(); )        it.next().accept(visitor);
    Note that this transformation is *not* safe without the construction of
    the SnapshotIterator, as the iterator returned by hc.getElementsI() will
    get *very* confused if your visitor starts modifying elements underneath
    it.  The next commit will make the iterator "fail-fast" (see
    http://java.sun.com/docs/books/tutorial/collections/implementations/general.html
    ) so that bugs of this sort are detected.
    
    Also modified ReverseIterator so that it extends SnapshotIterator and does
    not duplicate code.

--- Util/Collections/ReverseIterator.java
+++ Util/Collections/ReverseIterator.java
@@ -10,20 +10,18 @@ import java.util.List;
 
 /**
  * A <code>ReverseIterator</code> iterates through an <code>Iterator</code>
- * in reverse order.
+ * in reverse order.  It extends <code>SnapshotIterator</code>, so is
+ * insensitive to changes in the underlying collection once construction
+ * is complete.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ReverseIterator.java,v 1.1 2002-08-30 22:15:06 cananian Exp $
+ * @version $Id: ReverseIterator.java,v 1.2 2002-08-30 23:27:16 cananian Exp $
  */
-public class ReverseIterator<E> extends UnmodifiableIterator<E> {
-    final ArrayList<E> l = new ArrayList<E>();
-    int i;
-
+public class ReverseIterator<E> extends SnapshotIterator<E> {
     /** Creates a <code>ReverseIterator</code> of <code>Iterator</code>
      *  <code>it</code>. */
     public ReverseIterator(Iterator<E> it) {
-	while (it.hasNext()) l.add(it.next());
-	l.trimToSize();
+	super(it);
 	i = l.size()-1;
     }
     public boolean hasNext() { return ( i >= 0 ); }
