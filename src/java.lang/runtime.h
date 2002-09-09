a046f88f8ae42541c942f34a97022f1ad42abad0
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-09-09 00:00:19 +0000

    Split off library-independent bits from sunjdk/java.lang/java_lang_Runtime.c into java.lang/runtime.h.
    @SPLIT: sunjdk/java.lang/java_lang_Runtime.c java.lang/runtime.h@

---
+++ src/java.lang/runtime.h
@@ -0,0 +1,66 @@
+#ifndef INCLUDED_FNI_RUNTIME_H
+#define INCLUDED_FNI_RUNTIME_H
+
+#include "config.h"
+#ifdef BDW_CONSERVATIVE_GC
+# include "gc.h"
+#endif
+
+#include <assert.h>
+#include <unistd.h>
+
+static inline
+void fni_runtime_exitInternal(JNIEnv *env, jint status) {
+#ifdef WITH_STATISTICS
+  /* print out collected statistics */
+  { void print_statistics(void); print_statistics(); }
+#endif
+    exit(status);
+}
+
+static inline
+jlong fni_runtime_freeMemory(JNIEnv *env) {
+#ifdef BDW_CONSERVATIVE_GC
+  return (jlong) GC_get_free_bytes();
+#elif defined(WITH_PRECISE_GC)
+  return precise_free_memory();
+#else
+  assert(0/*unimplemented*/);
+#endif
+}
+
+static inline
+jlong fni_runtime_totalMemory(JNIEnv *env) {
+#ifdef BDW_CONSERVATIVE_GC
+  return (jlong) GC_get_heap_size();
+#elif defined(WITH_PRECISE_GC)
+  return precise_get_heap_size();
+#else
+  assert(0/*unimplemented*/);
+#endif
+}
+
+static inline
+void fni_runtime_gc(JNIEnv *env) {
+#ifdef BDW_CONSERVATIVE_GC
+  GC_gcollect();
+#elif defined(WITH_PRECISE_GC)
+  precise_collect();
+#else
+  assert(0/*unimplemented*/);
+#endif
+}
+
+static inline
+void fni_runtime_runFinalization(JNIEnv *env) {
+#ifdef BDW_CONSERVATIVE_GC
+  GC_invoke_finalizers();
+#elif defined(WITH_PRECISE_GC)
+  /* unimplemented */
+  printf("WARNING: Finalization not implemented for precise GC.\n");
+#else
+  assert(0/*unimplemented*/);
+#endif
+}
+
+#endif /* INCLUDED_FNI_RUNTIME_H */
