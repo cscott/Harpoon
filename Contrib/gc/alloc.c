2d3aaf66e367f3750a686a52938e2a1c887b7bad
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-09-23 18:46:09 +0000

    Merged FLEX-local changes with BDW gc version 6.1 final.  No conflicts!

--- Contrib/gc/alloc.c
+++ Contrib/gc/alloc.c
@@ -628,6 +628,7 @@ void GC_finish_collection()
 	  GC_print_address_map();
 	}
 #   endif
+    COND_DUMP;
     if (GC_find_leak) {
       /* Mark all objects on the free list.  All objects should be */
       /* marked when we're done.				   */
@@ -771,6 +772,7 @@ void GC_gcollect GC_PROTO(())
 {
     GC_notify_full_gc();
     (void)GC_try_to_collect(GC_never_stop_func);
+    if (GC_have_errors) GC_print_all_errors();
 }
 
 word GC_n_heap_sects = 0;	/* Number of sections currently in heap. */
