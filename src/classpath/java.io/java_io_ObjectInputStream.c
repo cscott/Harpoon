2d4602d2c0e33ac32a8d46a9359ed737779f7f26
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-09-24 17:19:38 +0000

    Style cleanups.

--- src/classpath/java.io/java_io_ObjectInputStream.c
+++ src/classpath/java.io/java_io_ObjectInputStream.c
@@ -1,12 +1,15 @@
+#include <jni.h>
+#include "config.h"
 #include "java_io_ObjectInputStream.h"
 
 #ifdef WITH_INIT_CHECK
-JNIEXPORT jobject JNICALL
-Java_java_io_ObjectInputStream_allocateObject_00024_00024initcheck( JNIEnv * env,
-								    jobject self,
-								    jclass clazz )
-{
+#include "../../java.lang.reflect/reflect-util.h"
+#endif /* WITH_INIT_CHECK */
+
+#ifdef WITH_INIT_CHECK
+JNIEXPORT jobject JNICALL Java_java_io_ObjectInputStream_allocateObject_00024_00024initcheck
+  (JNIEnv *env, jobject self, jclass clazz) {
   if (!REFLECT_staticinit(env, clazz)) return NULL; // static init failed
   return Java_java_io_ObjectInputStream_allocateObject(env, self, clazz);
 }
-#endif
+#endif /* WITH_INIT_CHECK */
--- src/classpath/java.io/java_io_ObjectInputStream.h
+++ src/classpath/java.io/java_io_ObjectInputStream.h
@@ -1,17 +1,12 @@
 #include "config.h"
-
-#ifdef WITH_INIT_CHECK
-#include "../../java.lang.reflect/reflect-util.h"
+#include <jni.h>
 
 /* From classpath sources */
-JNIEXPORT jobject JNICALL
-Java_java_io_ObjectInputStream_allocateObject(JNIEnv *env,
-					      jobject self,
-					      jclass clazz);
+JNIEXPORT jobject JNICALL Java_java_io_ObjectInputStream_allocateObject
+  (JNIEnv *env, jobject self, jclass clazz);
 
-JNIEXPORT jobject JNICALL
-Java_java_io_ObjectInputStream_allocateObject_00024_00024initcheck(JNIEnv *env,
-								   jobject self,
-								   jclass clazz);
+#ifdef WITH_INIT_CHECK
+JNIEXPORT jobject JNICALL Java_java_io_ObjectInputStream_allocateObject_00024_00024initcheck
+  (JNIEnv *env, jobject self, jclass clazz);
 #endif
 
