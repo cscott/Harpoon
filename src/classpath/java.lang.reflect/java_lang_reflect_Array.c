0fe72077cfb5969c4327623b54f23b9388c6fb9d
C. Scott Ananian <cananian@alumni.princeton.edu>
2002-09-24 17:48:18 +0000

    Style cleanup.
    Also fix bug in $$initcheck version of Array.createObjectArray() -- we need
    to initialize the base *component type* of the array, not the array type.

--- src/classpath/java.lang.reflect/java_lang_reflect_Array.c
+++ src/classpath/java.lang.reflect/java_lang_reflect_Array.c
@@ -1,11 +1,24 @@
+#include <jni.h>
+#include "config.h"
 #include "java_lang_reflect_Array.h"
+#include "../../java.lang/class.h" /* for fni_class_getComponentType() */
 
 #ifdef WITH_INIT_CHECK
-JNIEXPORT jobject JNICALL 
-Java_java_lang_reflect_Array_createObjectArray_00024_00024initcheck
-(JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength) {
-  if (!REFLECT_staticinit(env, arrayType)) return NULL;
+#include "../../java.lang.reflect/reflect-util.h"
+#endif /* WITH_INIT_CHECK */
+
+#ifdef WITH_INIT_CHECK
+JNIEXPORT jobject JNICALL Java_java_lang_reflect_Array_createObjectArray_00024_00024initcheck
+  (JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength) {
+  /* find base component type */
+  jclass baseType = arrayType;
+  while (fni_class_isArray(env, baseType))
+    baseType = fni_class_getComponentType(env, baseType);
+  /* call the static initializer of the base type */
+  if (!REFLECT_staticinit(env, baseType))
+    return NULL; // exception in static initializer
+  /* okay, *now* call the standard createObjectArray() method */
   return Java_java_lang_reflect_Array_createObjectArray
     (env, thisClass, arrayType, arrayLength);
 }
-#endif
+#endif /* WITH_INIT_CHECK */
--- src/classpath/java.lang.reflect/java_lang_reflect_Array.h
+++ src/classpath/java.lang.reflect/java_lang_reflect_Array.h
@@ -1,14 +1,10 @@
 #include "config.h"
+#include <jni.h>
+
+JNIEXPORT jobject JNICALL Java_java_lang_reflect_Array_createObjectArray
+  (JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength);
 
 #ifdef WITH_INIT_CHECK
-#include "../../java.lang.reflect/reflect-util.h" 
-
-JNIEXPORT jobject JNICALL
-Java_java_lang_reflect_Array_createObjectArray
-(JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength);
-
-JNIEXPORT jobject JNICALL
-Java_java_lang_reflect_Array_createObjectArray_00024_00024initcheck
-(JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength);
-
-#endif
+JNIEXPORT jobject JNICALL Java_java_lang_reflect_Array_createObjectArray_00024_00024initcheck
+  (JNIEnv *env, jclass thisClass, jclass arrayType, jint arrayLength);
+#endif /* WITH_INIT_CHECK */
