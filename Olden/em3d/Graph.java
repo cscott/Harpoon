da6e002d530ae53764792ffc3df7226e7819dd0e
Wes Beebee <wbeebee@mit.edu>
2002-10-10 14:43:42 +0000

    The famous Olden benchmarks...

---
+++ Olden/em3d/Graph.java
@@ -0,0 +1,18 @@
+
+/** 
+ * A class that represents the irregular bipartite graph used in
+ * EM3D.  The graph contains two linked structures that represent the
+ * E nodes and the N nodes in the application.
+ **/
+class Graph
+{
+  /**
+   * Nodes that represent the electrical field.
+   **/
+  Node eNodes;
+  /**
+   * Nodes that representhe the magnetic field.
+   **/
+  Node hNodes;
+
+}
