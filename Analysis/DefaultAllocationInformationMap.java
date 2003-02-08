319b7aba2b21d089a8445af0ef28434569fc3762
Alexandru SALCIANU <salcianu@alum.mit.edu>
2003-02-08 19:36:07 +0000

    New class: combination of AllocationInformationMap and DefaultAllocationInformation.

---
+++ Analysis/DefaultAllocationInformationMap.java
@@ -0,0 +1,39 @@
+// DefaultAllocationInformationMap.java, created Sat Feb  8 14:21:19 2003 by salcianu
+// Copyright (C) 2000 Alexandru Salcianu <salcianu@MIT.EDU>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+package harpoon.Analysis;
+
+import harpoon.Analysis.Maps.AllocationInformation.AllocationProperties;
+import harpoon.Analysis.AllocationInformationMap;
+import harpoon.Analysis.DefaultAllocationInformation;
+
+import harpoon.ClassFile.HCodeElement;
+
+/** <code>DefaultAllocationInformationMap</code> is a combination of
+    <code>AllocationInformationMap</code> and
+    <code>DefaultAllocationInformation</code>.  Like
+    <code>AllocationInformationMap</code>, it is a growable map from
+    allocation sites to their corresponding allocation properties.
+    However, if no <code>AllocationProperties</code> object is
+    associated with a specific allocation site, <code>query</code>
+    will return a default one, as indicated by
+    <code>DefaultAllocationInformation</code>.
+
+    @author  Alexandru Salcianu <salcianu@MIT.EDU>
+    @version $Id: DefaultAllocationInformationMap.java,v 1.1 2003-02-08 19:36:07 salcianu Exp $ */
+public class DefaultAllocationInformationMap extends AllocationInformationMap
+    implements java.io.Serializable {
+    
+    /** Creates a <code>DefaultAllocationInformationMap</code>. */
+    public DefaultAllocationInformationMap() { }
+
+    /** Returns the <code>AllocationProperties</code> for an
+        allocation site.  If no such object exists, it returns a
+        default <code>AllocationProperties</code> object. */
+    public AllocationProperties query(HCodeElement allocationSite) {
+	AllocationProperties ap = super.query(allocationSite);
+	if(ap == null)
+	    ap = DefaultAllocationInformation.SINGLETON.query(allocationSite);
+	return ap;
+    }
+}
