e56a343fb7b8bf7176eb80bf366b769731cf652c
Wes Beebee <wbeebee@mit.edu>
2003-03-03 17:33:12 +0000

    Add P3 or P6 Save support

--- src/graph/Save.java
+++ src/graph/Save.java
@@ -7,7 +7,7 @@ import imagerec.util.ImageDataManip;
 
 /**
  * {@link Save} will save images that are sent to it.  
- * Currently only <code>.ppm.gz</code> format is supported.
+ * Currently only <code>.ppm.gz</code> format (with P3 or P6 PPM's) is supported.
  *
  * @see Load
  *
@@ -18,6 +18,7 @@ public class Save extends Node {
     private String filePrefix;
     private boolean inorder;
     private int count=0;
+    private boolean P3;
 
     /** Construct a new {@link Save} node that will save a series of images.
      *  The filenames are <code>filePrefix.#</code> where <code>#</code> represents
@@ -44,9 +45,21 @@ public class Save extends Node {
      * @param inorder Whether to use the <code>id</code> or count images coming in.
      */
     public Save(String filePrefix, boolean inorder) {
+	this(filePrefix, inorder, false);
+    }
+
+    /*
+     * {@link Save} can save in P3 (ASCII) or P6 (RAW) PPM mode 
+     *
+     * @param filePrefix The beginning of the fileName to save (see above).
+     * @param inorder Whether to use the <code>id</code> or count images coming in.
+     * @param P3 Whether the file is to be written in P3 or P6 format.
+     */
+    public Save(String filePrefix, boolean inorder, boolean P3) {
 	super();
 	this.filePrefix = filePrefix;
 	this.inorder = inorder;
+	this.P3 = P3;
     }
 
     /** Save the images as they come in.
@@ -55,6 +68,6 @@ public class Save extends Node {
      */
     public synchronized void process(ImageData id) {
 	System.out.println("Saving image "+filePrefix+"."+(inorder?id.id:count));
-	ImageDataManip.writePPM(id, filePrefix+"."+(inorder?id.id:(count++)));
+	ImageDataManip.writePPM(id, filePrefix+"."+(inorder?id.id:(count++)), P3);
     }
 }
