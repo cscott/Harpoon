0a41902c8d78927ffcac1ce4a5c8293344cd1517
Wes Beebee <wbeebee@mit.edu>
2003-03-24 19:40:51 +0000

    Changed Pause node so that it pauses before image send to next. - Benji

--- src/graph/Pause.java
+++ src/graph/Pause.java
@@ -19,17 +19,23 @@ public class Pause extends Node {
 	super(out);
     }
     
-    /** <code>process</code> an image by sending it to the next node, the
+    /** <code>process</code> an image by sending it to the next node, then
      *  asking for input from {@link System}<code>.in</code>.
      *
      *  @param id The image to process.
      */
     public synchronized void process(ImageData id) {
-	super.process(id);
+	//Why pause afterwards? Not intuitive.
+	//I changed code so the pause comes
+	//before the image is sent on to other nodes
+	// -- Benji
+	//super.process(id);
 	try {
 	    System.in.read();
 	} catch (Exception e) {
 	    throw new Error(e);
 	}
+	super.process(id);
+	
     }
 }
