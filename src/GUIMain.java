c1d12bec79d64e20203c04f3a649905d8596e0b5
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-04-17 19:33:33 +0000

    Changed Wes' first pipeline to use Node.link(), Node.linkL(), Node.linkR() methods so it looks prettier.
    Added two more pipelines: identification by color
                              identification by size then color
    
    Modified surrounding code to support more than 1 pipeline.

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -18,34 +18,187 @@ public class GUIMain {
      *  @param args foo
      */
     public static void main(String args[]) {
-	if (args.length<1) {
+	if (args.length<3) {
 	    System.out.println("Usage: java -jar GUI.jar <'pause'|'nopause'> <'corba'|'nocorba'> <pipeline #> [CORBA options]");
 	    System.exit(-1);
 	}
 
-	if (Integer.parseInt(args[2])!=1) {
-	    System.out.println("Pipeline #"+args[2]+" not implemented yet.");
+	int pipelineNumber = 0;
+	try {
+	    pipelineNumber = Integer.parseInt(args[2]);
+	}
+	catch (NumberFormatException nfe) {
+	    System.out.println("Error: Pipeline argument was not a valid integer.");
 	    System.exit(-1);
 	}
 
-	boolean pause = args[0].equalsIgnoreCase("pause");
-	boolean corba = args[1].equalsIgnoreCase("corba");
-	
-	Node command = new Command(Command.GET_IMAGE, null);
-	Node circle = new Circle(new Display("identified"), command);
-	Node command2 = new Command(Command.RETRIEVED_IMAGE, circle);
-	if (corba) circle = new Node(new RangeFind(new Alert(args)), circle);	
-	Node cache = new Cache(2, new Copy(new RobertsCross(new Thresholding(
-		     new Label(null, circle)))), command2);
-	command.setLeft(cache);
-	Node n1 = new Node(new Display("original"), cache);
-	if (pause) n1 = new Pause(n1);
-	n1 = corba?(Node)(new ATR(args, n1)):(Node)(new Load("GUI.jar", "tank.gz", 533, n1));
-
-	while (true) {
-	    n1.run();
+	if (pipelineNumber <= 0) {
+	    System.out.println("Error: Pipeline # must be > 0 (value was '"+pipelineNumber+")");
+	    System.exit(-1);
 	}
-	
-    }
+	else {
+	    boolean usePause = args[0].equalsIgnoreCase("pause");
+	    boolean useCorba = args[1].equalsIgnoreCase("corba");
+	    Node pause;
+	    //if pause is requested, then 'pause' is made to be a Pause node,
+	    //otherwise, it becomes a dummy Node, and ImageDatas will just be passed through
+	    //with minimal performance impact
+	    if (usePause) {
+		//'-1.0' says to wait for user input
+		//'1' says to pause before every image
+		pause = new Pause(-1.0, 1, null);
+	    }
+	    else {
+		pause = new Node();
+	    }
+	    Node pipe = null;
+	    if (pipelineNumber == 1) {
+		Node origDisp = new Display("Original");
+		Node endDisp = new Display("final");
+		Node circleCache = new Cache(1, null, null);
+		Node copy = new Copy(null);
+		Node robCross = new RobertsCross(null);
+		Node thresh = new Thresholding(null);
+		Label label = new Label(Label.DEFAULT1, null, null);
+		label.findOneObject(true);
+		Node circle = new Circle(null, null);
+		Node retrCmd = new Command(Command.RETRIEVED_IMAGE, null);
+		Node getCmd = new Command(Command.GET_IMAGE, null);
+		Node n = new Node();
+		Node n2 = new Node();
 
-}
+		pipe = 
+		    n.link(pause.linkL(origDisp),
+			   circleCache.link(copy.linkL(robCross.linkL(thresh.linkL(label.link(null,
+											      circle.link(endDisp,
+													  getCmd.linkL(circleCache)))))),
+					    retrCmd.linkL(circle)));
+		//if we are using corba,
+		//insert range tracking and alert nodes between label and circle nodes.
+		//and use ATR node instead of Load node
+		Node head;
+		if (useCorba) {
+		    Node range = new RangeFind(null);
+		    Node alert = new Alert(args);
+		    label.linkR(n2.link(range.linkL(alert),
+				       circle));
+		    head = new ATR(args, pipe);
+		}
+		else {
+		    head = new Load("GUI.jar", "tank.gz", 533, pipe);
+		}
+		pipe = head.linkL(n);
+	    }
+	    else if (pipelineNumber == 2) {
+		Node origDisp = new Display("Original");
+		Node endDisp = new Display("final");
+		Node labelBlue = new LabelBlue(null, null);
+		Node circleCache = new Cache(1, null, null);
+		Node circle = new Circle(null, null);
+		Node retrCmd = new Command(Command.RETRIEVED_IMAGE, null);
+		Node getCmd = new Command(Command.GET_IMAGE, null);
+		Node n = new Node();
+		Node n2 = new Node();
+		Node copy = new Copy(null);
+		pipe =
+		    n.link(origDisp,
+			   pause.linkL(circleCache.link(copy.linkL(labelBlue.link(null,
+										  circle.link(endDisp,
+											      getCmd.linkL(circleCache)))),
+							retrCmd.linkL(circle))));
+		//if we are using corba,
+		//insert range tracking and alert nodes between labelBlue and circle nodes.
+		//and use ATR node instead of Load node
+		Node head;
+		if (useCorba) {
+		    Node range = new RangeFind(null);
+		    Node alert = new Alert(args);
+		    Node n3 = new Node();
+		    labelBlue.linkR(n3.link(range.linkL(alert),
+				       circle));
+		    head = new ATR(args, pipe);
+		}
+		else {
+		    head = new Load("GUI.jar", "tank.gz", 533, pipe);
+		}
+		pipe = head.linkL(n);
+		
+	    }
+	    else if (pipelineNumber == 3) {
+		Node origDisp = new Display("Original");
+		Node endDisp = new Display("final");
+
+		Node circleCache = new Cache(1, null, null);
+		Node cleanCache = new Cache(1, null, null);
+		Node labelSmCache = new Cache(1, null, null);
+
+		Node robCross = new RobertsCross(null);
+		Node thresh = new Thresholding(null);
+		Node hyst = new Hysteresis(null);
+		Label label = new Label(null, null);
+
+		LabelBlue labelBlue = new LabelBlue(null, null);
+		labelBlue.calibrateAndProcessSeparately(true);
+
+		Node circle = new Circle(null, null);
+		Node copy = new Copy(null);
+
+		Node calibCmd = new Command(Command.CALIBRATION_IMAGE, null);
+		Node noneCmd = new Command(Command.NONE, null);
+		Node noneCmd2 = new Command(Command.NONE, null);
+		Node retrCmd = new Command(Command.RETRIEVED_IMAGE, null);
+		Node getCmd = new Command(Command.GET_IMAGE, null);
+		Node getCropCmd = new Command(Command.GET_CROPPED_IMAGE, null);
+		Node getLabelSmCmd = new Command(Command.GET_IMAGE, null);
+
+		
+		Node n = new Node();
+		Node n2 = new Node();
+		Node n3 = new Node();
+		pipe = 
+		    n.link(pause,
+			   origDisp.linkL(circleCache.link(cleanCache.link(n2.link(calibCmd.linkL(labelBlue),
+										   noneCmd.linkL(copy.linkL(robCross.link(null,
+															  thresh.link(null,
+																      hyst.link(null,
+																		label.link(null,
+																			   labelSmCache.linkL(getCropCmd.linkL(cleanCache))))))))),
+									   n3.link(null,
+										   labelBlue.link(null,
+												  getLabelSmCmd.link(labelSmCache,
+														     noneCmd2.linkL(circle.linkR(getCmd.linkL(circleCache))))))),
+							   retrCmd.linkL(circle.linkL(endDisp)))));
+		//if we are using corba,
+		//add thinning, range finding, and alert to LabelSmCache's right-hand branch
+		//and use ATR node instead of Load node
+		Node head;
+		if (useCorba) {
+		    Node thin = new Thinning(Thinning.BLUE, null);
+		    Node range = new RangeFind(null);
+		    Node alert = new Alert(args);
+		    Node n4 = new Node();
+		    labelSmCache.linkR(thin.linkL(n4.link(range.linkL(alert),
+							  circle)));
+		    head = new ATR(args, pipe);
+		}
+		else {
+		    head = new Load("GUI.jar", "tank.gz", 533, pipe);
+		}
+		pipe = head.linkL(n);
+		
+	    }
+	    else {
+		System.out.println("Error: Pipeline #"+pipelineNumber+" not implemented yet.");
+		System.exit(-1);
+	    }
+	    while (true) {
+		pipe.run();
+	    }
+	    
+	    
+	}//else [if(pipelineNumber <= 0)]
+	
+
+    }//public static void main()
+
+}//public class GUIMain
