0ccd5ab7be795f4b3cabf0cbb3070728d0a34ea2
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-04-17 18:22:15 +0000

    Modified first working pipeline so it uses the new Node.link(), Node.linkR(), Node.linkL() methods to look cleaner.
    Added 2 more pipelines: identification by color
    		        indentification by size, then color
    
    Modified surrounding code to support new pipelines.

--- src/ATRMain.java
+++ src/ATRMain.java
@@ -31,17 +31,90 @@ public class ATRMain {
 	    System.exit(-1);
 	}
 	
-	if (Integer.parseInt(args[1])!=1) {
-	    System.out.println("Pipeline #"+args[1]+" not implemented yet.");
+	int pipelineNumber = 0;
+	try {
+	    pipelineNumber = Integer.parseInt(args[1]);
+	}
+	catch (NumberFormatException nfe) {
+	    System.out.println("Error: Pipeline argument was not a valid integer.");
 	    System.exit(-1);
 	}
+	
+	if (pipelineNumber <= 0) {
+	    System.out.println("Error: Pipeline # must be > 0 (value was '"+pipelineNumber+")");
+	    System.exit(-1);
+	}
+	else {
+	    boolean timer = args[0].equalsIgnoreCase("timer");
+	    Node timer1;
+	    Node timer2;
+	    //if timer is requested, then timer1/timer2 are made to be Timer nodes,
+	    //otherwise, they become dummy Nodes, and ImageDatas will just be passed through
+	    //with minimal performance impact
+	    if (timer) {
+		timer1 = new Timer(true, false, null);
+		timer2 = new Timer(false, true, null);
+	    }
+	    else {
+		timer1 = new Node();
+		timer2 = new Node();
+	    }
+	    Node pipe = null;
+	    if (pipelineNumber == 1) {
+		
+		Node robCross = new RobertsCross(null);
+		Node thresh = new Thresholding(null);
+		Label label = new Label(Label.DEFAULT1, null, null);
+		label.findOneObject(true);
+		Node range = new RangeFind(null);
+		Node alert = new Alert(args);
+		pipe = timer1.linkL(robCross.linkL(thresh.linkL(label.link(null,
+									   range.linkL(timer2.linkL(alert))))));
+	    }
+	    else if (pipelineNumber == 2) {
+		Node labelBlue = new LabelBlue(null, null);
+		Node range = new RangeFind(null);
+		Node alert = new Alert(args);
+		pipe = timer1.linkL(labelBlue.link(null,
+						   range.linkL(timer2.linkL(alert))));
+	    }
+	    else if (pipelineNumber == 3) {
+		Node cleanCache = new Cache(1, null, null);
+		Node n = new Node();
+		LabelBlue labelBlue = new LabelBlue(null, null);
+		labelBlue.calibrateAndProcessSeparately(true);
+		Node calibCmd = new Command(Command.CALIBRATION_IMAGE, null);
+		Node noneCmd = new Command(Command.NONE, null);
+		Node copy = new Copy(null);
+		Node robCross = new RobertsCross(null);
+		Node thresh = new Thresholding(null);
+		Node hyst = new Hysteresis(null);
+		Node label = new Label(null, null);
+		Node labelSmCache = new Cache(1, null, null);
+		Node getCropCmd = new Command(Command.GET_CROPPED_IMAGE, null);
+		Node thin = new Thinning(Thinning.BLUE, null);
+		Node range = new RangeFind(null);
+		Node getLabelSmCmd = new Command(Command.GET_IMAGE, null);
+		
+		timer1.linkL(cleanCache.link(n.link(calibCmd.linkL(labelBlue),
+						    noneCmd.linkL(copy.linkL(robCross.link(null,
+											   thresh.link(null,
+												       hyst.link(null,
+														 label.link(null,
+															    labelSmCache.link(getCropCmd.linkL(cleanCache),
+																	      thin.link(null,
+																			range.linkL(timer2)))))))))),
+					    
+					     labelBlue.link(null,
+							    getLabelSmCmd.linkL(labelSmCache))));
+    
+	    }
+	    else {
+		System.out.println("Error: Pipeline #"+pipelineNumber+" not implemented yet.");
+		System.exit(-1);
+	    }
+	    (new ATR(args, pipe)).run();
 
-	boolean timer = args[0].equalsIgnoreCase("timer");
-
-	Node pipe = new Alert(args);
-	if (timer) pipe = new Timer(false, true, pipe);
-	pipe = new RobertsCross(new Thresholding(new Label(null, new RangeFind(pipe))));
-	if (timer) pipe = new Timer(true, false, pipe);
-	(new ATR(args, pipe)).run();
-    }
-}
+	}//else [if(pipelineNumber <= 0)]
+    }//public static void main()
+}//public class ATRMain
