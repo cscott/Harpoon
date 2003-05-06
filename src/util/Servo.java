628c62c227886dda980fabe3c642728f42db37d9
Wes Beebee <wbeebee@mit.edu>
2003-05-06 13:32:27 +0000

    Should be statically accessable...

--- src/util/Servo.java
+++ src/util/Servo.java
@@ -3,7 +3,7 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package imagerec.util;
 
-import servo.IPaqServoController;
+import servo.ServoThread;
 
 /**
  * {@link Servo} maneuvers the vehicle.
@@ -11,57 +11,8 @@ import servo.IPaqServoController;
  * @author Wes Beebee <<a href="mailto:wbeebee@mit.edu">wbeebee@mit.edu</a>>
  */
 public class Servo {
-    static final IPaqServoController car = new IPaqServoController();
-
-    private static final Servo servo1 = new ServoThread(1);
-    private static final Servo servo2 = new ServoThread(2);
-
-    /**
-     * {@link ServoThread} is a thread which controls the servos.
-     *
-     * @author Wes Beebee <<a href="mailto:wbeebee@mit.edu">wbeebee@mit.edu</a>>
-     */
-    class ServoThread extends Thread {
-	private final int servo;
-	private byte start, stop;
-	private long time;
-
-	/** 
-	 * Construct a new servo controlling thread and start it.
-	 *
-	 * @param servo the servo number to control (1-8).
-	 */
-	ServoThread(int servo) { 
-	    this.servo = servo;
-	    start(); 
-	}
-
-	/** 
-	 * Execute a command on the servo.
-	 */
-	public synchronized void run() {
-	    while (true) {
-		wait();
-		car.moveLocal(servo, start);
-		sleep(time);
-		car.moveLocal(servo, stop);
-	    }
-	}
-
-	/** 
-	 * Send a command to the servo.
-	 *
-	 * @param start The starting motion.
-	 * @param time How long to do it (in milliseconds).
-	 * @param stop The stopping motion.
-	 */
-	synchronized void command(byte start, long time, byte stop) {
-	    this.start = start;
-	    this.time = time;
-	    this.stop = stop;
-	    notify();
-	}
-    }
+    private static final ServoThread servo1 = new ServoThread(1);
+    private static final ServoThread servo2 = new ServoThread(2);
 
     /**
      * Move the vehicle forward.
@@ -69,7 +20,7 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend moving
      *             the car forward.
      */
-    public void forward(long time) {
+    public static void forward(long time) {
 	forward(time, 22);
     }
     
@@ -81,8 +32,8 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend moving
      *             the car forward.
      */              
-    public void forward(long time, byte speed) {
-	servo2.command(128+speed, time, 128);
+    public static void forward(long time, int speed) {
+	servo2.command((byte)(128+speed), time, (byte)128);
     }
 
     /**
@@ -91,7 +42,7 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend moving
      *             the car backward.
      */
-    public void backward(long time) {
+    public static void backward(long time) {
 	backward(time, 18);
     }
 
@@ -103,8 +54,8 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend moving
      *             the car backward.
      */
-    public void backward(long time, byte speed) {
-	servo2.command(128-speed, time, 128);
+    public static void backward(long time, int speed) {
+	servo2.command((byte)(128-speed), time, (byte)128);
     }
 
     /**
@@ -113,7 +64,7 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend with
      *             the steering locked to the left.
      */
-    public void left(long time) {
+    public static void left(long time) {
 	left(time, 127);
     }
 
@@ -125,8 +76,8 @@ public class Servo {
      * @param degree A number from 0 to 127 indicating the degree 
      *               to which the front wheels should turn left.
      */
-    public void left(long time, byte degree) {
-	servo1.command(128-degree, time, 128);
+    public static void left(long time, int degree) {
+	servo1.command((byte)(128-degree), time, (byte)128);
     }
      
     /**
@@ -135,7 +86,7 @@ public class Servo {
      * @param time The amount of time (in milliseconds) to spend with
      *             the steering locked to the right.
      */
-    public void right(long time) {
+    public static void right(long time) {
 	right(time, 127);
     }
 
@@ -147,8 +98,8 @@ public class Servo {
      * @param degree A number from 0 to 127 indicating the degree
      *               to which the front wheels should turn right.
      */
-    public void right(long time, byte degree) {
-	servo1.command(128+degree, time, 128);
+    public static void right(long time, int degree) {
+	servo1.command((byte)(128+degree), time, (byte)128);
     }
 }
 
