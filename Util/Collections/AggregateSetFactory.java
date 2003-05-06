41516ab2e6114b13bee36a2ec8d9526e599c1640
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-05-06 15:30:54 +0000

    Reworked the AggregateSetFactory to implement remove() efficiently (in O(1) time) and to fix a bug where the set implementation would inadvertently keep live the set providing their initial contents.

--- Util/Collections/AggregateSetFactory.java
+++ Util/Collections/AggregateSetFactory.java
@@ -8,28 +8,26 @@ import harpoon.Util.Default;
 import java.util.AbstractSet;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 /**
- * <code>AggregateSetFactory</code> uses a single <code>HashSet</code>
+ * <code>AggregateSetFactory</code> uses a single <code>HashMap</code>
  * as backing store for the many smaller <code>Set</code>s created
  * by this <code>SetFactory</code>.  This means that we use much
  * less space and rehash less frequently than if we were using
  * the standard <code>Factories.hashSetFactory</code>.
- * Be aware that the <code>remove()</code> method of the subsets is slow,
- * but the iterator is as fast as <code>ArrayList</code>.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: AggregateSetFactory.java,v 1.3 2002-04-10 03:07:10 cananian Exp $
+ * @version $Id: AggregateSetFactory.java,v 1.4 2003-05-06 15:30:54 cananian Exp $
  */
 public class AggregateSetFactory<V> extends SetFactory<V>
     implements java.io.Serializable {
-    private static final class ID { }
-    private final Set<Map.Entry<ID,V>> s = new HashSet<Map.Entry<ID,V>>();
+    private final Map<Map.Entry<EntryList<V>,V>,EntryList<V>> m =
+	new HashMap<Map.Entry<EntryList<V>,V>,EntryList<V>>();
 
     /** Creates an <code>AggregateSetFactory</code>. */
     public AggregateSetFactory() { /* nothing to do here */ }
@@ -41,55 +39,91 @@ public class AggregateSetFactory<V> extends SetFactory<V>
      *  The <code>remove()</code> method of the returned <code>Set</code>
      *  is very slow.
      */
-    public <T extends V> Set<V> makeSet(final Collection<T> c) {
-	return new AbstractSet<V>() {
-	    final ID IDENTITY;
-	    /* backing store for efficient iteration */
-	    final List<V> elements;
+    public <T extends V> Set<V> makeSet(Collection<T> c) {
+	return new AggregateSet(c);
+    }
+    private class AggregateSet extends AbstractSet<V> {
+	// list header is the IDENTITY object for the set.
+	// this is also the backing store for efficient iteration.
+	final EntryList<V> IDENTITY = new EntryList<V>(null);
+	int size=0;
 
-	    /* constructor */ { 
-	    IDENTITY = new ID();
-	    elements = new ArrayList<V>(c.size());
-	    addAll(c); 
-	    }
-	    public boolean add(V o) {
-		boolean r = s.add(Default.entry(IDENTITY, o));
-		if (r) elements.add(o);
-		return r;
-	    }
-	    public boolean contains(Object o) {
-		return s.contains(Default.entry(IDENTITY, o));
-	    }
-	    public boolean isEmpty() {
-		return elements.isEmpty();
-	    }
-	    public Iterator<V> iterator() {
-		return new Iterator<V>() {
-		    private int i=0;
-		    public boolean hasNext() { return i < elements.size(); }
-		    public V next() {
-			if (!hasNext()) throw new NoSuchElementException();
-			removeValid = true;
-			return elements.get(i++);
-		    }
-		    public void remove() {
-			if (!removeValid) throw new IllegalStateException();
-			Object o = elements.remove(--i);
-			s.remove(Default.entry(IDENTITY, o));
-			removeValid = false;
-		    }
-		    private boolean removeValid = false;
-		};
-	    }
-	    // XXX: remove is slow!
-	    // making it fast would require replacing the ArrayList with
-	    // our own custom linked-list impl, a la Util.WorkSet does.
-	    //public boolean remove(Object o);
-	    public int size() { return elements.size(); }
-	    // garbage-collect entries in backing store, too!
-	    protected void finalize() {
-		clear();
-	    }
-	};
+	<T extends V> AggregateSet(Collection<T> c) {
+	    addAll(c);
+	}
+	public boolean add(V o) {
+	    Map.Entry<EntryList<V>,V> key = Default.entry(IDENTITY, o);
+	    if (m.containsKey(key)) return false;
+	    EntryList<V> entry = new EntryList<V>(o);
+	    IDENTITY.add(entry);
+	    m.put(key, entry);
+	    size++;
+	    return true;
+	}
+	public boolean contains(Object o) {
+	    return m.containsKey(Default.entry(IDENTITY, o));
+	}
+	public boolean isEmpty() {
+	    assert (size==0)==(IDENTITY.next==null);
+	    return size==0;
+	}
+	public Iterator<V> iterator() {
+	    return new Iterator<V>() {
+		EntryList<V> entry = IDENTITY;
+		public boolean hasNext() { return entry.next!=null; }
+		public V next() {
+		    if (!hasNext()) throw new NoSuchElementException();
+		    removeValid = true;
+		    entry = entry.next;
+		    return entry.value;
+		}
+		public void remove() {
+		    if (!removeValid) throw new IllegalStateException();
+		    EntryList<V> oentry = entry;
+		    entry = entry.prev;
+		    AggregateSet.this.remove(oentry);
+		    removeValid = false;
+		}
+		private boolean removeValid = false;
+	    };
+	}
+	private void remove(EntryList<V> entry) {
+	    EntryList<V> e = m.remove(Default.entry(IDENTITY, entry.value));
+	    assert e == entry;
+	    entry.remove();
+	    size--;
+	}
+	public boolean remove(Object o) {
+	    EntryList<V> entry = m.get(Default.entry(IDENTITY, o));
+	    if (entry==null) return false;
+	    remove(entry);
+	    return true;
+	}
+	public int size() { return size; }
+	// garbage-collect entries in backing store, too!
+	protected void finalize() {
+	    clear();
+	}
+    }
+    /** Linked list of set elements. */
+    private static final class EntryList<E> implements java.io.Serializable {
+	final E value;
+	EntryList<E> prev=null, next=null;
+	EntryList(E value) { this.value = value; }
+
+	// utility.
+	/** Remove this entry from the list. */
+	void remove() {
+	    // always a predecessor; maybe no successor.
+	    this.prev.next = this.next;
+	    if (this.next!=null) this.next.prev = this.prev;
+	    this.next = this.prev = null; // safety.
+	}
+	/** Link in the supplied entry after this one. */
+	void add(EntryList<E> nel) {
+	    nel.next = this.next;
+	    nel.prev = this;
+	    this.next = nel.next.prev = nel;
+	}
     }
 }
