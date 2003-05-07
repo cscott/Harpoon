77f782708f3de505fe54769da1b244262cc40e76
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-05-07 22:20:26 +0000

    Extracted some constants and made them static final fields.

--- src/graph/Hysteresis.java
+++ src/graph/Hysteresis.java
@@ -10,6 +10,10 @@ package imagerec.graph;
  *  @author Wes Beebee <<a href="mailto:wbeebee@mit.edu">wbeebee@mit.edu</a>>
  */
 public class Hysteresis extends Node {
+
+    private static final int T1 = 127;
+    private static final int T2 = 64;
+
     /** Construct a new {@link Hysteresis} node that passes its output to <code>out</code>. */
     public Hysteresis(Node out) {
 	super(out);
@@ -18,15 +22,16 @@ public class Hysteresis extends Node {
     /** Process an image, sending the sharpened image to <code>out</code>. */ 
     public void process(ImageData id) {
 	byte[] in = id.gvals;
+	boolean[] seen = new boolean[id.gvals.length];
 	int w = id.width;
 	int h = id.height;
 	for (int i=w+1;i<(w*(h-2)-1);i++) {
-	    if (in[i]==127) {
-		recurse(i, in, w, h);
+	    if (in[i]==T1) {
+		recurse(i, in, w, h, seen);
 	    }
 	}
 	for (int i=0; i<in.length; i++) {
-	    if (in[i]==64) { /* Get rid of "maybe edges which are left". */
+	    if (in[i]==T2) { /* Get rid of "maybe edges which are left". */
 		in[i]=0;
 	    }
 	} 
@@ -34,12 +39,16 @@ public class Hysteresis extends Node {
     }
     
     /** Recursively determine whether a possible edge is a real edge. */
-    public static void recurse(int i, byte[] in, int w, int h) {
-	if (i<w || i>w*(h-1) || (i%w)==0 || ((i+1)%w)==0) return;
-	in[i] = (byte)127;
+    public static void recurse(int i, byte[] in, int w, int h, boolean[] seen) {
+	if (i<w || i>w*(h-1) || (i%w)==0 || ((i+1)%w)==0)
+	    return;
+	in[i] = (byte)T1;
+	//seen[i] = true;
 	int[] pos = new int[] {i-w-1,i-w,i-w+1,i-1,i+1,i+w-1,i+w,i+w+1};
 	for (int j=0; j<pos.length; j++) {
-	    if (in[pos[j]]==64) recurse(pos[j], in, w, h);
+	    if (in[pos[j]]==T2) {
+		recurse(pos[j], in, w, h, seen);
+	    }
 	}
-    } 
+    }
 }
