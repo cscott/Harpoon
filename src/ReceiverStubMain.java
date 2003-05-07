315c404cb289e49e7fd032648c603ce447d053db
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-05-07 20:53:31 +0000

    Added a second pipeline that can receive a feedback 'heartbeat' so that it knows when to send the next image.
    Adding the second pipeline required adding extra command arguments
    for selecting pipeline# and for choosing CORBA nameservice names.
    
    I have updated the BUILDING file to reflect these changes.

--- src/ReceiverStubMain.java
+++ src/ReceiverStubMain.java
@@ -1,5 +1,5 @@
-// ReceiverStubMain.java, created by wbeebee
-// Copyright (C) 2003 Wes Beebee <wbeebee@mit.edu>
+// ReceiverStubMain.java, created by benster
+// Copyright (C) 2003 Reuben Sterling <benster@mit.edu>
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package imagerec;
 
@@ -11,7 +11,7 @@ import imagerec.graph.*;
  * This is the main program for the receiver stub that can send a series of images
  * to the ATR in place of the UAV OEP receiver.
  *
- * @author Wes Beebee <<a href="mailto:wbeebee@mit.edu">wbeebee@mit.edu</a>>
+ * @author Reuben Sterling <<a href="mailto:benster@mit.edu">benster@mit.edu</a>>
  */
 
 public class ReceiverStubMain {
@@ -21,20 +21,66 @@ public class ReceiverStubMain {
      *  @param args Should include parameters for contacting the CORBA nameservice.
      */
     public static void main(String args[]) {
-	if (args.length<4) {
-	    System.out.println("Usage: java -jar receiverStub.jar <jarFile|'none'> <filePrefix> <num> <CORBA name for ATR> [CORBA options]");
+	if (args.length<6) {
+	    System.out.println("Usage: java -jar receiverStub.jar <jarFile|'none'> <filePrefix> <# of images>");
+	    System.out.println("            <pipeline #> <CORBA name for ATR> <CORBA name for feedback server> ");
+	    System.out.println("            [CORBA options]");
 	    System.exit(-1);
 	}
 
-	Node atr = new ATRClient(new CORBA(args), args[3]);
-	Node load;
-	if (args[0].equalsIgnoreCase("none")) {
-	    load = new Load(args[1], Integer.parseInt(args[2]), atr);
-	} else {
-	    load = new Load(args[0], args[1], Integer.parseInt(args[2]), atr);
+	int pipelineNumber = 0;
+	try {
+	    pipelineNumber = Integer.parseInt(args[3]);
 	}
-	while (true) {
-	    load.run();
+	catch (NumberFormatException nfe) {
+	    System.out.println("Error: Pipeline argument was not a valid integer.");
+	    System.exit(-1);
+	}
+	
+	if (pipelineNumber <= 0) {
+	    System.out.println("Error: Pipeline # must be > 0 (value was '"+pipelineNumber+")");
+	    System.exit(-1);
+	}
+	else {
+	    if (pipelineNumber == 1) {
+		Node atrClient = new ATRClient(new CORBA(args), args[4]);
+		Node load;
+		if (args[0].equalsIgnoreCase("none")) {
+		    load = new Load(args[1], Integer.parseInt(args[2]), atrClient);
+		} else {
+		    load = new Load(args[0], args[1], Integer.parseInt(args[2]), atrClient);
+		}
+		while (true) {
+		    load.run();
+		}
+	    }
+	    else if (pipelineNumber == 2) {
+		AlertServer feedbackServer = new AlertServer(new CORBA(args), args[5], null);
+
+		Node set = new SetCommonValue("heartbeat", new Boolean(true));
+
+		feedbackServer.linkL(set);
+		Thread t = new Thread(feedbackServer);
+		t.start(); //calls feedbackServer.run();
+		
+
+		Node atrClient = new ATRClient(new CORBA(args), args[4]);
+		Node regulate = new Regulator("heartbeat");
+		Node load;
+		if (args[0].equalsIgnoreCase("none")) {
+		    load = new Load(args[1], Integer.parseInt(args[2]), null);
+		} else {
+		    load = new Load(args[0], args[1], Integer.parseInt(args[2]), null);
+		}
+		load.linkL(regulate.linkL(atrClient));
+		while (true) {
+		    load.run();
+		}
+	    }
+	    else {
+		System.out.println("Error: Pipeline #"+pipelineNumber+" not implemented yet.");
+		System.exit(-1);
+	    }
 	}
     }
 }
