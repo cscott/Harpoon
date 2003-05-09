6cad3863b97e450f6a00131472cc8a654b219e5d
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-05-09 00:28:20 +0000

    GJ-ize harpoon.IR.Bytecode.Instr so that it can implement the a properly-parameterized CFGraphable interface.  Add it to gj-files, along with some subclasses, and rebuild gjlib.jar.

--- IR/Bytecode/Instr.java
+++ IR/Bytecode/Instr.java
@@ -22,7 +22,7 @@ import java.util.List;
  * a unique numeric identifier.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: Instr.java,v 1.5 2002-04-10 03:04:37 cananian Exp $
+ * @version $Id: Instr.java,v 1.6 2003-05-09 00:28:27 cananian Exp $
  * @see InGen
  * @see InCti
  * @see InMerge
@@ -30,7 +30,8 @@ import java.util.List;
  * @see Code
  */
 public abstract class Instr 
-  implements HCodeElement, harpoon.IR.Properties.CFGraphable, Comparable {
+  implements HCodeElement, harpoon.IR.Properties.CFGraphable<Instr,InstrEdge>,
+	     Comparable<Instr> {
   final String sourcefile;
   final int linenumber;
   final int id;
@@ -60,9 +61,9 @@ public abstract class Instr
   /** Returns the java bytecode of this instruction. */
   public abstract byte getOpcode();
   /** Natural ordering on <code>Instr</code>s. */
-  public int compareTo(Object o) {
+  public int compareTo(Instr o) {
     /* ordering is by id. */
-    return ((Instr)o).id - this.id;
+    return o.id - this.id;
   }
   public boolean equals(Object o) { // exploit global uniqueness of id.
     if (this==o) return true;
@@ -73,25 +74,25 @@ public abstract class Instr
   public int hashCode() { return id; } // exploit global uniqueness of id.
 
   /** Array Factory: makes <code>Instr[]</code>s. */
-  public static final ArrayFactory arrayFactory =
-    new ArrayFactory() {
-      public Object[] newArray(int len) { return new Instr[len]; }
+  public static final ArrayFactory<Instr> arrayFactory =
+    new ArrayFactory<Instr>() {
+      public Instr[] newArray(int len) { return new Instr[len]; }
     };
 
   /** Return a list of all the <code>Instr</code>s that can precede
    *  this one. */
   public Instr[] prev() {
-    return (Instr[]) prev.toArray(new Instr[prev.size()]);
+    return prev.toArray(new Instr[prev.size()]);
   }
   /** Return a list of all the possible <code>Instr</code>s that may
    *  succeed this one. */
   public Instr[] next() {
-    return (Instr[]) next.toArray(new Instr[next.size()]);
+    return next.toArray(new Instr[next.size()]);
   }
   /** Return the specified successor of this <code>Instr</code>. */
-  public Instr next(int i) { return (Instr) next.get(i); }
+  public Instr next(int i) { return next.get(i); }
   /** Return the specified predecessor of this <code>Instr</code>. */
-  public Instr prev(int i) { return (Instr) prev.get(i); }
+  public Instr prev(int i) { return prev.get(i); }
 
   /** Add a predecessor to this <code>Instr</code>. */
   void addPrev(Instr prev) { this.prev.add(prev); }
@@ -103,46 +104,49 @@ public abstract class Instr
   void removeNext(Instr next) { this.next.remove(next); }
 
   /** Internal predecessor list. */
-  final List prev = new ArrayList(2);
+  final List<Instr> prev = new ArrayList<Instr>(2);
   /** Internal successor list. */
-  final List next = new ArrayList(2);
+  final List<Instr> next = new ArrayList<Instr>(2);
 
   // CFGraphable interface:
-  public CFGEdge newEdge(final Instr from, final Instr to) {
-    return new CFGEdge() {
-      public CFGraphable fromCFG() { return from; }
-      public CFGraphable toCFG() { return to; }
-      public boolean equals(Object o) {
+  public InstrEdge newEdge(Instr from, Instr to) {
+      return new InstrEdge(from, to);
+  }
+  public InstrEdge[] succ() {
+    InstrEdge[] r = new InstrEdge[next.size()];
+    for (int i=0; i<r.length; i++)
+      r[i] = newEdge(this, next.get(i));
+    return r;
+  }
+  public InstrEdge[] pred() {
+    InstrEdge[] r = new InstrEdge[prev.size()];
+    for (int i=0; i<r.length; i++)
+      r[i] = newEdge(prev.get(i), this);
+    return r;
+  }
+  public InstrEdge[] edges() {
+    InstrEdge[] n = succ();
+    InstrEdge[] p = pred();
+    InstrEdge[] r = new InstrEdge[n.length + p.length];
+    System.arraycopy(n, 0, r, 0, n.length);
+    System.arraycopy(p, 0, r, n.length, p.length);
+    return r;
+  }
+  public Collection<InstrEdge> edgeC() { return Arrays.asList(edges()); }
+  public Collection<InstrEdge> predC() { return Arrays.asList(pred()); }
+  public Collection<InstrEdge> succC() { return Arrays.asList(succ()); }
+}
+class InstrEdge extends CFGEdge<Instr,InstrEdge> {
+    final Instr from, to;
+    InstrEdge(Instr from, Instr to) { this.from = from; this.to = to; }
+    public Instr fromCFG() { return from; }
+    public Instr toCFG() { return to; }
+    public boolean equals(Object o) {
 	CFGEdge hce;
 	if (this==o) return true;
 	if (o==null) return false;
 	try { hce=(CFGEdge)o; } catch (ClassCastException e) {return false; }
 	return hce.from() == from && hce.to() == to;
-      }
-      public int hashCode() { return from.hashCode() ^ to.hashCode(); }
-    };
-  }
-  public CFGEdge[] succ() {
-    CFGEdge[] r = new CFGEdge[next.size()];
-    for (int i=0; i<r.length; i++)
-      r[i] = newEdge(this, (Instr) next.get(i));
-    return r;
-  }
-  public CFGEdge[] pred() {
-    CFGEdge[] r = new CFGEdge[prev.size()];
-    for (int i=0; i<r.length; i++)
-      r[i] = newEdge((Instr)prev.get(i), this);
-    return r;
-  }
-  public CFGEdge[] edges() {
-    CFGEdge[] n = succ();
-    CFGEdge[] p = pred();
-    CFGEdge[] r = new CFGEdge[n.length + p.length];
-    System.arraycopy(n, 0, r, 0, n.length);
-    System.arraycopy(p, 0, r, n.length, p.length);
-    return r;
-  }
-  public Collection edgeC() { return Arrays.asList(edges()); }
-  public Collection predC() { return Arrays.asList(pred()); }
-  public Collection succC() { return Arrays.asList(succ()); }
+    }
+    public int hashCode() { return from.hashCode() ^ to.hashCode(); }
 }
