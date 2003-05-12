4cc8f9c08e29894f7320a31c08b851f6482cd6b8
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-05-12 19:14:52 +0000

    Generalize DataFlowSolver to work over arbitrary Graphs, not just Graphs made up of HCodeElements and HCodeEdges.

--- Analysis/Companions/DataFlowSolver.java
+++ Analysis/Companions/DataFlowSolver.java
@@ -6,6 +6,7 @@ package harpoon.Analysis.Companions;
 import harpoon.ClassFile.HCode;
 import harpoon.IR.Properties.CFGEdge;
 import harpoon.IR.Properties.CFGraphable;
+import harpoon.Util.Collections.Graph;
 import harpoon.Util.Collections.WorkSet;
 
 import java.util.Collection;
@@ -14,48 +15,58 @@ import java.util.Iterator;
 import java.util.Map;
 /**
  * The <code>DataFlowSolver</code> class provides a parameterized framework
- * for building simple data flow analyses for various IRs.
+ * for building simple data flow analyses for various IRs.  The
+ * <code>DataFlowSolver.Forward</code> and <code>DataFlowSolver.Backward</code>
+ * subclasses contain code for forward and backward analyses, respectively.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: DataFlowSolver.java,v 1.2 2003-05-09 00:44:05 cananian Exp $
+ * @version $Id: DataFlowSolver.java,v 1.3 2003-05-12 19:14:52 cananian Exp $
  */
-public abstract class DataFlowSolver
-    <HCE extends CFGraphable<HCE,E>, E extends CFGEdge<HCE,E>, FACT> {
+public abstract class DataFlowSolver<N extends Graph.Node<N,E>,
+				     E extends Graph.Edge<N,E>,
+				     FACT> {
     /** Compute the join operation on two dataflow <code>FACT</code>s. */
     protected abstract FACT join(FACT e1, FACT e2);
     /** Return the initial value of the dataflow <code>FACT</code> for the
-     *  given <code>HCodeElement</code> <code>hce</code>. */
-    protected abstract FACT init(HCE hce);
-    /** Compute all dataflow facts for the given <code>HCode</code>. */
-    public abstract Map<HCE,FACT> compute(HCode<HCE> hc);
+     *  given <code>Node</code> (often, an <code>HCodeElement</code>). */
+    protected abstract FACT init(N hce);
+    /** Compute all dataflow facts on the given <code>Graph</code>.
+     *  Note that <code>HCode</code> subclasses with elements implementing
+     *  <code>CFGraphable</code> are typically also instances of
+     *  <code>Graph</code>. */
+    public abstract Map<N,FACT> compute(Graph<N,E> hc);
 
     /**
      * <code>DataFlowSolver.Forward</code> is a dataflow solver for
      * forward dataflow analyses.  You need only define a <code>join</code>
      * operation on your <code>FACT</code> type, an initial value for
-     * each quad (via the return value of the <code>init()</code> method),
-     * and a means for computing the <code>OUT</code> dataflow fact
-     * given the <code>IN</code> fact for a <code>HCodeElement</code>.
+     * each node (usually, an <code>HCodeElement</code> implementation which
+     * also implements <code>Graph.Node</code>, such as <code>Quad</code>)
+     * via the return value of the <code>init()</code> method), and a means
+     * for computing the <code>OUT</code> dataflow fact given the
+     * <code>IN</code> fact for a node (<code>HCodeElement</code>).
      * Calling <code>compute()</code> will then return a mapping from
-     * every <code>HCodeElement</code> to the final <code>IN</code>
+     * every <code>Graph.Node</code> to the final <code>IN</code>
      * dataflow fact for that element. */
     public static abstract class Forward
-	<HCE extends CFGraphable<HCE,E>, E extends CFGEdge<HCE,E>, FACT>
-	extends DataFlowSolver<HCE,E,FACT> {
+	<N extends Graph.Node<N,E>, E extends Graph.Edge<N,E>, FACT>
+	extends DataFlowSolver<N,E,FACT> {
 	/** Compute the <code>OUT</code> fact given the <code>IN</code>
-	 *  fact for an <code>HCodeElement</code> <code>hce</code>. */
-	protected abstract FACT out(HCE hce, FACT in);
+	 *  fact for a <code>Node</code> (<code>HCodeElement</code>). */
+	protected abstract FACT out(N hce, FACT in);
 	
-	private final FACT lookup(HCE hce, Map<HCE,FACT> map) {
+	/** Look up the FACT for the Node, adding the initial value to the
+	 *  map if no value was present. */
+	private final FACT lookup(N hce, Map<N,FACT> map) {
 	    if (!map.containsKey(hce))
 		map.put(hce, init(hce));
 	    return map.get(hce);
 	}
-	public final Map<HCE,FACT> compute(HCode<HCE> hc) {
-	    final Map<HCE,FACT> OUTresult = new HashMap<HCE,FACT>();
-	    final WorkSet<HCE> worklist = new WorkSet<HCE>(hc.getElementsL());
+	public final Map<N,FACT> compute(Graph<N,E> g) {
+	    final Map<N,FACT> OUTresult = new HashMap<N,FACT>();
+	    final WorkSet<N> worklist = new WorkSet<N>(g.nodes());
 	    while (!worklist.isEmpty()) {
-		HCE hce = worklist.removeFirst();
+		N hce = worklist.removeFirst();
 		// compute in using out result.
 		FACT in = computeIN(hce, OUTresult);
 		// now recompute new out.
@@ -68,15 +79,15 @@ public abstract class DataFlowSolver
 		}
 	    }
 	    // done!  but we want to return IN set, not OUT set.
-	    final Map<HCE,FACT> INresult = new HashMap<HCE,FACT>();
-	    for (Iterator<HCE> it=hc.getElementsI(); it.hasNext(); ) {
-		HCE hce = it.next();
+	    final Map<N,FACT> INresult = new HashMap<N,FACT>();
+	    for (Iterator<N> it=g.nodes().iterator(); it.hasNext(); ) {
+		N hce = it.next();
 		INresult.put(hce, computeIN(hce, OUTresult));
 	    }
 	    // now really done.
 	    return INresult;
 	}
-	private final FACT computeIN(HCE hce, Map<HCE,FACT> outMap) {
+	private final FACT computeIN(N hce, Map<N,FACT> outMap) {
 	    Iterator<E> it=hce.predC().iterator();
 	    if (!it.hasNext()) return init(hce);
 	    FACT f = lookup(it.next().from(), outMap);
