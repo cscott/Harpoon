f04386181a03db982104e107f8c50d21bf990d13
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-05-16 00:35:36 +0000

    Add useC() and defC() methods to the QNodes in QuadFlowGraph in order to separate the uses/defs associated with the phis/sigmas with the other uses and defs in the node.

--- Analysis/Companions/QuadFlowGraph.java
+++ Analysis/Companions/QuadFlowGraph.java
@@ -3,13 +3,23 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Analysis.Companions;
 
+import harpoon.IR.Quads.CALL;
+import harpoon.IR.Quads.CJMP;
 import harpoon.IR.Quads.PHI;
 import harpoon.IR.Quads.Quad;
 import harpoon.IR.Quads.SIGMA;
+import harpoon.IR.Quads.SWITCH;
+import harpoon.IR.Quads.TYPESWITCH;
+import harpoon.Temp.Temp;
 import harpoon.Util.Collections.AbstractGraph;
 import harpoon.Util.Collections.WorkSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 /**
  * <code>QuadFlowGraph</code> is an expanded flow graphs for Quads, where
@@ -19,7 +29,7 @@ import java.util.Map;
  * conceptually before (after) the node, on the incoming (outgoing) edge.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 public class QuadFlowGraph extends AbstractGraph<QNode,QEdge> {
     
@@ -76,7 +86,7 @@ public class QuadFlowGraph extends AbstractGraph<QNode,QEdge> {
  *  may have different edges than the underlying <code>Quad</code>
  *  returned by the <code>baseQuad()</code> method.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 abstract class QNode extends AbstractGraph.Node<QNode,QEdge> {
     QNode(QuadFlowGraph parent) { super(parent); }
@@ -84,13 +94,15 @@ abstract class QNode extends AbstractGraph.Node<QNode,QEdge> {
     abstract boolean isPhiEntrance();
     abstract boolean isSigmaExit();
     abstract int whichEdge();
+    abstract Collection<Temp> useC();
+    abstract Collection<Temp> defC();
 }
 /** A <code>NormalNode</code> wraps an arbitrary <code>Quad</code> and
  *  corresponds directly to a node in the underlying flow graph,
  *  although the edges may differ if the underlying quad is a
  *  <code>PHI</code> or <code>SIGMA</code>.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 final class NormalNode extends QNode {
     final Quad baseQuad;
@@ -102,13 +114,39 @@ final class NormalNode extends QNode {
     boolean isPhiEntrance() { return false; }
     boolean isSigmaExit() { return false; }
     int whichEdge() { throw new RuntimeException("a normal quad"); }
+    Collection<Temp> useC() {
+	if (baseQuad instanceof PHI)
+	    return Collections.EMPTY_LIST;
+	if (baseQuad instanceof CALL)
+	    return Arrays.asList(((CALL)baseQuad).params());
+	if (baseQuad instanceof CJMP)
+	    return Collections.singletonList(((CJMP)baseQuad).test());
+	if (baseQuad instanceof SWITCH)
+	    return Collections.singletonList(((SWITCH)baseQuad).index());
+	if (baseQuad instanceof TYPESWITCH)
+	    return Collections.singletonList(((TYPESWITCH)baseQuad).index());
+	return baseQuad.useC();
+    }
+    Collection<Temp> defC() {
+	if (baseQuad instanceof CALL) {
+	    CALL call = (CALL) baseQuad;
+	    List<Temp> defs = new ArrayList<Temp>(2);
+	    if (call.retval()!=null) defs.add(call.retval());
+	    if (call.retex()!=null) defs.add(call.retex());
+	    return defs;
+	}
+	if (baseQuad instanceof PHI ||
+	    baseQuad instanceof SIGMA)
+	    return Collections.EMPTY_LIST;
+	return baseQuad.defC();
+    }
 }
 /** A <code>PhiEntranceNode</code> is an artificial node in the
  * <code>QuadFlowGraph</code> which is inserted on the incoming
  * edge of a <code>PHI</code>.  It reports its <code>baseQuad()</code>
  * as the affiliated <code>PHI</code>.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 final class PhiEntranceNode extends QNode {
     final PHI baseQuad;  final int whichEdge;
@@ -120,13 +158,25 @@ final class PhiEntranceNode extends QNode {
     boolean isPhiEntrance() { return true; }
     boolean isSigmaExit() { return false; }
     int whichEdge() { return whichEdge; }
+    List<Temp> defC() {
+	List<Temp> defs = new ArrayList<Temp>(baseQuad.numPhis());
+	for (int i=0; i<baseQuad.numPhis(); i++)
+	    defs.add(baseQuad.dst(i));
+	return defs;
+    }
+    List<Temp> useC() {
+	List<Temp> uses = new ArrayList<Temp>(baseQuad.numPhis());
+	for (int i=0; i<baseQuad.numPhis(); i++)
+	    uses.add(baseQuad.src(i, whichEdge));
+	return uses;
+    }
 }
 /** A <code>SigmaExitNode</code> is an artificial node in the
  * <code>QuadFlowGraph</code> which is inserted on the outgoing
  * edge of a <code>SIGMA</code>.  It reports its <code>baseQuad()</code>
  * as the affiliated <code>SIGMA</code>.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 final class SigmaExitNode extends QNode {
     final SIGMA baseQuad;  final int whichEdge;
@@ -138,6 +188,18 @@ final class SigmaExitNode extends QNode {
     boolean isPhiEntrance() { return false; }
     boolean isSigmaExit() { return true; }
     int whichEdge() { return whichEdge; }
+    List<Temp> defC() {
+	List<Temp> defs = new ArrayList<Temp>(baseQuad.numSigmas());
+	for (int i=0; i<baseQuad.numSigmas(); i++)
+	    defs.add(baseQuad.dst(i, whichEdge));
+	return defs;
+    }
+    List<Temp> useC() {
+	List<Temp> uses = new ArrayList<Temp>(baseQuad.numSigmas());
+	for (int i=0; i<baseQuad.numSigmas(); i++)
+	    uses.add(baseQuad.src(i));
+	return uses;
+    }
 }
 /**
  * <code>QEdge</code> represents edges between <code>QNode</code>
@@ -145,7 +207,7 @@ final class SigmaExitNode extends QNode {
  * but not in the case of inserted <code>PhiEntranceNode</code>s and
  * <code>SigmaExitNode</code>s.
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: QuadFlowGraph.java,v 1.1 2003-05-12 19:28:14 cananian Exp $
+ * @version $Id: QuadFlowGraph.java,v 1.2 2003-05-16 00:35:36 cananian Exp $
  */
 final class QEdge extends AbstractGraph.Edge<QNode,QEdge> {
     QEdge(QNode from, QNode to) { super(from, to); }
