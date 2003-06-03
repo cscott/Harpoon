1c5ac91d3a8fd0cb4f906097b3064d36f20ab6d7
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-06-03 11:30:30 +0000

    System.out changes.

--- src/graph/Servo.java
+++ src/graph/Servo.java
@@ -23,44 +23,55 @@ public class Servo extends Node {
      *            control the servos.
      */
     public void process(ImageData id) {
+	//System.out.println("Servo.process()");
 	switch (Command.read(id)) {
-	case Command.SERVO_LEFT: { 
+	case Command.SERVO_LEFT: {
+	    //System.out.println("Servo: turning left for "+id.time);
 	    imagerec.util.Servo.left(id.time); 
 	    break; 
 	}
 	case Command.SERVO_RIGHT: { 
+	    //System.out.println("Servo: turning right for "+id.time);
 	    imagerec.util.Servo.right(id.time); 
 	    break;
 	}
 	case Command.SERVO_FORWARD: {
+	    //System.out.println("Servo: going forward for "+id.time);
 	    imagerec.util.Servo.forward(id.time);
 	    break;
 	}
 	case Command.SERVO_BACKWARD: {
+	    //System.out.println("Servo: going backward for "+id.time);
 	    imagerec.util.Servo.backward(id.time);
 	    break;
 	}
 	case Command.SERVO_LEFT_CONTINUE: {
+	    //System.out.println("Servo: going left indefinitely");
 	    imagerec.util.Servo.left();
 	    break;
 	}
 	case Command.SERVO_RIGHT_CONTINUE: {
+	    //System.out.println("Servo: going right indefinitely");
 	    imagerec.util.Servo.right();
 	    break;
 	}
 	case Command.SERVO_FORWARD_CONTINUE: {
+	    //System.out.println("Servo: going forward indefinitely");
 	    imagerec.util.Servo.forward();
 	    break;
 	}
 	case Command.SERVO_BACKWARD_CONTINUE: {
+	    //System.out.println("Servo: going backward indefinitely");
 	    imagerec.util.Servo.backward();
 	    break;
 	}
 	case Command.SERVO_STOP_TURN: {
+	    //System.out.println("Servo: straightening wheels");
 	    imagerec.util.Servo.stop_turn();
 	    break;
 	}
 	case Command.SERVO_STOP_MOVING: {
+	    //System.out.println("Servo: stopping movement");
 	    imagerec.util.Servo.stop_moving();
 	    break;
 	}
