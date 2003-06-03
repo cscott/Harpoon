586313e09b4a8205fd3f01e9250d28f04e67384a
Reuben Sterling <benster@fbb.lcs.mit.edu>
2003-06-03 11:48:23 +0000

    An interface that HumanRecognition implements that allows the latency slider to interact with it.

---
+++ src/graph/VariableLatency.java
@@ -0,0 +1,9 @@
+// VariableLatency.java, created by benster 5/27/03
+// Copyright (C) 2003 Reuben Sterling <benster@mit.edu>
+// Licensed under the terms of the GNU GPL; see COPYING for details.
+
+package imagerec.graph;
+
+public interface VariableLatency {
+    public void setLatency(int time);
+}
