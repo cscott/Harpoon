a8d676c4daa3ac902fd95ab9a8f22f34a8b4e3e2
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-06-08 16:51:06 +0000

    Final stage of the PersistentTreeNode restructuring: parameterize the allocation function --- which also requires adding an extra recursive parameter to the PersistentTreeNode class.
    Provide a sample implementation, 'WithValue', with illustrates how to
    extend the base class with a 'value' field, as well as to cache map
    hash codes.

--- Util/Collections/PersistentTreeNode.java
+++ Util/Collections/PersistentTreeNode.java
@@ -22,25 +22,18 @@ import java.util.Iterator;
  * equality tests for treaps.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: PersistentTreeNode.java,v 1.10 2003-06-08 16:45:58 cananian Exp $
+ * @version $Id: PersistentTreeNode.java,v 1.11 2003-06-08 16:51:06 cananian Exp $
  */
-// XXX implement the javadoc
-// parameterize the allocation function. (pass it in as an argument?)
-// maybe parameterize tree node such that 'value' is not necessarily present?
-// XXX do we want fast hashCode()?
-class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V> 
-    implements java.io.Serializable {
+abstract class PersistentTreeNode<N extends PersistentTreeNode<N,K,V>,K,V>
+    extends AbstractMapEntry<K,V> implements java.io.Serializable {
 
     public final K key;
-    //public final V value;//optional
-    public final PersistentTreeNode<K,V> left;
-    public final PersistentTreeNode<K,V> right;
+    /* also an optional value field of type 'V'; not included in base class.*/
+    public final N left;
+    public final N right;
 
-    PersistentTreeNode(K key, V value,
-		       PersistentTreeNode<K,V> left,
-		       PersistentTreeNode<K,V> right) {
-	this.key = key;  //this.value = value;
-	this.left= left; this.right = right;
+    PersistentTreeNode(K key, N left, N right) {
+	this.key = key; this.left= left; this.right = right;
     }
     // ACCESSOR FUNCTIONS for Map.Entry.
     public final K getKey() { return key; }
@@ -51,7 +44,7 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	return "["+key+"->"+getValue()+", left="+left+", right="+right+"]";
     }
     /** Returns the length of the longest path from the root to a leaf. */
-    static <K,V> int depth(PersistentTreeNode<K,V> n) {
+    static <N extends PersistentTreeNode<N,K,V>,K,V> int depth(N n) {
 	if (n==null) return 0;
 	return 1+Math.max(depth(n.left), depth(n.right));
     }
@@ -59,7 +52,7 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
      * @return 0 if <code>n==null</code>, else
      *         <code>1+size(n.left)+size(n.right)</code>.
      */
-    static <K,V> int size(PersistentTreeNode<K,V> n) {
+    static <N extends PersistentTreeNode<N,K,V>,K,V> int size(N n) {
 	return (n==null) ? 0 : (1 + size(n.left) + size(n.right));
     }
 
@@ -80,28 +73,35 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	return (n1==null)?(n2==null):(n1.isSame(n2));
     }
 
+    // factory class.
+    static abstract class Allocator<N extends PersistentTreeNode<N,K,V>,K,V> {
+	abstract N newNode(K key, V value, N left, N right);
+    }
+
     // TREE UTILITY FUNCTIONS.
     /** Creates a new node iff the created node would not be identical
      *  to the given <code>n</code>. */
-    private static <K,V> 
-	PersistentTreeNode<K,V> newNode(PersistentTreeNode<K,V> n,
-					K key, V value,
-					PersistentTreeNode<K,V> left,
-					PersistentTreeNode<K,V> right) {
+    private static <N extends PersistentTreeNode<N,K,V>,K,V> 
+			      N newNode(N n, K key, V value,
+					N left, N right,
+					Allocator<N,K,V> allocator) {
 	if (n != null && n.key.equals(key) && isSame(n.getValue(), value) &&
 	    n.left == left && n.right == right)
 	    return n;
 	// check heap condition
 	assert left==null || heapKey(key) < heapKey(left.key);
 	assert right==null|| heapKey(key) <= heapKey(right.key);
-	return new PersistentTreeNode<K,V>(key, value, left, right);
+	// the actual work of allocation is deferred to the allocator.
+	// (so that the proper subclass can be created!)
+	return allocator.newNode(key, value, left, right);
     }
     /** Creates a new node (possibly reusing existing node n) rebalancing
      *  the left side if necessary. */
-    private static <K,V> 
-	PersistentTreeNode<K,V> newNode_balanceLeft
-	(PersistentTreeNode<K,V> n, K key, V value,
-	 PersistentTreeNode<K,V> left, PersistentTreeNode<K,V> right) {
+    private static <N extends PersistentTreeNode<N,K,V>,K,V> 
+			      N newNode_balanceLeft(N n, K key, V value,
+						    N left, N right,
+						    Allocator<N,K,V> allocator)
+    {
 	// tie goes to the left (lesser tree key)
 	if (left!=null && left!=n.left/*speed optimization*/ &&
 	    heapKey(left.key) <= heapKey(key)) { // needs rebalancing!
@@ -113,17 +113,19 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	    // a   c         c   e
 	    return newNode(left, left.key, left.getValue(),
 			   left.left, newNode(n, key, value,
-					      left.right, right));
+					      left.right, right, allocator),
+			   allocator);
 	} else { // okay as is
-	    return newNode(n, key, value, left, right);
+	    return newNode(n, key, value, left, right, allocator);
 	}
     }
     /** Creates a new node (possibly reusing existing node n) rebalancing
      *  the right side if necessary. */
-    private static <K,V> 
-	PersistentTreeNode<K,V> newNode_balanceRight
-	(PersistentTreeNode<K,V> n, K key, V value,
-	 PersistentTreeNode<K,V> left, PersistentTreeNode<K,V> right) {
+    private static <N extends PersistentTreeNode<N,K,V>,K,V> 
+			      N newNode_balanceRight(N n, K key, V value,
+						     N left, N right,
+						     Allocator<N,K,V>allocator)
+    {
 	// tie goes to the top (lesser tree key)
 	if (right!=null && right!=n.right/*speed optimization*/ &&
 	    heapKey(right.key) < heapKey(key)) { // needs rebalancing!
@@ -134,17 +136,16 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	    //      / \    / \
 	    //     c   e  a   c
 	    return newNode(right, right.key, right.getValue(),
-			   newNode(n, key, value, left, right.left),
-			   right.right);
+			   newNode(n, key, value, left, right.left, allocator),
+			   right.right, allocator);
 	} else { // okay as is
-	    return newNode(n, key, value, left, right);
+	    return newNode(n, key, value, left, right, allocator);
 	}
     }
     /** Returns the <code>PersistentTreeNode</code> matching <code>key</code>
      *  if any, else <code>null</code>. */
-    static <K,V> PersistentTreeNode<K,V> get(PersistentTreeNode<K,V> n,
-					     Comparator<K> c,
-					     K key) {
+    static <N extends PersistentTreeNode<N,K,V>,K,V>
+		      N get(N n, Comparator<K> c, K key) {
 	if (n==null) return null; /* no node with this key. */
 	int r = c.compare(key, n.key);
 	return
@@ -155,86 +156,95 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
     /** Returns a node rooting a tree containing all the mappings in
      *  the tree rooted at the given <code>n</code>, plus a mapping from
      *  <code>key</code> to <code>value</code>. */
-    static <K,V> PersistentTreeNode<K,V> put(PersistentTreeNode<K,V> n,
-					     Comparator<K> c,
-					     K key, V value) {
-	if (n==null) return newNode(null, key, value, null, null);
+    static <N extends PersistentTreeNode<N,K,V>,K,V>
+		      N put(N n, Comparator<K> c, K key, V value,
+			    Allocator<N,K,V> allocator) {
+	if (n==null) return newNode(null, key, value, null, null, allocator);
 	
 	int r = c.compare(key, n.key);
 	if (r==0)
 	    // already heap-balanced
-	    return newNode(n, key, value, n.left, n.right);
+	    return newNode(n, key, value, n.left, n.right, allocator);
 	if (r < 0)
 	    return newNode_balanceLeft(n, n.key, n.getValue(),
-				       put(n.left, c, key, value), n.right);
+				       put(n.left, c, key, value, allocator),
+				       n.right,
+				       allocator);
 	if (r > 0)
 	    return newNode_balanceRight(n, n.key, n.getValue(),
-					n.left, put(n.right, c, key, value));
+					n.left,
+					put(n.right, c, key, value, allocator),
+					allocator);
 	throw new Error("Impossible!");
     }
     /** Returns a node rooting a tree containing all the mappings in
      *  the tree rooted at the given <code>n</code> except that it does
      *  not contain a mapping for <code>key</code>. */
-    static <K,V> PersistentTreeNode<K,V> remove(PersistentTreeNode<K,V> n,
-						Comparator<K> c,
-						K key) {
+    static <N extends PersistentTreeNode<N,K,V>,K,V>
+		      N remove(N n, Comparator<K> c, K key,
+			       Allocator<N,K,V> allocator) {
 	if (n==null) return null; // key not found.
 
 	int r = c.compare(key, n.key);
 	if (r==0) // remove this node.
-	    return merge(n.left, n.right);
+	    return merge(n.left, n.right, allocator);
 	if (r < 0)
 	    return newNode_balanceLeft(n, n.key, n.getValue(),
-				       remove(n.left, c, key), n.right);
+				       remove(n.left, c, key, allocator),
+				       n.right,
+				       allocator);
 	if (r > 0)
 	    return newNode_balanceRight(n, n.key, n.getValue(),
-					n.left, remove(n.right, c, key));
+					n.left,
+					remove(n.right, c, key, allocator),
+					allocator);
 	throw new Error("Impossible!");
     }
     /** Merge two nodes into one. */
-    private static <K,V>
-	PersistentTreeNode<K,V> merge(PersistentTreeNode<K,V> left,
-				      PersistentTreeNode<K,V> right) {
+    private static <N extends PersistentTreeNode<N,K,V>,K,V>
+			      N merge(N left, N right,
+				      Allocator<N,K,V> allocator) {
 	if (left==null) return right;
 	if (right==null) return left;
 	// the node with the smallest heap key goes on top.
 	// in case of tie, the smallest tree key goes on top (left node)
 	if (heapKey(left.key) > heapKey(right.key))
 	    return newNode(null, right.key, right.getValue(),
-			   merge(left, right.left), right.right);
+			   merge(left, right.left, allocator), right.right,
+			   allocator);
 	else
 	    return newNode(null, left.key, left.getValue(),
-			   left.left, merge(left.right, right));
+			   left.left, merge(left.right, right, allocator),
+			   allocator);
     }
 
     /** Define an iterator over a tree (in tree order). */
-    public static <K,V>
-	Iterator<PersistentTreeNode<K,V>> iterator(PersistentTreeNode<K,V>
-						   root) {
-	return new NodeIterator<K,V>(root);
+    public static <N extends PersistentTreeNode<N,K,V>,K,V>
+			     Iterator<N> iterator(N root) {
+	return new NodeIterator<N,K,V>(root);
     }
     /** An iterator class over a tree of <code>PersistentTreeNode</code>s. */
-    private static class NodeIterator<K,V>
-	extends UnmodifiableIterator<PersistentTreeNode<K,V>> {
-	NodeList<K,V> stack = null;
-	NodeIterator(PersistentTreeNode<K,V> root) {
-	    for (PersistentTreeNode<K,V> n=root; n!=null; n=n.left)
-		stack = new NodeList<K,V>(n, stack);
+    private static class NodeIterator<N extends PersistentTreeNode<N,K,V>,K,V>
+	extends UnmodifiableIterator<N> {
+	NodeList<N,K,V> stack = null;
+	NodeIterator(N root) {
+	    for (N n=root; n!=null; n=n.left)
+		stack = new NodeList<N,K,V>(n, stack);
 	}
 	public boolean hasNext() { return stack==null; }
-	public PersistentTreeNode<K,V> next() {
-	    PersistentTreeNode<K,V> n = stack.head;
+	public N next() {
+	    N n = stack.head;
 	    stack = stack.tail;
 	    // now recurse down the left side of the right-hand node
-	    for (PersistentTreeNode<K,V> nn=n.right; nn!=null; nn=nn.left)
-		stack = new NodeList<K,V>(nn, stack);
+	    for (N nn=n.right; nn!=null; nn=nn.left)
+		stack = new NodeList<N,K,V>(nn, stack);
 	    // done.
 	    return n;
 	}
-	private static class NodeList<K,V> {
-	    PersistentTreeNode<K,V> head;
-	    NodeList<K,V> tail;
-	    NodeList(PersistentTreeNode<K,V> head, NodeList<K,V> tail) {
+	private static class NodeList<N extends PersistentTreeNode<N,K,V>,K,V>{
+	    N head;
+	    NodeList<N,K,V> tail;
+	    NodeList(N head, NodeList<N,K,V> tail) {
 		this.head = head; this.tail = tail;
 	    }
 	}
@@ -316,9 +326,38 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	// done with shuffle/permute initialization.
     }
 
+    /** Sample implementation of <code>PersistentTreeNode</code>.
+     *  Note how we implement a fast 'hashCode' method. */
+    private static class WithValue<K,V>
+	extends PersistentTreeNode<WithValue<K,V>,K,V> {
+	final V value;
+	/** Hash code of a map implementation with the contents of the
+	 *  tree rooted at this node. */
+	final int mapHashCode;
+	WithValue(K key, V value, WithValue<K,V> left, WithValue<K,V> right) {
+	    super(key, left, right);
+	    this.value = value;
+	    this.mapHashCode = this.hashCode() + // this entry
+		((left==null)?0:left.mapHashCode) + // hash of left tree
+		((right==null)?0:right.mapHashCode); // hash of right tree
+	}
+	public V getValue() { return value; }
+	/** Return the hash code of a <code>java.util.Map</code> with the
+	 *  contents of the tree rooted at this node. */
+	int mapHashCode() { return mapHashCode; }
+	static class Allocator<K,V>
+	    extends PersistentTreeNode.Allocator<WithValue<K,V>,K,V> {
+	    WithValue<K,V> newNode(K key, V value,
+				   WithValue<K,V> left, WithValue<K,V> right) {
+		return new WithValue<K,V>(key, value, left, right);
+	    }
+	}
+    }
     /** Self-test method for the class. */
     public static void main(String[] args) {
-	PersistentTreeNode<Integer,Integer> root = null;
+	WithValue<Integer,Integer> root = null;
+	WithValue.Allocator<Integer,Integer> allocator =
+	    new WithValue.Allocator<Integer,Integer>();
 	Comparator<Integer> c = new Comparator<Integer>() {
 	    public int compare(Integer a, Integer b) {
 		return a.compareTo(b);
@@ -328,11 +367,11 @@ class PersistentTreeNode<K,V> extends AbstractMapEntry<K,V>
 	// number of nodes is 'i'
 	// perfectly balanced tree has depth ceil(log2(i))
 	for (int i=1; i<=N; i++) {
-	    root = put(root, c, new Integer(i), new Integer(i));
+	    root = put(root, c, new Integer(i), new Integer(i), allocator);
 	    System.out.println(i+" DEPTH "+depth(root)+" IDEAL "+Util.log2c(i));
 	}
 	for (int i=N; i>=1; i--) {
-	    root = remove(root, c, new Integer(i));
+	    root = remove(root, c, new Integer(i), allocator);
 	    System.out.println(i+" DEPTH "+depth(root)+" IDEAL "+Util.log2c(i));
 	}
 	// XXX not a very good self-test at the moment: no automatic
