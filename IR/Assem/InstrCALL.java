4f3067d40fbe1d2647a59534cd3ab320ffde57ac
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-06-10 15:07:13 +0000

    Fix impedance mismatch between non-GJ-ized InstrCALL and InstrMEM and their GJ-ized superclass.

--- IR/Assem/InstrCALL.java
+++ IR/Assem/InstrCALL.java
@@ -4,6 +4,7 @@
 package harpoon.IR.Assem;
 
 import harpoon.ClassFile.HCodeElement;
+import harpoon.Temp.Label;
 import harpoon.Temp.Temp;
 import harpoon.Temp.TempMap;
 import java.util.List;
@@ -12,14 +13,14 @@ import java.util.List;
  * <code>InstrCALL</code>
  * 
  * @author  Felix S. Klock II <pnkfelix@mit.edu>
- * @version $Id: InstrCALL.java,v 1.2 2002-02-25 21:04:11 cananian Exp $
+ * @version $Id: InstrCALL.java,v 1.3 2003-06-10 15:07:13 cananian Exp $
  */
 public class InstrCALL extends Instr {
     
     /** Creates a <code>InstrCALL</code>. */
     public InstrCALL( InstrFactory inf, HCodeElement source,
 		      String assem, Temp[] dst, Temp[] src,
-		      boolean canFallThrough, List targets) {
+		      boolean canFallThrough, List<Label> targets) {
         super(inf, source, assem, dst, src, canFallThrough, targets);
     }
 
--- IR/Assem/InstrMEM.java
+++ IR/Assem/InstrMEM.java
@@ -4,6 +4,7 @@
 package harpoon.IR.Assem;
 
 import harpoon.ClassFile.HCodeElement;
+import harpoon.Temp.Label;
 import harpoon.Temp.Temp;
 import harpoon.Temp.TempMap;
 
@@ -14,14 +15,14 @@ import java.util.List;
  * assembly-level instruction representations.
  * 
  * @author  Felix S. Klock II <pnkfelix@mit.edu>
- * @version $Id: InstrMEM.java,v 1.2 2002-02-25 21:04:13 cananian Exp $ 
+ * @version $Id: InstrMEM.java,v 1.3 2003-06-10 15:07:13 cananian Exp $ 
  */
 public class InstrMEM extends Instr {
     
     /** Creates a <code>InstrMEM</code>. */
     public InstrMEM(InstrFactory inf, HCodeElement codeSrc, String assem, 
 		    Temp[] dst, Temp[] tempSrc, boolean canFallThrough,
-		    List targets) {
+		    List<Label> targets) {
         super(inf, codeSrc, assem, dst, tempSrc, canFallThrough, targets);
     }
 
