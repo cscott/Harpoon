d0f2614edc5b2e53917ba4ee2f230f1ac1ffe5e3
Harvey Jones <harveyj@fbb.lcs.mit.edu>
2003-07-08 18:27:22 +0000

    Die properly on too few arguments

--- src/WatermarkMain.java
+++ src/WatermarkMain.java
@@ -9,6 +9,7 @@ import imagerec.graph.*;
  * This is a node watermarking pipeline.
  *
  * @author Wes Beebee <<a href="mailto:wbeebee@mit.edu">wbeebee@mit.edu</a>>
+ * @author Harvey Jones <<a href="mailto:harveyj@mit.edu">harveyj@mit.edu</a>>
  */
 
 public class WatermarkMain {
@@ -21,6 +22,7 @@ public class WatermarkMain {
     public static void main(String args[]) {
 	if(args.length < 3){
 	    System.out.println("Usage: java -jar Watermark.jar fileprefix filesuffix numOfImages");
+	    System.exit(-1);
 	}
 	(new Load("Watermark.jar", args[0], java.lang.Integer.parseInt(args[2]), new Watermark(new Save(args[1], true, false)))).run();
 
