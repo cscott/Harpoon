cc86cb7068bb552f178a1d00cd22ca507417b9a5
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-07-13 05:39:57 +0000

    Added doubleword load-linked and store-conditional instructions.

--- arch/ix86/llsc.h
+++ arch/ix86/llsc.h
@@ -8,10 +8,17 @@
 #warning x86 does not have load linked/store conditional instructions
 #warning generated code will NOT be thread safe.
 
-static inline int32_t load_linked(uint32_t *ptr) {
+static inline int32_t load_linked(int32_t *ptr) {
   return *ptr;
 }
 static inline int store_conditional(int32_t *ptr, int32_t val) {
+  *ptr = val;
+  return 1;
+}
+static inline int64_t load_linked_double(int64_t *ptr) {
+  return *ptr;
+}
+static inline int store_conditional_double(int64_t *ptr, int64_t val) {
   *ptr = val;
   return 1;
 }
@@ -20,6 +27,8 @@ static inline void sync(void) {
 }
 
 #define LL(x) load_linked(x)
+#define LL_D(x) load_linked_double(x)
 #define SC(x,y) store_conditional(x,y)
+#define SC_D(x,y) store_conditional_double(x,y)
 
 #endif /* INCLUDED_LLSC_H */
--- arch/ppc32/llsc.h
+++ arch/ppc32/llsc.h
@@ -5,7 +5,7 @@
 #ifndef INCLUDED_LLSC_H
 #define INCLUDED_LLSC_H
 
-static inline int32_t load_linked(uint32_t *ptr) {
+static inline int32_t load_linked(int32_t *ptr) {
   uint32_t result;
   __asm__ volatile ("lwarx %0,0,%1" : "=r"(result) : "r"(ptr));
   return result;
@@ -21,11 +21,31 @@ static inline int store_conditional(int32_t *ptr, int32_t val) {
 " : "=r"(result) : "r"(ptr), "r"(val) : "cr0", "memory");
   return result;
 }
+
+static inline int64_t load_linked_double(int64_t *ptr) {
+  uint64_t result;
+  __asm__ volatile ("ldarx %0,0,%1" : "=r"(result) : "r"(ptr));
+  return result;
+}
+static inline int store_conditional_double(int64_t *ptr, int64_t val) {
+  int result;
+  __asm__ ("\
+	stdcx. %2,0,%1
+	li %0,0
+	bne- 0f
+	addi %0,1
+0:
+" : "=r"(result) : "r"(ptr), "r"(val) : "cr0", "memory");
+  return result;
+}
+
 static inline void sync(void) {
   __asm__ volatile ("sync" ::: "memory" );
 }
 
 #define LL(x) load_linked(x)
+#define LL_D(x) load_linked_double(x)
 #define SC(x,y) store_conditional(x,y)
+#define SC_D(x,y) store_conditional_double(x,y)
 
 #endif /* INCLUDED_LLSC_H */
