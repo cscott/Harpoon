79bf5490a494e7343baa59c3d26106c110773838
Cristian Cadar <cristic@mit.edu>
2003-07-24 03:14:17 +0000

    Third buggy program

---
+++ Debug3/Tool/stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_H
+#define STACK_H
+
+struct StackElement {
+  struct StackElement * next;
+  void * contents;
+};
+void pushstack(struct StackElement **septr,void * obj);
+void * popstack(struct StackElement **septr);
+#endif
