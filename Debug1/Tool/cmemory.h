8e23d7256301381915d9a52fc2fd2e5826c57c09
Cristian Cadar <cristic@mit.edu>
2003-07-25 01:42:18 +0000

    Removing unnecessary files from the first buggy program.

--- Debug1/Tool/cmemory.h
+++
@@ -1,8 +0,0 @@
-#ifndef CMEMORY_H
-#define CMEMORY_H
-#include "test.h"
-#define malloc(size) ourmalloc(size)
-#define calloc(memb,size) ourcalloc(memb,size)
-#define realloc(ptr,size) ourrealloc(ptr,size)
-#define free(size) ourfree(size)
-#endif
