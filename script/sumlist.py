7c405a882be7fd931b6f6936ea3381034886ba44
Harvey Jones <harveyj@fbb.lcs.mit.edu>
2003-08-05 22:32:55 +0000

    Add rate stats to the package.

--- script/sumlist.py
+++ script/sumlist.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
-f = open("../processing-stats.txt", "r")
-g = open("../receiving-stats.txt", "r")
-h = open("../tracking-stats.txt", "r")
-out = open("../total-stats.txt", "w")
+f = open("../processing-rate-stats.txt", "r")
+g = open("../receiving-rate-stats.txt", "r")
+h = open("../tracking-rate-stats.txt", "r")
+out = open("../total-rate-stats.txt", "w")
 
 fnums = map(int, f.readlines())
 gnums = map(int, g.readlines())
@@ -12,3 +12,18 @@ hnums = map(int, h.readlines())
 for i in range(0, min(len(fnums) - 1, len(gnums) - 1, len(hnums) - 1)):
     out.write(str(fnums[i] + gnums[i] + hnums[i]))
     out.write("\n")
+out.close()
+
+f = open("../processing-latency-stats.txt", "r")
+g = open("../receiving-latency-stats.txt", "r")
+h = open("../tracking-latency-stats.txt", "r")
+out = open("../total-latency-stats.txt", "w")
+
+fnums = map(int, f.readlines())
+gnums = map(int, g.readlines())
+hnums = map(int, h.readlines())
+
+for i in range(0, min(len(fnums) - 1, len(gnums) - 1, len(hnums) - 1)):
+    out.write(str(fnums[i] + gnums[i] + hnums[i]))
+    out.write("\n")
+out.close()
