ee773a04e0de022123965fbe4698e4ab74dc21e3
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-10-06 15:46:31 +0000

    Add trivial source file to otherwise-empty libraries to workaround those losing OSes (IRIX) which can't handle libraries without contents.

---
+++ src/dynsync/empty.c
@@ -0,0 +1,6 @@
+/* On IRIX build systems (and possibly others?) automake/libtool has
+ * problems with libraries which don't contain any source files.  So
+ * this is just a trivial source file to include in otherwise-empty
+ * libraries. --CSA 2003-10-06 */
+
+static int a_pointless_symbol=0;
