f9c67a61f5b45cd790edb23ac3d9647664fb2e91
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-11-02 17:26:03 +0000

    Add ability to stub out hardware transaction instructions, in order to debug HWTrans apps (in the single-threaded case, at least).

--- src/transact/hwtrans.h
+++ src/transact/hwtrans.h
@@ -1,15 +1,25 @@
 #ifndef INCLUDED_HWTRANS_H
 #define INCLUDED_HWTRANS_H
 
+#ifdef DONT_REALLY_DO_HWTRANS
+# define XBEGIN()	0
+# define XEND()		0
+# define NXBEGIN()	0
+# define NXEND()	0
+# define NOP()		0
+#else /* !DONT_REALLY_DO_HWTRANS */
+
+# ifdef _MIPS_ARCH_MIPS4
 /*   asm(string : output : input : clobbered hard registers) */
 /* xbegin is assumed to take a register snapshot. */
-#ifdef _MIPS_ARCH_MIPS4
-#define XBEGIN()	asm volatile (".word 0x0404FFFF # xbegin" ::: "memory")
-#define XEND()  	asm volatile (".word 0x00000029 # xend" ::: "memory")
-#define NXBEGIN()	asm volatile (".word 0x00000028 # nxbegin" :::"memory")
-#define NXEND() 	asm volatile (".word 0x00000035 # nxend" ::: "memory")
-#define NOP()		asm ("nop")
-#endif
+#  define XBEGIN()	asm volatile (".word 0x0404FFFF # xbegin" ::: "memory")
+#  define XEND()  	asm volatile (".word 0x00000029 # xend" ::: "memory")
+#  define NXBEGIN()	asm volatile (".word 0x00000028 # nxbegin" :::"memory")
+#  define NXEND() 	asm volatile (".word 0x00000035 # nxend" ::: "memory")
+#  define NOP()		asm ("nop")
+# endif /* _MIPS_ARCH_MIPS4 */
+
+#endif /* !DONT_REALLY_DO_HWTRANS */
 
 /* gcc3.3 manual takes about using __attribute__((always_inline)) for the
  * next two functions, but it doesn't seem to actually be supported in the
