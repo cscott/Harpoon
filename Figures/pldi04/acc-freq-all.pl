d03b0c24078deb2204ae085779857be7f4384fc3
C. Scott Ananian <cananian@alumni.princeton.edu>
2003-11-14 21:10:48 +0000

    trsize.{201,222}.100.txt completed; freq.222.100.txt completed.

--- Figures/pldi04/acc-freq-all.pl
+++ Figures/pldi04/acc-freq-all.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/env perl
 
-# missing: freq.{201,222}.100.txt
+# missing: freq.201.100.txt
 
 @benchmarks = ( 201, 202, 205, 209, 213, 222, 228 );
 %fullname = ( 200 => "check", 201 => "compress", 202 => "jess",
 	      205 => "raytrace", 209 => "db", 213 => "javac",
 	      222 => "mpegaudio", 227 => "mtrt", 228 => "jess" );
-%runsize = ( 201 => 10, 202 => 100, 205 => 100, 209 => 100, 213 => 100, 222 => 10 , 228 => 100);
+%runsize = ( 201 => 10, 202 => 100, 205 => 100, 209 => 100, 213 => 100, 222 => 100 , 228 => 100);
 
 foreach my $run (keys %runsize) {
     my $file = "freq.".$run.".".$runsize{$run}.".txt";
