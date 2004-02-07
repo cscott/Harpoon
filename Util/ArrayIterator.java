38f8450c08328df664c528033702e42b02153ff6
C. Scott Ananian <cananian@alumni.princeton.edu>
2004-02-07 19:26:09 +0000

    Extended ArrayIterator to make it implement ListIterator, not just Iterator.

--- Util/ArrayIterator.java
+++ Util/ArrayIterator.java
@@ -3,20 +3,18 @@
 // Licensed under the terms of the GNU GPL; see COPYING for details.
 package harpoon.Util;
 
-import harpoon.Util.Collections.UnmodifiableIterator;
-
-import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 /**
  * An <code>ArrayIterator</code> iterates over the elements of an array.
- * <p>The <code>remove()</code> method is <b>not</b> implemented.
+ * <p>The <code>add()</code>, <code>set()</code> and <code>remove()</code>
+ * methods are <b>not</b> implemented; this iterator is unmodifiable.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ArrayIterator.java,v 1.5 2002-08-30 22:57:23 cananian Exp $
+ * @version $Id: ArrayIterator.java,v 1.6 2004-02-07 19:26:09 cananian Exp $
  */
 
-public class ArrayIterator<E> extends UnmodifiableIterator<E>
-    implements Iterator<E> {
+public class ArrayIterator<E> implements ListIterator<E> {
     final E[] oa;
     int i = 0;
 
@@ -32,5 +30,26 @@ public class ArrayIterator<E> extends UnmodifiableIterator<E>
 	    return oa[i++];
 	else
 	    throw new NoSuchElementException();
+    }
+    public int nextIndex() { return i; }
+
+    public boolean hasPrevious() { return ( i > 0 ); }
+    public E previous() {
+	if (i > 0)
+	    return oa[--i];
+	else
+	    throw new NoSuchElementException();
+    }
+    public int previousIndex() { return i-1; }
+
+    /* unmodifiable */
+    public void add(E o) {
+	throw new UnsupportedOperationException();
+    }
+    public void remove() {
+	throw new UnsupportedOperationException();
+    }
+    public void set(E o) {
+	throw new UnsupportedOperationException();
     }
 }
