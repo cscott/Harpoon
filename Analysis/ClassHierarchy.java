d3adf87da145e209190d19c1049cd2826e08de3e
Alexandru SALCIANU <salcianu@alum.mit.edu>
2005-09-29 03:53:41 +0000

    Small change performed while debugging one of the previously mentioned bugs: the Javadoc of this class lacks a clear definition of usable/reachable class (reachable is not transitive subclassing).  I propose such a definition in a TODO comment.  Hopefully, this comment will motivate people to improve the Javadoc for ClassHierarchy.

--- Analysis/ClassHierarchy.java
+++ Analysis/ClassHierarchy.java
@@ -7,7 +7,6 @@ import harpoon.ClassFile.HClass;
 import harpoon.ClassFile.HConstructor;
 import harpoon.ClassFile.HMethod;
 import harpoon.Util.ArraySet;
-import net.cscott.jutil.Default;
 import harpoon.Util.HClassUtil;
 import harpoon.Util.Util;
 import net.cscott.jutil.WorkSet;
@@ -21,18 +20,25 @@ import java.util.Set;
  * and methods.
  * 
  * @author  C. Scott Ananian <cananian@alumni.princeton.edu>
- * @version $Id: ClassHierarchy.java,v 1.5 2004-02-08 01:49:03 cananian Exp $
+ * @version $Id: ClassHierarchy.java,v 1.6 2005-09-29 03:53:41 salcianu Exp $
  */
 public abstract class ClassHierarchy {
     // tree of callable classes
-    /** Returns all usable/reachable children of an <code>HClass</code>.
-     *  For an interface class <code>c</code>, the children include all
-     *  reachable classes which implement it as well as any reachable
-     *  interfaces which extend it.  For a non-interface class, children
-     *  are all reachable subclasses.
-     *	@return <code>Set</code> of <code>HClass</code>es.
-     */
+    /** Returns the set of all usable/reachable children of an
+     *  <code>HClass</code>.  For an interface class <code>c</code>,
+     *  the children include all reachable classes which implement it
+     *  as well as any reachable interfaces which extend it.  For a
+     *  non-interface class, children are all reachable subclasses.
+     *  Note: this method deals with direct children; i.e., it doesn't
+     *  return transitive (more than one level) subclassing children.
+     *
+     *  <p>[AS 09/28/05]: TODO: DOCUMENT: what does "reachable" mean?
+     *  It's not "transitive" (see above Note).  Here is a tentative
+     *  definition: a class is reachable iff (1) it is instantiated,
+     *  or (2) it is the superclass of a reachable class.  Is this OK?
+     * */
     public abstract Set<HClass> children(HClass c);
+
     /** Return the parents of an <code>HClass</code>.
      *  The parents of a class <code>c</code> are its superclass and 
      *  interfaces.  The results should be complementary to the 
@@ -80,28 +86,38 @@ public abstract class ClassHierarchy {
 	// okay, return as Set.
 	return new ArraySet<HClass>(parents);
     }
-    /** Returns a set of methods in the hierarchy (not necessary reachable
-     *  methods) which override the given method.  The set is only one
-     *  level deep; invoke children() again on each member of the returned
-     *  set to find the rest of the possible overriding methods.  Note
-     *  however that interface methods may introduce some imprecision:
-     *  in particular, for some hm2 in children(hm1) (where hm1 is an
-     *  interface method), hm2.getDeclaringClass() may not implement
-     *  hm1.getDeclaringClass().   For example, ListIterator.next() may
-     *  be implemented by A, but B (a subclass of A which doesn't override
-     *  A.next()) may be the class which implements ListIterator. */
+
+    /** Returns a set of methods in the hierarchy (not necessary
+     *  reachable methods) which override the given method
+     *  <code>hm</code>.  The set does not include <code>hm</code> and
+     *  is only one level deep; invoke <code>overrides()</code> again
+     *  on each member of the returned set to find the rest of the
+     *  possible overriding methods.
+     *
+     *  <p>Note however that interface methods may introduce some
+     *  imprecision: in particular, for some hm2 in overrides(hm1)
+     *  (where hm1 is an interface method), hm2.getDeclaringClass()
+     *  may not implement the interface hm1.getDeclaringClass().  For
+     *  example, <code>ListIterator.next()</code> may be implemented
+     *  by class <code>A</code> that does not implement the full
+     *  <code>ListIterator</code> interface; class <code>B</code> (a
+     *  subclass of <code>A</code> which does not override
+     *  <code>A.next()</code>) may be the class which implements
+     *  <code>ListIterator</code>. */
     public final Set<HMethod> overrides(HMethod hm) {
 	return overrides(hm.getDeclaringClass(), hm, false);
     }
+
     /** Returns the set of methods, excluding <code>hm</code>, declared
      *  in classes which are instances of <code>hc</code>, which override
      *  <code>hm</code>.  If <code>all</code> is true, returns all such
      *  methods in the class hierarchy; otherwise returns only the methods
      *  which *immediately* override <code>hm</code>. */
     public Set<HMethod> overrides(HClass hc, HMethod hm, boolean all) {
-	// non-virtual methods have no overrides.
+	// non-virtual, private and constructor methods have no overrides.
 	if (hm.isStatic() || Modifier.isPrivate(hm.getModifiers()) ||
-	    hm instanceof HConstructor) return Default.EMPTY_SET();
+	    hm instanceof HConstructor) return Collections.<HMethod>emptySet();
+
 	// determine overrides for virtual methods.
 	Set<HMethod> result = new WorkSet<HMethod>();
 	WorkSet<HClass> ws = new WorkSet<HClass>(this.children(hc));
@@ -126,12 +142,13 @@ public abstract class ClassHierarchy {
      *	@return <code>Set</code> of <code>HMethod</code>s.
      */
     public abstract Set<HMethod> callableMethods();
-    /** Returns the set of all reachable/usable classes.
-     *  If any method in a class is callable (including static methods),
-     *  then the class will be a member of the returned set.
-     *  @return <code>Set</code> of <code>HClass</code>es.
-     */
+
+    /** Returns the set of all reachable/usable classes.  If any
+     *  method in a class is callable (including static methods), then
+     *  the class will be a member of the returned set.
+     * */
     public abstract Set<HClass> classes();
+
     /** Returns the set of all *instantiated* classes.
      *  This is a subset of the set returned by the <code>classes()</code>
      *  method.  A class is included in the return set only if an
