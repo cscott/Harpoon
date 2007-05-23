4bd445f6615b8b9c89bf96881e9e7d2b11052f16
C. Scott Ananian <cananian@alumni.princeton.edu>
2007-05-23 21:11:49 +0000

    Fix statistics when compiling with transactions.

--- src/transact/transact-pc.h
+++ src/transact/transact-pc.h
@@ -7,6 +7,13 @@
 #define GC_malloc_atomic GC_malloc_atomic_trans
 #endif /* BDW_CONSERVATIVE_GC */
 
+# include "fni-stats.h" /* sometimes we're going to keep statistics */
+DECLARE_STATS_EXTERN(transact_readnt)
+DECLARE_STATS_EXTERN(transact_writent)
+DECLARE_STATS_EXTERN(transact_false_flag_read)
+DECLARE_STATS_EXTERN(transact_false_flag_write)
+DECLARE_STATS_EXTERN(transact_long_write)
+
 /* ------------------------------------------------------ */
 #ifdef DONT_REALLY_DO_TRANSACTIONS
 /* testing stubs */
